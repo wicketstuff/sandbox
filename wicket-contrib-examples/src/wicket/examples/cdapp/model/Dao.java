316368a751c60b3549f6be392670ac215e0a6b4d
Phil Kulak <pkulak@wicketstuff.org>
2005-07-29 18:57:12 +0000

    Rewrote the cdapp and replaced the data code with the sandbox.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@202 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-examples/src/wicket/examples/cdapp/model/Dao.java
@@ -0,0 +1,78 @@
+package wicket.examples.cdapp.model;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import static wicket.examples.cdapp.CdRequestCycle.COMPONENT_DAO;
+
+import wicket.contrib.data.model.OrderedPageableList;
+import wicket.contrib.data.model.QueryList;
+import wicket.contrib.data.model.hibernate.HibernateQueryList;
+
+/**
+ * A data access object that is meant to be created and destroyed on each
+ * request. It's probably a better design to make this a singleton, but that's
+ * best done with the help of Spring or EJB's and (for now) outside the scope
+ * of this application.
+ * 
+ * @author Phil Kulak
+ */
+public class Dao {
+	private Session session;
+
+	public Dao(final Session session) {
+		this.session = session;
+	}
+	
+	public void merge(final Object entity) {
+		session.saveOrUpdate(entity);
+	}
+	
+	public OrderedPageableList findAllCategories() {
+		return new HibernateQueryList("FROM Category e", COMPONENT_DAO)
+			.addOrder("name");
+	}
+	
+	public OrderedPageableList findAllAlbums() {
+		return new HibernateQueryList("FROM Album e", COMPONENT_DAO);
+	}
+	
+	public OrderedPageableList searchAlbums(String term) {
+		return new SearchAlbums(term);
+	}
+	
+	private static class SearchAlbums extends QueryList {
+		private static final String listQuery =
+			"FROM Album e " +
+			"WHERE LOWER(e.artist) = LOWER(:term) " +
+			"  OR LOWER(e.title) = LOWER(:term) " +
+			"  OR LOWER(e.category.name) = LOWER(:term)";
+		
+		private static final String countQuery = "SELECT COUNT(*) " + listQuery;
+		
+		private final String term;
+		
+		public SearchAlbums(String term) {
+			this.term = term;
+		}
+		
+		@Override
+		protected List getItems(int start, int max, String orderBy) {
+			return COMPONENT_DAO.getSession()
+				.createQuery(listQuery + orderBy)
+				.setString("term", term)
+				.setFirstResult(start)
+				.setMaxResults(max)
+				.list();
+		}
+
+		@Override
+		protected int getCount() {
+			return (Integer) COMPONENT_DAO.getSession()
+				.createQuery(countQuery)
+				.setString("term", term)
+				.uniqueResult();
+		}
+	}
+}
