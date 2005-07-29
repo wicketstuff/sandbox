80b80af2300c046a1b927323dc32936980dd4675
Unknown <unknown@wicketstuff.org>
2005-07-29 19:00:05 +0000

    This commit was manufactured by cvs2svn to create tag 'PHIL_1'.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/tags/PHIL_1@205 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-beanedit/src/java/net/sf/ipn/data/PrayerGroupCategory.java
+++
@@ -1,6 +0,0 @@
-package net.sf.ipn.data;
-
-public class PrayerGroupCategory extends net.sf.ipn.data.auto._PrayerGroupCategory
-{
-
-}
--- wicket-contrib-beanedit/src/java/net/sf/ipn/data/Subscription.java
+++
@@ -1,6 +0,0 @@
-package net.sf.ipn.data;
-
-public class Subscription extends net.sf.ipn.data.auto._Subscription
-{
-
-}
