ef0514f4e4073b18814a8b63e7ab99d3a88607b4
Phil Kulak <pkulak@wicketstuff.org>
2005-07-31 01:44:09 +0000

    Documentation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@213 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-examples-hibernate3/src/wicket/examples/cdapp2/page/CdPage.java
+++ wicket-contrib-examples-hibernate3/src/wicket/examples/cdapp2/page/CdPage.java
@@ -5,11 +5,22 @@ import wicket.examples.cdapp2.model.Dao;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.basic.Label;
 
+/**
+ * The base class for pages in this application. This uses markup inheritance
+ * to supply a header and footer.
+ * 
+ * @author Phil Kulak
+ */
 public class CdPage extends WebPage {
 	public CdPage(String title) {
 		add(new Label("cdPage_title", title));
 	}
 	
+    /**
+     * A convenient way to make our DAO available to our pages.
+     * 
+     * @return the DAO for this request
+     */
 	public Dao getDao() {
 		return ((CdRequestCycle) getRequestCycle()).getDao();
 	}
