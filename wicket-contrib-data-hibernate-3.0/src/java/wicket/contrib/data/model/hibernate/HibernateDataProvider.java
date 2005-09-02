691aa83ef0577b7a1a445c274b6232fd23f3efd9
Phil Kulak <pkulak@wicketstuff.org>
2005-09-02 22:18:18 +0000

    Added a helper function.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@274 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-data-hibernate-3.0/src/java/wicket/contrib/data/model/hibernate/HibernateDataProvider.java
+++ wicket-contrib-data-hibernate-3.0/src/java/wicket/contrib/data/model/hibernate/HibernateDataProvider.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import org.hibernate.Session;
 
 import wicket.contrib.data.model.hibernate.IHibernateDao.IHibernateCallback;
+import wicket.contrib.dataview.sort.SortParam;
 import wicket.contrib.dataview.sort.SortableDataProvider;
 import wicket.model.IModel;
 
@@ -49,6 +50,51 @@ public abstract class HibernateDataProvider extends SortableDataProvider
 		});
 
 		return result.intValue();
+	}
+	
+	/**
+	 * Formats the current sort states an a query string suitable for appending
+	 * at the end of SQL, HQL or other queries.
+	 * 
+	 * @param alias the column alias each sort field will reference
+	 * @return an ORDER BY query fragment
+	 */
+	protected String getSqlOrderBy(String alias)
+	{
+		StringBuffer orderBy = new StringBuffer();
+		if (getSortList().size() > 0)
+		{
+			orderBy.append(" ORDER BY");
+			boolean addComma = false;
+			for (Iterator i = getSortList().iterator(); i.hasNext();)
+			{
+				SortParam sortParam = (SortParam) i.next();
+				if (addComma) orderBy.append(",");
+				orderBy.append(" ");
+				
+				if (alias != null)
+				{
+					orderBy.append(alias + ".");
+				}
+				
+				orderBy.append(sortParam.getProperty());
+				orderBy.append(sortParam.isAscending() ? " ASC" : " DESC");
+			
+				addComma = true;
+			}
+		}
+		return orderBy.toString();
+	}
+	
+	/**
+	 * Formats the current sort states an a query string suitable for appending
+	 * at the end of SQL, HQL or other queries.
+	 * 
+	 * @return an ORDER BY query fragment
+	 */
+	protected String getSqlOrderBy()
+	{
+		return getSqlOrderBy(null);
 	}
 
 	protected abstract Iterator iterator(int first, int count, Session session);
