5a1f9c30c33dd830e9ffe31f573383506d5f3515
Eelco Hillenius <eelco12@wicketstuff.org>
2005-09-04 10:00:36 +0000

    *** empty log message ***
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@276 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-ajax-sandbox/src/java/wicket/contrib/ajax/sandbox/autocomplete/AutoCompleteTextFieldBorder.java
+++ wicket-contrib-ajax-sandbox/src/java/wicket/contrib/ajax/sandbox/autocomplete/AutoCompleteTextFieldBorder.java
@@ -1,3 +1,21 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package wicket.contrib.ajax.sandbox.autocomplete;
 
 import java.util.Iterator;
@@ -81,7 +99,7 @@ public class AutoCompleteTextFieldBorder extends Border {
 	}
 	
 	/**
-	 * @see wicket.markup.html.IHeaderContributor#printHead(wicket.markup.html.HtmlHeaderContainer)
+	 * @see wicket.markup.html.IHeaderContributor#renderHead(wicket.markup.html.HtmlHeaderContainer)
 	 */
 	public final void renderHead(HtmlHeaderContainer container)
 	{
