f0723b76a5dfa418017a850dc3e29b591a1f4df8
ryan sonnek <wireframe6464@wicketstuff.org>
2005-09-23 01:07:00 +0000

    added draggable target.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@317 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/ScriptaculousExamplesHomePage.java
+++ wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/ScriptaculousExamplesHomePage.java
@@ -3,7 +3,7 @@ package wicket.contrib.scriptaculous.examples;
 import wicket.contrib.scriptaculous.examples.autocomplete.AjaxAutocompleteExamplePage;
 import wicket.contrib.scriptaculous.examples.autocomplete.AutocompleteExamplePage;
 import wicket.contrib.scriptaculous.examples.autocomplete.CustomLayoutAjaxAutocompleteExamplePage;
-import wicket.contrib.scriptaculous.examples.dragdrop.DraggableImageExamplePage;
+import wicket.contrib.scriptaculous.examples.dragdrop.DragDropExamplePage;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.link.BookmarkablePageLink;
 
@@ -14,6 +14,6 @@ public class ScriptaculousExamplesHomePage extends WebPage {
 		add(new BookmarkablePageLink("ajaxAutocompleteExampleLink", AjaxAutocompleteExamplePage.class));
 		add(new BookmarkablePageLink("customLayoutAjaxAutocompleteExampleLink", CustomLayoutAjaxAutocompleteExamplePage.class));
 
-		add(new BookmarkablePageLink("draggableImageExamplePage", DraggableImageExamplePage.class));
+		add(new BookmarkablePageLink("draggableImageExamplePage", DragDropExamplePage.class));
 	}
 }
