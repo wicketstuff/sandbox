8361afd7e24205da472fd5d1e9e0a245f1232f03
ryan sonnek <wireframe6464@wicketstuff.org>
2005-09-24 04:19:14 +0000

    added page contribution for drag drop.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@322 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/dragdrop/DragDropExamplePageContribution.java
+++ wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/dragdrop/DragDropExamplePageContribution.java
@@ -1,13 +1,13 @@
 package wicket.contrib.scriptaculous.examples.dragdrop;
 
 import wicket.PageParameters;
-import wicket.markup.html.WebPage;
+import wicket.contrib.scriptaculous.dragdrop.DragDropPageContribution;
 import wicket.markup.html.basic.Label;
 
-public class DragDropExamplePageContribution extends WebPage {
+public class DragDropExamplePageContribution extends DragDropPageContribution {
 
 	public DragDropExamplePageContribution(PageParameters parameters) {
-		String input = parameters.getString("id");
-		add(new Label("productId", input));
+		super(parameters);
+		add(new Label("productId", getInputValue()));
 	}
 }
