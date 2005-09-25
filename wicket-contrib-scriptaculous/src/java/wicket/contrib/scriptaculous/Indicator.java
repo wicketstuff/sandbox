cadb4751164f32e481d265955e6d857836da8593
ryan sonnek <wireframe6464@wicketstuff.org>
2005-09-25 15:24:38 +0000

    added scriptaculous indicator to display while server side processing.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@329 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/Indicator.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/Indicator.java
@@ -7,11 +7,7 @@ import wicket.markup.html.panel.Panel;
 public class Indicator extends Panel {
 
 	public Indicator() {
-		this("indicator");
-	}
-
-	public Indicator(String id) {
-		super(id);
+		super("indicator");
 
 		add(new Image("indicatorImage", "indicator.gif"));
 		add(new Label("indicatorLabel", "Processing..."));
