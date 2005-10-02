d2120bea1d82cee31afe61945fb5e716d5be46e1
Iulian Costan <syca@wicketstuff.org>
2005-10-02 08:21:59 +0000

    fix import mistake
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@349 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/java/wicket/contrib/gmap/JSContainer.java
+++
@@ -1,9 +0,0 @@
-package wicket.contrib.gmap;
-
-/**
- * @author Iulian-Corneliu COSTAN
- */
-public abstract class JSContainer extends JSComponent {
-
-    protected abstract void define(String component);
-}
