881764b83f750115ea2c54e46367fa89257fdb33
Eelco Hillenius <eelco12@wicketstuff.org>
2005-10-19 17:21:20 +0000

    use markup inheritance instead of a border
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@385 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-examples/src/java/wicket/examples/navmenu/PageWithMenu.java
+++ wicket-contrib-examples/src/java/wicket/examples/navmenu/PageWithMenu.java
@@ -18,64 +18,22 @@
  */
 package wicket.examples.navmenu;
 
-import wicket.Component;
-import wicket.MarkupContainer;
-import wicket.contrib.markup.html.navmenu.style.tabs.TabsMenuBorder;
+import wicket.contrib.markup.html.navmenu.style.tabs.TabsNavigationMenu;
 import wicket.markup.html.WebPage;
-import wicket.markup.html.border.Border;
 
 /**
  * Base class for pages that have a navigation menu.
- *
+ * 
  * @author Eelco Hillenius
  */
 public abstract class PageWithMenu extends WebPage
 {
-	/** Border. */
-	private Border menuBorder;
-
 	/**
 	 * Constructor.
 	 */
 	public PageWithMenu()
 	{
 		super();
-		this.menuBorder = new TabsMenuBorder("menuBorder", NavMenuApplication.getMenu());
-		super.add(menuBorder);
-	}
-
-	/**
-	 * Voeg kinderen toe aan border.
-	 * @see wicket.MarkupContainer#add(wicket.Component)
-	 */
-	public MarkupContainer add(final Component child)
-	{
-		// Add children of the page to the page's border component
-		menuBorder.add(child);
-		return this;
-	}
-
-	/**
-	 * @see wicket.MarkupContainer#autoAdd(wicket.Component)
-	 */
-	public boolean autoAdd(Component component)
-	{
-		return menuBorder.autoAdd(component);
-	}
-	
-	/**
-	 * @see wicket.MarkupContainer#removeAll()
-	 */
-	public void removeAll()
-	{
-		menuBorder.removeAll();
-	}
-
-	/**
-	 * @see wicket.MarkupContainer#replace(wicket.Component)
-	 */
-	public MarkupContainer replace(Component child)
-	{
-		return menuBorder.replace(child);
+		add(new TabsNavigationMenu("navmenu", NavMenuApplication.getMenu()));
 	}
 }
