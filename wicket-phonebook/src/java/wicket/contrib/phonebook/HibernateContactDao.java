3b110360a0e5f44712f1055465d023baa263046f
Igor Vaynberg <ivaynberg@wicketstuff.org>
2005-10-20 23:02:09 +0000

    removed the use of spring template from the dao so now it is a pure hibernate dao usable outside of spring
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@391 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-phonebook/src/java/wicket/contrib/phonebook/HibernateContactDao.java
+++ wicket-phonebook/src/java/wicket/contrib/phonebook/HibernateContactDao.java
@@ -18,84 +18,96 @@
  */
 package wicket.contrib.phonebook;
 
-import java.sql.SQLException;
 import java.util.Iterator;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.springframework.orm.hibernate3.HibernateCallback;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.hibernate.SessionFactory;
 
 /**
- * Extend Spring's {@link HibernateDaoSupport} to implement {@link ContactDao}.
- *
+ * implements {@link ContactDao}.
+ * 
  * @author igor
  */
-public class HibernateContactDao extends HibernateDaoSupport implements ContactDao {
+public class HibernateContactDao implements ContactDao {
+
+	private SessionFactory factory;
+
+	/**
+	 * Setter for session factory. Spring will use this to inject the session
+	 * factory into the dao.
+	 * 
+	 * @param factory
+	 *            hibernate session factory
+	 */
+	public void setSessionFactory(SessionFactory factory) {
+		this.factory = factory;
+	}
+
+	/**
+	 * Helper method for retrieving hibernate session
+	 * 
+	 * @return
+	 */
+	protected Session getSession() {
+		return factory.getCurrentSession();
+	}
 
 	/**
 	 * Load a {@link Contact} from the DB, given it's <tt>id</tt> .
-	 * @param id The id of the Contact to load.
+	 * 
+	 * @param id
+	 *            The id of the Contact to load.
 	 * @return Contact
 	 */
 	public Contact load(long id) {
-		return (Contact) getHibernateTemplate().get(Contact.class, new Long(id));
+		return (Contact) getSession().get(Contact.class, new Long(id));
 	}
 
 	/**
 	 * Save the contact to the DB
+	 * 
 	 * @param contact
 	 */
 	public void save(Contact contact) {
-		getHibernateTemplate().save(contact);
+		getSession().merge(contact);
 	}
 
 	/**
 	 * Delete a {@link Contact} from the DB, given it's <tt>id</tt>.
-	 * @param id The id of the Contact to delete.
+	 * 
+	 * @param id
+	 *            The id of the Contact to delete.
 	 */
 	public void delete(long id) {
-		getHibernateTemplate().delete(load(id));
+		getSession().delete(load(id));
 	}
 
 	/**
 	 * Query the DB, using the supplied query details.
-	 * @param qp Query Paramaters to use.
+	 * 
+	 * @param qp
+	 *            Query Paramaters to use.
 	 * @return The results of the query as an Iterator.
 	 */
 	public Iterator find(final QueryParam qp) {
-		return (Iterator)getHibernateTemplate().execute(new HibernateCallback() {
 
-			public Object doInHibernate(Session session) throws HibernateException, SQLException {
-				StringBuffer hql=new StringBuffer("from Contact c ");
-				if (qp.hasSort()) {
-					hql.append("order by c.")
-						.append(qp.getSort())
-						.append((qp.isSortAsc())?" asc":" desc");
-				}
-				return session.createQuery(hql.toString())
-					.setFirstResult(qp.getFirst())
-					.setMaxResults(qp.getCount())
-					.iterate();
-			}
-		});
+		StringBuffer hql = new StringBuffer("from Contact c ");
+		if (qp.hasSort()) {
+			hql.append("order by c.").append(qp.getSort()).append(
+					(qp.isSortAsc()) ? " asc" : " desc");
+		}
+		return getSession().createQuery(hql.toString()).setFirstResult(
+				qp.getFirst()).setMaxResults(qp.getCount()).iterate();
 	}
 
 	/**
 	 * Return the number of Contacts in the DB.
+	 * 
 	 * @return count
 	 */
 	public int count() {
-		Integer count=(Integer)
-			getHibernateTemplate().execute(new HibernateCallback() {
-
-				public Object doInHibernate(Session session) throws HibernateException, SQLException {
-					return (Integer)
-						session.createQuery("select count(*) from Contact")
-							.uniqueResult();
-				}
-				
-			});
+		Integer count = (Integer) getSession().createQuery(
+				"select count(*) from Contact").uniqueResult();
 		return count.intValue();
 	}
 
