61e40786d73ba033848af5cc4a7ca08b014f751d
Iulian Costan <syca@wicketstuff.org>
2005-10-26 08:32:26 +0000

    word example improvement
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@399 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-tinymce-examples/src/java/wicket/contrib/examples/tinymce/WordTinyMCEPage.java
+++ wicket-contrib-tinymce-examples/src/java/wicket/contrib/examples/tinymce/WordTinyMCEPage.java
@@ -1,8 +1,7 @@
 package wicket.contrib.examples.tinymce;
 
 import wicket.contrib.tinymce.TinyMCEPanel;
-import wicket.contrib.tinymce.settings.TinyMCESettings;
-import wicket.contrib.tinymce.settings.Control;
+import wicket.contrib.tinymce.settings.*;
 import wicket.markup.html.form.TextArea;
 import wicket.model.Model;
 
@@ -14,22 +13,63 @@ public class WordTinyMCEPage extends TinyMCEBasePage {
     public WordTinyMCEPage() {
         TinyMCESettings settings = new TinyMCESettings(TinyMCESettings.Theme.advanced);
 
-        Control control = new Control(Control.DefaultButton.numlist, Control.Toolbar.first, Control.Position.before);
-        Control control1 = new Control(Control.DefaultButton.bullist, Control.Toolbar.first, Control.Position.before);
-        Control control2 = new Control(Control.DefaultButton.bullist, Control.Toolbar.first, Control.Position.after);
-        Control control3 = new Control(Control.DefaultButton.bullist, Control.Toolbar.second, Control.Position.before);
-        Control control4 = new Control(Control.DefaultButton.bullist, Control.Toolbar.second, Control.Position.after);
-        Control control5 = new Control(Control.DefaultButton.bullist, Control.Toolbar.third, Control.Position.before);
-        Control control6 = new Control(Control.DefaultButton.bullist, Control.Toolbar.third, Control.Position.after);
+        ContextMenuPlugin contextMenuPlugin = new ContextMenuPlugin();
+        settings.register(contextMenuPlugin);
 
-        settings.addControl(control);
-        settings.addControl(control1);
-        settings.addControl(control2);
-        settings.addControl(control3);
-        settings.addControl(control4);
-        settings.addControl(control5);
-        settings.addControl(control6);
+        // first toolbar
+        SavePlugin savePlugin = new SavePlugin();
+        settings.add(savePlugin.getSaveButton(), TinyMCESettings.Toolbar.first, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.newdocument, TinyMCESettings.Toolbar.first, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.first, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.fontselect, TinyMCESettings.Toolbar.first, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.fontsizeselect, TinyMCESettings.Toolbar.first, TinyMCESettings.Position.after);
 
+        // second toolbar
+        PastePlugin pastePlugin = new PastePlugin();
+        SearchReplacePlugin searchReplacePlugin = new SearchReplacePlugin();
+        DateTimePlugin dateTimePlugin = new DateTimePlugin();
+        PreviewPlugin previewPlugin = new PreviewPlugin();
+        ZoomPlugin zoomPlugin = new ZoomPlugin();
+        settings.add(TinyMCESettings.cut, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.copy, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(pastePlugin.getPasteButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(pastePlugin.getPasteTextButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(pastePlugin.getPasteWordButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(searchReplacePlugin.getSearchButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(searchReplacePlugin.getReplaceButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.before);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(dateTimePlugin.getDateButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(dateTimePlugin.getTimeButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(previewPlugin.getPreviewButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(zoomPlugin.getZoomButton(), TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.forecolor, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.backcolor, TinyMCESettings.Toolbar.second, TinyMCESettings.Position.after);
+
+        // third toolbar
+        TablePlugin tablePlugin = new TablePlugin();
+        EmotionsPlugin emotionsPlugin = new EmotionsPlugin();
+        SpellCheckPlugin spellCheckPlugin = new SpellCheckPlugin();
+        FlashPlugin flashPlugin = new FlashPlugin();
+        PrintPlugin printPlugin = new PrintPlugin();
+        FullScreenPlugin fullScreenPlugin = new FullScreenPlugin();
+        DirectionalityPlugin directionalityPlugin = new DirectionalityPlugin();
+        settings.add(tablePlugin.getTableControls(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.before);
+        settings.add(emotionsPlugin.getEmotionsButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(spellCheckPlugin.getSpellCheckButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(flashPlugin.getFlashButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(printPlugin.getPrintButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(directionalityPlugin.getLtrButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(directionalityPlugin.getRtlButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(TinyMCESettings.separator, TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+        settings.add(fullScreenPlugin.getFullscreenButton(), TinyMCESettings.Toolbar.third, TinyMCESettings.Position.after);
+
+        // other settings
         settings.setToolbarAlign(TinyMCESettings.Align.left);
         settings.setToolbarLocation(TinyMCESettings.Location.top);
         settings.setStatusbarLocation(TinyMCESettings.Location.bottom);
