0d0e3511e589663771ee66df051f5705e016c393
Eelco Hillenius <eelco12@wicketstuff.org>
2005-11-14 03:05:49 +0000

    *** empty log message ***
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@433 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-examples-hibernate3/src/wicket/examples/cdapp2/CdRequestCycle.java
+++ wicket-contrib-examples-hibernate3/src/wicket/examples/cdapp2/CdRequestCycle.java
@@ -3,7 +3,7 @@ package wicket.examples.cdapp2;
 import org.springframework.context.ApplicationContext;
 
 import wicket.Response;
-import wicket.contrib.spring.SpringContextLocator;
+import wicket.contrib.spring.old.SpringContextLocator;
 import wicket.examples.cdapp2.model.Dao;
 import wicket.protocol.http.WebRequest;
 import wicket.protocol.http.WebRequestCycle;
