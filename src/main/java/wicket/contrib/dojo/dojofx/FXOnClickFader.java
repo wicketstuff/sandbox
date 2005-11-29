c48fc3e8cd665122949acdc2c63ba02a84b8541e
Eelco Hillenius <eelco12@wicketstuff.org>
2005-11-29 05:51:03 +0000

    several code std improvements
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo@460 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/dojofx/FXOnClickFader.java
+++ src/main/java/wicket/contrib/dojo/dojofx/FXOnClickFader.java
@@ -1,3 +1,21 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package wicket.contrib.dojo.dojofx;
 
 import wicket.AttributeModifier;
@@ -6,9 +24,9 @@ import wicket.markup.html.HtmlHeaderContainer;
 import wicket.model.Model;
 
 /**
- * Class that can add fading functionality to a vissible Wicket Component
- * This class let's you set a trigger, a duration, a start opacity and
- * an end opacity.
+ * Class that can add fading functionality to a vissible Wicket Component This
+ * class let's you set a trigger, a duration, a start opacity and an end
+ * opacity.
  * 
  * @TODO usage examples
  * 
@@ -17,22 +35,24 @@ import wicket.model.Model;
  */
 public class FXOnClickFader extends DojoFXHandler
 {
-	
+
 	private final boolean startDisplay;
 	private final String type;
 	private String HTMLID;
 	private String componentId;
 	private double startOpac;
 	private double endOpac;
-	
-	
-	
+
+
 	/**
-	 * convenience contructor with 2 parameters; 
-	 * sets startDisplay default to false
+	 * convenience contructor with 2 parameters; sets startDisplay default to
+	 * false
 	 * 
-	 * @param duration duration of the fade effect
-	 * @param trigger Component which ONCLICK method will be bound to trigger the fade effect.
+	 * @param duration
+	 *            duration of the fade effect
+	 * @param trigger
+	 *            Component which ONCLICK method will be bound to trigger the
+	 *            fade effect.
 	 */
 	public FXOnClickFader(int duration, Component trigger)
 	{
@@ -41,12 +61,18 @@ public class FXOnClickFader extends DojoFXHandler
 		this.type = "fade";
 	}
 
-	
+
 	/**
 	 * Constructor to make a simple fade effect with a trigger and a duration.
-	 * @param duration duration of the fade effect
-	 * @param trigger Component which ONCLICK method will be bound to trigger the fade effect.
-	 * @param startDisplay default false; true if component should start with full opacity
+	 * 
+	 * @param duration
+	 *            duration of the fade effect
+	 * @param trigger
+	 *            Component which ONCLICK method will be bound to trigger the
+	 *            fade effect.
+	 * @param startDisplay
+	 *            default false; true if component should start with full
+	 *            opacity
 	 */
 	public FXOnClickFader(int duration, Component trigger, boolean startDisplay)
 	{
@@ -56,131 +82,160 @@ public class FXOnClickFader extends DojoFXHandler
 	}
 
 	/**
-	 * Constructor for fade effect. If allowHide is true, component's display style will be ajusted on a fade.
-	 * @param duration duration of the fade effect
-	 * @param trigger Component which ONCLICK method will be bound to trigger the fade effect.
-	 * @param startDisplay default false; true if component should start with full opacity
-	 * @param allowHide if true component's display will be set to non when wiped-out, hence component will be removed from rendered page.
+	 * Constructor for fade effect. If allowHide is true, component's display
+	 * style will be ajusted on a fade.
+	 * 
+	 * @param duration
+	 *            duration of the fade effect
+	 * @param trigger
+	 *            Component which ONCLICK method will be bound to trigger the
+	 *            fade effect.
+	 * @param startDisplay
+	 *            default false; true if component should start with full
+	 *            opacity
+	 * @param allowHide
+	 *            if true component's display will be set to non when wiped-out,
+	 *            hence component will be removed from rendered page.
 	 */
 	public FXOnClickFader(int duration, Component trigger, boolean startDisplay, boolean allowHide)
 	{
 		super("Onclick", duration, trigger);
 		this.startDisplay = startDisplay;
-		
-		if(allowHide)
+
+		if (allowHide)
 		{
 			this.type = "fadeHide";
-		} else 
+		}
+		else
 		{
 			this.type = "fade";
 		}
 	}
 
 	/**
-	 * Constructor for a fade effect where the starting opacity and the ending opacity are set.
-	 * @param duration duration of the fade effect
-	 * @param trigger Component which ONCLICK method will be bound to trigger the fade effect.
-	 * @param startDisplay default false; true if component should start with starting opacity (startOpac)
-	 * @param startOpac starting opacity between 1 and 0. (0 for transparant, 1 for opaque)
-	 * @param endOpac ending opacity between 1 and 0. (0 for transparant, 1 for opaque)
+	 * Constructor for a fade effect where the starting opacity and the ending
+	 * opacity are set.
+	 * 
+	 * @param duration
+	 *            duration of the fade effect
+	 * @param trigger
+	 *            Component which ONCLICK method will be bound to trigger the
+	 *            fade effect.
+	 * @param startDisplay
+	 *            default false; true if component should start with starting
+	 *            opacity (startOpac)
+	 * @param startOpac
+	 *            starting opacity between 1 and 0. (0 for transparant, 1 for
+	 *            opaque)
+	 * @param endOpac
+	 *            ending opacity between 1 and 0. (0 for transparant, 1 for
+	 *            opaque)
 	 */
-	public FXOnClickFader(int duration, Component trigger, boolean startDisplay, double startOpac, double endOpac)
+	public FXOnClickFader(int duration, Component trigger, boolean startDisplay, double startOpac,
+			double endOpac)
 	{
 		super("Onclick", duration, trigger);
 		this.startDisplay = startDisplay;
-		
+
 		this.type = "fadeOpac";
-		
-		if((startOpac < 0.0) || (startOpac > 1.0))
+
+		if ((startOpac < 0.0) || (startOpac > 1.0))
 		{
 			System.out.println("WARNING: startOpac has to be between 0 and 1");
 		}
-		
-		if((endOpac < 0.0) || (endOpac > 1.0))
+
+		if ((endOpac < 0.0) || (endOpac > 1.0))
 		{
 			System.out.println("WARNING: endOpac has to be between 0 and 1");
 		}
-		
+
 		this.startOpac = startOpac;
-		this.endOpac = endOpac;			
+		this.endOpac = endOpac;
 	}
-	
-	/* 
+
+	/*
 	 * @see wicket.AjaxHandler#getBodyOnloadContribution()
 	 */
 	protected String getBodyOnloadContribution()
 	{
-		//set initial opacity to fadeOpac if type is fadeOpac, else to 0
-		double initOpac = (type=="fadeOpac"?startOpac:0);
-		//if starDisplay is false, set starting opacity of component to initOpac.
-		if(!startDisplay)
+		// set initial opacity to fadeOpac if type is fadeOpac, else to 0
+		double initOpac = (type == "fadeOpac" ? startOpac : 0);
+		// if starDisplay is false, set starting opacity of component to
+		// initOpac.
+		if (!startDisplay)
 		{
-			return "dojo.html.setOpacity(document.getElementById('"+ HTMLID + "'), "+ initOpac + ");";
-		} 
+			return "dojo.html.setOpacity(document.getElementById('" + HTMLID + "'), " + initOpac
+					+ ");";
+		}
 		else
 		{
 			return null;
-		}		
-	
-	}
-	
+		}
 
-	/* (non-Javadoc)
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see wicket.AjaxHandler#renderHeadContribution(wicket.markup.html.HtmlHeaderContainer)
 	 */
 	protected void renderHeadContribution(HtmlHeaderContainer container)
 	{
-		//String to be written to the header
+		// String to be written to the header
 		String s;
-		//dojo function calls for fadein/out
+		// dojo function calls for fadein/out
 		String fadeInFunction;
 		String fadeOutFunction;
-		
-		//set the correct dojo functions for the type of fader
-		if(type=="fadeHide")
+
+		// set the correct dojo functions for the type of fader
+		if (type == "fadeHide")
 		{
-			fadeInFunction = "dojo.fx.html.fadeShow(node, duration, function(){"+ componentId  + "_faderState='fadedIn';})";
-		    fadeOutFunction = "dojo.fx.html.fadeHide(node, duration, function(){"+ componentId  + "_faderState='fadedOut';})";
-		} 
-		else if(type == "fadeOpac") 
+			fadeInFunction = "dojo.fx.html.fadeShow(node, duration, function(){" + componentId
+					+ "_faderState='fadedIn';})";
+			fadeOutFunction = "dojo.fx.html.fadeHide(node, duration, function(){" + componentId
+					+ "_faderState='fadedOut';})";
+		}
+		else if (type == "fadeOpac")
 		{
-			fadeInFunction = "dojo.fx.html.fade(node, duration,"+ startOpac+ "," + endOpac + ", function(){"+ componentId  + "_faderState='fadedIn';});";
-		    fadeOutFunction = "dojo.fx.html.fade(node, duration,"+ endOpac+ "," + startOpac + ", function(){"+ componentId  + "_faderState='fadedOut';});";
+			fadeInFunction = "dojo.fx.html.fade(node, duration," + startOpac + "," + endOpac
+					+ ", function(){" + componentId + "_faderState='fadedIn';});";
+			fadeOutFunction = "dojo.fx.html.fade(node, duration," + endOpac + "," + startOpac
+					+ ", function(){" + componentId + "_faderState='fadedOut';});";
 		}
 		else
 		{
-			fadeInFunction = "dojo.fx.html.fadeIn(node, duration, function(){"+ componentId  + "_faderState='fadedIn';})";
-		    fadeOutFunction = "dojo.fx.html.fadeOut(node, duration, function(){"+ componentId  + "_faderState='fadedOut'});";
+			fadeInFunction = "dojo.fx.html.fadeIn(node, duration, function(){" + componentId
+					+ "_faderState='fadedIn';})";
+			fadeOutFunction = "dojo.fx.html.fadeOut(node, duration, function(){" + componentId
+					+ "_faderState='fadedOut'});";
 		}
-		
-		//set the correct state for the startDisplay value
-		if(startDisplay)
+
+		// set the correct state for the startDisplay value
+		if (startDisplay)
 		{
-			s = "\t<script language=\"JavaScript\" type=\"text/javascript\">\n" +
-				"\t"+ componentId  + "_faderState = 'fadedIn'; \n";
-		} else {
-			s = "\t<script language=\"JavaScript\" type=\"text/javascript\">\n" +
-			"\t"+ componentId  + "_faderState = 'fadedOut'; \n";		
+			s = "\t<script language=\"JavaScript\" type=\"text/javascript\">\n" + "\t"
+					+ componentId + "_faderState = 'fadedIn'; \n";
 		}
-			
-			s = s + "\tfunction "+ componentId  + "_fade(id, duration) { \n" +
-			"\t\tif("+ componentId  + "_faderState!='fading'){\n"+
-			"\t\t\tnode = document.getElementById(id);\n" +
-			"\t\t\tif("+ componentId  + "_faderState == 'fadedOut') \n" + 
-			"\t\t\t{ \n" +
-			"\t\t\t\t"+ componentId  + "_faderState = 'fading';\n" +
-			"\t\t\t\t" + fadeInFunction + "\n" +
-			"\t\t\t} else {\n" +
-			"\t\t\t\t"+ componentId  + "_faderState = 'fading';\n" +
-			"\t\t\t\t" + fadeOutFunction + "\n" +
-			"\t\t\t}\n" +
-			"\t\t}\n"+
-			"\t}\n" + 
-			"\t</script>\n";
+		else
+		{
+			s = "\t<script language=\"JavaScript\" type=\"text/javascript\">\n" + "\t"
+					+ componentId + "_faderState = 'fadedOut'; \n";
+		}
+
+		s = s + "\tfunction " + componentId + "_fade(id, duration) { \n" + "\t\tif(" + componentId
+				+ "_faderState!='fading'){\n" + "\t\t\tnode = document.getElementById(id);\n"
+				+ "\t\t\tif(" + componentId + "_faderState == 'fadedOut') \n" + "\t\t\t{ \n"
+				+ "\t\t\t\t" + componentId + "_faderState = 'fading';\n" + "\t\t\t\t"
+				+ fadeInFunction + "\n" + "\t\t\t} else {\n" + "\t\t\t\t" + componentId
+				+ "_faderState = 'fading';\n" + "\t\t\t\t" + fadeOutFunction + "\n" + "\t\t\t}\n"
+				+ "\t\t}\n" + "\t}\n" + "\t</script>\n";
 		container.getResponse().write(s);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see wicket.AjaxHandler#onBind()
 	 */
 	protected void onBind()
@@ -188,13 +243,15 @@ public class FXOnClickFader extends DojoFXHandler
 		Component c = getComponent();
 		this.component = (Component)c;
 		this.componentId = c.getId();
-		
-		//create a unique HTML for the wipe component
+
+		// create a unique HTML for the wipe component
 		HTMLID = this.component.getId() + "_" + component.getPath();
-		//Add ID to component, and bind effect to trigger
+		// Add ID to component, and bind effect to trigger
 		this.component.add(new AttributeModifier("id", true, new Model(HTMLID)));
-		this.getTrigger().add(new AppendAttributeModifier(getEventName(),true, new Model(componentId + "_fade('"+ HTMLID +"', " + getDuration() + ");")));
-		
+		this.getTrigger().add(
+				new AppendAttributeModifier(getEventName(), true, new Model(componentId + "_fade('"
+						+ HTMLID + "', " + getDuration() + ");")));
+
 	}
 
 }
--- src/main/java/wicket/contrib/markup/html/form/validation/FXFeedbackIndicator.java
+++ src/main/java/wicket/contrib/markup/html/form/validation/FXFeedbackIndicator.java
@@ -1,5 +1,20 @@
+/*
+ * $Id$ $Revision$ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package wicket.contrib.markup.html.form.validation;
-
 
 
 import wicket.Component;
@@ -9,22 +24,24 @@ import wicket.markup.html.image.Image;
 import wicket.model.Model;
 
 /**
- * Feedback indicator that adds a small image when validation fails.
- * Also creates an FXFeedbackTooltip connected to the image that
- * shows the errormessages for connected formcomponent.
+ * Feedback indicator that adds a small image when validation fails. Also
+ * creates an FXFeedbackTooltip connected to the image that shows the
+ * errormessages for connected formcomponent.
  * 
  * @author Marco van de Haar
  * @author Ruud Booltink
- *
+ * 
  */
 public class FXFeedbackIndicator extends FormComponentFeedbackIndicator
 {
 	private Tooltip feedbackTooltip;
 	private Component toCheck;
-	
+
 	/**
 	 * Construct
-	 * @param id Component ID
+	 * 
+	 * @param id
+	 *            Component ID
 	 */
 	public FXFeedbackIndicator(String id)
 	{
@@ -32,14 +49,13 @@ public class FXFeedbackIndicator extends FormComponentFeedbackIndicator
 		initIndicator();
 		feedbackTooltip = new Tooltip("feedbacktooltip", new FXFeedbackTooltip(this, 20, 30));
 		add(feedbackTooltip);
-		
+
 	}
-	
-	
+
+
 	/**
-	 * method to innitialize Indicator.
-	 * Necessary here to load static Image. 
-	 * subclasses can overwrite. 
+	 * method to innitialize Indicator. Necessary here to load static Image.
+	 * subclasses can overwrite.
 	 */
 	protected void initIndicator()
 	{
@@ -47,7 +63,7 @@ public class FXFeedbackIndicator extends FormComponentFeedbackIndicator
 		add(i);
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see wicket.markup.html.form.validation.FormComponentFeedbackIndicator#setIndicatorFor(wicket.Component)
 	 */
 	public void setIndicatorFor(Component component)
