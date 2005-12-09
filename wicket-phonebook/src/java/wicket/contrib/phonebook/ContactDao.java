4332194e8795063786f7c898321ef5057b126842
Igor Vaynberg <ivaynberg@wicketstuff.org>
2005-12-09 07:44:08 +0000

    updated for new datatable changes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@475 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-phonebook/src/java/wicket/contrib/phonebook/ContactDao.java
+++ wicket-phonebook/src/java/wicket/contrib/phonebook/ContactDao.java
@@ -20,44 +20,60 @@
 package wicket.contrib.phonebook;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
- * The implementation-independent DAO interface.
- * Defines the operations required to be supported by an implementation.
- *
+ * The implementation-independent DAO interface. Defines the operations required
+ * to be supported by an implementation.
+ * 
  * @author igor
  */
 public interface ContactDao {
 	/**
 	 * Load a {@link Contact} from the DB, given it's <tt>id</tt>.
-	 * @param id The id of the Contact to load.
+	 * 
+	 * @param id
+	 *            The id of the Contact to load.
 	 * @return Contact
 	 */
 	Contact load(long id);
-	
+
 	/**
 	 * Save the contact to the DB
+	 * 
 	 * @param contact
 	 * @return persistent instance of contact
 	 */
 	Contact save(Contact contact);
-	
+
 	/**
 	 * Delete a {@link Contact} from the DB, given it's <tt>id</tt>.
-	 * @param id The id of the Contact to delete.
+	 * 
+	 * @param id
+	 *            The id of the Contact to delete.
 	 */
 	void delete(long id);
-	
+
 	/**
 	 * Query the DB, using the supplied query details.
-	 * @param qp Query Paramaters to use.
+	 * 
+	 * @param qp
+	 *            Query Paramaters to use.
 	 * @return The results of the query as an Iterator.
 	 */
 	Iterator find(QueryParam qp, Contact filter);
-	
+
 	/**
 	 * Return the number of Contacts in the DB.
+	 * 
 	 * @return count
 	 */
 	int count(Contact filter);
+
+	/**
+	 * Returns the list of all unique last names in the database
+	 * 
+	 * @return the list of all unique last names in the database
+	 */
+	List getUniqueLastNames();
 }
