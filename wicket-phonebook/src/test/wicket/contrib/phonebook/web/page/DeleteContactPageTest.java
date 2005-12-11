6f32820e3f1f9c7afecdd68adaaa463a1815011a
Igor Vaynberg <ivaynberg@wicketstuff.org>
2005-12-11 09:06:10 +0000

    beginnings of unit testing example
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@480 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-phonebook/src/test/wicket/contrib/phonebook/web/page/DeleteContactPageTest.java
@@ -0,0 +1,86 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.phonebook.web.page;
+
+import javax.servlet.ServletException;
+
+import org.easymock.MockControl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.StaticApplicationContext;
+
+import junit.framework.TestCase;
+import wicket.Page;
+import wicket.contrib.phonebook.Contact;
+import wicket.contrib.phonebook.ContactDao;
+import wicket.contrib.spring.SpringInjector;
+import wicket.contrib.spring.SpringInjectorLocator;
+import wicket.contrib.spring.test.ApplicationContextMock;
+import wicket.contrib.spring.test.SpringContextLocatorMock;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.form.Button;
+import wicket.markup.html.form.Form;
+import wicket.util.tester.DummyHomePage;
+import wicket.util.tester.WicketTester;
+
+public class DeleteContactPageTest extends TestCase {
+
+	
+	
+	public void test() throws ServletException {
+		
+		Contact contact=new Contact();
+		
+		MockControl daoCtrl=MockControl.createControl(ContactDao.class);
+		ContactDao dao=(ContactDao) daoCtrl.getMock();
+		
+		daoCtrl.expectAndReturn(dao.load(10), contact);
+		dao.delete(10);
+		
+		daoCtrl.replay();
+		
+		ApplicationContextMock appctx=new ApplicationContextMock();
+		appctx.putBean("contactDao", dao);
+		
+		SpringContextLocatorMock ctxLocator=new SpringContextLocatorMock(appctx);
+		
+		SpringInjector injector=new SpringInjector(ctxLocator);
+		
+		SpringInjectorLocator.setInjector(injector);
+		
+		
+
+		WicketTester app=new WicketTester();
+		
+		Page page=new DeleteContactPage(new DummyHomePage(), 10);
+		
+		app.startPage(page);
+		app.assertRenderedPage(DeleteContactPage.class);
+		app.assertComponent("confirmForm", Form.class);
+		app.assertComponent("confirmForm:confirm", Button.class);
+		app.setParameterForNextRequest("confirmForm:confirm", "pressed");
+		app.submitForm("confirmForm");
+		
+		daoCtrl.verify();
+		
+	}
+	
+	
+	
+	
+}
