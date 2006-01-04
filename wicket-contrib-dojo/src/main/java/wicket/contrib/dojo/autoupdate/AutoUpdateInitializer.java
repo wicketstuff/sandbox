881d2bfc5c2425cac4030f81702162a9ee73141f
Marco van de haar <marcovandehaar@wicketstuff.org>
2006-01-04 10:20:52 +0000

    dojo auto update classes added
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@510 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/autoupdate/AutoUpdateInitializer.java
@@ -0,0 +1,27 @@
+package wicket.contrib.dojo.autoupdate;
+
+import wicket.Application;
+import wicket.IInitializer;
+import wicket.contrib.markup.html.tooltip.TooltipPanel;
+import wicket.markup.html.PackageResource;
+
+/**
+ * Innitializer class for autoupdate handler
+ * 
+ * @author Marco van de Haar
+ * @author Ruud Booltink
+ *
+ */
+public class AutoUpdateInitializer implements IInitializer
+{
+
+	/* (non-Javadoc)
+	 * @see wicket.IInitializer#init(wicket.Application)
+	 */
+	public void init(Application application)
+	{
+		PackageResource.bind(application, DojoAutoUpdateHandler.class, "autoupdate.js");
+		
+	}
+
+}
---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/autoupdate/IUpdatable.java
@@ -0,0 +1,17 @@
+package wicket.contrib.dojo.autoupdate;
+
+/**
+ * Interface for components which need to be compatible with  DojoAutoUpdatehandler. 
+ * @author Ruud Booltink
+ * @author Marco van de Haar
+ */
+public interface IUpdatable
+{
+
+/**
+ * This method is called on any component implementing Updatable before it is rerendered.
+ */
+void update();
+
+
+}
