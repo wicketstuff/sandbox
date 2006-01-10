363077b8312cf7506762c278c4983d1de6199fc7
Eelco Hillenius <eelco12@wicketstuff.org>
2006-01-10 12:50:18 +0000

    get it compiling again
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@535 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/ExampleApplication.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/ExampleApplication.java
@@ -11,39 +11,37 @@ import wicket.util.time.Duration;
 /**
  * Runs the ExampleApplication when invoked from command line.
  */
-public class ExampleApplication extends WebApplication
-{    
+public class ExampleApplication extends WebApplication {
 	/** Logging */
 	private static final Log log = LogFactory.getLog(ExampleApplication.class);
 
-    /**
-     * Constructor
-     */
-	public ExampleApplication()
-	{
-		getPages().setHomePage(Index.class);
-		if (!Boolean.getBoolean("cache-markup"))
-		{
-			getSettings().setResourcePollFrequency(Duration.ONE_SECOND);
+	/**
+	 * Constructor
+	 */
+	public ExampleApplication() {
+		if (!Boolean.getBoolean("cache-markup")) {
+			getResourceSettings().setResourcePollFrequency(Duration.ONE_SECOND);
 			log.info("Markup caching is INACTIVE");
-		}
-		else
-		{
+		} else {
 			log.info("Markup caching is ACTIVE");
 		}
 	}
 
-    /**
-     * @see wicket.protocol.http.WebApplication#getSessionFactory()
-     */
-    public ISessionFactory getSessionFactory()
-    {
-        return new ISessionFactory()
-        {        	
-			public Session newSession()
-            {
-                return new ExampleSession(ExampleApplication.this);
-            }
-        };
-    }
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	public Class getHomePage() {
+		return Index.class;
+	}
+
+	/**
+	 * @see wicket.protocol.http.WebApplication#getSessionFactory()
+	 */
+	public ISessionFactory getSessionFactory() {
+		return new ISessionFactory() {
+			public Session newSession() {
+				return new ExampleSession(ExampleApplication.this);
+			}
+		};
+	}
 }
