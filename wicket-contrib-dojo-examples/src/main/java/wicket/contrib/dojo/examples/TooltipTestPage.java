ebc537586194b5fdfd02e9fbe308843843586ef2
Marco van de haar <marcovandehaar@wicketstuff.org>
2006-01-25 15:48:24 +0000

    minor fixes to tooltip page and validation page
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@561 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/TooltipTestPage.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/TooltipTestPage.java
@@ -2,10 +2,7 @@ package wicket.contrib.dojo.examples;
 
 import wicket.PageParameters;
 import wicket.contrib.dojo.DojoAjaxHandler;
-import wicket.contrib.dojo.autoupdate.DojoAutoUpdateHandler;
-import wicket.contrib.dojo.examples.rssreader.DescriptionModel;
 import wicket.contrib.dojo.examples.rssreader.DescriptionPanel;
-import wicket.contrib.dojo.examples.rssreader.FeedContainer;
 import wicket.contrib.dojo.examples.rssreader.UpdateLabel;
 import wicket.contrib.markup.html.tooltip.SimpleTooltip;
 import wicket.contrib.markup.html.tooltip.Tooltip;
@@ -14,7 +11,6 @@ import wicket.markup.html.WebPage;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.image.Image;
 import wicket.model.CompoundPropertyModel;
-import wicket.model.Model;
 
 public class TooltipTestPage extends WebPage{
 	
@@ -37,7 +33,7 @@ public class TooltipTestPage extends WebPage{
 		add(target = new Label("target","point here for a simple tooltip"));
 		add(new Tooltip("stooltip", new SimpleTooltip(target, "help!")));
 		add(target2 = new Label("target2","point here for a Dynamic tooltip"));
-		add(new Tooltip("tooltip2",new MyTooltip(new CompoundPropertyModel(new MyTooltipModel("this tooltip is a bit more dynamic with a model")) ,  target2, 100, 20)));
+		add(new Tooltip("tooltip2",new MyTooltip(new CompoundPropertyModel(new MyTooltipModel("this tooltip is a bit more dynamic with a model")) ,  target2, 100, 50)));
 		
 		//		add student example
         add(stlabel = new Label("stlabel", "Marco van de Haar"));
