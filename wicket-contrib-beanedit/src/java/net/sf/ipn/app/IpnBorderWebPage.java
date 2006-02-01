3b2f364eb359d1302b13b93fb34059afbddabb03
Juergen Donnerstag <jdonnerstag@wicketstuff.org>
2006-02-01 23:16:36 +0000

    as a side effect of Border.setTransparentResolver(boolean) it is now much easier to create bordered pages
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@570 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-beanedit/src/java/net/sf/ipn/app/IpnBorderWebPage.java
+++ wicket-contrib-beanedit/src/java/net/sf/ipn/app/IpnBorderWebPage.java
@@ -5,8 +5,7 @@ package net.sf.ipn.app;
 
 import java.util.List;
 
-import wicket.Component;
-import wicket.MarkupContainer;
+import wicket.markup.html.border.Border;
 
 /**
  * Ensures that user is authenticated in session. If no user is signed in, a sign in is
@@ -22,7 +21,6 @@ import wicket.MarkupContainer;
  */
 public abstract class IpnBorderWebPage extends IpnWebPage
 {
-
 	// Do these belong elsewhere?
 	public static final String HOME_TAB = "Home";
 	public static final String MY_PROFILE_TAB = "My Profile";
@@ -33,39 +31,9 @@ public abstract class IpnBorderWebPage extends IpnWebPage
 	public IpnBorderWebPage()
 	{
 		super();
+		Border border = getMainBorder();
+		border.setTransparentResolver(true);
 		super.add(getMainBorder());
-	}
-
-	/**
-	 * Adding children to instances of this class causes those children to be added to the
-	 * border child instead.
-	 * @see wicket.MarkupContainer#add(wicket.Component)
-	 */
-	public MarkupContainer add(final Component child)
-	{
-		getMainBorder().add(child);
-		return this;
-	}
-
-
-	/**
-	 * Removing children from instances of this class causes those children to be removed
-	 * from the border child instead.
-	 * @see wicket.MarkupContainer#removeAll()
-	 */
-	public void removeAll()
-	{
-		getMainBorder().removeAll();
-	}
-
-	/**
-	 * Replacing children on instances of this class causes those children to be replaced
-	 * on the border child instead.
-	 * @see wicket.MarkupContainer#replace(wicket.Component)
-	 */
-	public MarkupContainer replace(Component child)
-	{
-		return getMainBorder().replace(child);
 	}
 
 	/**
