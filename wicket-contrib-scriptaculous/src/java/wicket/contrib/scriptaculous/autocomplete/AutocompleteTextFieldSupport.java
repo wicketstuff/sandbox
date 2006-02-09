af4a36afaa3ab0ed04f69629f0c7ee7ded2f7eb8
Johan Compagner <joco01@wicketstuff.org>
2006-02-09 21:35:20 +0000

    resource reference refactor
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@575 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AutocompleteTextFieldSupport.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AutocompleteTextFieldSupport.java
@@ -48,7 +48,7 @@ public class AutocompleteTextFieldSupport extends TextField {
 
         private void addCssReference(HtmlHeaderContainer container,
                         ResourceReference ref) {
-                String url = container.getPage().urlFor(ref.getPath());
+                String url = container.getPage().urlFor(ref);
                 String s = "\t<link rel=\"stylesheet\" type=\"text/css\" href=\""
                                 + url + "\"/>\n";
                 write(container, s);
