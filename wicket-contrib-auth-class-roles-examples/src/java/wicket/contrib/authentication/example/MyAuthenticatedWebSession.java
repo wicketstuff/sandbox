afd15acb9631daba2b40294939bbae08ab03a628
Gili Tzabari <cowwoc@wicketstuff.org>
2006-02-27 05:55:59 +0000

    Initial commit
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@597 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-auth-class-roles-examples/src/java/wicket/contrib/authentication/example/MyAuthenticatedWebSession.java
@@ -0,0 +1,50 @@
+package wicket.contrib.authentication.example;
+
+import wicket.contrib.authentication.AuthenticatedWebApplication;
+import wicket.contrib.authentication.AuthenticatedWebSession;
+import wicket.contrib.authorization.strategies.role.example.AdminRole;
+
+/**
+ * Authenticated session subclass
+ *
+ * @author Jonathan Locke
+ * @author Gili Tzabari
+ */
+public class MyAuthenticatedWebSession extends AuthenticatedWebSession
+{
+  /**
+   * Construct.
+   *
+   * @param application
+   *            The application
+   */
+  public MyAuthenticatedWebSession(final AuthenticatedWebApplication application)
+  {
+    super(application);
+  }
+  
+  /**
+   * @see wicket.authentication.AuthenticatedWebSession#authenticate(java.lang.String,
+   *      java.lang.String)
+   */
+  @Override
+    public boolean authenticate(final String username, final String password)
+  {
+    // Check username and password
+    return username.equals("wicket") && password.equals("wicket");
+  }
+  
+  /**
+   * @see wicket.authentication.AuthenticatedWebSession#getRole()
+   */
+  @Override
+    public Class getRole()
+  {
+    if (isSignedIn())
+    {
+      // If the user is signed in, they have these roles
+      return AdminRole.class;
+    }
+    return null;
+  }
+}
---
+++ wicket-contrib-auth-class-roles-examples/src/java/wicket/contrib/authorization/strategies/role/example/AdminRole.java
@@ -0,0 +1,19 @@
+/*
+ * AdminRole.java
+ *
+ * Created on February 26, 2006, 11:52 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package wicket.contrib.authorization.strategies.role.example;
+
+/**
+ * An administrator role.
+ *
+ * @author Gili Tzabari
+ */
+public interface AdminRole
+  extends UserRole
+{}
