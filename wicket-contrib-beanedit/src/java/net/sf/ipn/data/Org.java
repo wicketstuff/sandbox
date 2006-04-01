bffd5630a3b23e052cd685677089c4789e3483e0
Eelco Hillenius <eelco12@wicketstuff.org>
2006-04-01 03:17:42 +0000

    removed stale project
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@640 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-beanedit/src/java/net/sf/ipn/data/Org.java
+++
@@ -1,6 +0,0 @@
-package net.sf.ipn.data;
-
-public class Org extends net.sf.ipn.data.auto._Org
-{
-
-}
--- wicket-contrib-beanedit/src/java/net/sf/ipn/data/SubscriptionRequest.java
+++
@@ -1,6 +0,0 @@
-package net.sf.ipn.data;
-
-public class SubscriptionRequest extends net.sf.ipn.data.auto._SubscriptionRequest
-{
-
-}
