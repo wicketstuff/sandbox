89cb41f0e739ac8f20a2fff0f45757744dba4a4a
Joshua Lim <josh_lim@wicketstuff.org>
2006-04-01 13:36:14 +0000

    repackaged from "extensions" to "contrib" fixed Calendar.java
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@642 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/wicket/contrib/markup/html/yui/AbstractYuiPanel.java
+++ wicket-contrib-yui/src/main/java/wicket/contrib/markup/html/yui/AbstractYuiPanel.java
@@ -16,7 +16,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package wicket.extensions.markup.html.yui;
+package wicket.contrib.markup.html.yui;
 
 import wicket.Application;
 import wicket.IInitializer;
