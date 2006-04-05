b521b919d086efdc5e41e7b7924be25351a442f5
Joshua Lim <josh_lim@wicketstuff.org>
2006-04-05 15:39:18 +0000

    wip - repackage, customisable slider using SliderSetting instead of css
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@669 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-yui/src/main/java/wicket/contrib/InlineStyle.java
@@ -0,0 +1,84 @@
+package wicket.contrib;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class InlineStyle {
+
+    // valid Styles....
+    
+    Map styleMap = new HashMap();
+    
+    /**
+     * 
+     *
+     */
+    public InlineStyle() {
+    }
+
+    /**
+     * 
+     * @param map
+     */
+    public InlineStyle(Map map) {
+        this.styleMap.putAll(map);
+    }
+
+    /**
+     * @returns a ":" separated String to be used a an Inline Style
+     * 
+     */
+    
+    public String getStyle() {
+        
+        final StringBuffer buffer = new StringBuffer();
+        for (final Iterator iterator = styleMap.entrySet().iterator(); iterator.hasNext();)
+        {
+            final Map.Entry entry = (Map.Entry)iterator.next();
+            final Object value = entry.getValue();
+            if (value != null)
+            {
+                buffer.append(entry.getKey());
+                buffer.append(" : ");
+                buffer.append(value);
+                buffer.append(" ; ");
+            }
+        }
+        return buffer.toString();
+    }
+
+    /**
+     * adds a style element with teh value...
+     * TODO : check for valid element string and value ? override? 
+     * 
+     * @param element
+     * @param value
+     */
+    
+    public void add(String element, String value) {
+        
+        if (isValid(element, value)) {
+//            if (styleMap.get(element) != null) {
+//            }
+            styleMap.put(element,value);
+        }
+    }
+
+    /**
+     * validate is this style is valid for this element value...
+     * now it just returns true all the time...
+     *  
+     * @param element
+     * @param value
+     * @return
+     */
+    private boolean isValid(String element, String value) {
+        // TODO Auto-generated method stub
+        return true;
+    }
+    
+    
+    
+    
+}
