87845323e4f172550aa7643a4f10959ffd5fc2e1
Joshua Lim <josh_lim@wicketstuff.org>
2006-04-07 10:07:24 +0000

    WIP - Slider Bars
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@674 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/wicket/contrib/markup/html/yui/slider/SliderSettings.java
+++ wicket-contrib-yui/src/main/java/wicket/contrib/markup/html/yui/slider/SliderSettings.java
@@ -1,14 +1,20 @@
 package wicket.contrib.markup.html.yui.slider;
 
+import wicket.RequestCycle;
+import wicket.ResourceReference;
 import wicket.contrib.InlineStyle;
 import wicket.markup.html.PackageResource;
+import wicket.markup.html.PackageResourceReference;
 
 public class SliderSettings {
 
     private static final long serialVersionUID = 1L;
     
-    private PackageResource rightCornerResource;
-    private PackageResource leftCornerResource;
+    /* Resources needed to generate the slider bar */
+    private ResourceReference rightCornerResource;
+    private ResourceReference leftCornerResource;
+    private ResourceReference backgroundResource;
+    private ResourceReference thumbResource;
     
     private InlineStyle background = new InlineStyle();
     private InlineStyle handle = new InlineStyle();
@@ -17,7 +23,9 @@ public class SliderSettings {
     private String leftUp;
     private String rightDown;
     private String tick;
-
+    private String rightDownEnd;
+    private String tickSize;
+    
     /**
      * Contructor 
      * TODO : allow for dynamic creation of other styles...
@@ -27,7 +35,7 @@ public class SliderSettings {
     }
     
     /**
-     * Default Style constructor
+     * Default Style constructor. If no Style specified, creates a default style slider
      * 
      * @param leftUp
      * @param rightDown
@@ -36,35 +44,45 @@ public class SliderSettings {
     
     public SliderSettings(Integer leftUp, Integer rightDown, Integer tick) {
 
+        int thumbWidth = 18;
+        int left = leftUp.intValue() - thumbWidth;
+        int height = 25;
+        int bgLength = leftUp.intValue() + rightDown.intValue();
+        int tickSize = (int)(((float) thumbWidth /2) + tick.intValue());
+        
+        setTickSize(Integer.toString(tickSize));
+        String width = Integer.toString(bgLength - thumbWidth);
+        
         setLeftUp(leftUp.toString());
         setRightDown(rightDown.toString());
         setTick(tick.toString());
+        setRightDownEnd(Integer.toString(thumbWidth + bgLength));
         
-        String width = Integer.toString(leftUp.intValue() + rightDown.intValue());
         
         /* background */
-        getBackground().add("background", "url(" + PackageResource.get(Slider.class, "style/bg.gif").getResourceStream() + ")");
-        getBackground().add("background-repeat", "repeat-x");
-        getBackground().add("height", "15px");
-        getBackground().add("width", width + "px");
+        setBackgroundResource(new PackageResourceReference(Slider.class, "style/bg.gif"));
+        getBackground().add("background", "url(" + RequestCycle.get().urlFor(getBackgroundResource()) + ") repeat-x");
+        getBackground().add("height",     "25px");
+        getBackground().add("width",      width + "px");
         
         /* left */
-        setLeftCornerResource(PackageResource.get(Slider.class, "style/left.gif"));
+        setLeftCornerResource(new PackageResourceReference(Slider.class, "style/left.gif"));
         
         /* right */
-        setRightCornerResource(PackageResource.get( Slider.class, "style/right.gif"));
+        setRightCornerResource(new PackageResourceReference( Slider.class, "style/right.gif"));
         
         /* handle */
-        getHandle().add("width", "18px");
-        getHandle().add("height", "15px"); 
-        getHandle().add("top", "0px");
-        getHandle().add("left", "-15px"); // TODO : calculate ??
+        getHandle().add("width",    thumbWidth + "px");
+        getHandle().add("height",   height + "px"); 
+        getHandle().add("top",      "0px");
+        getHandle().add("left",     left + "px");
         
         /* thumb */
-        getThumb().add("background", "url(" + PackageResource.get(Slider.class, "style/thumb.png").getResourceStream() + ")");
-        getThumb().add("background-repeat", "no-repeat");
-        getThumb().add("height", "15px");
-        getThumb().add("width", "18px");
+        setThumbResource(new PackageResourceReference(Slider.class, "style/thumb.png"));
+        getThumb().add("background",        "url(" + RequestCycle.get().urlFor(getThumbResource()) + ") no-repeat");
+        getThumb().add("background-position", "center");
+        getThumb().add("height",            "100%");
+        getThumb().add("width",             thumbWidth + "px");
     }
 
     public InlineStyle getBackground() {
@@ -91,22 +109,6 @@ public class SliderSettings {
         this.thumb = thumb;
     }
 
-    public PackageResource getLeftCornerResource() {
-        return leftCornerResource;
-    }
-
-    public void setLeftCornerResource(PackageResource leftCornerResource) {
-        this.leftCornerResource = leftCornerResource;
-    }
-
-    public PackageResource getRightCornerResource() {
-        return rightCornerResource;
-    }
-
-    public void setRightCornerResource(PackageResource rightCornerResource) {
-        this.rightCornerResource = rightCornerResource;
-    }
-
     public String getLeftUp() {
         return leftUp;
     }
@@ -129,5 +131,53 @@ public class SliderSettings {
 
     public void setTick(String tick) {
         this.tick = tick;
+    }
+
+    public ResourceReference getBackgroundResource() {
+        return backgroundResource;
+    }
+
+    public void setBackgroundResource(ResourceReference backgroundResource) {
+        this.backgroundResource = backgroundResource;
+    }
+
+    public ResourceReference getLeftCornerResource() {
+        return leftCornerResource;
+    }
+
+    public void setLeftCornerResource(ResourceReference leftCornerResource) {
+        this.leftCornerResource = leftCornerResource;
+    }
+
+    public ResourceReference getRightCornerResource() {
+        return rightCornerResource;
+    }
+
+    public void setRightCornerResource(ResourceReference rightCornerResource) {
+        this.rightCornerResource = rightCornerResource;
+    }
+
+    public ResourceReference getThumbResource() {
+        return thumbResource;
+    }
+
+    public void setThumbResource(ResourceReference thumbResource) {
+        this.thumbResource = thumbResource;
+    }
+
+    public String getRightDownEnd() {
+        return rightDownEnd;
+    }
+
+    public void setRightDownEnd(String rightDownEnd) {
+        this.rightDownEnd = rightDownEnd;
+    }
+
+    public String getTickSize() {
+        return tickSize;
+    }
+
+    public void setTickSize(String tickSize) {
+        this.tickSize = tickSize;
     }
 }
