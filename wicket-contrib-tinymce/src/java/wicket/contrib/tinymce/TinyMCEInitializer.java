6ee7d9b09819dea75d56922d14e5418f8c1d43ec
Iulian Costan <syca@wicketstuff.org>
2006-04-08 14:26:14 +0000

    update to use wicket's package resource initialization
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@678 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-tinymce/src/java/wicket/contrib/tinymce/TinyMCEInitializer.java
+++ wicket-contrib-tinymce/src/java/wicket/contrib/tinymce/TinyMCEInitializer.java
@@ -17,20 +17,11 @@
  */
 package wicket.contrib.tinymce;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.regex.Pattern;
 
 import wicket.Application;
 import wicket.IInitializer;
 import wicket.markup.html.PackageResource;
-import wicket.util.string.Strings;
 
 /**
  * Wicket initializer for TinyMCE component.
@@ -39,86 +30,12 @@ import wicket.util.string.Strings;
  */
 public class TinyMCEInitializer implements IInitializer {
 
-	private static final Log log = LogFactory.getLog(TinyMCEInitializer.class);
-
-	private static final String TINY_MCE = "tiny_mce";
+	private static final String TINY_MCE = "^tiny_mce.*(.js|.gif|.html|.css)$";
 
 	private Class scope = TinyMCEPanel.class;
 
 	public void init(Application application) {
-
-		// TODO resource registration works recursively now;
-		// consider using that instead of this customized search
-
-		String protocol = scope.getResource("").getProtocol();
-		if ("file".equals(protocol)) {
-			initFromClasses(application);
-
-		} else if ("jar".equals(protocol)) {
-			initFromJar(application);
-
-		} else {
-			if (log.isErrorEnabled()) {
-				log
-						.error("unknown protocol, only file and jar are implemented");
-			}
-			throw new UnsupportedOperationException("protocol " + protocol
-					+ "not implemented");
-		}
-	}
-
-	private void initFromJar(Application application) {
-		ZipFile zipFile = null;
-		try {
-			String basePath = scope.getResource(
-					Strings.afterLast(scope.getName(), '.') + ".class")
-					.getPath();
-			String jarFilePath = new URL(Strings.beforeLast(basePath, '!'))
-					.getPath();
-			zipFile = new ZipFile(jarFilePath);
-		} catch (IOException e) {
-			if (log.isErrorEnabled()) {
-				log.error("tinymce.jar file exception", e);
-			}
-			throw new RuntimeException(e);
-		}
-
-		Enumeration entries = zipFile.entries();
-		while (entries.hasMoreElements()) {
-			ZipEntry zipEntry = (ZipEntry) entries.nextElement();
-			if (zipEntry.getName().indexOf(TINY_MCE) > 0) {
-				String name = zipEntry.getName();
-				bindResource(application, name);
-			}
-		}
-	}
-
-	private void initFromClasses(Application application) {
-		String basePath = scope.getResource("").getPath();
-		initResources(application, basePath);
-	}
-
-	private void initResources(Application application, String path) {
-		File tinyMceDir = new File(path + "/" + TINY_MCE);
-		recursiveInitialization(application, tinyMceDir);
-	}
-
-	private void recursiveInitialization(Application application, File dir) {
-		File[] files = dir.listFiles();
-		for (int i = 0; i < files.length; i++) {
-			File file = files[i];
-			if (file.isDirectory()) {
-				recursiveInitialization(application, file);
-			} else {
-				String uri = file.toURI().toString();
-				bindResource(application, uri);
-			}
-		}
-	}
-
-	private void bindResource(Application application, String uri) {
-		String resource = uri.substring(uri.indexOf(TINY_MCE));
-		PackageResource.bind(application, scope, resource);
+		PackageResource.bind(application, scope, Pattern.compile(TINY_MCE), true);
 	}
 
 	/* used for testing purpose */
