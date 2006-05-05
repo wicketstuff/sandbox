0c7289e3a91bfb08a80770f6dd0bc99d3d35d2a3
Joshua Lim <josh_lim@wicketstuff.org>
2006-05-05 08:11:12 +0000

    Yui Slider Refactorings
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-yui@705 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/ImageResourceInfo.java
@@ -0,0 +1,108 @@
+package wicket.contrib;
+
+import java.io.InputStream;
+
+import org.devlib.schmidt.imageinfo.ImageInfo;
+
+import wicket.ResourceReference;
+import wicket.markup.html.PackageResource;
+import wicket.util.resource.IResourceStream;
+import wicket.util.resource.ResourceStreamNotFoundException;
+
+/**
+ * Class to handle Image Info
+ * 
+ * @author josh
+ */
+
+public class ImageResourceInfo
+{
+	private PackageResource resource = null;
+	private ImageInfo imageInfo = new ImageInfo();
+	
+	/**
+	 * Construct
+	 * @param resource
+	 */
+	public ImageResourceInfo(PackageResource resource)
+	{
+		this.resource = resource;
+		this.imageInfo.setInput(getInputStream());
+		this.imageInfo.check();
+	}
+
+	/**
+	 * Construct.
+	 * 
+	 * @param reference
+	 */
+	public ImageResourceInfo(ResourceReference reference)
+	{
+		this(PackageResource.get(reference.getScope(), reference.getName()));
+	}
+
+	/**
+	 * get the Input Stream for this resource
+	 * 
+	 * @return
+	 */
+	
+	private InputStream getInputStream() 
+	{	
+		IResourceStream rs = this.resource.getResourceStream();
+		try
+		{
+			return rs.getInputStream();
+		}
+		catch (ResourceStreamNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		return null;  
+	}
+	
+	/**
+	 * gets the height 
+	 * 
+	 * @return
+	 */
+	public int getHeight()
+	{
+		return getImageInfo().getHeight();
+	}
+	
+	/**
+	 * gets teh width from ImageInfo ... is there a way to do this 
+	 * wrapping by delegating all calls to the wrapped ImageInfo object ?
+	 * 
+	 * @return
+	 */
+	public int getWidth()
+	{
+		return getImageInfo().getWidth();
+	}
+	
+	/*
+	 * ACCESSORS 
+	 */
+
+	public PackageResource getResource()
+	{
+		return resource;
+	}
+
+	public void setResource(PackageResource resource)
+	{
+		this.resource = resource;
+	}
+
+	public ImageInfo getImageInfo()
+	{
+		return imageInfo;
+	}
+
+	public void setImageInfo(ImageInfo imageInfo)
+	{
+		this.imageInfo = imageInfo;
+	}
+}
