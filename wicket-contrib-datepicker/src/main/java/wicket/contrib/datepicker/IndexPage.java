b33a4d66b1b1d4f1a9387b5747fccce8786fc5bf
Janne Hietamäki <jannehietamaki@wicketstuff.org>
2006-07-07 06:44:57 +0000

    add wicket locale support to the DatepickerField
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@807 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-datepicker/src/main/java/wicket/contrib/datepicker/IndexPage.java
+++ wicket-contrib-datepicker/src/main/java/wicket/contrib/datepicker/IndexPage.java
@@ -1,13 +1,33 @@
 package wicket.contrib.datepicker;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import wicket.contrib.datepicker.datepicker.DatepickerField;
 import wicket.markup.html.WebPage;
+import wicket.markup.html.form.DropDownChoice;
 import wicket.markup.html.form.Form;
+import wicket.model.PropertyModel;
 
 public class IndexPage extends WebPage {
+	
+	Date date=new Date();
+	
+	public Date getDate(){
+		return date;
+	}
+	
+	public void setDate(Date date){
+		this.date=date;
+	}
+	
 	public IndexPage() {
 		Form form=new Form("form");
 		add(form);
-		form.add(new DatepickerField("dp"));
+		form.add(new DatepickerField("dp",new PropertyModel(this,"date"),getLocale()));
+		List locales=Arrays.asList(Locale.getAvailableLocales());
+		form.add(new DropDownChoice("locale",new PropertyModel(getSession(),"locale"),locales));
 	}
 }
