44ff72ba9853700b6f7681a1817bd43062804bbc
Eelco Hillenius <eelco12@wicketstuff.org>
2006-07-20 10:02:05 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@849 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-yui-examples/src/main/java/wicket/contrib/yui/examples/WicketExamplePage.java
@@ -0,0 +1,62 @@
+/*
+ * $Id: WicketExamplePage.java 5838 2006-05-24 20:44:49 +0000 (Wed, 24 May 2006)
+ * joco01 $ $Revision: 5947 $ $Date: 2006-05-24 20:44:49 +0000 (Wed, 24 May
+ * 2006) $
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.yui.examples;
+
+import wicket.markup.html.WebPage;
+import wicket.model.IModel;
+import wicket.util.string.Strings;
+
+/**
+ * Base class for all example pages.
+ * 
+ * @param <T>
+ *            Type
+ * 
+ * @author Jonathan Locke
+ */
+public class WicketExamplePage<T> extends WebPage<T>
+{
+	/**
+	 * Constructor
+	 */
+	public WicketExamplePage()
+	{
+		this(null);
+	}
+
+	/**
+	 * Construct.
+	 * 
+	 * @param model
+	 */
+	public WicketExamplePage(IModel<T> model)
+	{
+		super(model);
+		final String packageName = getClass().getPackage().getName();
+		new WicketExampleHeader(this, "mainNavigation", Strings.afterLast(packageName, '.'), this);
+		explain();
+	}
+
+	/**
+	 * Override base method to provide an explanation
+	 */
+	protected void explain()
+	{
+	}
+}
---
+++ wicket-contrib-yui-examples/src/main/java/wicket/examples/debug/RequestsPage.java
@@ -0,0 +1,129 @@
+/*
+ * $Id$ $Revision$ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/*
+ * $Id$ $Revision$ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.examples.debug;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.image.Image;
+import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.PageableListView;
+import wicket.markup.html.navigation.paging.PagingNavigator;
+import wicket.model.IModel;
+import wicket.model.Model;
+import wicket.protocol.http.RequestLogger.RequestData;
+import wicket.protocol.http.RequestLogger.SessionData;
+import wicket.util.convert.IConverter;
+import wicket.util.convert.converters.DateConverter;
+
+/**
+ * @author jcompagner
+ */
+public class RequestsPage extends WebPage
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param sessionData
+	 */
+	public RequestsPage(final SessionData sessionData)
+	{
+		new Image(this, "bug");
+
+		new SessionView(this, "session", sessionData.getSession());
+
+		IModel<List<RequestData>> requestsModel = new Model<List<RequestData>>()
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public List<RequestData> getObject()
+			{
+				return new ArrayList<RequestData>(sessionData.getRequests());
+			}
+		};
+		PageableListView<RequestData> listView = new PageableListView<RequestData>(this, "requests", requestsModel, 50)
+		{
+			private static final long serialVersionUID = 1L;
+
+			private final SimpleDateFormat sdf = new SimpleDateFormat("dd MMM hh:mm:ss.SSS");
+
+			@Override
+			protected void populateItem(ListItem item)
+			{
+				RequestData rd = (RequestData)item.getModelObject();
+				new Label(item, "startDate", new Model<Date>(rd.getStartDate()))
+				{
+					private static final long serialVersionUID = 1L;
+
+					/**
+					 * @see wicket.Component#getConverter(Class)
+					 */
+					@Override
+					public IConverter getConverter(Class type)
+					{
+
+						return new DateConverter()
+						{
+							private static final long serialVersionUID = 1L;
+
+							/**
+							 * @see wicket.util.convert.converters.DateConverter#getDateFormat(java.util.Locale)
+							 */
+							@Override
+							public DateFormat getDateFormat(Locale locale)
+							{
+								return sdf;
+							}
+						};
+					}
+				};
+				new Label(item, "timeTaken", new Model<Long>(rd.getTimeTaken()));
+				new Label(item, "eventTarget", new Model<String>(rd.getEventTargert()));
+				new Label(item, "responseTarget", new Model<String>(rd.getResponseTarget()));
+				new Label(item, "alteredObjects", new Model<String>(rd.getAlteredObjects()))
+						.setEscapeModelStrings(false);
+			}
+		};
+
+		new PagingNavigator(this, "navigator", listView);
+	}
+}
