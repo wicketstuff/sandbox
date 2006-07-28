a7ec9dca44b07f4c66485400fdcc001241a88289
Iulian Costan <syca@wicketstuff.org>
2006-07-28 08:06:32 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@862 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-tinymce-examples/src/java/wicket/contrib/examples/WicketExampleHeader.java
+++ wicket-contrib-tinymce-examples/src/java/wicket/contrib/examples/WicketExampleHeader.java
@@ -1,20 +1,20 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Id: WicketExampleHeader.java 578 2006-02-12 20:55:02 +0000 (Sun, 12 Feb
+ * 2006) syca $ $Revision$ $Date: 2006-02-12 20:55:02 +0000 (Sun, 12 Feb
+ * 2006) $
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.contrib.examples;
 
@@ -24,23 +24,26 @@ import wicket.markup.html.panel.Panel;
 
 /**
  * Navigation panel for the examples project.
- *
+ * 
  * @author Eelco Hillenius
  */
 public final class WicketExampleHeader extends Panel
 {
- 	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	/**
-     * Construct.
-     *
-     * @param id           id of the component
-     * @param exampleTitle title of the example
-     */
-    public WicketExampleHeader(MarkupContainer parent, String id, String exampleTitle)
-    {
-        super(parent, id);
-        
-        new Label(this, "exampleTitle", exampleTitle);
-    }
+	 * Construct.
+	 * 
+	 * @param id
+	 *            id of the component
+	 * @param exampleTitle
+	 *            title of the example
+	 * @param parent
+	 *            TODO
+	 */
+	public WicketExampleHeader(MarkupContainer parent, String id, String exampleTitle)
+	{
+		super(parent, id);
+		new Label(this, "exampleTitle", exampleTitle);
+	}
 }
