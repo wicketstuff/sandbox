ba26dc5af0dac49cb488a0cc9f43c3592c0e37fe
Eelco Hillenius <eelco12@wicketstuff.org>
2006-09-17 08:03:38 +0000

    2.0, fixes, fixes and fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo@926 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/markup/html/form/ImmediateCheckBox.js
@@ -0,0 +1,7 @@
+function immediateCheckBox(componentUrl, componentPath, val) {
+	dojo.io.bind({
+	  url: componentUrl + '&' + componentPath + '=' + val,
+	  mimetype: "text/plain",
+	  load: function(type, data, evt) {}
+	});
+}
--- src/main/java/wicket/contrib/markup/html/tooltip/SimpleTooltip.java
+++ src/main/java/wicket/contrib/markup/html/tooltip/SimpleTooltip.java
@@ -1,41 +1,52 @@
 package wicket.contrib.markup.html.tooltip;
 
 import wicket.Component;
+import wicket.MarkupContainer;
 import wicket.markup.html.basic.Label;
 
 /**
- * @author Marco van de Haar
- * simple form of Tooltip.java which has a default markup, style and holds a Label.
- *
+ * @author Marco van de Haar simple form of Tooltip.java which has a default
+ *         markup, style and holds a Label.
+ * 
  */
 public class SimpleTooltip extends TooltipPanel
 {
 
 	/**
-	 * @param id component ID
-	 * @param target target component
-	 * @param label text String for the label
+	 * @param parent
+	 * @param id
+	 *            component ID
+	 * @param target
+	 *            target component
+	 * @param label
+	 *            text String for the label
 	 * 
 	 * constructor with default positioning
 	 */
-	public SimpleTooltip(Component target, String label)
-		{
-			super(target);
-			add(new Label("tooltiplabel", label));
-		}
-	
+	public SimpleTooltip(MarkupContainer parent, Component target, String label)
+	{
+		super(parent, target);
+		new Label(this, "tooltiplabel", label);
+	}
+
 
 	/**
-	 * @param id component ID
-	 * @param target target component
-	 * @param label text String for the label
-	 * @param x x-offset
-	 * @param y y-offset
+	 * @param parent
+	 * @param id
+	 *            component ID
+	 * @param target
+	 *            target component
+	 * @param label
+	 *            text String for the label
+	 * @param x
+	 *            x-offset
+	 * @param y
+	 *            y-offset
 	 */
-	public SimpleTooltip(Component target, String label, int x, int y)
+	public SimpleTooltip(MarkupContainer parent, Component target, String label, int x, int y)
 	{
-		super(target, x, y);
-		add(new Label("tooltiplabel", label));
+		super(parent, target, x, y);
+		new Label(this, "tooltiplabel", label);
 	}
 
 }
