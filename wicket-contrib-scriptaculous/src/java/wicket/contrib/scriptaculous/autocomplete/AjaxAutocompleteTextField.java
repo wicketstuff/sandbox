22dc55663567848e7e8aa4301195c5cf74c0bf7f
ryan sonnek <wireframe6464@wicketstuff.org>
2006-10-06 04:18:29 +0000

    remove dependency on behaviour javascript library
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@955 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AjaxAutocompleteTextField.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AjaxAutocompleteTextField.java
@@ -2,14 +2,14 @@
  * $Id: AjaxAutocompleteTextField.java 612 2006-03-06 22:46:35 -0800 (Mon, 06
  * Mar 2006) eelco12 $ $Revision$ $Date: 2006-03-06 22:46:35 -0800 (Mon,
  * 06 Mar 2006) $
- * 
+ *
  * ==================================================================== Licensed
  * under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the
  * License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,13 +20,14 @@ package wicket.contrib.scriptaculous.autocomplete;
 
 import wicket.MarkupContainer;
 import wicket.RequestCycle;
+import wicket.contrib.scriptaculous.JavascriptBuilder;
 import wicket.contrib.scriptaculous.ScriptaculousAjaxHandler;
 import wicket.markup.html.form.FormComponent;
 import wicket.markup.html.internal.HtmlHeaderContainer;
 import wicket.util.resource.StringBufferResourceStream;
 
 /**
- * 
+ *
  * @author <a href="mailto:wireframe6464@users.sourceforge.net">Ryan Sonnek</a>
  */
 public abstract class AjaxAutocompleteTextField<T> extends AutocompleteTextFieldSupport<T>
@@ -76,12 +77,12 @@ public abstract class AjaxAutocompleteTextField<T> extends AutocompleteTextField
 	{
 		super.renderHead(container);
 
-		container.getResponse().write(
-				"<script type=\"text/javascript\">\n" + "var myrules = { \n" + "\t'#" + getId()
-						+ "' : function(el){ \n" + "\t\tnew Ajax.Autocompleter('" + getId()
-						+ "', '" + getAutocompleteId() + "', '" + handler.getCallbackUrl()
-						+ "', {});\n" + "\t} \n" + "} \n" + "Behaviour.register(myrules);\n"
-						+ "</script>\n");
+		JavascriptBuilder builder = new JavascriptBuilder();
+		builder.addLine("new Ajax.Autocompleter(");
+		builder.addLine("  '" + getId() + "', ");
+		builder.addLine("  '" + getAutocompleteId() + "', ");
+		builder.addLine("  '" + handler.getCallbackUrl() + "', {} );");
+		container.getResponse().write(builder.buildScriptTagString());
 	}
 
 	protected abstract String[] getResults(String input);
