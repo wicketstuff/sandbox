44b4fddf96b598a0cc8852c887c0586451d3f3e2
ryan sonnek <wireframe6464@wicketstuff.org>
2006-10-06 04:10:17 +0000

    remove unused variable/import
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@954 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/ScriptaculousAjaxHandler.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/ScriptaculousAjaxHandler.java
@@ -2,14 +2,14 @@
  * $Id: ScriptaculousAjaxHandler.java 759 2006-06-05 14:07:46 -0700 (Mon, 05 Jun
  * 2006) eelco12 $ $Revision$ $Date: 2006-06-05 14:07:46 -0700 (Mon, 05
  * Jun 2006) $
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,7 +18,6 @@
  */
 package wicket.contrib.scriptaculous;
 
-import wicket.Application;
 import wicket.Component;
 import wicket.ResourceReference;
 import wicket.behavior.AbstractAjaxBehavior;
@@ -31,7 +30,7 @@ import wicket.markup.html.IHeaderResponse;
  * As header contributions are done once per class, you can have multiple
  * instances/ subclasses without having duplicate header contributions.
  * </p>
- * 
+ *
  * @see <a href="http://script.aculo.us/">script.aculo.us</a>
  */
 public abstract class ScriptaculousAjaxHandler extends AbstractAjaxBehavior
@@ -65,7 +64,6 @@ public abstract class ScriptaculousAjaxHandler extends AbstractAjaxBehavior
 	{
 		super.renderHead(response);
 		// add our basic javascript needs to the header
-		Application application = Application.get();
 		response.renderJavascriptReference(new ResourceReference(ScriptaculousAjaxHandler.class,
 				"prototype.js"));
 		response.renderJavascriptReference(new ResourceReference(ScriptaculousAjaxHandler.class,
