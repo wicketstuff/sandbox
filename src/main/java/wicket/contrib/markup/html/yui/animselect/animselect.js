b11e7cae0e58495a7b99f87293edeba9e0869d25
CP Tan <cptan@wicketstuff.org>
2006-10-12 02:00:36 +0000

    - Upgrade to YUI 0.11.4 - Added AnimSelect, Selection - Add Comments - Still work in progress needs refactoring...
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2/wicket-contrib-yui@969 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/markup/html/yui/animselect/animselect.js
+++ src/main/java/wicket/contrib/markup/html/yui/animselect/animselect.js
@@ -1,14 +1,6 @@
-var anim${boxId}_${javaScriptId};
-var easing${boxId}_${javaScriptId} = ${easing};
-var duration${boxId}_${javaScriptId} = ${duration};
-var maxSelection${boxId}_${javaScriptId} = ${maxSelection};
-var visible${boxId}_${javaScriptId} = {opacity: {to: 1}};
-var invisible${boxId}_${javaScriptId} = {opacity: {to: 0}};
-var noOfBoxes${boxId}_${javaScriptId} = ${noOfBoxes}; 
 var boxes${boxId}_${javaScriptId} = ['DefaultImg${boxId}_${javaScriptId}', 'DefaultImgOver${boxId}_${javaScriptId}', 'SelectedImg${boxId}_${javaScriptId}', 'SelectedImgOver${boxId}_${javaScriptId}'];
 var count${boxId}_${javaScriptId} = 0;
-var value${boxId}_${javaScriptId} = ${value};
-var message${boxId}_${javaScriptId} = '${message}';
+var message_${javaScriptId} = '${message}';
 
 function fnMouseOver${boxId}_${javaScriptId}(){
 	if(count${boxId}_${javaScriptId}%2 == 0){
@@ -30,14 +22,12 @@ function fnMouseOut${boxId}_${javaScriptId}(){
 
 function fnClick${boxId}_${javaScriptId}(){
 	maxCount=0;
-	
-	//single selection
-	if(maxSelection${boxId}_${javaScriptId} == 1 ){ 
-		for(m=0; m< noOfBoxes${boxId}_${javaScriptId}; m++){ 
+	if(maxSelection_${javaScriptId} == 1 ){ 
+		for(m=0; m< noOfBoxes_${javaScriptId}; m++){ 
 			if(eval("boxes"+m+"_${javaScriptId}[0]") == this.id){	
 				eval("fnShow"+m+"_${javaScriptId}('SelectedImg"+m+"_${javaScriptId}')");
 				eval("count"+m+"_${javaScriptId}++");
-				//document.getElementById("selectedValue${boxId}_${javaScriptId}").value= value${boxId}_${javaScriptId};
+				document.getElementById("selectedValue_${javaScriptId}").value= selectedValues_${javaScriptId}[m];
 			}
 			else{
 				eval("fnShow"+m+"_${javaScriptId}('DefaultImg"+m+"_${javaScriptId}')");
@@ -45,61 +35,57 @@ function fnClick${boxId}_${javaScriptId}(){
 			}
 		}
 	}
-	
-	//multiple selection
 	else{
-		for(i=0; i< noOfBoxes${boxId}_${javaScriptId}; i++){
+		for(i=0; i< noOfBoxes_${javaScriptId}; i++){
 			if((eval("count"+i+"_${javaScriptId}"))%2 == 1){
 				maxCount++;	
 			}
 		}
-		if(maxCount>= maxSelection${boxId}_${javaScriptId} && count${boxId}_${javaScriptId}%2==0){
-			alert(message${boxId}_${javaScriptId});
+		if(maxCount>= maxSelection_${javaScriptId} && count${boxId}_${javaScriptId}%2==0){
+			alert(message_${javaScriptId});
 		}
 		else{
 			if(count${boxId}_${javaScriptId}%2 == 0) {
 				fnShow${boxId}_${javaScriptId}('SelectedImg${boxId}_${javaScriptId}');
 				count${boxId}_${javaScriptId}++;
-				fn_addSelection${boxId}_${javaScriptId}(value${boxId}_${javaScriptId});
+				fn_addSelection_${javaScriptId}(selectedValues_${javaScriptId}[${boxId}]);
 			}
 			else {	
 				fnShow${boxId}_${javaScriptId}('DefaultImgOver${boxId}_${javaScriptId}');
 				count${boxId}_${javaScriptId}++;
-				fn_removeSelection${boxId}_${javaScriptId}(value${boxId}_${javaScriptId});
+				fn_removeSelection_${javaScriptId}(selectedValues_${javaScriptId}[${boxId}]);
 			}
 		}
 	}
 }
 
-function fn_addSelection${boxId}_${javaScriptId}(selectedValue){
+function fn_addSelection_${javaScriptId}(selectedValue){
 	newValue="";
-	currentValue= document.getElementById("selectedValue${boxId}_${javaScriptId}").value;
+	currentValue= document.getElementById("selectedValue_${javaScriptId}").value;
 	if(currentValue == ""){
 		newValue=selectedValue;
 	}
 	else{
 		newValue=currentValue+","+selectedValue;
 	}
-	document.getElementById("selectedValue${boxId}_${javaScriptId}").value = newValue;
+	document.getElementById("selectedValue_${javaScriptId}").value = newValue;
 }
 
-function fn_removeSelection${boxId}_${javaScriptId}(unselectedValue){
+function fn_removeSelection_${javaScriptId}(unselectedValue){
 	newValue="";
-	currentValue= ","+document.getElementById("selectedValue${boxId}_${javaScriptId}").value;
+	currentValue= ","+document.getElementById("selectedValue_${javaScriptId}").value;
 	newValue= currentValue.replace(","+unselectedValue, "");
-	document.getElementById("selectedValue${boxId}_${javaScriptId}").value = newValue.substring(1, newValue.length);
+	document.getElementById("selectedValue_${javaScriptId}").value = newValue.substring(1, newValue.length);
 }
 
 function fnShow${boxId}_${javaScriptId}(elementId){
-
 	for(i=0; i<boxes${boxId}_${javaScriptId}.length; i++){
-	
 		if(elementId == boxes${boxId}_${javaScriptId}[i]){
-			anim = new YAHOO.util.Anim(boxes${boxId}_${javaScriptId}[i], visible${boxId}_${javaScriptId} , duration${boxId}_${javaScriptId} , easing${boxId}_${javaScriptId});
+			anim = new YAHOO.util.Anim(boxes${boxId}_${javaScriptId}[i], visible_${javaScriptId} , duration_${javaScriptId} , easing_${javaScriptId});
 			anim.animate();	
 		}
 		else{
-			anim = new YAHOO.util.Anim(boxes${boxId}_${javaScriptId}[i], invisible${boxId}_${javaScriptId}, duration${boxId}_${javaScriptId} , easing${boxId}_${javaScriptId});
+			anim = new YAHOO.util.Anim(boxes${boxId}_${javaScriptId}[i], invisible_${javaScriptId}, duration_${javaScriptId} , easing_${javaScriptId});
 			anim.animate();
 		}
 	}	
