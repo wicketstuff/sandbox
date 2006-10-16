efa680220d988f5d84becae586eefb10fde41706
CP Tan <cptan@wicketstuff.org>
2006-10-16 04:36:54 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2/wicket-contrib-yui@980 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/YuiSortConstants.java
@@ -0,0 +1,14 @@
+package wicket.contrib;
+
+import java.io.Serializable;
+
+/**
+ * YuiSortConstants allows specification of the type of sorting effects
+ * 
+ * @author cptan
+ *
+ */
+public class YuiSortConstants implements Serializable {
+    public static final String POINT = "POINT";
+    public static final String INTERSECT = "INTERSECT";
+}  
