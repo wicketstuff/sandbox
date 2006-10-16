72a48eb9d9dd80d9628d7510332fa963d73783dc
CP Tan <cptan@wicketstuff.org>
2006-10-16 09:27:58 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2/wicket-contrib-yui@992 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/markup/html/yui/anim/AnimGroup.java
+++ src/main/java/wicket/contrib/markup/html/yui/anim/AnimGroup.java
@@ -16,21 +16,24 @@ import wicket.model.AbstractReadOnlyModel;
  * An AnimGroup groups the options
  * 
  * @author cptan
+ * 
  */
 public class AnimGroup extends WebMarkupContainer {
 	private static final long serialVersionUID = 1L;
 
-	private List<AnimOption> animSelectOptionList;
+	private String javaScriptId;
 
-	private double duration;
+	private List<AnimOption> animOptionList;
 
 	private String easing;
 
-	private String javaScriptId;
+	private double duration;
 
 	private int maxSelection;
 
 	private String selectedValues = "";
+
+	private String valueId;
 
 	/**
 	 * Creates an AnimGroup
@@ -40,16 +43,18 @@ public class AnimGroup extends WebMarkupContainer {
 	 * @param settings -
 	 *            defines the animation settings
 	 */
-	public AnimGroup(String id, AnimSettings settings) {
+	public AnimGroup(String id, AnimSettings settings, String valueId) {
 		super(id);
 
-		this.animSelectOptionList = settings.getAnimOptionList();
+		this.animOptionList = settings.getAnimOptionList();
 		this.easing = settings.getEasing();
 		this.duration = settings.getDuration();
 		this.maxSelection = settings.getMaxSelection();
+		this.valueId = valueId;
 
-		for (int i = 0; i < animSelectOptionList.size(); i++) {
-			AnimOption animSelectOption = animSelectOptionList.get(i);
+		for (int i = 0; i < animOptionList.size(); i++) {
+			AnimOption animSelectOption = (AnimOption) animOptionList
+					.get(i);
 			String value = animSelectOption.getSelectedValue();
 			if (selectedValues.equals("")) {
 				selectedValues = "'" + value + "'";
@@ -61,7 +66,6 @@ public class AnimGroup extends WebMarkupContainer {
 		Label initialization = new Label("init", new AbstractReadOnlyModel() {
 			private static final long serialVersionUID = 1L;
 
-			@Override
 			public Object getObject(Component component) {
 				return getJavaScriptComponentInitializationScript();
 			}
@@ -79,13 +83,14 @@ public class AnimGroup extends WebMarkupContainer {
 	protected String getJavaScriptComponentInitializationScript() {
 		PackagedTextTemplate template = new PackagedTextTemplate(
 				AnimGroup.class, "init.js");
-		Map<String, Object> variables = new HashMap<String, Object>(6);
+		Map<Object, Object> variables = new HashMap<Object, Object>(7);
 		variables.put("javaScriptId", javaScriptId);
 		variables.put("easing", "YAHOO.util.Easing." + easing);
 		variables.put("duration", new Double(duration));
 		variables.put("maxSelection", new Integer(maxSelection));
-		variables.put("noOfBoxes", new Integer(animSelectOptionList.size()));
+		variables.put("noOfBoxes", new Integer(animOptionList.size()));
 		variables.put("selectedValues", selectedValues);
+		variables.put("valueId", "'" + valueId + "'");
 		template.interpolate(variables);
 		return template.getString();
 	}
@@ -93,7 +98,6 @@ public class AnimGroup extends WebMarkupContainer {
 	/**
 	 * Get the markup Id on attach
 	 */
-	@Override
 	protected void onAttach() {
 		super.onAttach();
 		javaScriptId = getMarkupId();
@@ -102,15 +106,147 @@ public class AnimGroup extends WebMarkupContainer {
 	/**
 	 * 
 	 */
-	@Override
-	protected void onComponentTagBody(MarkupStream markupStream,
-			ComponentTag openTag) {
-		super.onComponentTagBody(markupStream, openTag);
-		// StringBuffer buffer = new StringBuffer();
-		// buffer.append("\n<script type=\"text/javascript\">")
-		// .append(getJavaScriptComponentInitializationScript())
-		// .append("\n</script>\n");
-		// replaceComponentTagBody(markupStream, openTag, buffer.toString());
+	protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
+		super.onComponentTagBody(markupStream, openTag);		
+//		 StringBuffer buffer = new StringBuffer();
+//		 buffer.append("\n<script type=\"text/javascript\">")
+//		 .append(getJavaScriptComponentInitializationScript())
+//		 .append("\n</script>\n");
+//		 replaceComponentTagBody(markupStream, openTag, buffer.toString());
 	}
+
+	/**
+	 * Get the animOptionList
+	 * 
+	 * @return the animOptionList
+	 */
+	public List<AnimOption> getAnimOptionList() {
+		return animOptionList;
+	}
+
+	/**
+	 * Set the animOptionList
+	 * 
+	 * @param animOptionList
+	 *            the animOptionList to set
+	 */
+	public void setAnimOptionList(List<AnimOption> animOptionList) {
+		this.animOptionList = animOptionList;
+	}
+
+	/**
+	 * Get the duration
+	 * 
+	 * @return the duration
+	 */
+	public double getDuration() {
+		return duration;
+	}
+
+	/**
+	 * Set the duration
+	 * 
+	 * @param duration
+	 *            the duration to set
+	 */
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 * Get the easing
+	 * 
+	 * @return the easing
+	 */
+	public String getEasing() {
+		return easing;
+	}
+
+	/**
+	 * Set the easing
+	 * 
+	 * @param easing
+	 *            the easing to set
+	 */
+	public void setEasing(String easing) {
+		this.easing = easing;
+	}
+
+	/**
+	 * Get the JavaScriptId
+	 * 
+	 * @return the javaScriptId
+	 */
+	public String getJavaScriptId() {
+		return javaScriptId;
+	}
+
+	/**
+	 * Set the javaScriptId
+	 * 
+	 * @param javaScriptId
+	 *            the javaScriptId to set
+	 */
+	public void setJavaScriptId(String javaScriptId) {
+		this.javaScriptId = javaScriptId;
+	}
+
+	/**
+	 * Get the maximum selection allowed
+	 * 
+	 * @return the maxSelection
+	 */
+	public int getMaxSelection() {
+		return maxSelection;
+	}
+
+	/**
+	 * Set the maximum selections allowed
+	 * 
+	 * @param maxSelection
+	 *            the maxSelection to set
+	 */
+	public void setMaxSelection(int maxSelection) {
+		this.maxSelection = maxSelection;
+	}
+
+	/**
+	 * Get the selected values
+	 * 
+	 * @return the selectedValues
+	 */
+	public String getSelectedValues() {
+		return selectedValues;
+	}
+
+	/**
+	 * Set the selected values
+	 * 
+	 * @param selectedValues
+	 *            the selectedValues to set
+	 */
+	public void setSelectedValues(String selectedValues) {
+		this.selectedValues = selectedValues;
+	}
+
+	/**
+	 * Get the value id
+	 * 
+	 * @return the valueId
+	 */
+	public String getValueId() {
+		return valueId;
+	}
+
+	/**
+	 * Set the value id
+	 * 
+	 * @param valueId
+	 *            the valueId to set
+	 */
+	public void setValueId(String valueId) {
+		this.valueId = valueId;
+	}
+
 
 }
