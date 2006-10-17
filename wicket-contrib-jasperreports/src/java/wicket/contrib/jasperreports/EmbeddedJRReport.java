44de68b02b0e567fb3e17f3eafa14943c664abf1
Justin Lee <evanchooly@wicketstuff.org>
2006-10-17 17:51:39 +0000

    updating for wicket 2.0 and java 1.5
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1009 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-jasperreports/src/java/wicket/contrib/jasperreports/EmbeddedJRReport.java
+++ wicket-contrib-jasperreports/src/java/wicket/contrib/jasperreports/EmbeddedJRReport.java
@@ -1,15 +1,14 @@
 /*
- * $Id: EmbeddedJRReport.java 627 2006-03-20 07:12:13 +0000 (Mon, 20 Mar 2006)
- * eelco12 $ $Revision$ $Date: 2006-03-20 07:12:13 +0000 (Mon, 20 Mar
- * 2006) $
- * 
+ * $Id$
+ * $Revision$ $Date$
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -25,59 +24,35 @@ import wicket.markup.ComponentTag;
 import wicket.markup.html.WebComponent;
 
 /**
- * Component for embedding a jasper report in a page. This component must be
- * attached to an &lt;object&gt; tag. If you don't want to embed the report, but
- * have a link to it instead, use {@link ResourceReference}.
- * 
+ * Component for embedding a jasper report in a page. This component must be attached to an &lt;object&gt; tag. If you
+ * don't want to embed the report, but have a link to it instead, use {@link ResourceReference}.
+ *
  * @author <a href="mailto:evanchooly@gmail.com">Justin Lee</a>
  */
-public final class EmbeddedJRReport extends WebComponent implements IResourceListener
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public final class EmbeddedJRReport extends WebComponent implements IResourceListener {
+    private static final long serialVersionUID = 1L;
+    private final JRResource resource;
 
-	private final JRResource resource;
+    public EmbeddedJRReport(MarkupContainer parent, String id, JRResource report) {
+        super(parent, id);
+        resource = report;
+    }
 
-	/**
-	 * Construcxt.
-	 * 
-	 * @param parent
-	 *            The parent
-	 * @param id
-	 *            component id
-	 * @param resource
-	 *            the resource
-	 */
-	public EmbeddedJRReport(MarkupContainer parent, String id, JRResource resource)
-	{
-		super(parent, id);
-		this.resource = resource;
-	}
+    /**
+     * @see IResourceListener#onResourceRequested()
+     */
+    public void onResourceRequested() {
+        resource.onResourceRequested();
+    }
 
-	/**
-	 * @see wicket.IResourceListener#onResourceRequested()
-	 */
-	public void onResourceRequested()
-	{
-		resource.onResourceRequested();
-	}
-
-	/**
-	 * @see wicket.Component#onComponentTag(wicket.markup.ComponentTag)
-	 */
-	protected void onComponentTag(ComponentTag tag)
-	{
-		if (!"object".equalsIgnoreCase(tag.getName()))
-		{
-			findMarkupStream().throwMarkupException(
-					"Component "
-							+ getId() + " must be applied to a tag of type 'object' not "
-							+ tag.toUserDebugString());
-		}
-		tag.put("data", getResponse().encodeURL(urlFor(IResourceListener.INTERFACE)));
-		tag.put("type", resource.getContentType());
-		super.onComponentTag(tag);
-	}
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        if(!"object".equalsIgnoreCase(tag.getName())) {
+            findMarkupStream().throwMarkupException("Component " + getId() + " must be applied to a tag of type "
+                + "'object' not " + tag.toUserDebugString());
+        }
+        tag.put("data", getResponse().encodeURL(urlFor(IResourceListener.INTERFACE)));
+        tag.put("type", resource.getContentType());
+        super.onComponentTag(tag);
+    }
 }
--- wicket-contrib-jasperreports/src/java/wicket/contrib/jasperreports/JRXlsResource.java
+++ wicket-contrib-jasperreports/src/java/wicket/contrib/jasperreports/JRXlsResource.java
@@ -5,83 +5,72 @@ import java.io.InputStream;
 import java.net.URL;
 
 import net.sf.jasperreports.engine.JRAbstractExporter;
+import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 
 /**
  * Resource class for jasper reports Excell resources.
- * 
+ *
  * @author Justin Lee
  */
-public final class JRXlsResource extends JRResource
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public final class JRXlsResource extends JRResource {
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Construct without a report. You must provide a report before you can use
-	 * this resource.
-	 */
-	public JRXlsResource()
-	{
-		super();
-	}
+    /**
+     * Construct without a report. You must provide a report before you can use this resource.
+     */
+    public JRXlsResource() {
+        super();
+    }
 
-	/**
-	 * Construct.
-	 * 
-	 * @param report
-	 *            the report input stream
-	 */
-	public JRXlsResource(InputStream report)
-	{
-		super(report);
-	}
+    /**
+     * Construct.
+     *
+     * @param report the report input stream
+     */
+    public JRXlsResource(InputStream report) {
+        super(report);
+    }
 
-	/**
-	 * Construct.
-	 * 
-	 * @param report
-	 *            the report input stream
-	 */
-	public JRXlsResource(URL report)
-	{
-		super(report);
-	}
+    /**
+     * Construct.
+     *
+     * @param report the report input stream
+     */
+    public JRXlsResource(JasperReport report) {
+        super(report);
+    }
 
-	/**
-	 * Construct.
-	 * 
-	 * @param report
-	 *            the report input stream
-	 */
-	public JRXlsResource(File report)
-	{
-		super(report);
-	}
+    /**
+     * Construct.
+     *
+     * @param report the report input stream
+     */
+    public JRXlsResource(URL report) {
+        super(report);
+    }
 
-	/**
-	 * @see JRResource#newExporter()
-	 */
-	public JRAbstractExporter newExporter()
-	{
-		return new JRXlsExporter();
-	}
+    /**
+     * Construct.
+     *
+     * @param report the report input stream
+     */
+    public JRXlsResource(File report) {
+        super(report);
+    }
 
-	/**
-	 * @see JRResource#getContentType()
-	 */
-	public String getContentType()
-	{
-		return "application/excel";
-	}
+    @Override
+    public JRAbstractExporter newExporter() {
+        return new JRXlsExporter();
+    }
 
-	/**
-	 * @see wicket.contrib.jasperreports.JRResource#getExtension()
-	 */
-	public String getExtension()
-	{
-		return "xls";
-	}
+    @Override
+    public String getContentType() {
+        return "application/excel";
+    }
+
+    @Override
+    public String getExtension() {
+        return "xls";
+    }
 }
