36f6279cc5990ac5704a0793781de8e52fd5a946
CP Tan <cptan@wicketstuff.org>
2006-10-19 01:51:29 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2/wicket-contrib-yui@1017 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/markup/html/yui/sort/sort.js
+++ src/main/java/wicket/contrib/markup/html/yui/sort/sort.js
@@ -1,3 +1,10 @@
 var dd${id}_${javaScriptId} = new YAHOO.example.DDSwap${isIntersect}(${classId} , ${groupId});
 
+function fn_toShift_dd${id}_${javaScriptId}(){
+	alert("dd${id}_${javaScriptId}");
+}
+
+//how to determine the target?
+
 YAHOO.util.Event.addListener("dd${id}_${javaScriptId}", "mouseover", fn_${javaScriptId});
+YAHOO.util.Event.addListener("dd${id}_${javaScriptId}", "mousedown", fn_toShift_dd${id}_${javaScriptId});
