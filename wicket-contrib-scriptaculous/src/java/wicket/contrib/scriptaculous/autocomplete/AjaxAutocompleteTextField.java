fa4efc06e2a58e5ebc204f72af6857fb2227bb4f
Eelco Hillenius <eelco12@wicketstuff.org>
2006-10-23 07:57:20 +0000

    fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1024 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AjaxAutocompleteTextField.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/AjaxAutocompleteTextField.java
@@ -18,6 +18,7 @@
  */
 package wicket.contrib.scriptaculous.autocomplete;
 
+import wicket.Component;
 import wicket.MarkupContainer;
 import wicket.RequestCycle;
 import wicket.contrib.scriptaculous.JavascriptBuilder;
@@ -36,7 +37,7 @@ public abstract class AjaxAutocompleteTextField<T> extends AutocompleteTextField
 	{
 		private static final long serialVersionUID = 1L;
 
-		public void onRequest()
+		public void onRequest(Component component)
 		{
 			StringBufferResourceStream s = new StringBufferResourceStream();
 			FormComponent formComponent = (FormComponent)getComponent();
