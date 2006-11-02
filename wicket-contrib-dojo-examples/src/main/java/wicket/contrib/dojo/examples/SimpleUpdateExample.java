9d55fd6924241973f6120f722a87389d6db87fdd
Vincent Demay <vdemay@wicketstuff.org>
2006-11-02 23:30:58 +0000

    Exemple for DojoUpdateHandler
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1079 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/SimpleUpdateExample.java
@@ -0,0 +1,83 @@
+package wicket.contrib.dojo.examples;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wicket.Component;
+import wicket.MarkupContainer;
+import wicket.behavior.AttributeAppender;
+import wicket.contrib.dojo.autoupdate.DojoAutoUpdateHandler;
+import wicket.contrib.dojo.update.DojoUpdateHandler;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.form.TextField;
+import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.ListView;
+import wicket.model.Model;
+
+public class SimpleUpdateExample extends WebPage {
+
+	Label label;
+	TextField textField;
+	ListView list;
+	
+	String labelString;
+	String textFieldString;
+	List listList;
+	WebMarkupContainer listContainer;
+	
+	public SimpleUpdateExample() {
+		
+		listList = new ArrayList();
+		listList.add("a string in listView");
+		textFieldString = "changeMe";
+		
+		
+		labelString = "Hello ! Try to click me";
+		label = new Label(this, "label", new Model<String>(labelString));
+		label.add(new DojoUpdateHandler(){
+
+			@Override
+			public void updateComponents(List<Component> components, Component submitter, String value) {
+				listList.add("You click on Hello");	
+				components.add(listContainer);
+			}
+			
+		});
+		
+		
+		textField = new TextField(this,"textField", new Model<String>(textFieldString));
+		textField.add(new DojoUpdateHandler(){
+
+			@Override
+			public void updateComponents(List<Component> components, Component submitter, String value) {
+				String toAdd = value;
+				if (!toAdd.equals("")){
+					listList.add(toAdd);
+					components.add(listContainer);
+				}
+			}
+			
+		});
+		
+		
+		
+		listContainer = new WebMarkupContainer(this, "content");
+		
+		list = new ListView(listContainer, "list", listList){
+
+			@Override
+			protected void populateItem(final ListItem item) {
+				final String comment = (String)item.getModelObject();
+				new Label(item, "result", comment);
+			}
+			
+		};
+		listContainer.setOutputMarkupId(true);
+	}
+	
+	
+	
+}
