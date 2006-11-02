2768285acf5bf463cafc22967090308efa75bde9
Vincent Demay <vdemay@wicketstuff.org>
2006-11-02 20:28:11 +0000

    AutoUpdate simple example
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1071 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/SimpleAutoRefreshExample.java
@@ -0,0 +1,39 @@
+package wicket.contrib.dojo.examples;
+
+import wicket.Component;
+import wicket.contrib.dojo.autoupdate.DojoAutoUpdateHandler;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.model.Model;
+
+public class SimpleAutoRefreshExample extends WebPage {
+
+	Label label;
+	String display;
+	int timer;
+	
+	public SimpleAutoRefreshExample() {
+		timer = 0;
+		updateTimer();
+		label = new Label(this, "label", new Model<String>(display));
+		label.setOutputMarkupId(true);
+		label.add(new DojoAutoUpdateHandler(1000){
+
+			@Override
+			protected void update(Component component) {
+				updateTimer();
+				component.setModel(new Model<String>(display));
+				
+			}
+			
+		});
+	}
+	
+	public void updateTimer(){
+		display = "resfreshed " + timer + " time(s)";
+		timer++;
+	}
+	
+	
+
+}
