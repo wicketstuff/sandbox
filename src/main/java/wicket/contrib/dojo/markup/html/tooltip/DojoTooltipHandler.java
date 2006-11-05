d08abc6958e3705ab5883601c4a49e7bd4f915cb
Vincent Demay <vdemay@wicketstuff.org>
2006-11-05 20:08:24 +0000

    Sipmlier Tooltip
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo@1087 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/markup/html/tooltip/DojoTooltipHandler.java
@@ -0,0 +1,42 @@
+package wicket.contrib.dojo.markup.html.tooltip;
+
+import wicket.ajax.AjaxRequestTarget;
+import wicket.contrib.dojo.DojoAjaxHandler;
+import wicket.markup.html.IHeaderResponse;
+
+/**
+ * A Dojo Dialog Box Handler
+ * @author <a href="http://www.demay-fr.net/blog">Vincent Demay</a>
+ *
+ */
+public class DojoTooltipHandler extends DojoAjaxHandler
+{
+	@Override
+	protected void respond(AjaxRequestTarget target)
+	{
+		//DO NOTHING
+	}
+	
+	/* (non-Javadoc)
+	 * @see wicket.contrib.dojo.DojoAjaxHandler#renderHead(wicket.markup.html.IHeaderResponse)
+	 */
+	public void renderHead(IHeaderResponse response)
+	{
+		super.renderHead(response);
+		String require = "";
+		require += "<script language=\"JavaScript\" type=\"text/javascript\">\n";
+		require += "	dojo.require(\"dojo.widget.Tooltip\")\n";
+		require += "\n";
+		require += "</script>\n";
+
+		response.renderString(require);
+	}
+
+	@Override
+	protected void onBind()
+	{
+		super.onBind();
+		((DojoTooltip)getComponent()).getTooltipedComponent().setOutputMarkupId(true);
+	}
+
+}
