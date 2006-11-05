d8f723f2d2cdfa5319e841ba1c31d2369abed4b0
Vincent Demay <vdemay@wicketstuff.org>
2006-11-05 20:10:22 +0000

    new rss Exemple
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1088 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/RssReader.java
@@ -0,0 +1,68 @@
+package wicket.contrib.dojo.examples;
+
+import java.util.List;
+
+import wicket.Component;
+import wicket.ResourceReference;
+import wicket.contrib.dojo.examples.rssreader.model.FeedListMaker;
+import wicket.contrib.dojo.examples.rssreader.model.FeedModel;
+import wicket.contrib.dojo.examples.rssreader.widgets.FeedPanel;
+import wicket.contrib.dojo.markup.html.dialog.DojoDialog;
+import wicket.contrib.dojo.markup.html.dialog.DojoDialogCloser;
+import wicket.contrib.dojo.markup.html.dialog.DojoDialogOpener;
+import wicket.contrib.dojo.markup.html.floatingpane.DojoFloatingPane;
+import wicket.contrib.dojo.markup.html.tooltip.DojoTooltip;
+import wicket.contrib.dojo.toggle.DojoExplodeToggle;
+import wicket.contrib.dojo.toggle.DojoWipeToggle;
+import wicket.contrib.dojo.update.DojoUpdateHandler;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.image.Image;
+import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.ListView;
+import wicket.model.Model;
+
+public class RssReader extends WebPage {
+	
+	
+	public RssReader()
+	{
+		final Label loading = new Label(this, "loading","Loading ...");
+		
+		//a list with all selectable feeds
+		FeedListMaker.createFeedList();
+		ListView<FeedModel> feedList = new ListView(this, "feedList", FeedListMaker.getList()){
+			protected void populateItem(ListItem item) {
+				Label label = new Label(item, "name", ((FeedModel)item.getModelObject()).getName());
+				DojoTooltip tooltip = new DojoTooltip(item,"tooltip",label);
+				tooltip.setToggle(new DojoExplodeToggle(200));
+				new Image(item, "image", new ResourceReference(RssReader.class, "rssreader/" + ((FeedModel)item.getModelObject()).getImage()));
+				final String url = ((FeedModel)item.getModelObject()).getUrl();
+				label.add(new DojoUpdateHandler(loading){
+					public void updateComponents(List<Component> components, Component submitter, String value) {
+						FeedPanel feed = (FeedPanel) ((WebMarkupContainer)getPage().get("window")).get("rss");
+						feed = new FeedPanel(((WebMarkupContainer)getPage().get("window")), "rss", new Model<String>(url));
+						components.add(feed);
+					}
+				});
+			}
+		};
+		
+		WebMarkupContainer window = new WebMarkupContainer(this, "window");
+		FeedPanel feed = new FeedPanel(window, "rss", new Model<String>("http://www.demay-fr.net/blog/rss.php?type=co"));
+		feed.setOutputMarkupId(true);
+		
+		//about
+		DojoDialog about = new DojoDialog(this, "about");
+		about.setToggle(new DojoWipeToggle(200));
+		new DojoDialogOpener(this, "open", about);
+		new DojoDialogCloser(about, "close", about);
+		
+		new Image(this, "logo");
+	}
+	
+	
+}
+
+
