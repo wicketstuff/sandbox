6522c0535b5dcffeda6126899e800307928b11f3
Vincent Demay <vdemay@wicketstuff.org>
2006-11-05 20:08:24 +0000

    Sipmlier Tooltip
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1087 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/tooltip/DojoTooltip.java
@@ -0,0 +1,43 @@
+package wicket.contrib.dojo.markup.html.tooltip;
+
+import wicket.Component;
+import wicket.MarkupContainer;
+import wicket.behavior.AttributeAppender;
+import wicket.contrib.dojo.toggle.DojoToggle;
+import wicket.markup.ComponentTag;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.model.Model;
+
+public class DojoTooltip extends WebMarkupContainer
+{
+	private Component component;
+	
+	public DojoTooltip(MarkupContainer parent, String id, Component component)
+	{
+		super(parent, id);
+		this.component = component;
+		add(new DojoTooltipHandler());
+	}
+	
+	
+	@Override
+	protected void onComponentTag(ComponentTag tag)
+	{
+		tag.put("dojoType", "tooltip");
+		tag.put("connectId", component.getMarkupId());
+	}
+	
+	public Component getTooltipedComponent(){
+		return component;
+	}
+	
+	/**
+	 * Set the dialog effect
+	 * @param toggle
+	 */
+	public void setToggle(DojoToggle toggle){
+		this.add(new AttributeAppender("toggle", new Model<String>(toggle.getToggle()),""));
+		this.add(new AttributeAppender("toggleDuration", new Model<String>(toggle.getDuration() + ""),""));
+	}
+
+}
