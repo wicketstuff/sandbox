bb1c6517f14fc10ee4753e372db6148e171d66e3
Vincent Demay <vdemay@wicketstuff.org>
2006-11-12 20:04:15 +0000

    DnD listView
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1115 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/DojoOrderableListViewContainer.java
@@ -0,0 +1,22 @@
+package wicket.contrib.dojo.html.list;
+
+import wicket.MarkupContainer;
+import wicket.contrib.dojo.dojodnd.DojoDragContainer;
+import wicket.contrib.dojo.dojodnd.DojoDropContainer;
+
+public class DojoOrderableListViewContainer extends DojoDropContainer
+{
+
+	public DojoOrderableListViewContainer(MarkupContainer parent, String id)
+	{
+		super(parent, id);
+	}
+
+	@Override
+	public void onDrop(DojoDragContainer container, int position)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
