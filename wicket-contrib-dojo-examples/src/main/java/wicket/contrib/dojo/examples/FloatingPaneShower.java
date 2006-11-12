6d08bfbd6d621f9cc5dd0e4435f73e3eb8c07b10
Vincent Demay <vdemay@wicketstuff.org>
2006-11-12 17:08:42 +0000

    Adapt samples
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1112 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/FloatingPaneShower.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/FloatingPaneShower.java
@@ -11,8 +11,14 @@ public class FloatingPaneShower extends WebPage {
 		DojoFloatingPane pane2 = new DojoFloatingPane(pane1, "pane2");
 		DojoFloatingPane pane3 = new DojoFloatingPane(this, "pane3");
 		
-		pane2.setTitle("a title here");
+		
 		pane3.setHasShadow(true);
+		pane3.setHeight("300px");
+		pane3.setWidth("300px");
+		
 		pane1.setDisplayCloseAction(false);
+		pane1.setHeight("300px");
+		pane1.setWidth("300px");
+		pane2.setTitle("a title here");
 	}
 }
