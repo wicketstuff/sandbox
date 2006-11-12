d2b244b76156a19afe7ed65f7624d46cf5933d56
Vincent Demay <vdemay@wicketstuff.org>
2006-11-12 20:04:15 +0000

    DnD listView
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1115 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/DojoOrderableListView.java
@@ -0,0 +1,89 @@
+package wicket.contrib.dojo.html.list;
+
+import java.util.List;
+
+import wicket.MarkupContainer;
+import wicket.ajax.AjaxRequestTarget;
+import wicket.behavior.AttributeAppender;
+import wicket.contrib.dojo.AbstractRequireDojoBehavior;
+import wicket.contrib.dojo.dojodnd.DojoDragContainerHandler;
+import wicket.markup.ComponentTag;
+import wicket.markup.html.IHeaderResponse;
+import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.ListView;
+import wicket.model.IModel;
+import wicket.model.Model;
+
+public abstract class DojoOrderableListView extends ListView
+{
+	DojoOrderableListViewContainer container;
+	int pos = 0; 
+	private String dragId;
+
+	public DojoOrderableListView(DojoOrderableListViewContainer parent, String id, IModel model)
+	{
+		super(parent, id, model);
+		container = parent;
+	}
+
+	public DojoOrderableListView(DojoOrderableListViewContainer parent, String id, List list)
+	{
+		super(parent, id, list);
+		container = parent;
+	}
+
+	public DojoOrderableListView(DojoOrderableListViewContainer parent, String id)
+	{
+		super(parent, id);
+		container = parent;
+	}
+	
+	private String generateId(){
+		return container.getMarkupId() + (pos++);
+		
+	}
+
+	@Override
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+	}
+
+	@Override
+	protected void renderItem(ListItem item)
+	{
+		String id = generateId();
+		item.add(new AttributeAppender("id", true, new Model<String>(id),""));
+		dragId = "*";
+		super.renderItem(item);
+	}
+
+	@Override
+	protected void populateItem(ListItem item)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response)
+	{
+		super.renderHead(response);
+		for (int i =0; i < getList().size(); i++){
+			response.renderString(generateDragDefinition(container.getMarkupId() + i));
+		}
+	}
+	
+	private String generateDragDefinition(String id){
+		String toReturn = "";
+		toReturn += "<script language=\"JavaScript\" type=\"text/javascript\">\n";
+		toReturn += "function initDrag" + id + "(){\n";
+		toReturn += "	var dl = byId(\"" + id + "\");\n";
+		toReturn += "	var drag = new dojo.dnd.HtmlDragSource(dl, \"" + id + "\");\n";
+		toReturn += "}\n";
+		toReturn += "dojo.event.connect(dojo, \"loaded\", \"initDrag" + id + "\");\n";
+		toReturn += "</script>\n";
+		return toReturn;
+	}
+
+}
