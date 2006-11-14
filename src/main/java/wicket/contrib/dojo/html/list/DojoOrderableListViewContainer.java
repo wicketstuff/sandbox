88a4c6314106b1c8275d7e829d8788832cd3259f
Vincent Demay <vdemay@wicketstuff.org>
2006-11-14 18:26:19 +0000

    DragAndDrop Orderable listView
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo@1117 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/html/list/DojoOrderableListViewContainer.java
+++ src/main/java/wicket/contrib/dojo/html/list/DojoOrderableListViewContainer.java
@@ -1,9 +1,20 @@
 package wicket.contrib.dojo.html.list;
 
+import java.util.Collections;
+import java.util.List;
+
 import wicket.MarkupContainer;
+import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.dojodnd.DojoDragContainer;
 import wicket.contrib.dojo.dojodnd.DojoDropContainer;
+import wicket.markup.html.list.ListItem;
 
+
+/**
+ * 
+ * @author vdemay
+ * TODO refactor me 
+ */
 public class DojoOrderableListViewContainer extends DojoDropContainer
 {
 
@@ -12,11 +23,48 @@ public class DojoOrderableListViewContainer extends DojoDropContainer
 		super(parent, id);
 	}
 
-	@Override
-	public void onDrop(DojoDragContainer container, int position)
+
+	public void onDrop(DojoOrderableListView container,  int oldPosition, int newPosition, AjaxRequestTarget target)
 	{
-		// TODO Auto-generated method stub
+		if (oldPosition != newPosition){
+			List list = container.getList();
+			Object drag = list.remove(oldPosition);
+			list.add(newPosition, drag);
+			container.setList(list);
+			target.appendJavascript(getChangeIDScript());
+		}
+		onDrop(null, newPosition);
+	}
+	
+	private String getChangeIDScript(){
+		String containerId = getMarkupId();
+		String changeId = "";
+		changeId += "var children = document.getElementById('" + containerId + "').getElementsByTagName('div');\n";
+		changeId += "for(var i=0; i < children.length; i++){\n";
+		changeId += "	children[i].id = '" + containerId + "_list_'+i\n";
+		changeId += "}";
+		return changeId;
 		
 	}
+	
+	/**
+	 * Called after the model is updated. Use this method to e.g. update the
+	 * persistent model. Does nothing by default.
+	 */
+	protected void onAjaxModelUpdated(AjaxRequestTarget target)
+	{
+		String dragSource = getRequest().getParameter("dragSource");
+		int newPosition = Integer.parseInt(getRequest().getParameter("position"));
+		MarkupContainer container = getPage(); 
+		String[] ids = dragSource.split("_");
+		for (int i=0; i < ids.length - 1; i++){
+			container = (MarkupContainer)container.get(ids[i]);
+		}
+		int oldPosition = Integer.parseInt(ids[ids.length - 1]);
+		onDrop((DojoOrderableListView) container, oldPosition, newPosition, target);  
+	}
+
+	@Override
+	public void onDrop(DojoDragContainer container, int newPosition){}
 
 }
