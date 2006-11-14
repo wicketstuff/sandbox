8f8d12cb060bc1d646e446695ba51d4e419ef28c
Vincent Demay <vdemay@wicketstuff.org>
2006-11-14 18:26:58 +0000

    Sample for orderable List View
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo-examples@1118 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/examples/OrderableList.java
+++ src/main/java/wicket/contrib/dojo/examples/OrderableList.java
@@ -11,15 +11,25 @@ import wicket.markup.html.list.ListItem;
 
 public class OrderableList extends WebPage {
 
-	static List<String> objList;
+	static final List<String> objList  = new  ArrayList<String>();
+
 	
 	public OrderableList() {
 		super();
-		objList = new  ArrayList<String>();
-		objList.add("foo1");
-		objList.add("bar1");
-		objList.add("foo2");
-		objList.add("bar2");
+		if (objList.size() == 0){
+			objList.add("foo1");
+			objList.add("bar1");
+			objList.add("foo2");
+			objList.add("bar2");
+			objList.add("foo3");
+			objList.add("bar3");
+			objList.add("foo4");
+			objList.add("bar4");
+			objList.add("foo5");
+			objList.add("bar5");
+			objList.add("foo6");
+			objList.add("bar6");
+		}
 		DojoOrderableListViewContainer container = new DojoOrderableListViewContainer(this, "container");
 		DojoOrderableListView list = new DojoOrderableListView(container, "list", objList){
 
