fcbf5a5ad8a68995be57edad6baeef6344336169
Vincent Demay <vdemay@wicketstuff.org>
2006-11-16 17:23:27 +0000

    Base Wicket-dojo-contrib on wicket-existant ajax implementation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1123 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/AbstractDojoTimerBehavior.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/AbstractDojoTimerBehavior.java
@@ -17,10 +17,9 @@ import wicket.util.time.Duration;
  *
  * TODO : show getJsTimeoutCall in {@link AbstractAjaxTimerBehavior} to implement it
  */
-public abstract class AbstractDojoTimerBehavior extends AbstractDefaultDojoBehavior
+public abstract class AbstractDojoTimerBehavior extends AbstractAjaxTimerBehavior
 {
-	private final Duration interval;
-	private String LoadingId = "";
+	private String loadingId = "";
 	
 	/**
 	 * 
@@ -28,7 +27,7 @@ public abstract class AbstractDojoTimerBehavior extends AbstractDefaultDojoBehav
 	 */
 	public AbstractDojoTimerBehavior(final Duration updateInterval)
 	{
-		this.interval = updateInterval;
+		super(updateInterval);
 	}
 	
 	/**
@@ -37,76 +36,9 @@ public abstract class AbstractDojoTimerBehavior extends AbstractDefaultDojoBehav
 	 */
 	public AbstractDojoTimerBehavior(final Duration updateInterval, String loadingId)
 	{
-		this.interval = updateInterval;
-		this.LoadingId = loadingId;
-	}
-	
-	/**
-	 * Subclasses should call super.onBind()
-	 * 
-	 * @see wicket.ajax.AbstractDefaultAjaxBehavior#onBind()
-	 */
-	@Override
-	protected void onBind()
-	{
-		super.onBind();
-	}
-	
-
-	/**
-	 * @see wicket.contrib.dojo.AbstractDefaultDojoBehavior#renderHead(wicket.markup.html.IHeaderResponse)
-	 */
-	@Override
-	public void renderHead(IHeaderResponse response)
-	{
-		super.renderHead(response);
-		String require = "";
-		require += "<script language=\"JavaScript\" type=\"text/javascript\">\n";
-		require += "\n";
-		require += "function initAutoRefreshFor" + getComponent().getId() + "(){\n";
-		require += "	" + getCallbackScript() + "\n";
-		require += "}\n";
-		require += "dojo.addOnLoad(initAutoRefreshFor" + getComponent().getId() + ");\n";
-		require += "</script>\n";
+		super(updateInterval);
+		loadingId = loadingId;
 		
-		response.renderString(require);
 	}
 	
-	protected CharSequence getCallbackScript(boolean recordPageVersion)
-	{
-		return getCallbackScript("dojoAutoUpdate(" + interval.getMilliseconds() + ",'" + getCallbackUrl(recordPageVersion) + "'", null, null, null);
-	}
-	
-	
-	/**
-	 * the javascript function expects the node corresponding to loadingId to have 
-	 * a style attribute: visibility=hidden; and sets it to visible during laoding.
-	 * This is not very good method for doing this and should probably be replaced by
-	 *  a a generic js function which is called during loading.
-	 * @return the CSS id for the loading node.
-	 */
-	private String findIndicatorId()
-	{
-		return this.LoadingId;
-	}
-
-	/**
-	 * 
-	 * @param target 
-	 * @see wicket.ajax.AbstractDefaultAjaxBehavior#respond(wicket.ajax.AjaxRequestTarget)
-	 */
-	@Override
-	protected final void respond(final AjaxRequestTarget target)
-	{
-		onTimer(target);
-	}
-	
-	/**
-	 * Listener method for the AJAX timer event.
-	 * 
-	 * @param target
-	 *            The request target
-	 */
-	protected abstract void onTimer(final AjaxRequestTarget target);
-    
 }
