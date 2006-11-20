49011bf75c726b08afafead40f3dba443c9fda50
Eelco Hillenius <eelco12@wicketstuff.org>
2006-11-20 01:01:15 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1148 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/dragdrop/DraggableImage.java
@@ -0,0 +1,30 @@
+package wicket.contrib.scriptaculous.dragdrop;
+
+import wicket.contrib.scriptaculous.ScriptaculousAjaxHandler;
+import wicket.markup.ComponentTag;
+import wicket.markup.MarkupStream;
+import wicket.markup.html.image.Image;
+
+public class DraggableImage extends Image {
+
+    private final String id;
+
+    public DraggableImage(String wicketId, String id, String img) {
+        super(wicketId, img);
+        this.id = id;
+
+		add(ScriptaculousAjaxHandler.newJavascriptBindingHandler());
+    }
+
+    protected void onRender(MarkupStream markupStream) {
+        super.onRender(markupStream);
+
+        getResponse().write("\n<script type=\"text/javascript\">new Draggable('" + id + "', {revert:true})</script>");
+    }
+
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        tag.put("id", id);
+        tag.put("class", getId());
+    }
+}
