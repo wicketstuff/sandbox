390230602488e9d0eb6ad1a9d835a9089cb17ce8
Eelco Hillenius <eelco12@wicketstuff.org>
2006-11-20 01:01:54 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1149 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/autocomplete/AjaxAutocompleteExamplePage.java
@@ -0,0 +1,50 @@
+package wicket.contrib.scriptaculous.examples.autocomplete;
+
+import wicket.contrib.scriptaculous.autocomplete.AjaxAutocompleteTextField;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.form.Button;
+import wicket.markup.html.form.Form;
+import wicket.model.CompoundPropertyModel;
+
+public class AjaxAutocompleteExamplePage extends WebPage {
+
+	public AjaxAutocompleteExamplePage() {
+		super();
+		add(new AutocompleteExampleForm("searchForm"));
+	}
+
+	private class EmailSearchCommand {
+		private String emailAddress;
+
+		public String getEmailAddress()
+		{
+			return emailAddress;
+		}
+
+		public void setEmailAddress(String emailAddress)
+		{
+			this.emailAddress = emailAddress;
+		}
+
+	}
+
+	private class AutocompleteExampleForm extends Form {
+		public AutocompleteExampleForm(String id) {
+			super(id, new CompoundPropertyModel(new EmailSearchCommand()));
+
+			add(new AjaxAutocompleteTextField("emailAddress") {
+				public String[] getResults(String input) {
+					return new String[] {
+							"bill.gates@microsoft.com"
+							, "me@yourdomain.com"
+					};
+				}
+			});
+			add(new Button("submitButton"));
+		}
+
+		protected void onSubmit() {
+			//do something here
+		}
+	}
+}
