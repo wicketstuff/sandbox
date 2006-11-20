0ac8be08abc7be0a5cad7918a88ac8846b88347d
Eelco Hillenius <eelco12@wicketstuff.org>
2006-11-20 02:00:02 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1158 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-data-hibernate-3.0/src/java/wicket/contrib/data/model/hibernate/HibernateDaoSupport.java
@@ -0,0 +1,26 @@
+package wicket.contrib.data.model.hibernate;
+
+import org.hibernate.Session;
+
+/**
+ * A support class if you don't want to worry about handling execute callbacks
+ * yourself and would prefer to merely supply the session.
+ * 
+ * @author Phil Kulak
+ */
+public abstract class HibernateDaoSupport implements IHibernateDao
+{
+	/**
+	 * Returns a session that will be passed to any hibernate callbacks.
+	 * @return a hibernate session
+	 */
+	protected abstract Session getSession();
+	
+	/**
+	 * @see wicket.contrib.data.model.hibernate.IHibernateDao#execute(IHibernateCallback)
+	 */
+	public Object execute(IHibernateCallback callback)
+	{
+		return callback.execute(getSession());
+	}
+}
