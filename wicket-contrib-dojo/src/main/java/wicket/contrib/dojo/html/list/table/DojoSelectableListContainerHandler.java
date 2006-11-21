85afedfa4b021dce765effa33425bd2371ff9a62
Vincent Demay <vdemay@wicketstuff.org>
2006-11-21 15:31:40 +0000

    Add Selectable table
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1163 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/table/DojoSelectableListContainerHandler.java
@@ -0,0 +1,119 @@
+package wicket.contrib.dojo.html.list.table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import wicket.ResourceReference;
+import wicket.ajax.AjaxRequestTarget;
+import wicket.contrib.dojo.AbstractRequireDojoBehavior;
+import wicket.markup.html.IHeaderResponse;
+
+/**
+ * @author Vincent Demay
+ *
+ */
+public class DojoSelectableListContainerHandler extends AbstractRequireDojoBehavior
+{
+	
+	//child of this container
+	private DojoSelectableList listView;
+
+	public DojoSelectableListContainerHandler(DojoSelectableList selectableList)
+	{
+		super();
+		listView = selectableList;
+	}
+
+	@Override
+	public void setRequire(RequireDojoLibs libs)
+	{
+		//DO Nothing, the Widget is in the package
+	}
+
+	@Override
+	protected final void respond(AjaxRequestTarget target)
+	{
+		ArrayList selected = new ArrayList();
+		String indexList = getComponent().getRequest().getParameter("indexList");
+		if (indexList == null){
+			((DojoSelectableListContainer)getComponent()).onChoose(target);
+		}else{
+			StringTokenizer tokenizer = new StringTokenizer(indexList, ",");
+			List all = listView.getList();
+			
+			while (tokenizer.hasMoreTokens()){
+				int pos = Integer.parseInt(tokenizer.nextToken());
+				selected.add(all.get(pos));
+			}
+			
+			((DojoSelectableListContainer)getComponent()).setSelected(selected);
+			
+			((DojoSelectableListContainer)getComponent()).onSelection(target, selected);
+		}
+	}
+
+	/**
+	 * TODO find an other way to Render an as big javascript
+	 */
+	@Override
+	public void renderHead(IHeaderResponse response)
+	{
+		super.renderHead(response);
+		response.renderCSSReference(new ResourceReference(DojoSelectableListContainer.class, "DojoSelectableListContainer.css"));
+		response.renderJavascriptReference(new ResourceReference(DojoSelectableListContainer.class, "SelectableTable.js"));
+		
+		String toReturn="";
+		toReturn += "<script language=\"JavaScript\" type=\"text/javascript\">\n";
+		toReturn += "function getSelection(){\n";
+		toReturn += "	var container = dojo.widget.byId('" + getComponent().getMarkupId() + "');\n";
+		toReturn += "	var body = container.domNode.getElementsByTagName('tbody')[0];\n";
+		toReturn += "	var rows=body.getElementsByTagName('tr')\n";
+		toReturn += "	var selection = '';\n";
+		toReturn += "	var index = 0;\n";
+		toReturn += "	for(var i=0; i<rows.length; i++){\n";
+		toReturn += "		if(rows[i].parentNode==body){\n";
+		toReturn += "			if(dojo.html.getAttribute(rows[i],'selected')=='true'){\n";
+		toReturn += "				selection += index + ',';\n";
+		toReturn += "			}\n";
+		toReturn += "			index++;\n";
+		toReturn += "		}\n";
+		toReturn += "	}\n";
+		toReturn += "	return selection;\n";
+		toReturn += "};\n";
+		toReturn += "function initSelectable" + getComponent().getMarkupId() + "(){\n";
+		toReturn += "	dojo.event.connect(dojo.widget.byId('" + getComponent().getMarkupId() + "'), \"onSelect\", function(){" + getCallbackScript() + "});\n";
+		toReturn += "	dojo.event.connect(dojo.widget.byId('" + getComponent().getMarkupId() + "'), \"onChoose\", function(){" + getDoubleClickCallbackScripts() + "});\n";
+		toReturn += "}\n";
+		toReturn += "dojo.event.connect(dojo, \"loaded\", \"initSelectable" + getComponent().getMarkupId() + "\");\n";
+		toReturn += "</script>\n";
+		
+		response.renderString(toReturn);
+	}
+	
+	/**
+	 * @return javascript that will generate an ajax GET request to this
+	 *         behavior *
+	 * @param recordPageVersion
+	 *            if true the url will be encoded to execute on the current page
+	 *            version, otherwise url will be encoded to execute on the
+	 *            latest page version
+	 */
+	protected final CharSequence getCallbackScript(boolean recordPageVersion)
+	{
+		return getCallbackScript("wicketAjaxGet('" + super.getCallbackUrl(recordPageVersion) + "&indexList=' + getSelection()", null,
+				null);
+	}
+	
+	/**
+	 * return javascript that will be used to respond to Double click
+	 * @return javascript that will be used to respond to Double click
+	 */
+	protected final CharSequence getDoubleClickCallbackScripts(){
+		return getCallbackScript("wicketAjaxGet('" + super.getCallbackUrl(true) + "'", null,
+				null);
+	}
+
+	
+
+}
