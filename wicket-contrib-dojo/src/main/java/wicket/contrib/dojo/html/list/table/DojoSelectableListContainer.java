1b314f1b0a4391ae3b943b1e8dc93e0ea448f858
Vincent Demay <vdemay@wicketstuff.org>
2006-11-24 09:31:28 +0000

    Allow non ajax request on Choose event Add overwrite css class
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1188 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/table/DojoSelectableListContainer.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/table/DojoSelectableListContainer.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import wicket.MarkupContainer;
+import wicket.ResourceReference;
 import wicket.WicketRuntimeException;
 import static wicket.contrib.dojo.DojoIdConstants.*;
 import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.widgets.StylingWebMarkupContainer;
 import wicket.markup.ComponentTag;
+import wicket.model.IModel;
 
 /**
  * Selectable List container
@@ -33,15 +35,37 @@ public class DojoSelectableListContainer extends StylingWebMarkupContainer
 	private String tbodyClass;
 	
 	/**
+	 * flag to know if on choose meke a ajax request or not
+	 */
+	private boolean ajaxModeOnChoose;
+	
+	/**
+	 * Allow user to set another css to overwrite the default one
+	 */
+	private ResourceReference overwriteCss;
+	
+	
+	/**
 	 * Construct the selectable list containre
 	 * @param parent prent where DojoSelectableListContainer will be added
 	 * @param id container id
 	 */
 	public DojoSelectableListContainer(MarkupContainer parent, String id)
 	{
-		super(parent, id);
+		this(parent, id, null);
+	}
+	
+	/**
+	 * Construct the selectable list container
+	 * @param id container id
+	 * @param model model
+	 */
+	public DojoSelectableListContainer(MarkupContainer parent, String id, IModel model)
+	{
+		super(parent, id, model);
 		enableMultipleSelect = "true";
 		tbodyClass = "scrollContent";
+		ajaxModeOnChoose = true;
 	}
 
 	@Override
@@ -129,6 +153,42 @@ public class DojoSelectableListContainer extends StylingWebMarkupContainer
 	}
 	
 	/**
+	 * return boolean to know if ajax is enable on the choose(dblclick)
+	 * @return true if ajax is active on choose
+	 */
+	public boolean isAjaxModeOnChoose()
+	{
+		return ajaxModeOnChoose;
+	}
+	
+	/**
+	 * return the used css to overwrite the default one
+	 * @return the used css to overwrite the default one or null if none ios defined
+	 */
+	public ResourceReference getOverwriteCss()
+	{
+		return overwriteCss;
+	}
+
+	/**
+	 * set a css reference to overwrite the default one
+	 * @param overwriteCss  a css reference to overwrite the default one
+	 */
+	public void setOverwriteCss(ResourceReference overwriteCss)
+	{
+		this.overwriteCss = overwriteCss;
+	}
+
+	/**
+	 * set boolean to know if ajax is enable on the choose(dblclick)
+	 * @param ajaxModeOnChoose true if ajax is enable on the choose(dblclick)
+	 */
+	public void setAjaxModeOnChoose(boolean ajaxModeOnChoose)
+	{
+		this.ajaxModeOnChoose = ajaxModeOnChoose;
+	}
+	
+	/**
 	 * Triggered when selection change
 	 * @param target ajax target
 	 * @param selected List of selected item
@@ -145,6 +205,15 @@ public class DojoSelectableListContainer extends StylingWebMarkupContainer
 	public void onChoose(AjaxRequestTarget target, Object indexList)
 	{
 		
+	}
+	
+	/**
+	 * Triggered when double click on an item and ajaxOnChoose is disabled
+	 * <b>by default ajax is enabled</b>
+	 */
+	public void onLinkClicked()
+	{
+		System.out.println("onClick");
 	}
 
 
