b6cf4858b8a49c6e1857d022d42cfd50c9c30760
Vincent Demay <vdemay@wicketstuff.org>
2006-11-24 17:18:40 +0000

    Samples for selectable table
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo-examples@1195 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/examples/SelectableTable.java
+++ src/main/java/wicket/contrib/dojo/examples/SelectableTable.java
@@ -4,47 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 import wicket.ajax.AjaxRequestTarget;
-import wicket.contrib.dojo.html.list.table.DojoSelectableList;
 import wicket.contrib.dojo.html.list.table.DojoSelectableListContainer;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.ListView;
 
 public class SelectableTable extends WebPage {
 
 	static final List objList  = new  ArrayList();
+	static final List objList2  = new  ArrayList();
 
 	
 	public SelectableTable() {
 		super();
 		if (objList.size() == 0){
 			objList.add("foo1");
-			objList.add("bar1");
-			objList.add("foo2");
-			objList.add("bar2");
-			objList.add("foo3");
-			objList.add("bar3");
-			objList.add("foo4");
-			objList.add("bar4");
-			objList.add("foo5");
-			objList.add("bar5");
-			objList.add("foo6");
-			objList.add("bar6");
+			objList.add("foo1");
 		}
-		DojoSelectableList list = null;
+		if (objList2.size() == 0){
+			objList2.add("toto");
+			objList2.add("tata");
+		}
+		
+		ListView list = null;
 		DojoSelectableListContainer container = new DojoSelectableListContainer("container"){
 
-			public void onChoose(AjaxRequestTarget target) {
-				System.out.println("Choose");
+			public void onChoose(AjaxRequestTarget target, Object Selected) {
+				System.out.println("selected");
+				target.appendJavascript("alert('dblClick')");
+				//setResponsePage(DojoFXTestPage.class);
 			}
 
 			public void onSelection(AjaxRequestTarget target, List selected) {
-				System.out.println("Selection");
+				System.out.println("selected");
 			}
 			
 		};
+		container.setAjaxModeOnChoose(false);
 		this.add(container);
-		list = new DojoSelectableList("list", objList, container){
+		list = new ListView("list", objList){
 
 			protected void populateItem(final ListItem item) {
 				item.add(new Label("label",(String)item.getModelObject()));
@@ -52,6 +51,36 @@ public class SelectableTable extends WebPage {
 			
 		};
 		container.add(list);
+		
+		
+		
+		ListView list2 = null;
+		DojoSelectableListContainer container2 = new DojoSelectableListContainer("container2"){
+
+			public void onChoose(AjaxRequestTarget target, Object Selected) {
+				System.out.println("2Choose");
+				target.appendJavascript("alert('dblClick')");
+				setResponsePage(DojoFXTestPage.class);
+			}
+			@Override
+			public void onNonAjaxChoose(Object selected) {
+				System.out.println(selected);
+			}
+			public void onSelection(AjaxRequestTarget target, List selected) {
+				System.out.println(selected);
+			}
+
+		};
+		container2.setAjaxModeOnChoose(false);
+		this.add(container2);
+		list2 = new ListView("list2", objList2){
+
+			protected void populateItem(final ListItem item) {
+				item.add(new Label("label",(String)item.getModelObject()));
+			}
+			
+		};
+		container2.add(list2);
 	}
 
 }
