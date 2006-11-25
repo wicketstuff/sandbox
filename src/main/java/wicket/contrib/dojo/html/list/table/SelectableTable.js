ce714e0a5a012a4fc0763c59c62cad0cf5042b2a
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2006-11-25 00:10:15 +0000

    * Fix callback in Ajax mode (NPE with listView not initialized) * Externalize getSelection()
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1196 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/html/list/table/SelectableTable.js
+++ src/main/java/wicket/contrib/dojo/html/list/table/SelectableTable.js
@@ -558,3 +558,20 @@ dojo.widget.defineWidget(
 		}
 	}
 );
+
+function getSelection(id) {
+	var container = document.getElementById(id);
+	var body = container.getElementsByTagName('tbody')[0];
+	var rows=body.getElementsByTagName('tr')
+	var selection = '';
+	var index = 0;
+	for(var i=0; i<rows.length; i++) {
+		if(rows[i].parentNode==body) {
+			if(dojo.html.getAttribute(rows[i],'selected')=='true') {
+				selection += '&select=' + index;
+			}
+			index++;
+		}
+	}
+	return selection;
+}
