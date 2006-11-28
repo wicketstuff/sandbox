c842121cd0ab69b8faaa757c4a9249711a98f1a0
Vincent Demay <vdemay@wicketstuff.org>
2006-11-28 17:36:20 +0000

    WorkAround bug on resource with *.html name
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1232 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/dojo-0.4/src/widget/AccordionContainer.js
+++ src/main/java/wicket/contrib/dojo/dojo-0.4/src/widget/AccordionContainer.js
@@ -210,7 +210,7 @@ dojo.widget.defineWidget(
 	//	if true, this is the open pane
 	selected: false,
 
-	templatePath: dojo.uri.dojoUri("src/widget/templates/AccordionPane.html"),
+	templatePath: dojo.uri.dojoUri("src/widget/templates/AccordionPane.htm"),
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/AccordionPane.css"),
 
 	isContainer: true,
