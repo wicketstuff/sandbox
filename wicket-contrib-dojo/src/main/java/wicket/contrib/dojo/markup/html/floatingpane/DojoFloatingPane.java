fe3c46b4c8f67f89abdbef3eb3113f1b87940f39
Vincent Demay <vdemay@wicketstuff.org>
2006-11-28 16:22:52 +0000

    Floating pane, modal or not
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1225 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoFloatingPane.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoFloatingPane.java
@@ -10,15 +10,9 @@ import wicket.markup.ComponentTag;
  * @author vdemay
  *
  */
-public class DojoFloatingPane extends StylingWebMarkupContainer
+public class DojoFloatingPane extends DojoAbstractFloatingPane
 {
 
-	private String title;
-	private boolean rezisable;
-	private boolean displayMinimizeAction;
-	private boolean displayMaximizeAction;
-	private boolean displayCloseAction;
-	private boolean hasShadow;
 	private boolean constrainToContainer;
 	
 	/**
@@ -29,12 +23,6 @@ public class DojoFloatingPane extends StylingWebMarkupContainer
 	{
 		super(parent, id);
 		add(new DojoFloatingPaneHandler());
-		title = "";
-		rezisable = true;
-		displayCloseAction = true;
-		displayMaximizeAction = true;
-		displayCloseAction = true;
-		hasShadow = false;
 		constrainToContainer = true;
 	}
 	
@@ -44,122 +32,5 @@ public class DojoFloatingPane extends StylingWebMarkupContainer
 	{
 		super.onComponentTag(tag);
 		tag.put(DOJO_TYPE, DOJO_TYPE_FLOATINGPANE);
-		
-		tag.put("title", title);
-		
-		if (rezisable){
-			tag.put("rezisable", "true");
-		}
-		else{
-			tag.put("rezisable", "false");
-		}
-		
-		if (displayMinimizeAction){
-			tag.put("displayMinimizeAction", "true");
-		}
-		else{
-			tag.put("displayMinimizeAction", "false");
-		}
-		
-		if (displayMaximizeAction){
-			tag.put("displayMaximizeAction", "true");
-		}
-		else{
-			tag.put("displayMaximizeAction", "false");
-		}
-		
-		if (displayCloseAction){
-			tag.put("displayCloseAction", "true");
-		}
-		else{
-			tag.put("displayCloseAction", "false");
-		}
-		
-		if (hasShadow){
-			tag.put("hasShadow", "true");
-		}
-		else{
-			tag.put("hasShadow", "false");
-		}
-		
-		if (constrainToContainer){
-			tag.put("constrainToContainer", "true");
-		}
-		else{
-			tag.put("constrainToContainer", "false");
-		}
 	}
-
-
-	public boolean isDisplayCloseAction()
-	{
-		return displayCloseAction;
-	}
-
-
-	public void setDisplayCloseAction(boolean displayCloseAction)
-	{
-		this.displayCloseAction = displayCloseAction;
-	}
-
-
-	public boolean isDisplayMaximizeAction()
-	{
-		return displayMaximizeAction;
-	}
-
-
-	public void setDisplayMaximizeAction(boolean displayMaximizeAction)
-	{
-		this.displayMaximizeAction = displayMaximizeAction;
-	}
-
-
-	public boolean isDisplayMinimizeAction()
-	{
-		return displayMinimizeAction;
-	}
-
-
-	public void setDisplayMinimizeAction(boolean displayMinimizeAction)
-	{
-		this.displayMinimizeAction = displayMinimizeAction;
-	}
-
-
-	public boolean isHasShadow()
-	{
-		return hasShadow;
-	}
-
-
-	public void setHasShadow(boolean hasShadow)
-	{
-		this.hasShadow = hasShadow;
-	}
-
-
-	public boolean isRezisable()
-	{
-		return rezisable;
-	}
-
-
-	public void setRezisable(boolean rezisable)
-	{
-		this.rezisable = rezisable;
-	}
-
-
-	public String getTitle()
-	{
-		return title;
-	}
-
-
-	public void setTitle(String title)
-	{
-		this.title = title;
-	}
-
 }
