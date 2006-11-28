5407d4c74930f80656cd00b1577520b995b34693
Vincent Demay <vdemay@wicketstuff.org>
2006-11-28 17:16:44 +0000

    Add ids to constants
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1230 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/DojoIdConstants.java
+++ src/main/java/wicket/contrib/dojo/DojoIdConstants.java
@@ -8,9 +8,16 @@ package wicket.contrib.dojo;
  */
 public interface DojoIdConstants
 {
-	public static String DOJO_TYPE                 = "dojoType";
+	public static String DOJO_TYPE                       = "dojoType";
 	
-	public static String DOJO_TYPE_DIALOG          = "dialog";
-	public static String DOJO_TYPE_TOOLTIP         = "tooltip";
-	public static String DOJO_TYPE_FLOATINGPANE    = "FloatingPane";
+	public static String DOJO_TYPE_DIALOG                = "dialog";
+	public static String DOJO_TYPE_TOOLTIP               = "tooltip";
+	public static String DOJO_TYPE_FLOATINGPANE          = "FloatingPane";
+	public static String DOJO_TYPE_MODALFLOATINGPANE     = "ModalFloatingPane";
+	public static String DOJO_TYPE_WEEKCALENDAR          = "WeekCalendar";
+	
+	public static String DOJO_TYPE_TABCONTAINER          = "TabContainer";
+	public static String DOJO_TYPE_ACCORDIONCONTAINER    = "AccordionContainer";
+	public static String DOJO_TYPE_SPLITCONTAINER        = "SplitContainer";
+	public static String DOJO_TYPE_CONTENTPANE           = "ContentPane";
 }
