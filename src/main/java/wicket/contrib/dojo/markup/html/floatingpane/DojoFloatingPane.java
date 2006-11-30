50c6b231b3831944026c347bc983219e2cf860a5
Vincent Demay <vdemay@wicketstuff.org>
2006-11-30 17:48:47 +0000

    backport dialog and floating pane from trunck
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1244 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoFloatingPane.java
@@ -0,0 +1,33 @@
+package wicket.contrib.dojo.markup.html.floatingpane;
+
+import wicket.MarkupContainer;
+import wicket.contrib.dojo.DojoIdConstants;
+import wicket.markup.ComponentTag;
+
+/**
+ * @author vdemay
+ *
+ */
+public class DojoFloatingPane extends DojoAbstractFloatingPane
+{
+
+	private boolean constrainToContainer;
+	
+	/**
+	 * @param parent
+	 * @param id
+	 */
+	public DojoFloatingPane(MarkupContainer parent, String id)
+	{
+		super(id);
+		add(new DojoFloatingPaneHandler());
+		constrainToContainer = true;
+	}
+	
+	
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+		tag.put(DojoIdConstants.DOJO_TYPE, DojoIdConstants.DOJO_TYPE_FLOATINGPANE);
+	}
+}
---
+++ src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoFloatingPaneHandler.java
@@ -0,0 +1,38 @@
+package wicket.contrib.dojo.markup.html.floatingpane;
+
+import wicket.ajax.AjaxRequestTarget;
+import wicket.contrib.dojo.AbstractDefaultDojoBehavior;
+import wicket.markup.html.IHeaderResponse;
+
+/**
+ * A Dojo Dialog Box Handler
+ * @author <a href="http://www.demay-fr.net/blog">Vincent Demay</a>
+ *
+ */
+public class DojoFloatingPaneHandler extends AbstractDefaultDojoBehavior
+{
+	protected void respond(AjaxRequestTarget target)
+	{
+		//DO NOTHING
+	}
+	
+	/* (non-Javadoc)
+	 * @see wicket.contrib.dojo.DojoAjaxHandler#renderHead(wicket.markup.html.IHeaderResponse)
+	 */
+	public void renderHead(IHeaderResponse response)
+	{
+		super.renderHead(response);
+		String require = "";
+		require += "<script language=\"JavaScript\" type=\"text/javascript\">\n";
+		require += "	dojo.require(\"dojo.widget.FloatingPane\")\n";
+		require += "\n";
+		require += "function getFloatingPane(id){\n";
+		require += "	var fp = dojo.widget.byId(id);\n";
+		require += "	return fp;\n";
+		require += "}\n";
+		require += "</script>\n";
+
+		response.renderString(require);
+	}
+
+}
