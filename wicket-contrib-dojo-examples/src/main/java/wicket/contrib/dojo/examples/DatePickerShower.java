d4d25cdca215e4e586a4c5ab84f02d90177eb7f6
Vincent Demay <vdemay@wicketstuff.org>
2006-11-30 09:46:51 +0000

    Make samples more attractives
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1239 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DatePickerShower.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DatePickerShower.java
@@ -2,19 +2,13 @@ package wicket.contrib.dojo.examples;
 
 import java.util.Date;
 
-import wicket.MarkupContainer;
 import wicket.PageParameters;
-import wicket.Response;
-import wicket.contrib.dojo.AbstractDojoTimerBehavior;
 import wicket.contrib.dojo.toggle.DojoExplodeToggle;
 import wicket.contrib.dojo.toggle.DojoFadeToggle;
 import wicket.contrib.dojo.toggle.DojoWipeToggle;
 import wicket.contrib.markup.html.form.DatePicker;
-import wicket.contrib.markup.html.form.ImmediateCheckBox;
-import wicket.contrib.markup.html.form.ImmediateTextField;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.form.Form;
-import wicket.model.IModel;
 import wicket.model.Model;
 
 public class DatePickerShower extends WebPage {
