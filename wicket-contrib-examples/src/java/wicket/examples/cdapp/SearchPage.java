09f3804fad67d25ccc05c562c9d4ea0919cd6d38
Eelco Hillenius <eelco12@wicketstuff.org>
2006-11-30 06:54:56 +0000

    formatting and some fixes for warnings
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1235 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-examples/src/java/wicket/examples/cdapp/SearchPage.java
+++ wicket-contrib-examples/src/java/wicket/examples/cdapp/SearchPage.java
@@ -25,9 +25,9 @@ import org.hibernate.HibernateException;
 import wicket.MarkupContainer;
 import wicket.PageParameters;
 import wicket.RequestCycle;
-import wicket.behavior.SimpleAttributeModifier;
 import wicket.examples.cdapp.model.CD;
 import wicket.examples.cdapp.model.CdDao;
+import wicket.extensions.behavior.SimpleAttributeModifier;
 import wicket.markup.html.WebMarkupContainer;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.form.Form;
@@ -51,12 +51,319 @@ import wicket.model.PropertyModel;
 public class SearchPage extends CdAppBasePage
 {
 	/**
-	 * 
+	 * Custom table navigation class that adds extra labels.
 	 */
-	private static final long serialVersionUID = 1L;
+	private static class CDTableNavigation extends PagingNavigation
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Construct.
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param id
+		 *            the name of the component
+		 * @param table
+		 *            the table
+		 */
+		public CDTableNavigation(MarkupContainer parent, String id, PageableListView table)
+		{
+			super(parent, id, table);
+		}
+
+		/**
+		 * @see wicket.markup.html.list.Loop#populateItem(wicket.markup.html.list.Loop.LoopItem)
+		 */
+		@Override
+		protected void populateItem(final LoopItem iteration)
+		{
+			final PagingNavigationLink link = new PagingNavigationLink(iteration, "pageLink",
+					pageable, iteration.getIteration());
+
+			if (iteration.getIteration() > 0)
+			{
+				new Label(iteration, "separator", "|");
+			}
+			else
+			{
+				new Label(iteration, "separator", "");
+			}
+			new Label(link, "pageNumber", String.valueOf(iteration.getIteration() + 1));
+			new Label(link, "pageLabel", "page");
+		}
+	}
+
+	/** Link for deleting a row. */
+	private final class DeleteLink extends Link<Long>
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Construct.
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param name
+		 *            name of the component
+		 * @param cd
+		 *            the cd
+		 */
+		public DeleteLink(MarkupContainer parent, String name, CD cd)
+		{
+			super(parent, name, new Model<Long>(cd.getId()));
+			add(new SimpleAttributeModifier("onclick", "if(!confirm('delete cd " + cd.getTitle()
+					+ " ?')) return false;"));
+		}
+
+		/**
+		 * @see wicket.markup.html.link.Link#onClick()
+		 */
+		@Override
+		public void onClick()
+		{
+			final Long id = getModelObject();
+
+			CdDao cdDao = getCdDao();
+			CD cd = null;
+			try
+			{
+				cd = cdDao.load(id);
+			}
+			catch (HibernateException e)
+			{
+				// For some reason (back button, concurrent acces?) the object
+				// does not exist
+				// anymore. Report an error and return
+				SearchPage.this.error("could not delete cd with id " + id
+						+ "; it was not found in the database");
+				return;
+			}
+
+			// inform the list component that a change in its model is about to
+			// happen
+			resultsListView.modelChanging();
+
+			getCdDao().delete(cd);
+
+			// infor the list component that a change in its model has happened
+			resultsListView.modelChanged();
+
+			SearchPage.this.info(" cd deleted");
+
+		}
+	}
+
+	/** link to detail edit page. */
+	private final class DetailLink extends Link<Long>
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Construct.
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param name
+		 *            name of the component
+		 * @param id
+		 *            the id of the cd
+		 */
+		public DetailLink(MarkupContainer parent, String name, Long id)
+		{
+			super(parent, name, new Model<Long>(id));
+		}
+
+		/**
+		 * @see wicket.markup.html.link.Link#onClick()
+		 */
+		@Override
+		public void onClick()
+		{
+			final RequestCycle requestCycle = getRequestCycle();
+			final Long id = getModelObject();
+			requestCycle.setResponsePage(new EditPage(SearchPage.this, id));
+		}
+	}
+
+	/**
+	 * Table for displaying search results.
+	 */
+	private class SearchCDResultsListView extends PageableListView<CD>
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Construct.
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param id
+		 *            id of the component
+		 * @param model
+		 *            the model
+		 * @param pageSizeInCells
+		 *            page size
+		 */
+		public SearchCDResultsListView(MarkupContainer parent, String id, IModel<List<CD>> model,
+				int pageSizeInCells)
+		{
+			super(parent, id, model, pageSizeInCells);
+		}
+
+		/**
+		 * @see wicket.Component#isVersioned()
+		 */
+		@Override
+		public boolean isVersioned()
+		{
+			return true;
+		}
+
+		/**
+		 * @see PageableListView#populateItem(ListItem)
+		 * @param item
+		 */
+		@Override
+		public void populateItem(final ListItem<CD> item)
+		{
+			final CD cd = item.getModelObject();
+			final Long id = cd.getId();
+
+			// add links to the details
+			new Label(new DetailLink(item, "title", id), "title", cd.getTitle());
+			new Label(new DetailLink(item, "performers", id), "performers", cd.getPerformers());
+			new Label(new DetailLink(item, "label", id), "label", cd.getLabel());
+			new Label(new DetailLink(item, "year", id), "year", (cd.getYear() != null) ? cd
+					.getYear().toString() : "");
+
+			// add a delete link for each found record
+			DeleteLink deleteLink = new DeleteLink(item, "delete", cd);
+		}
+	}
+
+	/**
+	 * Form for search actions.
+	 */
+	private class SearchForm extends Form
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		/** search property to set. */
+		private String search;
+
+		/**
+		 * Constructor
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param id
+		 *            id of the form component
+		 */
+		public SearchForm(MarkupContainer parent, final String id)
+		{
+			super(parent, id);
+			new TextField<String>(this, "search", new PropertyModel<String>(this, "search"));
+		}
+
+		/**
+		 * Gets search property.
+		 * 
+		 * @return search property
+		 */
+		public final String getSearch()
+		{
+			return search;
+		}
+
+		/**
+		 * @see wicket.markup.html.form.Form#onSubmit()
+		 */
+		@Override
+		public final void onSubmit()
+		{
+			searchModel.setSearchString(search); // set search query on model
+			setCurrentResultPageToFirst(); // start with first page
+			// SearchPage.this.modelChangedStructure();
+
+			if (search != null && (!search.trim().equals("")))
+			{
+				info(getNumberOfResults() + " results found for query '" + search + "'");
+			}
+		}
+
+		/**
+		 * Sets search property.
+		 * 
+		 * @param search
+		 *            search property
+		 */
+		public final void setSearch(String search)
+		{
+			this.search = search;
+		}
+	}
+
+	/** Link for sorting on a column. */
+	private final class SortLink extends Link
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		/** order by field. */
+		private final String field;
+
+		/**
+		 * Construct.
+		 * 
+		 * @param parent
+		 *            The parent
+		 * @param id
+		 *            name of component
+		 * @param field
+		 *            order by field
+		 */
+		public SortLink(MarkupContainer parent, String id, String field)
+		{
+			super(parent, id);
+			this.field = field;
+		}
+
+		/**
+		 * Add order by field to query of list.
+		 * 
+		 * @see wicket.markup.html.link.Link#onClick()
+		 */
+		@Override
+		public void onClick()
+		{
+			searchModel.addOrdering(field);
+			// SearchPage.this.modelChangedStructure();
+		}
+	}
 
 	/** Logger. */
 	private static Log log = LogFactory.getLog(SearchPage.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/** list view for search results. */
 	private SearchCDResultsListView resultsListView;
@@ -130,312 +437,5 @@ public class SearchPage extends CdAppBasePage
 	private int getNumberOfResults()
 	{
 		return ((List)resultsListView.getModelObject()).size();
-	}
-
-	/**
-	 * Form for search actions.
-	 */
-	private class SearchForm extends Form
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		/** search property to set. */
-		private String search;
-
-		/**
-		 * Constructor
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param id
-		 *            id of the form component
-		 */
-		public SearchForm(MarkupContainer parent, final String id)
-		{
-			super(parent, id);
-			new TextField<String>(this, "search", new PropertyModel<String>(this, "search"));
-		}
-
-		/**
-		 * @see wicket.markup.html.form.Form#onSubmit()
-		 */
-		@Override
-		public final void onSubmit()
-		{
-			searchModel.setSearchString(search); // set search query on model
-			setCurrentResultPageToFirst(); // start with first page
-			// SearchPage.this.modelChangedStructure();
-
-			if (search != null && (!search.trim().equals("")))
-			{
-				info(getNumberOfResults() + " results found for query '" + search + "'");
-			}
-		}
-
-		/**
-		 * Gets search property.
-		 * 
-		 * @return search property
-		 */
-		public final String getSearch()
-		{
-			return search;
-		}
-
-		/**
-		 * Sets search property.
-		 * 
-		 * @param search
-		 *            search property
-		 */
-		public final void setSearch(String search)
-		{
-			this.search = search;
-		}
-	}
-
-	/**
-	 * Table for displaying search results.
-	 */
-	private class SearchCDResultsListView extends PageableListView<CD>
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param id
-		 *            id of the component
-		 * @param model
-		 *            the model
-		 * @param pageSizeInCells
-		 *            page size
-		 */
-		public SearchCDResultsListView(MarkupContainer parent, String id, IModel<List<CD>> model,
-				int pageSizeInCells)
-		{
-			super(parent, id, model, pageSizeInCells);
-		}
-
-		/**
-		 * @see wicket.Component#isVersioned()
-		 */
-		@Override
-		public boolean isVersioned()
-		{
-			return true;
-		}
-
-		/**
-		 * @see PageableListView#populateItem(ListItem)
-		 * @param item
-		 */
-		@Override
-		public void populateItem(final ListItem<CD> item)
-		{
-			final CD cd = item.getModelObject();
-			final Long id = cd.getId();
-
-			// add links to the details
-			new Label(new DetailLink(item, "title", id), "title", cd.getTitle());
-			new Label(new DetailLink(item, "performers", id), "performers", cd.getPerformers());
-			new Label(new DetailLink(item, "label", id), "label", cd.getLabel());
-			new Label(new DetailLink(item, "year", id), "year", (cd.getYear() != null) ? cd
-					.getYear().toString() : "");
-
-			// add a delete link for each found record
-			DeleteLink deleteLink = new DeleteLink(item, "delete", cd);
-		}
-	}
-
-	/** link to detail edit page. */
-	private final class DetailLink extends Link<Long>
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param name
-		 *            name of the component
-		 * @param id
-		 *            the id of the cd
-		 */
-		public DetailLink(MarkupContainer parent, String name, Long id)
-		{
-			super(parent, name, new Model<Long>(id));
-		}
-
-		/**
-		 * @see wicket.markup.html.link.Link#onClick()
-		 */
-		@Override
-		public void onClick()
-		{
-			final RequestCycle requestCycle = getRequestCycle();
-			final Long id = getModelObject();
-			requestCycle.setResponsePage(new EditPage(SearchPage.this, id));
-		}
-	}
-
-	/** Link for deleting a row. */
-	private final class DeleteLink extends Link<Long>
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param name
-		 *            name of the component
-		 * @param cd
-		 *            the cd
-		 */
-		public DeleteLink(MarkupContainer parent, String name, CD cd)
-		{
-			super(parent, name, new Model<Long>(cd.getId()));
-			add(new SimpleAttributeModifier("onclick", "if(!confirm('delete cd " + cd.getTitle()
-					+ " ?')) return false;"));
-		}
-
-		/**
-		 * @see wicket.markup.html.link.Link#onClick()
-		 */
-		@Override
-		public void onClick()
-		{
-			final Long id = getModelObject();
-
-			CdDao cdDao = getCdDao();
-			CD cd = null;
-			try
-			{
-				cd = cdDao.load(id);
-			}
-			catch (HibernateException e)
-			{
-				// For some reason (back button, concurrent acces?) the object
-				// does not exist
-				// anymore. Report an error and return
-				SearchPage.this.error("could not delete cd with id " + id
-						+ "; it was not found in the database");
-				return;
-			}
-
-			// inform the list component that a change in its model is about to
-			// happen
-			resultsListView.modelChanging();
-
-			getCdDao().delete(cd);
-
-			// infor the list component that a change in its model has happened
-			resultsListView.modelChanged();
-
-			SearchPage.this.info(" cd deleted");
-
-		}
-	}
-
-	/** Link for sorting on a column. */
-	private final class SortLink extends Link
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		/** order by field. */
-		private final String field;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param id
-		 *            name of component
-		 * @param field
-		 *            order by field
-		 */
-		public SortLink(MarkupContainer parent, String id, String field)
-		{
-			super(parent, id);
-			this.field = field;
-		}
-
-		/**
-		 * Add order by field to query of list.
-		 * 
-		 * @see wicket.markup.html.link.Link#onClick()
-		 */
-		@Override
-		public void onClick()
-		{
-			searchModel.addOrdering(field);
-			// SearchPage.this.modelChangedStructure();
-		}
-	}
-
-	/**
-	 * Custom table navigation class that adds extra labels.
-	 */
-	private static class CDTableNavigation extends PagingNavigation
-	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param parent
-		 *            The parent
-		 * @param id
-		 *            the name of the component
-		 * @param table
-		 *            the table
-		 */
-		public CDTableNavigation(MarkupContainer parent, String id, PageableListView table)
-		{
-			super(parent, id, table);
-		}
-
-		/**
-		 * @see wicket.markup.html.list.Loop#populateItem(wicket.markup.html.list.Loop.LoopItem)
-		 */
-		@Override
-		protected void populateItem(final LoopItem iteration)
-		{
-			final PagingNavigationLink link = new PagingNavigationLink(iteration, "pageLink",
-					pageable, iteration.getIteration());
-
-			if (iteration.getIteration() > 0)
-			{
-				new Label(iteration, "separator", "|");
-			}
-			else
-			{
-				new Label(iteration, "separator", "");
-			}
-			new Label(link, "pageNumber", String.valueOf(iteration.getIteration() + 1));
-			new Label(link, "pageLabel", "page");
-		}
 	}
 }
