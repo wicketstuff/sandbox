7dfb08c4a999efe884b1064ba1b05e18567701f6
Vincent Demay <vdemay@wicketstuff.org>
2006-12-01 10:52:31 +0000

    Add Style attribute
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1253 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/widgets/StyleAttribute.java
@@ -0,0 +1,49 @@
+package wicket.contrib.dojo.widgets;
+
+import java.util.HashMap;
+
+/**
+ * Modelise a style Attribute on a tag
+ * @author vdemay
+ *
+ */
+public class StyleAttribute extends HashMap
+{
+
+	public final static String HEIGHT      = "height";
+	public final static String WIDTH       = "width";
+	public final static String DISPLAY     = "display";
+	public final static String MIN_HEIGHT  = "min-height";
+	public final static String MIN_WIDTH   = "min-width";
+	
+	/**
+	 * Constructor
+	 */
+	public StyleAttribute()
+	{
+		super();
+	}
+	
+	public void setHeight(String height){
+		put(HEIGHT, height);
+	}
+	
+	public void setWidth(String width){
+		put(WIDTH, width);
+	}
+	
+	public void setMinHeight(String minHeight){
+		put(MIN_HEIGHT, minHeight);
+	}
+	
+	public void setMinWidth(String minWidth){
+		put(MIN_WIDTH, minWidth);
+	}
+	
+	public void setDisplay(String display){
+		put(DISPLAY, display);
+	}
+	
+	
+	
+}
