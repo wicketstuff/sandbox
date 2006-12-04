14b605a0212e7df6bf4be89635c7ce555efda8b2
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2006-12-04 16:25:07 +0000

    Fixes, and added a Maven 2 POM
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1281 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/SliderSample.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/SliderSample.java
@@ -4,23 +4,34 @@ import wicket.PageParameters;
 import wicket.contrib.markup.html.form.sliders.DojoIntegerSlider;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.form.Form;
-import wicket.model.Model;
+import wicket.model.PropertyModel;
 
 public class SliderSample extends WebPage {
-	
-	private Integer int1;
+    private int value = 12;
 
-		
-	public SliderSample(PageParameters parameters){
+    public SliderSample(PageParameters params) throws Exception {
+        if (params.containsKey("value"))
+            value = params.getInt("value");
+        Form form = new Form("form") {
+            @Override
+            protected void onSubmit() {
+                PageParameters params = new PageParameters();
+                params.add("value", Integer.toString(value));
+                setResponsePage(SliderSample.class, params);
+            }
+        };
+        DojoIntegerSlider slider = new DojoIntegerSlider("slider1", new PropertyModel(this, "value"));
+        slider.setStart(0);
+        slider.setEnd(100);
+        add(form);
+        form.add(slider);
+    }
 
-		int1 = 12;
-		
-		Form form = new Form("form");
-		DojoIntegerSlider slider = new DojoIntegerSlider("slider1", new Model(int1));
-		slider.setStart(0);
-		slider.setEnd(100);
-		add(form);
-		form.add(slider);
+    public int getValue() {
+        return value;
+    }
 
-	}
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
