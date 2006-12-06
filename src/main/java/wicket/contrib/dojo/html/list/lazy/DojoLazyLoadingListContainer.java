fca1bc628e087844083765ea887277154d26c4a9
Vincent Demay <vdemay@wicketstuff.org>
2006-12-06 08:55:50 +0000

    A lazy loading list
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-dojo@1289 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/html/list/lazy/DojoLazyLoadingListContainer.java
@@ -0,0 +1,88 @@
+package wicket.contrib.dojo.html.list.lazy;
+
+
+import static wicket.contrib.dojo.DojoIdConstants.DOJO_TYPE;
+import static wicket.contrib.dojo.DojoIdConstants.DOJO_TYPE_LAZYTABLE;
+import wicket.Component;
+import wicket.MarkupContainer;
+import wicket.ResourceReference;
+import wicket.WicketRuntimeException;
+import wicket.markup.ComponentTag;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.model.IModel;
+
+/**
+ * A lazy loading Repeating view
+ * @author vincent demay
+ *
+ */
+public class DojoLazyLoadingListContainer extends  WebMarkupContainer
+{
+
+	public DojoLazyLoadingListContainer(MarkupContainer parent, String id, IModel model)
+	{
+		super(parent, id, model);
+		add(new DojoLazyLoadingListContainerHandler());
+	}
+
+	public DojoLazyLoadingListContainer(MarkupContainer parent, String id)
+	{
+		super(parent, id);
+		add(new DojoLazyLoadingListContainerHandler());
+	}
+	
+	@Override
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+		tag.put(DOJO_TYPE, DOJO_TYPE_LAZYTABLE);
+		tag.put("templatePath", urlFor(new ResourceReference(DojoLazyLoadingListContainerHandler.class, "LazyTable.htm")));
+	}
+	
+	/**
+	 * Find the list view in children
+	 * if none or more than one throw an exception!
+	 * 
+	 * @return the child ListView of this container
+	 */
+	public Component getChild()
+	{
+		ChildFinder visitor = new ChildFinder();
+		visitChildren(visitor);
+		return visitor.getChild();
+	}
+	
+	
+	/**
+	 * Check for child - retreive a 
+	 *   * DojoLazyLoadingRefreshingView
+	 * @author Vincent Demay
+	 *
+	 */
+	private class ChildFinder implements IVisitor{
+		private Component component = null;
+		private int componentNumber = 0;
+		
+		public Object component(Component component)
+		{
+			if (component instanceof DojoLazyLoadingRefreshingView){
+				this.component = component;
+				componentNumber ++;
+			}
+			return CONTINUE_TRAVERSAL_BUT_DONT_GO_DEEPER;
+		}
+		
+		public Component getChild(){
+			if (componentNumber != 1 ){
+				throw new WicketRuntimeException("A DojoLazyLoadingListContainer should contain exactly one DojoLazyLoadingRefreshingView as directly child");
+			}
+			return component;
+		}
+	}
+	
+	
+
+
+
+	
+}
