101646e8a4a7f431380bd40b1cca7b73e1906918
Vincent Demay <vdemay@wicketstuff.org>
2006-12-06 09:56:47 +0000

    A lazyu loading table Sample
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1291 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/LazyTableSample.java
@@ -0,0 +1,41 @@
+package wicket.contrib.dojo.examples;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import wicket.PageParameters;
+import wicket.contrib.dojo.html.list.lazy.DojoLazyLoadingListContainer;
+import wicket.contrib.dojo.html.list.lazy.DojoLazyLoadingRefreshingView;
+import wicket.extensions.markup.html.repeater.refreshing.Item;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+
+public class LazyTableSample extends WebPage {
+	
+	public LazyTableSample(PageParameters parameters){
+		DojoLazyLoadingListContainer container = new DojoLazyLoadingListContainer(this, "container");
+		DojoLazyLoadingRefreshingView list = new DojoLazyLoadingRefreshingView(container, "table"){
+
+			@Override
+			public Iterator iterator(int first, int count) {
+				ArrayList<String> list = new ArrayList<String>();
+				int i = 0;
+				while(i < count){
+					list.add("foo" + (first + i++));
+				}
+				
+				//fake a busy and slow machine
+				int j = 0;
+				while (j < 1000000000){j++;}
+				
+				return list.iterator();
+			}
+
+			@Override
+			protected void populateItem(Item item) {
+				new Label(item, "label",item.getModel());			
+			}
+			
+		};
+	}
+}
