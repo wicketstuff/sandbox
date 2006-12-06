0d236536ae21e6465b3bec75d277508747a8ad69
Vincent Demay <vdemay@wicketstuff.org>
2006-12-06 13:53:42 +0000

    Fix presentation bugs on selectionList
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1295 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/lazy/LazyTable.js
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/lazy/LazyTable.js
@@ -1,5 +1,6 @@
 dojo.require("dojo.widget.*");
 dojo.require("dojo.html.*");
+dojo.require("dojo.event.*");
 
 dojo.provide ("dojo.widget.LazyTable");
 
@@ -33,14 +34,15 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 	//debug purpose loading number
 	loaded: 0,
 	
-	requestUrl: "",
-	
 	templatePath: dojo.uri.dojoUri("../LazyTable.htm"),
 	
 	requestTimeOut: null,
 	
 	//last request interval
 	from: null,
+	//already turn element
+	turnElt: 0,
+	backTurnElt:0,
 	
 	/**
 	 * Important note : 
@@ -48,7 +50,19 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 	 *   ** this.infoArea is node where info are displayed
 	 *   ** this.scroller is the fake scroll container
 	 *   ** this.scrollerContent is the fake div used to calculate the fake scroll
+	 *
+	 *********************************************
+	 * The onReloadMethod should be implemented  *
+	 *********************************************
 	 */
+	 
+	 /**
+	 * Overwrite this method to do what you want on updating.
+	 * Be carrefull your new method should call postUpdate at the end of traitement
+	 * In order to test this widget you can use fakeReload on this method
+	 */
+	onReload: function(from, to){
+	},
 	
 	postCreate: function(args, fragment, parent){
 		//initialize element range
@@ -57,6 +71,35 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 		this.contentCoord = dojo.html.toCoordinateObject(this.content,false);
 		this.scrollerContent.style.height = (this.maxKnewItem * this.rowHeight) + "px";
 		dojo.event.connect(this.scroller, "onscroll", this, "scrollMoved");
+		
+		if(this.content.addEventListener){
+			// dojo.event.connect() doesn't seem to work with DOMMouseScroll
+			this.content.addEventListener('DOMMouseScroll', dojo.lang.hitch(this, "_mouseWheeled"), false); // Mozilla + Firefox + Netscape
+		}
+		
+		//fix a firefoxBug : 2 tbody are rendered at the first time : 
+		var toRemove = this.content.getElementsByTagName("tbody")["0"];
+		var toKeep = toRemove.getElementsByTagName("tbody")["0"];
+		this.contentTable.appendChild(toKeep);
+		this.contentTable.removeChild(toRemove);
+	},
+	
+	_mouseWheeled: function(evt){
+		var scrollAmount = 0;
+		if(typeof evt.wheelDelta == 'number'){ // IE
+			scrollAmount = evt.wheelDelta;
+		}else if (typeof evt.detail == 'number'){ // Mozilla+Firefox
+			scrollAmount = -evt.detail;
+		}
+		if (scrollAmount > 0){
+			//scrollUP : 
+			this.scroller.scrollTop = this.scroller.scrollTop - this.rowHeight;
+		}
+		if (scrollAmount < 0){
+			//scrollDown
+			this.scroller.scrollTop = this.scroller.scrollTop + this.rowHeight;
+		}
+		dojo.event.browser.stopEvent(evt);
 	},
 	
 	/**
@@ -83,18 +126,59 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 		var newScrollTop = (this.top - this.knewItemStart) * this.rowHeight;
 		this.content.scrollTop = newScrollTop;
 		if (this.top - 1 > this.knewItemEnd - this.rowNumber){		
-			this.turnTable();
+			this.turnTable(this.top -this.knewItemEnd + this.rowNumber);
 		}
-		
 		if (this.top + 1 < this.knewItemStart){
-			this.backTurnTable();
+			this.backTurnTable(this.knewItemStart - this.top);
 		}
 		
 		//reload content if necessary
 		this.reload();
 	},
+	
+	/**
+	 * fake scroll adding empty line at the bottom
+	 */ 
+	turnTable: function(number){
+		for (var i=0; i<number - this.turnElt; i++){
+			var toRemove = this.contentTable.getElementsByTagName("tbody")[0].firstChild;
+			var columns = this.contentTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName('td').length;
+			this.contentTable.getElementsByTagName("tbody")[0].removeChild(toRemove);
+			var tr = document.createElement('tr');
+			for (var j=0; j < columns; j++){
+				var td = document.createElement('td');
+				td.appendChild(document.createTextNode(' '));
+				tr.appendChild(td);
+			}
+			this.contentTable.getElementsByTagName("tbody")[0].appendChild(tr)
+		}
+		if (number > this.turnElt){
+			this.turnElt = number;
+		}
+	},
+	
+	/**
+	 * fake scroll adding emty line at the beggining
+	 */
+	backTurnTable: function(number){
+		for (var i=0; i<number - this.backTurnElt; i++){
+			var toRemove = this.contentTable.getElementsByTagName("tbody")[0].lastChild;
+			var columns = this.contentTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName('td').length;
+			this.contentTable.getElementsByTagName("tbody")[0].removeChild(toRemove);
+			var tr = document.createElement('tr');
+			for (var j=0; j < columns; j++){
+				var td = document.createElement('td');
+				td.appendChild(document.createTextNode(' '));
+				tr.appendChild(td);
+			}
+			this.contentTable.getElementsByTagName("tbody")[0].insertBefore(tr, this.contentTable.getElementsByTagName("tbody")[0].firstChild)
+		}
+		if (number > this.backTurnElt){
+			this.backTurnElt = number;
+		}
+	},
 
-	//TODO comment, make variable and check it
+	//Make the reload
 	reload: function(){
 		if (this.top < this.knewItemStart || this.top + this.rowNumber > this.knewItemEnd){
 			var start =  this.top + 1  - this.knewItemSize/2 ; 
@@ -108,41 +192,11 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 	reloadData: function(from, to){
 		this.infoArea.innerHTML += " from " + from + " - to " + to;
 		
-		//this.knewItemStart = from;
-		//this.knewItemEnd   = this.knewItemStart + this.knewItemSize;
-		
 		//make reload Here...
 		if (from < 0){from = 0; }
-		
 		if (to > this.maxKnewItem-1){to = this.maxKnewItem}
-		//this.fakeReload(from, to);
-		
-		//this.clearTable();
-		
-		//this.turnTable();
+
 		this.reloadItems(from, to);
-	},
-	
-	turnTable: function(){
-		var toRemove = this.contentTable.getElementsByTagName("tbody")[0].firstChild;
-		var columns = this.contentTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName('td').length;
-		this.contentTable.getElementsByTagName("tbody")[0].removeChild(toRemove);
-		var tr = document.createElement('tr');
-		for (var j=0; j < columns; j++){
-			tr.appendChild(document.createElement('td'));
-		}
-		this.contentTable.getElementsByTagName("tbody")[0].appendChild(tr)
-	},
-	
-	backTurnTable: function(){
-		var toRemove = this.contentTable.getElementsByTagName("tbody")[0].lastChild;
-		var columns = this.contentTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName('td').length;
-		this.contentTable.getElementsByTagName("tbody")[0].removeChild(toRemove);
-		var tr = document.createElement('tr');
-		for (var j=0; j < columns; j++){
-			tr.appendChild(document.createElement('td'));
-		}
-		this.contentTable.getElementsByTagName("tbody")[0].insertBefore(tr, this.contentTable.getElementsByTagName("tbody")[0].firstChild)
 	},
 	
 	/**
@@ -157,13 +211,11 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 	
 	reloadAndPlace: function(from, to ){
 		this.onReload(from, to);
-		//and set Table position
-		//this.placeTable();
 	},
 	
-	onReload: function(from, to){
-	},
-	
+	/**
+	 * All stuff to do after reloading
+	 */
 	postUpdate: function(){
 		this.knewItemStart = this.from; 
 		this.knewItemEnd = this.knewItemStart + this.knewItemSize;
@@ -178,7 +230,9 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 			for (var i=0; i< trToBeCreated; i++){
 				var tr = document.createElement('tr');
 				for (var j=0; j < columns; j++){
-					tr.appendChild(document.createElement('td'));
+					var td = document.createElement('td');
+					td.appendChild(document.createTextNode(' '));
+					tr.appendChild(td);
 				}
 				this.contentTable.getElementsByTagName("tbody")[0].insertBefore(tr, this.contentTable.getElementsByTagName("tbody")[0].firstChild)			
 			}
@@ -192,18 +246,21 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 			for (var i=0; i< trToBeCreated; i++){
 				var tr = document.createElement('tr');
 				for (var j=0; j < columns; j++){
-					tr.appendChild(document.createElement('td'));
+					var td = document.createElement('td');
+					td.appendChild(document.createTextNode(' '));
+					tr.appendChild(td);
 				}
 				this.contentTable.getElementsByTagName("tbody")[0].appendChild(tr);			
 			}
 		}
-		
+		this.turnElt = 0;
+		this.backTurnElt = 0;
 		this.placeTable();
 	},
 
-
-
-//debug purpose
+	/**
+	 * this method is used to simulate a reload
+	 */
 	fakeReload: function(start){
 		var begin = start;
 		var trs = this.content.getElementsByTagName("tr");
@@ -212,6 +269,7 @@ dojo.widget.defineWidget ("dojo.widget.LazyTable",dojo.widget.HtmlWidget,
 		for(var i=0; i <trs.length; i++){
 			trs[i].innerHTML="<td  class='cell'>" + begin++ +"</td><td class='cell'>++Loading time = " + this.loaded + "++</td>";
 		}
+		this.postUpdate();
 	}
 });
 	
