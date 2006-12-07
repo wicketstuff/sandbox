4fee34f60402100352e63cec99e273e3363d869a
Vincent Demay <vdemay@wicketstuff.org>
2006-12-07 09:27:35 +0000

    Add a DojoPackagedTextTemplate to render JS
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1304 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/templates/DojoPackagedTextTemplate.java
@@ -0,0 +1,87 @@
+package wicket.contrib.dojo.templates;
+
+import wicket.Component;
+import wicket.extensions.util.resource.PackagedTextTemplate;
+
+public class DojoPackagedTextTemplate extends PackagedTextTemplate
+{
+	private String fileName;
+	private Class clazz;
+	/**
+	 * Constructor.
+	 * 
+	 * @param clazz
+	 *            The clazz to be used for retrieving the classloader for
+	 *            loading the packaged template.
+	 * @param fileName
+	 *            the name of the file, relative to the clazz position
+	 * @param contentType
+	 *            The mime type of this resource, such as "image/jpeg" or
+	 *            "text/html"
+	 * @param encoding
+	 *            The file's encoding, e.g. 'UTF-8'
+	 */
+	public DojoPackagedTextTemplate(Class clazz, String fileName, String contentType, String encoding)
+	{
+		super(clazz, fileName, contentType, encoding);
+		this.clazz = clazz;
+		this.fileName = fileName;
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param clazz
+	 *            The clazz to be used for retrieving the classloader for
+	 *            loading the packaged template.
+	 * @param fileName
+	 *            the name of the file, relative to the clazz position
+	 * @param contentType
+	 *            The mime type of this resource, such as "image/jpeg" or
+	 *            "text/html"
+	 */
+	public DojoPackagedTextTemplate(Class clazz, String fileName, String contentType)
+	{
+		this(clazz, fileName, contentType, null);
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param component
+	 *            The component to be used for retrieving the classloader for
+	 *            loading the packaged template.
+	 * @param fileName
+	 *            The name of the file, relative to the clazz position
+	 */
+	public DojoPackagedTextTemplate(Class clazz, String fileName)
+	{
+		this(clazz, fileName, "text");
+	}
+	
+	
+	/**
+	 * Return a key to use in renderJavascript.
+	 * A Static Key is a Key <b>Unique for all widget of the same class</b>.
+	 * So if there are n widget on the same page, a resource rendered with a
+	 * StaticKey will be displayed once.
+	 * @return a key to use in renderJavascript
+	 */
+	public String getStaticKey(){
+		return clazz.getName() + this.fileName;
+	}
+	
+	/**
+	 * Return a key to use in renderJavascript.
+	 * A WidgetUnique Key is a Key <b>Unique for <u>one</u> widget of the class</b>.
+	 * So if there are n widget on the same page, a resource rendered with a
+	 * WidgetUniqueKey will be displayed n times. But if a same widget is render m times, this resource wil render once
+	 * @param component component for which the uniqueKey will be generated
+	 * @return a key to use in renderJavascript
+	 */
+	public String getWidgetUniqueKey(Component component){
+		//use memory adress to generate key
+		return component.toString() + this.fileName;
+	}
+
+}
