af489ed2f0d03e48810c777a4c17f4c34943c102
Vincent Demay <vdemay@wicketstuff.org>
2006-12-07 10:05:40 +0000

    Add Id for LazyTable
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1309 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/DojoIdConstants.java
+++ src/main/java/wicket/contrib/dojo/DojoIdConstants.java
@@ -15,6 +15,7 @@ public interface DojoIdConstants
 	public static String DOJO_TYPE_FLOATINGPANE          = "FloatingPane";
 	public static String DOJO_TYPE_MODALFLOATINGPANE     = "ModalFloatingPane";
 	public static String DOJO_TYPE_WEEKCALENDAR          = "WeekCalendar";
+	public static String DOJO_TYPE_LAZYTABLE			 = "LazyTable";
 	
 	public static String DOJO_TYPE_TABCONTAINER          = "TabContainer";
 	public static String DOJO_TYPE_ACCORDIONCONTAINER    = "AccordionContainer";
