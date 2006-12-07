c089062baa7ad03222c76406ef604586244cc7f7
Vincent Demay <vdemay@wicketstuff.org>
2006-12-07 09:43:32 +0000

    LazyListBackPort
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1308 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/lazy/DojoLazyLoadingListContainer.java
@@ -0,0 +1,90 @@
+package wicket.contrib.dojo.html.list.lazy;
+
+
+import wicket.Component;
+import wicket.ResourceReference;
+import wicket.WicketRuntimeException;
+import wicket.contrib.dojo.DojoIdConstants;
+import wicket.markup.ComponentTag;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.model.IModel;
+
+/**
+ * A lazy loading Repeating view
+ * @author vincent demay
+ *
+ */
+public class DojoLazyLoadingListContainer extends  WebMarkupContainer
+{
+	
+	private int max;
+
+	public DojoLazyLoadingListContainer(String id, IModel model, int maxItem)
+	{
+		super(id, model);
+		add(new DojoLazyLoadingListContainerHandler());
+		max = maxItem;
+	}
+
+	public DojoLazyLoadingListContainer(String id, int maxItem)
+	{
+		super(id);
+		add(new DojoLazyLoadingListContainerHandler());
+		max = maxItem;
+	}
+	
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+		tag.put(DojoIdConstants.DOJO_TYPE, DojoIdConstants.DOJO_TYPE_LAZYTABLE);
+		tag.put("templatePath", urlFor(new ResourceReference(DojoLazyLoadingListContainerHandler.class, "LazyTable.htm")));
+		tag.put("maxKnewItem","" + max);
+	}
+	
+	/**
+	 * Find the list view in children
+	 * if none or more than one throw an exception!
+	 * 
+	 * @return the child ListView of this container
+	 */
+	public Component getChild()
+	{
+		ChildFinder visitor = new ChildFinder();
+		visitChildren(visitor);
+		return visitor.getChild();
+	}
+	
+	
+	/**
+	 * Check for child - retreive a 
+	 *   * DojoLazyLoadingRefreshingView
+	 * @author Vincent Demay
+	 *
+	 */
+	private class ChildFinder implements IVisitor{
+		private Component component = null;
+		private int componentNumber = 0;
+		
+		public Object component(Component component)
+		{
+			if (component instanceof DojoLazyLoadingRefreshingView){
+				this.component = component;
+				componentNumber ++;
+			}
+			return CONTINUE_TRAVERSAL_BUT_DONT_GO_DEEPER;
+		}
+		
+		public Component getChild(){
+			if (componentNumber != 1 ){
+				throw new WicketRuntimeException("A DojoLazyLoadingListContainer should contain exactly one DojoLazyLoadingRefreshingView as directly child");
+			}
+			return component;
+		}
+	}
+	
+	
+
+
+
+	
+}
---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/lazy/DojoLazyLoadingRefreshingView.java
@@ -0,0 +1,80 @@
+package wicket.contrib.dojo.html.list.lazy;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import wicket.extensions.markup.html.repeater.data.IDataProvider;
+import wicket.extensions.markup.html.repeater.refreshing.RefreshingView;
+import wicket.extensions.markup.html.repeater.refreshing.ReuseIfModelsEqualStrategy;
+import wicket.model.IModel;
+import wicket.model.Model;
+
+public abstract class DojoLazyLoadingRefreshingView extends RefreshingView implements IDataProvider
+{
+	private int first = 0;
+	private int count = 30;
+	
+	private DojoLazyLoadingListContainer parent;
+	
+	public DojoLazyLoadingRefreshingView(String id, IModel model) {
+		super(id, model);
+		this.parent = parent;
+	}
+
+
+	public DojoLazyLoadingRefreshingView(String id) {
+		super(id);
+		this.parent = parent;
+	}
+
+
+	/**
+	 * @param first 
+	 * @param count 
+	 * @return 
+	 * 
+	 */
+	public abstract Iterator iterator(int first, int count);
+	
+	
+	public IModel model(Serializable object){
+		return new Model(object);
+	}
+
+	public void detach(){}
+
+	public int getCount()
+	{
+		return count;
+	}
+
+	public void setCount(int count)
+	{
+		this.count = count;
+	}
+
+	public int getFirst()
+	{
+		return first;
+	}
+
+	public void setFirst(int first)
+	{
+		this.first = first;
+	}
+
+	protected final Iterator getItemModels(){
+		
+		final List toReturn = new ArrayList();
+		
+		Iterator it = iterator(first, count);
+		while (it.hasNext())
+		{
+			toReturn.add(model(it.next()));
+		}
+
+		return toReturn.iterator();
+	}
+}
