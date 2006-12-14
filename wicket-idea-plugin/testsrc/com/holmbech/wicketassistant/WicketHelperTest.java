2c7359a47476e97fdee0e43b5b27bb25c0cc8b3f
Anders Holmbech Brandt <ahnielsen@wicketstuff.org>
2006-12-14 21:16:52 +0000

    created abstract testcase
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1355 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-idea-plugin/testsrc/com/holmbech/wicketassistant/WicketHelperTest.java
+++ wicket-idea-plugin/testsrc/com/holmbech/wicketassistant/WicketHelperTest.java
@@ -20,12 +20,43 @@ package com.holmbech.wicketassistant;
  * Date: 2006-10-22
  */
 
-import junit.framework.TestCase;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.GlobalSearchScope;
 
-public class WicketHelperTest extends TestCase {
-    WicketHelper wicketHelper;
+public class WicketHelperTest extends AbstractTestCase {
 
     public void testGetWicketIdsFromJavaFile() throws Exception {
-        fail("Test is not implemented");
+        Module module = ModuleManager.getInstance(myProject).findModuleByName("test wicket project");
+        PsiManager psiManager = PsiManager.getInstance(myProject);
+        PsiClass aClass = psiManager.findClass("dk.test.TestPage", GlobalSearchScope.moduleWithDependenciesScope(module));
+        String[] strings = WicketHelper.getWicketIdsFromJavaFile(aClass);
+
+        assertEquals(5, strings.length);
+        assertEquals("label_1", strings[0]);
+        assertEquals("label_2", strings[1]);
+        assertEquals("testid", strings[2]);
+        assertEquals("someid", strings[3]);
+        assertEquals("list", strings[4]);
     }
+
+    public void testGetWicketIdsFromHtmlFile() throws Exception {
+        /*
+        Module module = ModuleManager.getInstance(myProject).findModuleByName("test wicket project");
+        PsiManager psiManager = PsiManager.getInstance(myProject);
+        PsiClass aClass = psiManager.;
+        PsiClass aClass = psiManager.findFile("dk.test.TestPage", GlobalSearchScope.moduleWithDependenciesScope(module));
+        String[] strings = WicketHelper.getWicketIdsFromJavaFile(aClass);
+
+        assertEquals(5, strings.length);
+        assertEquals("label_1", strings[0]);
+        assertEquals("label_2", strings[1]);
+        assertEquals("testid", strings[2]);
+        assertEquals("someid", strings[3]);
+        assertEquals("list", strings[4]);
+        */
+    }
+
 }
