31afd5866896cd8cd3155e966680f569979778e4
Vincent Demay <vdemay@wicketstuff.org>
2006-12-14 14:03:30 +0000

    Licence headers
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1346 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/markup/html/form/ImmediateCheckBox.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/markup/html/form/ImmediateCheckBox.java
@@ -1,19 +1,18 @@
 /*
- * $Id: ImmediateCheckBox.java 673 2006-04-06 12:53:07 -0700 (Thu, 06 Apr 2006)
- * joco01 $ $Revision$ $Date$
- * 
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package wicket.contrib.markup.html.form;
 
@@ -25,8 +24,6 @@ import wicket.markup.ComponentTag;
 import wicket.markup.html.IHeaderResponse;
 import wicket.markup.html.form.CheckBox;
 import wicket.model.IModel;
-import wicket.util.resource.IResourceStream;
-import wicket.util.resource.StringBufferResourceStream;
 import wicket.util.string.AppendingStringBuffer;
 import wicket.util.value.IValueMap;
 
