31dd67df94a5bdcba0c532d0a9d86efe1dd8e4d0
Vincent Demay <vdemay@wicketstuff.org>
2006-12-14 17:24:05 +0000

    Add support to RepeatingView
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1348 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/table/DojoSelectableListContainerHandler.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/html/list/table/DojoSelectableListContainerHandler.java
@@ -17,15 +17,20 @@
 package wicket.contrib.dojo.html.list.table;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import wicket.ResourceReference;
 import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.AbstractRequireDojoBehavior;
+import wicket.extensions.markup.html.repeater.RepeatingView;
+import wicket.extensions.markup.html.repeater.refreshing.Item;
 import wicket.markup.ComponentTag;
 import wicket.markup.html.IHeaderResponse;
+import wicket.markup.html.WebMarkupContainer;
 import wicket.markup.html.link.ILinkListener;
 import wicket.markup.html.list.ListView;
+import wicket.model.Model;
 
 /**
  * Handler associated with {@link DojoSelectableListContainer}
@@ -36,16 +41,16 @@ public class DojoSelectableListContainerHandler extends AbstractRequireDojoBehav
 {
 	
 	//child of this container
-	private ListView listView;
+	private WebMarkupContainer child;
 
 	/**
 	 * 
 	 * @param listView
 	 */
-	public DojoSelectableListContainerHandler(ListView listView)
+	public DojoSelectableListContainerHandler(WebMarkupContainer child)
 	{
 		super();
-		this.listView = listView;
+		this.child = child;
 	}
 
 
@@ -71,11 +76,32 @@ public class DojoSelectableListContainerHandler extends AbstractRequireDojoBehav
 				((DojoSelectableListContainer)getComponent()).onChoose(target, null);
 			}
 		}else{
-			List all = listView.getList();
-			int pos;
-			for (int i=0; i < indexList.length; i++){
-				pos = Integer.parseInt(indexList[i]);
-				selected.add(all.get(pos));
+			if (child instanceof ListView){
+				ListView listView = (ListView) child;
+				List all = listView.getList();
+				int pos;
+				for (int i=0; i < indexList.length; i++){
+					pos = Integer.parseInt(indexList[i]);
+					selected.add(all.get(pos));
+				}
+			}else if (child instanceof RepeatingView){
+				RepeatingView repeatingView = (RepeatingView) child;
+				Iterator ite = repeatingView.iterator();
+				//get All Selected in anb ArrayList
+				ArrayList selectedIndexes = new ArrayList();
+				int pos;
+				for (int i=0; i < indexList.length; i++){
+					pos = Integer.parseInt(indexList[i]);
+					selectedIndexes.add(pos);
+				}
+				pos = 0;
+				while (ite.hasNext()){
+					Object element = ite.next();
+					if (selectedIndexes.contains(pos)){
+						selected.add(((Item)element).getModelObject());
+					}
+					pos++;
+				}
 			}
 			
 			((DojoSelectableListContainer)getComponent()).setSelected(selected);
