6faee2ed2f6d38de4e8d01193cb204458e9e1c53
Vincent Demay <vdemay@wicketstuff.org>
2006-12-18 14:05:08 +0000

    BackGround color and opacity + toggle
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1369 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoModalFloatingPane.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoModalFloatingPane.java
@@ -19,7 +19,10 @@ package wicket.contrib.dojo.markup.html.floatingpane;
 import static wicket.contrib.dojo.DojoIdConstants.DOJO_TYPE;
 import static wicket.contrib.dojo.DojoIdConstants.DOJO_TYPE_MODALFLOATINGPANE;
 import wicket.MarkupContainer;
+import wicket.behavior.AttributeAppender;
+import wicket.contrib.dojo.toggle.DojoToggle;
 import wicket.markup.ComponentTag;
+import wicket.model.Model;
 
 /**
  * <p>
@@ -56,7 +59,15 @@ import wicket.markup.ComponentTag;
  */
 public class DojoModalFloatingPane extends DojoAbstractFloatingPane
 {
+	
+	private String bgColor="white";
+	private String bgOpacity="0.5";
 
+	/**
+	 * Modal floasting pane constructor
+	 * @param parent parent where the widget will be added
+	 * @param id widget Id
+	 */
 	public DojoModalFloatingPane(MarkupContainer parent, String id)
 	{
 		super(parent, id);
@@ -69,6 +80,53 @@ public class DojoModalFloatingPane extends DojoAbstractFloatingPane
 	{
 		super.onComponentTag(tag);
 		tag.put(DOJO_TYPE, DOJO_TYPE_MODALFLOATINGPANE);
+		tag.put("bgColor", bgColor);
+		tag.put("bgOpacity", bgOpacity);
+	}
+
+	/**
+	 * Background color for floating pane
+	 * @return Background color for floating pane
+	 */
+	public String getBgColor()
+	{
+		return bgColor;
+	}
+
+	/**
+	 * Background color for floating pane
+	 * @param bgColor Background color for floating pane
+	 */
+	public void setBgColor(String bgColor)
+	{
+		this.bgColor = bgColor;
+	}
+
+	/**
+	 * Background opacity for floating pane
+	 * @return Background opacity for floating pane
+	 */
+	public String getBgOpacity()
+	{
+		return bgOpacity;
+	}
+
+	/**
+	 * Background opacity for floating pane
+	 * @param bgOpacity Background opacity for floating pane
+	 */
+	public void setBgOpacity(String bgOpacity)
+	{
+		this.bgOpacity = bgOpacity;
+	}
+	
+	/**
+	 * Set the dialog effect : see {@link DojoToggle}
+	 * @param toggle
+	 */
+	public void setToggle(DojoToggle toggle){
+		this.add(new AttributeAppender("toggle", new Model<String>(toggle.getToggle()),""));
+		this.add(new AttributeAppender("toggleDuration", new Model<String>(toggle.getDuration() + ""),""));
 	}
 	
 }
