ab6c798fb517d7c8915c5291544f012f86f6fee9
Vincent Demay <vdemay@wicketstuff.org>
2006-12-18 14:04:20 +0000

    BackGround color and opacity + toggle
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1368 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoModalFloatingPane.java
+++ src/main/java/wicket/contrib/dojo/markup/html/floatingpane/DojoModalFloatingPane.java
@@ -1,10 +1,15 @@
 package wicket.contrib.dojo.markup.html.floatingpane;
 
+import wicket.behavior.AttributeAppender;
 import wicket.contrib.dojo.DojoIdConstants;
+import wicket.contrib.dojo.toggle.DojoToggle;
 import wicket.markup.ComponentTag;
+import wicket.model.Model;
 
 public class DojoModalFloatingPane extends DojoAbstractFloatingPane
 {
+	private String bgColor="white";
+	private String bgOpacity="0.5";
 
 	public DojoModalFloatingPane(String id)
 	{
@@ -17,6 +22,54 @@ public class DojoModalFloatingPane extends DojoAbstractFloatingPane
 	{
 		super.onComponentTag(tag);
 		tag.put(DojoIdConstants.DOJO_TYPE, DojoIdConstants.DOJO_TYPE_MODALFLOATINGPANE);
+		tag.put("bgColor", bgColor);
+		tag.put("bgOpacity", bgOpacity);
 	}
+	
+	/**
+	 * Background color for floating pane
+	 * @return Background color for floating pane
+	 */
+	public String getBgColor()
+	{
+		return bgColor;
+	}
+
+	/**
+	 * Background color for floating pane
+	 * @param bgColor Background color for floating pane
+	 */
+	public void setBgColor(String bgColor)
+	{
+		this.bgColor = bgColor;
+	}
+
+	/**
+	 * Background opacity for floating pane
+	 * @return Background opacity for floating pane
+	 */
+	public String getBgOpacity()
+	{
+		return bgOpacity;
+	}
+
+	/**
+	 * Background opacity for floating pane
+	 * @param bgOpacity Background opacity for floating pane
+	 */
+	public void setBgOpacity(String bgOpacity)
+	{
+		this.bgOpacity = bgOpacity;
+	}
+	
+	/**
+	 * Set the dialog effect : see {@link DojoToggle}
+	 * @param toggle
+	 */
+	public void setToggle(DojoToggle toggle){
+		this.add(new AttributeAppender("toggle", new Model(toggle.getToggle()),""));
+		this.add(new AttributeAppender("toggleDuration", new Model(toggle.getDuration() + ""),""));
+	}
+	
 	
 }
