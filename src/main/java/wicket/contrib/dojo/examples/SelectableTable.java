967432f12c6b3596ed55384c7235630872aa8c28
Vincent Demay <vdemay@wicketstuff.org>
2006-12-19 17:50:15 +0000

    Backport repeatingView in selectable list from trunk
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo-examples@1394 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/examples/SelectableTable.java
+++ src/main/java/wicket/contrib/dojo/examples/SelectableTable.java
@@ -1,87 +1,106 @@
 package wicket.contrib.dojo.examples;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import wicket.ajax.AjaxEventBehavior;
 import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.html.list.table.DojoSelectableListContainer;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.list.ListItem;
 import wicket.markup.html.list.ListView;
+import wicket.markup.repeater.Item;
+import wicket.markup.repeater.RefreshingView;
+import wicket.markup.repeater.RepeatingView;
+import wicket.model.Model;
 
 public class SelectableTable extends WebPage {
 
-	static final List objList  = new  ArrayList();
-	static final List objList2  = new  ArrayList();
+	static final List<String> objList  = new  ArrayList<String>();
 
 	
 	public SelectableTable() {
 		super();
 		if (objList.size() == 0){
 			objList.add("foo1");
-			objList.add("foo1");
+			objList.add("bar1");
+			objList.add("foo2");
+			objList.add("bar2");
+			objList.add("foo3");
+			objList.add("bar3");
+			objList.add("foo4");
+			objList.add("bar4");
+			objList.add("foo5");
+			objList.add("bar5");
+			objList.add("foo6");
+			objList.add("bar6");
 		}
-		if (objList2.size() == 0){
-			objList2.add("toto");
-			objList2.add("tata");
-		}
-		
 		ListView list = null;
 		DojoSelectableListContainer container = new DojoSelectableListContainer("container"){
-
-			public void onChoose(AjaxRequestTarget target, Object Selected) {
-				System.out.println("selected");
+			@Override
+			public void onChoose(AjaxRequestTarget target, Object o) {
 				target.appendJavascript("alert('dblClick')");
-				//setResponsePage(DojoFXTestPage.class);
+				
 			}
-
-			public void onSelection(AjaxRequestTarget target, List selected) {
-				System.out.println("selected");
-			}
-			
 		};
-		container.setAjaxModeOnChoose(false);
-		this.add(container);
+		add(container);
 		list = new ListView("list", objList){
 
+			@Override
 			protected void populateItem(final ListItem item) {
 				item.add(new Label("label",(String)item.getModelObject()));
+				item.add(new AjaxEventBehavior("dblClick"){
+
+					@Override
+					protected void onEvent(AjaxRequestTarget target) {
+						target.appendJavascript("alert('You dblClick on item number " + item.getIndex() + "')");
+						
+					}
+					
+				});
 			}
 			
 		};
 		container.add(list);
 		
 		
+		/*------*/
 		
-		ListView list2 = null;
+		RepeatingView repeater = null;
 		DojoSelectableListContainer container2 = new DojoSelectableListContainer("container2"){
-
-			public void onChoose(AjaxRequestTarget target, Object Selected) {
-				System.out.println("2Choose");
-				target.appendJavascript("alert('dblClick')");
-				setResponsePage(DojoFXTestPage.class);
+			@Override
+			public void onChoose(AjaxRequestTarget target, Object o) {
+				System.out.println(o);
+				
 			}
-
-			public void onNonAjaxChoose(Object selected) {
+			@Override
+			public void onSelection(AjaxRequestTarget target, List selected){
 				System.out.println(selected);
 			}
-
-			public void onSelection(AjaxRequestTarget target, List selected) {
-				System.out.println(selected);
-			}
-
 		};
-		container2.setAjaxModeOnChoose(false);
-		this.add(container2);
-		list2 = new ListView("list2", objList2){
+		add(container2);
+		
+		repeater = new RefreshingView("repeater"){
 
-			protected void populateItem(final ListItem item) {
-				item.add(new Label("label",(String)item.getModelObject()));
+			@Override
+			protected Iterator getItemModels() {
+				ArrayList list = new ArrayList();
+				Iterator ite = objList.iterator();
+				while (ite.hasNext()){
+					list.add(new Model((String)ite.next()));
+				}
+				return list.iterator();
+			}
+
+			@Override
+			protected void populateItem(Item item) {
+				item.add(new Label( "label2", item.getModel()));
 			}
 			
 		};
-		container2.add(list2);
+		container2.add(repeater);
 	}
 
 }
