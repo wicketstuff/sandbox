66a5b3610141aace3672402d774a9b69e0f85788
Vincent Demay <vdemay@wicketstuff.org>
2007-01-02 15:29:38 +0000

    Refresh CalendarSample
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1430 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/WeekOrganizerSample.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/WeekOrganizerSample.java
@@ -1,6 +1,8 @@
 package wicket.contrib.dojo.examples;
 
 import wicket.PageParameters;
+import wicket.ajax.AjaxRequestTarget;
+import wicket.ajax.markup.html.AjaxLink;
 import wicket.contrib.dojo.markup.html.calendar.DojoWeekOrganizer;
 import wicket.contrib.dojo.markup.html.calendar.model.WeekTimeRange;
 import wicket.markup.html.WebPage;
@@ -12,7 +14,13 @@ public class WeekOrganizerSample extends WebPage {
 	
 	public WeekOrganizerSample(PageParameters parameters){
 		
-		DojoWeekOrganizer organizer = new DojoWeekOrganizer(this, "organizer", new Model<WeekTimeRange>(times)); 
+		final DojoWeekOrganizer organizer = new DojoWeekOrganizer(this, "organizer", new Model<WeekTimeRange>(times)); 
 		
+		AjaxLink link = new AjaxLink(this, "link"){
+			public void onClick(AjaxRequestTarget target) {
+				target.addComponent(organizer);
+			}
+			
+		};
 	}
 }
