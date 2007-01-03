54282a9406206054e6586bd1bf6d3e536d0b2aa4
Vincent Demay <vdemay@wicketstuff.org>
2007-01-03 17:36:07 +0000

    Add a DojoIndicator on the calendar sample
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1449 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/WeekOrganizerSample.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/WeekOrganizerSample.java
@@ -2,7 +2,9 @@ package wicket.contrib.dojo.examples;
 
 import wicket.PageParameters;
 import wicket.ajax.AjaxRequestTarget;
-import wicket.ajax.markup.html.AjaxLink;
+import wicket.contrib.dojo.html.DojoLink;
+import wicket.contrib.dojo.indicator.DojoImageIndicator;
+import wicket.contrib.dojo.indicator.behavior.DojoIndicatorBehavior;
 import wicket.contrib.dojo.markup.html.calendar.DojoWeekOrganizer;
 import wicket.contrib.dojo.markup.html.calendar.model.WeekTimeRange;
 import wicket.markup.html.WebPage;
@@ -16,11 +18,14 @@ public class WeekOrganizerSample extends WebPage {
 		
 		final DojoWeekOrganizer organizer = new DojoWeekOrganizer(this, "organizer", new Model<WeekTimeRange>(times)); 
 		
-		AjaxLink link = new AjaxLink(this, "link"){
+		final DojoImageIndicator indicator = new DojoImageIndicator(this, "indicator");
+		
+		DojoLink link = new DojoLink(this, "link"){
 			public void onClick(AjaxRequestTarget target) {
 				target.addComponent(organizer);
 			}
-			
 		};
+		
+		link.add(new DojoIndicatorBehavior(indicator));
 	}
 }
