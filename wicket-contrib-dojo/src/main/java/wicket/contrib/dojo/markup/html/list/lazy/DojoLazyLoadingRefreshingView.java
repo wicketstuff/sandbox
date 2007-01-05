f0062c4aa9be229cad5173887312d74184bcfcdd
Vincent Demay <vdemay@wicketstuff.org>
2007-01-05 17:29:27 +0000

    Fix package organisation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1468 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/list/lazy/DojoLazyLoadingRefreshingView.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/list/lazy/DojoLazyLoadingRefreshingView.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package wicket.contrib.dojo.html.list.lazy;
+package wicket.contrib.dojo.markup.html.list.lazy;
 
 import java.util.ArrayList;
 import java.util.Iterator;
