a9af0b670f3e76311cc133d750bda4408359fae0
Igor Vaynberg <ivaynberg@wicketstuff.org>
2007-01-05 19:53:11 +0000

    Initial import.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.x@1476 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-phonebook-1.x/src/java/wicket/contrib/phonebook/web/PhonebookApplication.java
@@ -0,0 +1,37 @@
+/*
+ * $Id: PhonebookApplication.java 517 2006-01-04 21:53:23Z ivaynberg $
+ * $Revision: 517 $
+ * $Date: 2006-01-04 13:53:23 -0800 (Wed, 04 Jan 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.phonebook.web;
+
+import wicket.contrib.phonebook.web.page.ListContactsPage;
+import wicket.protocol.http.WebApplication;
+import wicket.spring.injection.annot.SpringComponentInjector;
+
+public class PhonebookApplication extends WebApplication {
+
+	public Class getHomePage() {
+		return ListContactsPage.class;
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		// setup component injection
+		addComponentInstantiationListener(new SpringComponentInjector(this));
+	}
+}
---
+++ wicket-phonebook-1.x/src/java/wicket/contrib/phonebook/web/page/ListContactsPage.java
@@ -0,0 +1,160 @@
+/*
+ * $Id: ListContactsPage.java 903 2006-08-30 09:08:51Z ivaynberg $
+ * $Revision: 903 $
+ * $Date: 2006-08-30 02:08:51 -0700 (Wed, 30 Aug 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.phonebook.web.page;
+
+import java.util.Arrays;
+
+import wicket.Component;
+import wicket.contrib.phonebook.Contact;
+import wicket.contrib.phonebook.ContactDao;
+import wicket.contrib.phonebook.web.ContactsDataProvider;
+import wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
+import wicket.extensions.markup.html.repeater.data.table.IColumn;
+import wicket.extensions.markup.html.repeater.data.table.filter.ChoiceFilteredPropertyColumn;
+import wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import wicket.extensions.markup.html.repeater.data.table.filter.FilterToolbar;
+import wicket.extensions.markup.html.repeater.data.table.filter.FilteredAbstractColumn;
+import wicket.extensions.markup.html.repeater.data.table.filter.GoAndClearFilter;
+import wicket.extensions.markup.html.repeater.data.table.filter.TextFilteredPropertyColumn;
+import wicket.markup.html.link.Link;
+import wicket.markup.html.panel.Panel;
+import wicket.markup.repeater.Item;
+import wicket.model.IModel;
+import wicket.model.LoadableDetachableModel;
+import wicket.model.Model;
+import wicket.spring.injection.annot.SpringBean;
+
+/**
+ * Display a Pageable List of Contacts.
+ * 
+ * @author igor
+ */
+public class ListContactsPage extends BasePage {
+
+	@SpringBean
+	private ContactDao dao;
+
+	/**
+	 * Constructor. Having this constructor public means that the page is
+	 * 'bookmarkable' and hence can be called/ created from anywhere.
+	 */
+	public ListContactsPage() {
+
+		add(new Link("createLink") {
+			/**
+			 * Go to the Edit page when the link is clicked, passing an empty
+			 * Contact details
+			 */
+			public void onClick() {
+				setResponsePage(new EditContactPage(getPage(), new Model(
+						new Contact())));
+			}
+
+		});
+
+		IColumn[] columns = new IColumn[5];
+
+		/*
+		 * This is a composite column, created by extending
+		 * FilteredAbstractColumn. This column adds a UserActionsPanel as its
+		 * cell contents. It also provides the go-and-clear filter control
+		 * panel.
+		 */
+		columns[0] = new FilteredAbstractColumn(new Model("Actions")) {
+
+			// addd the UserActionsPanel to the cell item
+			public void populateItem(Item cellItem, String componentId,
+					IModel model) {
+				cellItem.add(new UserActionsPanel(componentId, model));
+			}
+
+			// return the go-and-clear filter for the filter toolbar
+			public Component getFilter(String componentId, FilterForm form) {
+				return new GoAndClearFilter(componentId, form);
+			}
+
+		};
+
+		// creates a column with a text filter
+		columns[1] = new TextFilteredPropertyColumn(new Model("First Name"),
+				"firstname", "firstname");
+
+		columns[2] = new ChoiceFilteredPropertyColumn(new Model("Last Name"),
+				"lastname", "lastname", new LoadableDetachableModel() {
+
+					protected Object load() {
+						return dao.getUniqueLastNames();
+					}
+
+				});
+
+		columns[3] = new TextFilteredPropertyColumn(new Model("Phone Number"),
+				"phone", "phone");
+
+		columns[4] = new TextFilteredPropertyColumn(new Model("Email"),
+				"email", "email");
+
+		// set up data provider
+		ContactsDataProvider dataProvider = new ContactsDataProvider(dao);
+
+		// create the data table
+		DefaultDataTable users = new DefaultDataTable("users", Arrays
+				.asList(columns), dataProvider, 10);
+
+		users.addTopToolbar(new FilterToolbar(users, dataProvider));
+		add(users);
+
+	}
+
+	/**
+	 * Provides a composite User Actions panel for the Actions column.
+	 * 
+	 * @author igor
+	 */
+	private static class UserActionsPanel extends Panel {
+
+		public UserActionsPanel(String id, IModel contactModel) {
+			super(id);
+
+			add(new Link("editLink", contactModel) {
+				/**
+				 * Go to the Edit page, passing this page and the id of the
+				 * Contact involved.
+				 */
+				public void onClick() {
+					setResponsePage(new EditContactPage(getPage(), getModel()));
+				}
+
+			});
+
+			add(new Link("deleteLink", contactModel) {
+				/**
+				 * Go to the Delete page, passing this page and the id of the
+				 * Contact involved.
+				 */
+				public void onClick() {
+					setResponsePage(new DeleteContactPage(getPage(), getModel()));
+				}
+
+			});
+
+		}
+
+	}
+}
