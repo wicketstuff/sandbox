676c31c1601a9c2b120133336f059a5275b91a8b
Igor Vaynberg <ivaynberg@wicketstuff.org>
2007-01-05 19:53:11 +0000

    Initial import.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.x@1476 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-phonebook-1.x/src/java/wicket/contrib/phonebook/web/page/DeleteContactPage.java
@@ -0,0 +1,114 @@
+/*
+ * $Id: DeleteContactPage.java 634 2006-03-26 18:28:10 -0800 (Sun, 26 Mar 2006) ivaynberg $
+ * $Revision: 634 $
+ * $Date: 2006-03-26 18:28:10 -0800 (Sun, 26 Mar 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.phonebook.web.page;
+
+import wicket.Component;
+import wicket.Page;
+import wicket.contrib.phonebook.Contact;
+import wicket.contrib.phonebook.ContactDao;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.form.Button;
+import wicket.markup.html.form.Form;
+import wicket.markup.html.link.Link;
+import wicket.model.AbstractReadOnlyModel;
+import wicket.model.IModel;
+import wicket.model.PropertyModel;
+import wicket.spring.injection.annot.SpringBean;
+import wicket.util.collections.MicroMap;
+import wicket.util.string.interpolator.MapVariableInterpolator;
+
+/**
+ * Delete the Contact.
+ * 
+ * @author igor
+ * 
+ */
+public class DeleteContactPage extends BasePage {
+	private Page backPage;
+
+	@SpringBean
+	private ContactDao contactDao;
+
+	/**
+	 * Constructor. Display the summary (names) before asking for confirmation.
+	 * Note that if you don't need the page to be bookmarkable, you can use
+	 * whatever constructor you need, such as is done here.
+	 * 
+	 * @param backPage
+	 *            The page that the user was on before coming here
+	 * @param contact
+	 *            Model that containst he contact to be deleted
+	 */
+	public DeleteContactPage(Page backPage, IModel contact) {
+		this.backPage = backPage;
+		setModel(contact);
+
+		add(new Label("name", getContact().getFullName()));
+
+		/*
+		 * notice in markup this link is attached to <input type='button'/> tag,
+		 * the link is smart enough to know to generate an onclick instead of
+		 * href
+		 */
+		add(new Link("confirm") {
+
+			@Override
+			public void onClick() {
+				final Contact deleted = getContact();
+
+				contactDao.delete(deleted.getId());
+
+				String msg = MapVariableInterpolator.interpolate(getLocalizer()
+						.getString("status.deleted", this), new MicroMap(
+						"name", deleted.getFullName()));
+
+				getSession().info(msg);
+
+				setResponsePage(DeleteContactPage.this.backPage);
+			}
+
+		});
+
+		add(new Link("cancel") {
+
+			@Override
+			public void onClick() {
+				String msg = MapVariableInterpolator.interpolate(getLocalizer()
+						.getString("status.cancelled", this), new MicroMap(
+						"name", getContact().getFullName()));
+
+				getSession().info(msg);
+
+				setResponsePage(DeleteContactPage.this.backPage);
+			}
+
+		});
+
+	}
+
+	/**
+	 * Type-safe way to retrieve the contact from the page's model
+	 * 
+	 * @return
+	 */
+	private Contact getContact() {
+		return (Contact) getModelObject();
+	}
+
+}
