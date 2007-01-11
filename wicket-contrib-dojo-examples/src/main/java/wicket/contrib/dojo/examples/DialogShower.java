11579a900649ad090c8299056ffe8cefd28aca1d
Vincent Demay <vdemay@wicketstuff.org>
2007-01-11 11:29:25 +0000

    Clear Samples
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1517 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DialogShower.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DialogShower.java
@@ -4,7 +4,6 @@ import wicket.PageParameters;
 import wicket.contrib.dojo.markup.html.dialog.DojoDialog;
 import wicket.contrib.dojo.markup.html.dialog.DojoDialogCloser;
 import wicket.contrib.dojo.markup.html.dialog.DojoDialogOpener;
-import wicket.contrib.dojo.toggle.DojoFadeToggle;
 import wicket.contrib.dojo.toggle.DojoWipeToggle;
 import wicket.markup.html.WebPage;
 
