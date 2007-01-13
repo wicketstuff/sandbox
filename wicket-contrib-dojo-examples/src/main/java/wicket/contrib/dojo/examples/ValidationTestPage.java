82a1f9ce92d1bd13610d0a270268be0364f8e1f0
Vincent Demay <vdemay@wicketstuff.org>
2007-01-13 15:30:13 +0000

    Validation Exemple
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1552 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/ValidationTestPage.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/ValidationTestPage.java
@@ -10,6 +10,8 @@ import wicket.markup.html.form.RequiredTextField;
 import wicket.markup.html.form.TextArea;
 import wicket.markup.html.form.validation.IntegerValidator;
 import wicket.markup.html.form.validation.LengthValidator;
+import wicket.markup.html.form.validation.NumberValidator;
+import wicket.markup.html.form.validation.StringValidator;
 import wicket.markup.html.panel.FeedbackPanel;
 import wicket.model.PropertyModel;
 
@@ -33,39 +35,24 @@ public class ValidationTestPage extends WebPage{
 			add(form);
 			//testfield = new RequiredTextField("text", new PropertyModel(ValidationTestPage.this, "textValue"));
 			testfield =  new TextArea("text",new PropertyModel(ValidationTestPage.this, "textValue"));
-			testfield.add(LengthValidator.max(5));
+			testfield.add(StringValidator.maximumLength(5));
 			testfield.add(new FXValidationAjaxHandler("onblur"));
-			
 			form.add(testfield);
 			
 			FXFeedbackIndicator indicator = new FXFeedbackIndicator("indicator");
-	        indicator.setIndicatorFor(testfield);
 	        form.add(indicator);
 
 			
-	        FormComponent tx = new RequiredTextField("integerInRangeProperty", new PropertyModel(ValidationTestPage.this, "integerInRangeProperty"), Integer.class).add(IntegerValidator.range(0, 100));
+	        FormComponent tx = new RequiredTextField("integerInRangeProperty", new PropertyModel(ValidationTestPage.this, "integerInRangeProperty"),Integer.class).add(NumberValidator.range(0, 100));
 	        form.add(tx);
 	        tx.add(new FXValidationAjaxHandler("onblur"));
 			
-			
 
 			//test multiple ajax textfields
-			FormComponent tx2 = new RequiredTextField("integerInRangeProperty2", new PropertyModel(ValidationTestPage.this, "integerInRangeProperty2"),Integer.class).add(IntegerValidator.range(0, 200));
+			FormComponent tx2 = new RequiredTextField("integerInRangeProperty2", new PropertyModel(ValidationTestPage.this, "integerInRangeProperty2"),Integer.class).add(NumberValidator.range(0, 200));
 			form.add(tx2);
 			tx2.add(new FXValidationAjaxHandler("onblur"));
-			
-			
 
-			
-			/*TextArea tarea = new TextArea("tarea", new PropertyModel(ValidationTestPage.this, "textValue"));
-	           tarea.add(LengthValidator.max(10));
-	                     //add the component first before you add the handler
-	          
-	           tarea.add(new FXValidationAjaxHandler("onblur", 251, 156, 71)); 
-	        form.add(tarea);*/
-	        
-	        final FeedbackPanel feedback = new FeedbackPanel("feedback");
-			add(feedback);
 	        
 	}
 	public String getTextValue()
