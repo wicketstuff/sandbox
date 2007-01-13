42c62d36bb855531898dd41f1eabd6a8e51c0701
ryan sonnek <wireframe6464@wicketstuff.org>
2007-01-13 16:22:08 +0000

    removed page contribution for draggable target.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1553 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/Indicator.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/Indicator.java
@@ -1,14 +1,14 @@
 /*
  * $Id$
  * $Revision$ $Date$
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -35,6 +35,8 @@ public class Indicator<T> extends Panel<T>
 	public Indicator(MarkupContainer parent)
 	{
 		super(parent, "indicator");
+
+		setOutputMarkupId(true);
 
 		new Image(this, "indicatorImage", "indicator.gif");
 		new Label(this, "indicatorLabel", "Processing...");
--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/dragdrop/DraggableTarget.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/dragdrop/DraggableTarget.java
@@ -4,86 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 
 import wicket.MarkupContainer;
-import wicket.Page;
-import wicket.PageParameters;
-import wicket.contrib.scriptaculous.Indicator;
+import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.scriptaculous.JavascriptBuilder;
 import wicket.contrib.scriptaculous.ScriptaculousAjaxBehavior;
 import wicket.contrib.scriptaculous.JavascriptBuilder.JavascriptFunction;
+import wicket.contrib.scriptaculous.effects.Effect;
 import wicket.markup.MarkupStream;
 import wicket.markup.html.WebMarkupContainer;
 
-public class DraggableTarget<T> extends WebMarkupContainer<T>
+/**
+ *
+ * @see http://wiki.script.aculo.us/scriptaculous/show/Droppables.add
+ */
+public abstract class DraggableTarget<T> extends WebMarkupContainer<T>
 {
 	private static final long serialVersionUID = 1L;
-	private String draggableClass;
-	private String indicatorId;
-	private final Class<? extends Page> pageContribution;
+	private final ScriptaculousAjaxBehavior onDropBehavior;
+	private final Map<String,Object> dropOptions = new HashMap<String,Object>();
 
-	public DraggableTarget(MarkupContainer parent, String id, Class<? extends Page> pageContribution)
+	public DraggableTarget(MarkupContainer parent, String id)
 	{
 		super(parent, id);
-		this.pageContribution = pageContribution;
 
 		setOutputMarkupId(true);
-		add(ScriptaculousAjaxBehavior.newJavascriptBindingBehavior());
+		this.onDropBehavior = new ScriptaculousAjaxBehavior() {
+
+			public void onRequest()
+			{
+				String input = getRequest().getParameter("id");
+				AjaxRequestTarget target = new AjaxRequestTarget();
+				getRequestCycle().setRequestTarget(target);
+				target.addComponent(DraggableTarget.this);
+				target.appendJavascript(new Effect.Highlight(DraggableTarget.this).toJavascript());
+
+				onDrop(input, target);
+			}
+
+		};
+		add(onDropBehavior);
+
+		dropOptions.put("onDrop", new JavascriptFunction("function() { wicketAjaxGet('" + onDropBehavior.getCallbackUrl() + "'); }"));
 	}
+
+	protected abstract void onDrop(String input, AjaxRequestTarget target);
 
 	public void accepts(DraggableImage image)
 	{
-		this.draggableClass = image.getStyleClass();
-	}
-
-	public void setIndicator(Indicator indicator)
-	{
-		this.indicatorId = indicator.getId();
+		//TODO: this should build a string array of classes so that one target can accept multiple classes.
+		dropOptions.put("accept", image.getStyleClass());
 	}
 
 	protected void onRender(MarkupStream markupStream)
 	{
 		super.onRender(markupStream);
 
-
-		final CharSequence url = urlFor(null, pageContribution, new PageParameters());
-
-		renderInitialAjaxRequest(url);
-
-		final Map<String,Object> updaterOptions = new HashMap<String,Object>() {{
-			if (null != indicatorId)
-			{
-				put("onLoading", new JavascriptFunction("function(request){ Element.show('indicator')}"));
-				put("onComplete", new JavascriptFunction("function(request){Element.hide('indicator')}"));
-			}
-			put("parameters", "");
-			put("evalScripts", Boolean.TRUE);
-			put("asynchronous", Boolean.TRUE);
-
-		}};
-
-		final JavascriptBuilder builder = new JavascriptBuilder();
-		Map<String,Object> dropOptions = new HashMap<String,Object>() {{
-			put("accept", draggableClass);
-			put("onDrop", new JavascriptFunction("function(element) { new Ajax.Updater('" + getId() + "', '"+ url+ "' " + builder.formatAsJavascriptHash(updaterOptions) + ") }"));
-			put("hoverclass", getId() + "-active");
-		}};
-		builder.addLine("Droppables.add('" + getId() + "', ");
+		JavascriptBuilder builder = new JavascriptBuilder();
+		builder.addLine("Droppables.add('" + getMarkupId() + "', ");
 		builder.addOptions(dropOptions);
 		builder.addLine(");");
 
-
-		getResponse().write(builder.buildScriptTagString());
-	}
-
-	private void renderInitialAjaxRequest(CharSequence url)
-	{
-		Map<String,Object> options = new HashMap<String,Object>() {{
-			put("evalScripts", Boolean.TRUE);
-			put("asynchronous", Boolean.TRUE);
-		}};
-		JavascriptBuilder builder = new JavascriptBuilder();
-		builder.addLine("new Ajax.Updater('" + getId() + "', ");
-		builder.addLine("  '" + url+ "', ");
-		builder.addOptions(options);
 		getResponse().write(builder.buildScriptTagString());
 	}
 }
