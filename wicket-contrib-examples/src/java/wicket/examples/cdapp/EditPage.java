1f279dc9aa8b54905f9e748dc7462f127e50a183
Eelco Hillenius <eelco12@wicketstuff.org>
2007-01-15 06:57:44 +0000

    var fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1580 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-examples/src/java/wicket/examples/cdapp/EditPage.java
+++ wicket-contrib-examples/src/java/wicket/examples/cdapp/EditPage.java
@@ -18,9 +18,6 @@ package wicket.examples.cdapp;
 
 import java.sql.Blob;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import wicket.MarkupContainer;
 import wicket.Resource;
 import wicket.contrib.data.model.PersistentObjectModel;
@@ -31,7 +28,6 @@ import wicket.extensions.markup.html.image.resource.ThumbnailImageResource;
 import wicket.markup.html.PackageResource;
 import wicket.markup.html.basic.Label;
 import wicket.markup.html.form.Form;
-import wicket.markup.html.form.RequiredTextField;
 import wicket.markup.html.form.TextField;
 import wicket.markup.html.form.upload.FileUpload;
 import wicket.markup.html.form.upload.FileUploadField;
@@ -45,19 +41,16 @@ import wicket.util.lang.Bytes;
 import wicket.validation.validator.NumberValidator;
 import wicket.validation.validator.StringValidator;
 
-
 /**
  * Page for editing CD's.
  * 
  * @author Eelco Hillenius
  */
-public final class EditPage extends CdAppBasePage
-{
+public final class EditPage extends CdAppBasePage {
 	/**
 	 * Deletes the cd image.
 	 */
-	private final class DeleteImageLink extends Link<CD>
-	{
+	private final class DeleteImageLink extends Link<CD> {
 		/**
 		 * 
 		 */
@@ -70,8 +63,8 @@ public final class EditPage extends CdAppBasePage
 		 * @param name
 		 * @param cdModel
 		 */
-		public DeleteImageLink(MarkupContainer parent, String name, IModel<CD> cdModel)
-		{
+		public DeleteImageLink(MarkupContainer parent, String name,
+				IModel<CD> cdModel) {
 			super(parent, name, cdModel);
 		}
 
@@ -79,8 +72,7 @@ public final class EditPage extends CdAppBasePage
 		 * @see wicket.Component#isVisible()
 		 */
 		@Override
-		public boolean isVisible()
-		{
+		public boolean isVisible() {
 			// only set visible when there is an image set
 			return getModelObject().getImage() != null;
 		}
@@ -89,8 +81,7 @@ public final class EditPage extends CdAppBasePage
 		 * @see wicket.markup.html.link.Link#onClick()
 		 */
 		@Override
-		public void onClick()
-		{
+		public void onClick() {
 			CD cd = getModelObject();
 			cd.setImage(null);
 			thumbnailImage.setImageResource(getThumbnail());
@@ -101,8 +92,7 @@ public final class EditPage extends CdAppBasePage
 	/**
 	 * form for detail editing.
 	 */
-	private final class DetailForm extends Form<CD>
-	{
+	private final class DetailForm extends Form<CD> {
 		/**
 		 * 
 		 */
@@ -119,28 +109,27 @@ public final class EditPage extends CdAppBasePage
 		 * @param cdModel
 		 *            the model
 		 */
-		public DetailForm(MarkupContainer parent, String name, IModel<CD> cdModel)
-		{
+		public DetailForm(MarkupContainer parent, String name,
+				IModel<CD> cdModel) {
 			super(parent, name, new CompoundPropertyModel<CD>(cdModel));
-			RequiredTextField titleField = new RequiredTextField(this, "title");
+			TextField titleField = new TextField(this, "title");
+			titleField.setRequired(true);
 			titleField.add(StringValidator.maximumLength(50));
-			RequiredTextField performersField = new RequiredTextField(this, "performers");
+			TextField performersField = new TextField(this, "performers");
+			performersField.setRequired(true);
 			performersField.add(StringValidator.maximumLength(50));
 			TextField labelField = new TextField(this, "label");
 			labelField.add(StringValidator.maximumLength(50));
-			RequiredTextField yearField = new RequiredTextField<Integer>(this, "year",
+			TextField yearField = new TextField<Integer>(this, "year",
 					Integer.class);
+			yearField.setRequired(true);
 			yearField.add(NumberValidator.POSITIVE);
-			new Link(this, "cancelButton")
-			{
-				/**
-				 * 
-				 */
+			new Link(this, "cancelButton") {
+
 				private static final long serialVersionUID = 1L;
 
 				@Override
-				public void onClick()
-				{
+				public void onClick() {
 					setResponsePage(searchCDPage);
 				}
 			};
@@ -150,23 +139,19 @@ public final class EditPage extends CdAppBasePage
 		 * @see wicket.markup.html.form.Form#onSubmit()
 		 */
 		@Override
-		public void onSubmit()
-		{
+		public void onSubmit() {
 			CD cd = getModelObject();
 			boolean isNew = (cd.getId() == null);
 			// note that, as we used the Ognl property model, the fields are
 			// allready updated
 			getCdDao().save(cd);
 
-			if (isNew)
-			{
+			if (isNew) {
 				// if it is a new cd, start with a fresh page
 				SearchPage searchPage = new SearchPage();
 				searchPage.info("cd " + cd.getTitle() + " created");
 				setResponsePage(searchPage);
-			}
-			else
-			{
+			} else {
 				// set message for search page to display on next rendering
 				searchCDPage.info("cd " + cd.getTitle() + " saved");
 				setResponsePage(searchCDPage); // navigate back to search page
@@ -177,12 +162,12 @@ public final class EditPage extends CdAppBasePage
 	/**
 	 * Form for uploading an image and attaching that image to the cd.
 	 */
-	private final class ImageUploadForm extends Form<CD>
-	{
+	private final class ImageUploadForm extends Form<CD> {
 		/**
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
+
 		private FileUploadField uploadField;
 
 		/**
@@ -193,8 +178,7 @@ public final class EditPage extends CdAppBasePage
 		 * @param cdModel
 		 */
 		public ImageUploadForm(MarkupContainer parent, String name,
-				PersistentObjectModel<CD, Long> cdModel)
-		{
+				PersistentObjectModel<CD, Long> cdModel) {
 			super(parent, name, cdModel);
 
 			// set this form to multipart mode
@@ -205,12 +189,10 @@ public final class EditPage extends CdAppBasePage
 		}
 
 		@Override
-		protected void onSubmit()
-		{
+		protected void onSubmit() {
 			// get the uploaded file
 			FileUpload upload = uploadField.getFileUpload();
-			if (upload != null)
-			{
+			if (upload != null) {
 				CD cd = getModelObject();
 				cd.setImageBytes(upload.getBytes());
 				thumbnailImage.setImageResource(getThumbnail());
@@ -223,9 +205,6 @@ public final class EditPage extends CdAppBasePage
 	 * static resource from this package; references image 'questionmark.gif'.
 	 */
 	private static Resource IMG_UNKNOWN;
-
-	/** Logger. */
-	private static Log log = LogFactory.getLog(SearchPage.class);
 
 	/**
 	 * 
@@ -248,20 +227,22 @@ public final class EditPage extends CdAppBasePage
 	 * @param id
 	 *            the id of the cd to edit
 	 */
-	public EditPage(final SearchPage searchCDPage, Long id)
-	{
-		cdModel = new HibernateObjectModel<CD, Long>(id, CD.class, new HibernateSessionDelegate());
+	public EditPage(final SearchPage searchCDPage, Long id) {
+		cdModel = new HibernateObjectModel<CD, Long>(id, CD.class,
+				new HibernateSessionDelegate());
 		this.searchCDPage = searchCDPage;
 		new Label(this, "cdTitle", new TitleModel(cdModel));
-		FeedbackPanel feedback = new FeedbackPanel(this, "feedback");
+		new FeedbackPanel(this, "feedback");
 		new DetailForm(this, "detailForm", cdModel);
-		ImageUploadForm imageUploadForm = new ImageUploadForm(this, "imageUpload", cdModel);
+		ImageUploadForm imageUploadForm = new ImageUploadForm(this,
+				"imageUpload", cdModel);
 		imageUploadForm.setMaxSize(Bytes.kilobytes(200));
 
 		getThumbnail();
 
 		// create a link that displays the full image in a popup page
-		ImagePopupLink popupImageLink = new ImagePopupLink(this, "popupImageLink", cdModel);
+		ImagePopupLink popupImageLink = new ImagePopupLink(this,
+				"popupImageLink", cdModel);
 
 		// create an image using the image resource
 		thumbnailImage = new Image(popupImageLink, "cdimage", getThumbnail());
@@ -274,41 +255,33 @@ public final class EditPage extends CdAppBasePage
 	 * @see wicket.Component#initModel()
 	 */
 	@Override
-	protected IModel initModel()
-	{
+	protected IModel initModel() {
 		return cdModel;
 	}
 
 	/**
 	 * Gets either the cd's thumbnail image, or a special question mark image.
 	 */
-	private Resource getThumbnail()
-	{
+	private Resource getThumbnail() {
 		// create an image resource that displays a question mark when no image
 		// is set on the cd, or displays a thumbnail of the cd's image when
 		// there is one
 		final CD cd = cdModel.getObject();
-		if (cd.getImage() == null)
-		{
-			if (IMG_UNKNOWN == null)
-			{
-				IMG_UNKNOWN = PackageResource.get(EditPage.class, "questionmark.gif").setCacheable(
-						false);
+		if (cd.getImage() == null) {
+			if (IMG_UNKNOWN == null) {
+				IMG_UNKNOWN = PackageResource.get(EditPage.class,
+						"questionmark.gif").setCacheable(false);
 			}
 			return IMG_UNKNOWN;
-		}
-		else
-		{
-			BlobImageResource img = new BlobImageResource()
-			{
+		} else {
+			BlobImageResource img = new BlobImageResource() {
 				/**
 				 * 
 				 */
 				private static final long serialVersionUID = 1L;
 
 				@Override
-				protected Blob getBlob()
-				{
+				protected Blob getBlob() {
 					return cd.getImage();
 				}
 			};
