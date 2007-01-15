59f3b695c380e0ea6164ce0fa70f14b268681dc8
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-01-15 13:04:57 +0000

    Refactor DojoDatePicker: use SimpleDateFormat to parse and format the Dojo normalized value yyyy-MM-dd, and pass requested display format as the displayFormat Dojo Attribute
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1587 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/markup/html/form/DojoDatePicker.java
+++ src/main/java/wicket/contrib/dojo/markup/html/form/DojoDatePicker.java
@@ -30,13 +30,11 @@ import wicket.util.convert.ConversionException;
  * </p>
  * 
  * @author <a href="http://www.demay-fr.net/blog">Vincent Demay</a>
- * 
- * FIXME : authorize i18n and pattern setting
  */
 public class DojoDatePicker extends TextField{
 	
 	private SimpleDateFormat formatter;
-	private String pattern;
+	private String displayFormat;
 
 	/**
 	 * @param parent
@@ -44,60 +42,26 @@ public class DojoDatePicker extends TextField{
 	 * @param model
 	 * @param pattern
 	 */
-	public DojoDatePicker(String id, IModel model/*, String pattern*/)
+	public DojoDatePicker(String id, IModel model, String displayFormat)
 	{
 		super(id, model);
+		this.displayFormat = displayFormat;
 		add(new DojoDatePickerHandler());
 		this.setOutputMarkupId(true);
-		//setDatePattern(pattern);
-		pattern = "MM/dd/yyyy";
-		formatter = new SimpleDateFormat(pattern);
+		formatter = new SimpleDateFormat("yyyy-MM-dd");
 	}
 	
 	
-	public DojoDatePicker(String id){
-		this(id, null);
+	public DojoDatePicker(String id, String displayFormat){
+		this(id, null, displayFormat);
 	}
-	
-	/**
-	 * Set the date pattern
-	 * @param pattern date pattern example %d/%m/%y
-	 */
-	/*public void setDatePattern(String pattern){
-		this.pattern = pattern;
-		formatter = new SimpleDateFormat(getSimpleDatePattern());
-	}*/
-	
-	/*private String getSimpleDatePattern(){
-		return pattern.replace("%d", "dd").replace("%Y", "yyyy").replace("%m", "MM");
-	}*/
 
 	protected void onComponentTag(ComponentTag tag)
 	{
 		super.onComponentTag(tag);
-		String[] value = getInputAsArray();
-		if (isDojoValue(value)){							//value returned by Dojo - Classic
-			tag.put("date", value[1]);
-			tag.put("value", value[1]);
-		}else if(getValue() != null && ( value == null || value.length < 2)){	//value returned when js is inactive - TestCase
-			tag.put("date", getValue());
-			tag.put("value", getValue());
-		}else{
-			tag.put("date", "");
-			tag.put("value", "");
-		}
-		tag.put("dojoType", "dropdowndatepicker");
-		tag.put("dateFormat", "%m/%d/%Y");
-		tag.put("inputName", this.getId());
-	}
-	
-	/**
-	 * return true if it is a Dojo Request field or false otherwise (in test for exemple)
-	 * @param value request argument giving the value
-	 * @return true if it is a Dojo Request field or false otherwise (in test for exemple)
-	 */
-	private boolean isDojoValue(String[] value){
-		return (value != null && value.length > 1 && !("".equals(value[1])) );
+
+		tag.put("dojoType", "DropdownDatePicker");
+		tag.put("displayFormat", getDisplayFormat());
 	}
 
 	/**
@@ -123,24 +87,29 @@ public class DojoDatePicker extends TextField{
 
 	protected Object convertValue(String[] value) throws ConversionException
 	{
-		String usableValue;
-		
-		if (isDojoValue(value)){							//value returned by Dojo - Classic 
-			usableValue = value[1];
-		}else if (! "".equals(getValue()) && (value == null || value.length < 2)) {						//value returned when js is inactive - TestCase
-			usableValue = getValue();
-		}else{
-			return null;									//No value
-		}
+		if (value == null || "".equals(value[0]))
+			return null;
 		
 		try
 		{
-			return formatter.parse(usableValue);
+			return formatter.parse(value[0]);
 		}
 		catch (ParseException e)
 		{
 			throw new ConversionException(e);
 		}
 		
+	}
+
+
+	public String getDisplayFormat()
+	{
+		return displayFormat;
+	}
+
+
+	public void setDisplayFormat(String displayFormat)
+	{
+		this.displayFormat = displayFormat;
 	}
 }
