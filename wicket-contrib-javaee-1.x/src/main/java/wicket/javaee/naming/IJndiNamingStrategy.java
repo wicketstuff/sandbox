7e861fed476efcf390379fb932745275d96d5825
Filippo Diotalevi <fdiotalevi@wicketstuff.org>
2007-01-21 19:53:29 +0000

    moved to wicket1.3 branch
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1649 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-javaee-1.x/src/main/java/wicket/javaee/naming/IJndiNamingStrategy.java
+++
@@ -1,9 +0,0 @@
-package wicket.javaee.naming;
-
-import java.io.Serializable;
-
-public interface IJndiNamingStrategy extends Serializable{
-
-	String calculateName(String ejbName, Class ejbType);
-
-}
