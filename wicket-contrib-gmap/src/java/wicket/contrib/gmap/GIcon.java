0ee1dda62769d8cffebf9ba644d084878c1f5e24
Eelco Hillenius <eelco12@wicketstuff.org>
2007-01-25 02:31:27 +0000

    to branch 1.3 with fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1677 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/GIcon.java
@@ -0,0 +1,49 @@
+/*
+ * $Id: GIcon.java 577 2006-02-12 20:46:53Z syca $
+ * $Revision: 577 $
+ * $Date: 2006-02-12 12:46:53 -0800 (Sun, 12 Feb 2006) $
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wicket.contrib.gmap;
+
+import java.io.Serializable;
+
+/**
+ * todo to be implemented
+ *
+ * @author Iulian-Corneliu COSTAN
+ */
+public class GIcon implements Serializable
+{
+
+    private String image;
+    private GPoint anchor;
+
+    public GIcon(String image, GPoint anchor)
+    {
+        this.image = image;
+        this.anchor = anchor;
+    }
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public GPoint getAnchor()
+    {
+        return anchor;
+    }
+}
---
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/JavaScriptComponent.java
@@ -0,0 +1,37 @@
+package wicket.contrib.gmap;
+
+import wicket.AttributeModifier;
+import wicket.markup.ComponentTag;
+import wicket.markup.MarkupStream;
+import wicket.markup.html.WebComponent;
+import wicket.model.Model;
+
+/**
+ * Base wicket component that match &ltscript&gt html tag.
+ *
+ * @author Iulian-Corneliu Costan
+ */
+abstract class JavaScriptComponent extends WebComponent
+{
+
+    public JavaScriptComponent(final String id)
+    {
+        super(id);
+    }
+
+    protected void onComponentTag(final ComponentTag tag)
+    {
+        checkComponentTag(tag, "script");
+        add(new AttributeModifier("type", true, new Model("text/javascript")));
+        add(new AttributeModifier("language", true, new Model("JavaScript")));
+
+    }
+
+    protected void onComponentTagBody(final MarkupStream markupStream, final ComponentTag openTag)
+    {
+        String js = onJavaScriptComponentTagBody();
+        replaceComponentTagBody(markupStream, openTag, js);
+    }
+
+    public abstract String onJavaScriptComponentTagBody();
+}
