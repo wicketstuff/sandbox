7290749611a8aef213f487868549234dd8a2c631
Eelco Hillenius <eelco12@wicketstuff.org>
2007-01-25 02:31:27 +0000

    to branch 1.3 with fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1677 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/GMapContainer.java
@@ -0,0 +1,35 @@
+package wicket.contrib.gmap;
+
+import wicket.markup.html.WebMarkupContainer;
+import wicket.markup.html.list.Loop;
+
+import java.util.List;
+
+/**
+ * @author Iulian-Corneliu Costan
+ */
+class GMapContainer extends WebMarkupContainer
+{
+
+    /**
+     * Construct.
+     * @param gmap
+     */
+    public GMapContainer(final GMap gmap)
+    {
+        super(ID);
+        final List<Overlay> overlays = gmap.getOverlays();
+
+        add(new GMapComponent(gmap));
+        add(new Loop("gmarkersLoop", overlays.size())
+        {
+            protected void populateItem(LoopItem item)
+            {
+                Overlay gmarker = overlays.get(item.getIteration());
+                item.add(new GMarkerContainer((GMarker) gmarker));
+            }
+        });
+    }
+
+    public static final String ID = "gmapContainer";
+}
