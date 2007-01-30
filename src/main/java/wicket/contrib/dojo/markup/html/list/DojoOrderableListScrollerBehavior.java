9df62150ed3592b0e153c152347efd13e0bb36e3
Vincent Demay <vdemay@wicketstuff.org>
2007-01-30 16:52:59 +0000

    Scrollable drag Area
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1711 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListScrollerBehavior.java
+++ src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListScrollerBehavior.java
@@ -13,15 +13,26 @@ import wicket.markup.html.IHeaderResponse;
 public class DojoOrderableListScrollerBehavior extends AbstractBehavior implements IHeaderContributor{
 
 	private Component scrollComponent;
+	private String scrollId;
 	
 	public DojoOrderableListScrollerBehavior(Component scrollComponent) {
 		super();
 		this.scrollComponent = scrollComponent;
 	}
+	
+	public DojoOrderableListScrollerBehavior(String scrollId) {
+		super();
+		this.scrollId = scrollId;
+	}
 
 	public void renderHead(IHeaderResponse response) {
 		response.renderJavascriptReference(new ResourceReference(DojoOrderableListScrollerBehavior.class, "DojoOrderableListScrollerBehaviorTemplate.js"));
-		String js = "dojo.event.connect(dojo, \"loaded\", function(){ lookupScrolling(\"" + scrollComponent.getMarkupId() + "\")});";
+		String js;
+		if (scrollComponent != null){
+			js= "dojo.event.connect(dojo, \"loaded\", function(){ lookupScrolling(\"" + scrollComponent.getMarkupId() + "\")});";
+		}else{
+			js= "dojo.event.connect(dojo, \"loaded\", function(){ lookupScrolling(\"" + scrollId + "\")});";
+		}
 		response.renderJavascript(js, js);
 	}
 	
@@ -35,7 +46,12 @@ public class DojoOrderableListScrollerBehavior extends AbstractBehavior implemen
 		IRequestTarget target = RequestCycle.get().getRequestTarget();
 		if (target instanceof AjaxRequestTarget){
 			AjaxRequestTarget current = (AjaxRequestTarget) target;
-			current.appendJavascript("lookupScrolling(\"" + scrollComponent.getMarkupId() + "\");");
+			if (scrollComponent != null){
+				current.appendJavascript("put = false; lookupScrolling(\"" + scrollComponent.getMarkupId() + "\");");
+			}else{
+				current.appendJavascript("put = false; lookupScrolling(\"" + scrollId + "\");");
+			}
+			
 		}
 	}
 }
--- src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListScrollerBehaviorTemplate.js
+++ src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListScrollerBehaviorTemplate.js
@@ -1,29 +1,24 @@
 //FIXME : DROPTARGET[ manager.dropTargets.length -1] should be replace by getting the right node
 
-
 function lookupScrolling(scrollId /**dropId*/){
-	//var scrollId = "scrollable1";
-	//var dropId   = "container0";
 	var position = dojo.html.toCoordinateObject(scrollId);
 	var currentScroll = dojo.byId(scrollId).scrollTop;
 	dojo.byId(scrollId).scrollTop = 0;
 	var timeout = null;
 	var manager = dojo.dnd.dragManager;
 	
-	//for(var i=0; i<manager.dropTargets.length; i++){
-		dojo.event.connect(manager.dropTargets[ manager.dropTargets.length -1], 'onDragMove', function(e) {
-			
-			if (position.top + currentScroll + 60 > e.pageY ){
-				dojo.byId(scrollId).scrollTop = dojo.byId(scrollId).scrollTop - 4;
-				computePosition(timeout);
-			}
-			
-			if (position.top + position.height + currentScroll - 60 < e.pageY ){
-				dojo.byId(scrollId).scrollTop = dojo.byId(scrollId).scrollTop + 4;
-				computePosition(timeout);
-			}
-		});
-	//}
+	dojo.event.connectOnce(manager.dropTargets[ manager.dropTargets.length -1], 'onDragMove', function(e) {
+		
+		if (position.top + currentScroll + 60 > e.pageY ){
+			dojo.byId(scrollId).scrollTop = dojo.byId(scrollId).scrollTop - 4;
+			timeout = computePosition(timeout);
+		}
+		
+		if (position.top + position.height + currentScroll - 60 < e.pageY ){
+			dojo.byId(scrollId).scrollTop = dojo.byId(scrollId).scrollTop + 4;
+			timeout = computePosition(timeout);
+		}
+	});
 }
 
 function computePosition(timeout, currentScroll){
@@ -33,9 +28,7 @@ function computePosition(timeout, currentScroll){
 	}
 	
 	timeout = window.setTimeout(function(){
-		var manager = dojo.dnd.dragManager;
-		
-		//for(var i=0; i<manager.dropTargets.length; i++){		
+		var manager = dojo.dnd.dragManager;	
 			var current = manager.dropTargets[ manager.dropTargets.length -1];
 			current.childBoxes = [];
 			for (var i = 0, child; i < current.domNode.childNodes.length; i++) {
@@ -47,6 +40,7 @@ function computePosition(timeout, currentScroll){
 					left: pos.x, right: pos.x+inner.width, height: inner.height, 
 					width: inner.width, node: child});
 			}
-		//}
-	}, 400);
+	}, 200);
+	
+	return timeout;
 }
