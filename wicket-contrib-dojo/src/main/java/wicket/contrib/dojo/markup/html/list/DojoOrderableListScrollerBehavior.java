8b3c5f3acbbf53b141fd23bea9faa506905728e5
Vincent Demay <vdemay@wicketstuff.org>
2007-01-30 14:37:46 +0000

    Add a scroll behavior on dnd
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1709 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListScrollerBehavior.java
@@ -0,0 +1,42 @@
+package wicket.contrib.dojo.markup.html.list;
+
+import wicket.Component;
+import wicket.IRequestTarget;
+import wicket.RequestCycle;
+import wicket.ResourceReference;
+import wicket.ajax.AjaxRequestTarget;
+import wicket.behavior.AbstractBehavior;
+import wicket.behavior.IBehaviorListener;
+import wicket.markup.html.IHeaderContributor;
+import wicket.markup.html.IHeaderResponse;
+
+public class DojoOrderableListScrollerBehavior extends AbstractBehavior implements IHeaderContributor{
+
+	private Component scrollComponent;
+	
+	public DojoOrderableListScrollerBehavior(Component scrollComponent) {
+		super();
+		this.scrollComponent = scrollComponent;
+	}
+
+	public void renderHead(IHeaderResponse response) {
+		response.renderJavascriptReference(new ResourceReference(DojoOrderableListScrollerBehavior.class, "DojoOrderableListScrollerBehaviorTemplate.js"));
+		String js = "dojo.event.connect(dojo, \"loaded\", function(){ lookupScrolling(\"" + scrollComponent.getMarkupId() + "\")});";
+		response.renderJavascript(js, js);
+	}
+	
+	public void updateScrollComponent(Component scrollComponent){
+		this.scrollComponent = scrollComponent;
+	}
+
+	@Override
+	public void onRendered(Component component) {
+		super.onRendered(component);
+		
+		IRequestTarget target = RequestCycle.get().getRequestTarget();
+		if (target instanceof AjaxRequestTarget){
+			AjaxRequestTarget current = (AjaxRequestTarget) target;
+			current.appendJavascript("lookupScrolling(\"" + scrollComponent.getMarkupId() + "\");");
+		}
+	}
+}
