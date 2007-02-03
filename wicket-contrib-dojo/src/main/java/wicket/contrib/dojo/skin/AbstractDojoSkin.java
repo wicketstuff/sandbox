92f8c9517c4fd1bf30d89f957ad8c47ff1cb9e7e
Vincent Demay <vdemay@wicketstuff.org>
2007-02-03 16:18:23 +0000

    Javadoc For SkinManager containing exemple http://wicketstuff.org/jira/browse/DOJO-5 Allow widget to be skinned with Dojo Template and Css : Fixed
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1725 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/skin/AbstractDojoSkin.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/skin/AbstractDojoSkin.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package wicket.contrib.dojo.skin;
 
 import java.io.File;
@@ -6,16 +22,44 @@ import java.net.URL;
 import wicket.Component;
 import wicket.ResourceReference;
 import wicket.contrib.dojo.AbstractRequireDojoBehavior;
+import wicket.contrib.dojo.skin.manager.SkinManager;
 
+/**
+ * Abstract class to define a new Skin for dojo widget.<br/>
+ * A dojo skin is associated with a set of html templates and css. Templates and css should be in the same package
+ * as the skin class. If a widget css/template is not defined, the default skin will be used. <br/>
+ * <p>Template and css should have the same name as the widget class.<br/>
+ * For exemple, if you want skin DojoFloatingPane you should write a new class 
+ * extending this one and 2 files <code>DojoFloatingPane.css</code> and <code>DojoFloatingPane.htm</code>.<br/>
+ * Widgets without skin define in the package will be used default skin.
+ * </p>
+ * <p>
+ * See {@link SkinManager} to know how to used a skin.
+ * </p>
+ * 
+ * @author Vincent Demay
+ *
+ */
 public abstract class AbstractDojoSkin {
 	
 	protected abstract Class getRessourceClass();
 	
+	/**
+	 * Check if the file exists
+	 * @param file file path
+	 * @return true if the file exist and false otherwise
+	 */
 	private final boolean exists(String file){
 		URL res = getRessourceClass().getClassLoader().getResource(getRessourceClass().getPackage().getName().replace('.', File.separatorChar) + File.separatorChar + file);
  		return res!=null;
 	}
 	
+	/**
+	 * Get the css to used for the widget is this file exist otherwise return null
+	 * @param component Component to skin
+	 * @param behavior Dojo component behavior
+	 * @return the css to used for the widget is this file exist otherwise return null
+	 */
 	public final String getTemplateCssPath(Component component, AbstractRequireDojoBehavior behavior){
 		String cssTemplate = getClassName(behavior.getClass().getName()).replaceAll("Handler", "") + ".css";
 		 if (exists(cssTemplate)){
@@ -25,6 +69,12 @@ public abstract class AbstractDojoSkin {
 		}
 	}
 	
+	/**
+	 * Get the htm to used for the widget is this file exist otherwise return null
+	 * @param component Component to skin
+	 * @param behavior Dojo component behavior
+	 * @return the htm to used for the widget is this file exist otherwise return null
+	 */
 	public final String getTemplateHtmlPath(Component component, AbstractRequireDojoBehavior behavio){
 		String htmlTemplate = getClassName(component.getClass().getName().replaceAll("Handler", "")) + ".htm";
 		 if (exists(htmlTemplate)){
@@ -34,6 +84,11 @@ public abstract class AbstractDojoSkin {
 		}
 	}
 	
+	/**
+	 * Return the name that should be used to find templates
+	 * @param clazz
+	 * @return
+	 */
 	private final String getClassName(String clazz){
 		return clazz.substring(clazz.lastIndexOf(".")+1, clazz.length());
 	}
