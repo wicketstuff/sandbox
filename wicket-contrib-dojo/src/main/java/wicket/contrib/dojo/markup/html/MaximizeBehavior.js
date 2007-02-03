1b979d81e5258ef1c941ed155ae850c413d64788
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-02-03 21:13:55 +0000

    * Introduce optional vertical margin * Use MaximizeBehaviour.class instead of getClass() for the ResourceReference in   case the behaviour is overridden * Reformat * Call onResized() if this function is available on the widget, in order to give   a chance to TabContainer or SplitPane to resize itself
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1734 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/MaximizeBehavior.js
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/MaximizeBehavior.js
@@ -1,6 +1,5 @@
- function maximize(id){
+ function maximize(id, vertmargin){
  	var el = dojo.byId(id);
-    var viewport = dojo.html.getViewport();
- 	dojo.html.setContentBox(el, {width: dojo.html.getContentBox(el).width, height: viewport.height});
- 	
+	var viewport = dojo.html.getViewport();
+ 	dojo.html.setContentBox(el, {width: dojo.html.getContentBox(el).width, height: viewport.height - vertmargin});
  }
