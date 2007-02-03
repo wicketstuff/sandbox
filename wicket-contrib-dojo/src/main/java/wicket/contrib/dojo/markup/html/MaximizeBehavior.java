cd68a09c8fa5acb07d09218c92c7272e2ec40c53
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-02-03 21:13:55 +0000

    * Introduce optional vertical margin * Use MaximizeBehaviour.class instead of getClass() for the ResourceReference in   case the behaviour is overridden * Reformat * Call onResized() if this function is available on the widget, in order to give   a chance to TabContainer or SplitPane to resize itself
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1734 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/MaximizeBehavior.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/markup/html/MaximizeBehavior.java
@@ -23,44 +23,45 @@ import wicket.contrib.dojo.AbstractRequireDojoBehavior;
 import wicket.markup.html.IHeaderResponse;
 
 /**
- * Dojo behaviour that maximizes an HTML element to the whole window's viewport size
+ * Dojo behaviour that maximizes an HTML element to the whole window's viewport
+ * size
  */
-public class MaximizeBehavior extends AbstractRequireDojoBehavior
-{
+public class MaximizeBehavior extends AbstractRequireDojoBehavior {
 	/** container handler is attached to. */
 	private Component container;
-	
+
 	/**
 	 * @see wicket.AjaxHandler#onBind()
 	 */
-	protected void onBind()
-	{
+	protected void onBind() {
 		this.container = getComponent();
 	}
-	
+
+	protected int getVerticalMargin() {
+		return 0;
+	}
+
 	/**
 	 * @see wicket.contrib.dojo.DojoAjaxHandler#renderHead(wicket.markup.html.IHeaderResponse)
 	 */
-	public void renderHead(IHeaderResponse response)
-	{
+	public void renderHead(IHeaderResponse response) {
 		super.renderHead(response);
-		response.renderJavascriptReference(new ResourceReference(getClass(), "MaximizeBehavior.js"));
+		response.renderJavascriptReference(new ResourceReference(MaximizeBehavior.class, "MaximizeBehavior.js"));
 		response.renderJavascript(generateDefinition(), MaximizeBehavior.class.getName() + container.getMarkupId());
 	}
-	
-	private String generateDefinition(){
+
+	private String generateDefinition() {
 		StringBuffer toReturn = new StringBuffer();
-		toReturn.append("dojo.addOnLoad( function() { maximize('" + container.getMarkupId() + "'); });\n");
+		toReturn.append("dojo.addOnLoad( function() { maximize('" + container.getMarkupId() + "', " + getVerticalMargin() + "); var widget = dojo.widget.byId('"
+				+ container.getMarkupId() + "'); if (widget.onResized) widget.onResized()});\n");
 		return toReturn.toString();
 	}
-	
-	public void setRequire(RequireDojoLibs libs)
-	{
+
+	public void setRequire(RequireDojoLibs libs) {
 		libs.add("dojo.html");
 	}
 
-	protected void respond(AjaxRequestTarget target)
-	{
+	protected void respond(AjaxRequestTarget target) {
 		// Not used
 	}
 }
