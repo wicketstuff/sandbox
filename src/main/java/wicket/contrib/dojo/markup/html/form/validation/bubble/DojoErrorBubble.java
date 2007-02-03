cdbd92f1395f63eee69429213e22591d7b36a10f
Vincent Demay <vdemay@wicketstuff.org>
2007-02-03 16:29:44 +0000

    JavaDoc for Bubble widget and Bubble validation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1726 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/markup/html/form/validation/bubble/DojoErrorBubble.java
+++ src/main/java/wicket/contrib/dojo/markup/html/form/validation/bubble/DojoErrorBubble.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package wicket.contrib.dojo.markup.html.form.validation.bubble;
 
 import wicket.contrib.dojo.DojoIdConstants;
@@ -6,17 +22,27 @@ import wicket.markup.ComponentTag;
 import wicket.model.IModel;
 
 /**
+ * A red {@link AbstractDojoBubble} show error message
  * 
  * @author Vincent Demay
  *
  */
 public class DojoErrorBubble extends AbstractDojoBubble {
 
+	/**
+	 * Constructs
+	 * @param id
+	 * @param model
+	 */
 	public DojoErrorBubble(String id, IModel model) {
 		super(id, model);
 		add(new DojoErrorBubbleHandler());
 	}
 
+	/**
+	 * Constructs
+	 * @param id
+	 */
 	public DojoErrorBubble(String id) {
 		super(id);
 		add(new DojoErrorBubbleHandler());
