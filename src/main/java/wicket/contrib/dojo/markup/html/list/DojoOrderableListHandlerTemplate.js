2ae684eb08044c516d29a3e441f138aec65ca483
Vincent Demay <vdemay@wicketstuff.org>
2007-02-06 14:50:22 +0000

    It should not be constrained
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1739 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListHandlerTemplate.js
+++ src/main/java/wicket/contrib/dojo/markup/html/list/DojoOrderableListHandlerTemplate.js
@@ -2,6 +2,5 @@ function initDragTable(markupId){
 	var children = document.getElementById(markupId).getElementsByTagName('div');
 	for(var i=0;  children.length > i ; i++){
 		var drag = new dojo.dnd.HtmlDragSource(children[i], '*');
-		drag.constrainTo(dojo.byId(markupId));
 	}
 }
