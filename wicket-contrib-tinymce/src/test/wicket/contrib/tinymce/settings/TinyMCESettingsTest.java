7c65d8bd12b0b2e0098ab54a1fd0096ae8dd64f6
Iulian Costan <syca@wicketstuff.org>
2007-02-10 13:43:59 +0000

    tinymce exact mode implementation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1742 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-tinymce/src/test/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
+++ wicket-contrib-tinymce/src/test/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
@@ -3,138 +3,172 @@ package wicket.contrib.tinymce.settings;
 import java.util.regex.Pattern;
 
 import junit.framework.TestCase;
+import wicket.Component;
 import wicket.contrib.tinymce.settings.SavePlugin;
 import wicket.contrib.tinymce.settings.DateTimePlugin;
 import wicket.contrib.tinymce.settings.TinyMCESettings;
+import wicket.contrib.tinymce.settings.TinyMCESettings.Mode;
+import wicket.markup.html.form.TextArea;
 
 /**
  * @author Iulian-Corneliu Costan (iulian.costan@gmail.com)
  * @author Frank Bille (fbille@avaleo.net)
  */
-public class TinyMCESettingsTest extends TestCase {
+public class TinyMCESettingsTest extends TestCase
+{
 
-    private TinyMCESettings settings;
-    private StringBuffer buffer;
+	private TinyMCESettings settings;
+	private StringBuffer buffer;
 
-    protected void setUp() throws Exception {
-        settings = new TinyMCESettings();
-        buffer = new StringBuffer();
-    }
+	protected void setUp() throws Exception
+	{
+		settings = new TinyMCESettings();
+		buffer = new StringBuffer();
+	}
 
-    protected void tearDown() throws Exception {
-        settings = null;
-        buffer = null;
-    }
+	protected void tearDown() throws Exception
+	{
+		settings = null;
+		buffer = null;
+	}
 
-    public void testAddStatusbarLocation() throws Exception {
-        settings.setStatusbarLocation(TinyMCESettings.Location.top);
-        settings.addStatusbarLocation(buffer);
+	public void testAddStatusbarLocation() throws Exception
+	{
+		settings.setStatusbarLocation(TinyMCESettings.Location.top);
+		settings.addStatusbarLocation(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\ttheme_advanced_statusbar_location : \"top\"", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_statusbar_location : \"top\"", buffer.toString());
+	}
 
-    public void testAddToolbarLocation() throws Exception {
-        settings.setToolbarLocation(TinyMCESettings.Location.top);
-        settings.addToolbarLocation(buffer);
+	public void testAddToolbarLocation() throws Exception
+	{
+		settings.setToolbarLocation(TinyMCESettings.Location.top);
+		settings.addToolbarLocation(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\ttheme_advanced_toolbar_location : \"top\"", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_toolbar_location : \"top\"", buffer.toString());
+	}
 
-    public void testAddToolbarAlign() throws Exception {
-        settings.setToolbarAlign(TinyMCESettings.Align.left);
-        settings.addToolbarAlign(buffer);
+	public void testAddToolbarAlign() throws Exception
+	{
+		settings.setToolbarAlign(TinyMCESettings.Align.left);
+		settings.addToolbarAlign(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\ttheme_advanced_toolbar_align : \"left\"", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_toolbar_align : \"left\"", buffer.toString());
+	}
 
-    public void testAddVerticalResizing() throws Exception {
-        settings.setVerticalResizing(true);
-        settings.addVerticalResizing(buffer);
+	public void testAddVerticalResizing() throws Exception
+	{
+		settings.setVerticalResizing(true);
+		settings.addVerticalResizing(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\ttheme_advanced_resizing : true", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_resizing : true", buffer.toString());
+	}
 
-    public void testAddHorizontalResizing() throws Exception {
-        settings.setHorizontalResizing(true);
-        settings.addHorizontalResizing(buffer);
+	public void testAddHorizontalResizing() throws Exception
+	{
+		settings.setHorizontalResizing(true);
+		settings.addHorizontalResizing(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\ttheme_advanced_resize_horizontal : true", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_resize_horizontal : true", buffer.toString());
+	}
 
-    public void testAddPlugins() throws Exception {
-        SavePlugin savePlugin = new SavePlugin();
-        DateTimePlugin dateTimePlugin = new DateTimePlugin();
+	public void testAddPlugins() throws Exception
+	{
+		SavePlugin savePlugin = new SavePlugin();
+		DateTimePlugin dateTimePlugin = new DateTimePlugin();
 
-        settings.register(savePlugin);
-        settings.register(dateTimePlugin);
+		settings.register(savePlugin);
+		settings.register(dateTimePlugin);
 
-        settings.addPlugins(buffer);
+		settings.addPlugins(buffer);
 
-        assertTrue(buffer.capacity() > 0);
-        assertEquals(",\n\tplugins : \"save, insertdatetime\"", buffer.toString());
-    }
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\tplugins : \"save, insertdatetime\"", buffer.toString());
+	}
 
-    public void testAdd() throws Exception {
-        SavePlugin savePlugin = new SavePlugin();
-        settings.register(savePlugin);
-        assertNotNull(settings.getPlugins());
-        assertEquals(1, settings.getPlugins().size());
-        settings.add(savePlugin.getSaveButton(), TinyMCESettings.Toolbar.first, TinyMCESettings.Position.before);
-        assertEquals(1, settings.getPlugins().size());
-    }
-    
-    public void testDisableButton1() {
-    	settings.disableButton(TinyMCESettings.bold);
-    	
-    	settings.addDisabledButtons(buffer);
-    	
-    	assertTrue(buffer.capacity() > 0);
-    	assertEquals(",\n\ttheme_advanced_disable : \"bold\"", buffer.toString());
-    }
-    
-    public void testDisableButton2() {
-    	settings.disableButton(TinyMCESettings.bold);
-    	settings.disableButton(TinyMCESettings.italic);
-    	
-    	settings.addDisabledButtons(buffer);
-    	
-    	assertTrue(buffer.capacity() > 0);
-    	assertEquals(",\n\ttheme_advanced_disable : \"bold, italic\"", buffer.toString());
-    }
-    
-    public void testDateTimePlugin() {
-    	DateTimePlugin plugin = new DateTimePlugin();
-    	plugin.setDateFormat("%Y-%m-%d");
-    	settings.register(plugin);
-    	
-    	String javascript = settings.toJavaScript();
-    	Pattern pattern = Pattern.compile(".*,\n\tplugin_insertdate_dateFormat : \"%Y-%m-%d\"", Pattern.DOTALL);
+	public void testAdd() throws Exception
+	{
+		SavePlugin savePlugin = new SavePlugin();
+		settings.register(savePlugin);
+		assertNotNull(settings.getPlugins());
+		assertEquals(1, settings.getPlugins().size());
+		settings.add(savePlugin.getSaveButton(), TinyMCESettings.Toolbar.first,
+				TinyMCESettings.Position.before);
+		assertEquals(1, settings.getPlugins().size());
+	}
+
+	public void testDisableButton1()
+	{
+		settings.disableButton(TinyMCESettings.bold);
+
+		settings.addDisabledButtons(buffer);
+
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_disable : \"bold\"", buffer.toString());
+	}
+
+	public void testDisableButton2()
+	{
+		settings.disableButton(TinyMCESettings.bold);
+		settings.disableButton(TinyMCESettings.italic);
+
+		settings.addDisabledButtons(buffer);
+
+		assertTrue(buffer.capacity() > 0);
+		assertEquals(",\n\ttheme_advanced_disable : \"bold, italic\"", buffer.toString());
+	}
+
+	public void testDateTimePlugin()
+	{
+		DateTimePlugin plugin = new DateTimePlugin();
+		plugin.setDateFormat("%Y-%m-%d");
+		settings.register(plugin);
+
+		String javascript = settings.toJavaScript();
+		Pattern pattern = Pattern.compile(".*,\n\tplugin_insertdate_dateFormat : \"%Y-%m-%d\"",
+				Pattern.DOTALL);
 		assertTrue(pattern.matcher(javascript).matches());
-    }
-    
-    public void testDefinePluginExtensions() {
-    	Plugin plugin = new Plugin("mockplugin") {
+	}
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
+	public void testDefinePluginExtensions()
+	{
+		Plugin plugin = new Plugin("mockplugin")
+		{
 			protected void definePluginExtensions(StringBuffer buffer)
 			{
 				buffer.append("alert('Hello Mock World');");
 			}
-    		
-    	};
-    	
-    	settings.register(plugin);
-    	
-    	String javascript = settings.getAdditionalPluginJavaScript();
-    	assertEquals("alert('Hello Mock World');", javascript);
-    }
+
+		};
+
+		settings.register(plugin);
+
+		String javascript = settings.getAdditionalPluginJavaScript();
+		assertEquals("alert('Hello Mock World');", javascript);
+	}
+
+	public testExactMode()
+	{
+		Component c1 = new TextArea();
+		Component c2 = new TextArea();
+		c2.setOutputMarkupId(true);
+		
+		TinyMCESettings settings = new TinyMCESettings(Mode.exact);
+		try
+		{
+			settings.addComponent(c1);
+			fail();
+		}
+		catch (IllegalArgumentException e)
+		{
+			assertTrue(true);
+		}
+		settings.addComponent(c2);
+		String js = settings.toJavaScript();
+	}
 }
