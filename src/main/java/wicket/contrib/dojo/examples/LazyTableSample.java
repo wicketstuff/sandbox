998ed28f0ebd54a9b60bcd4a7a93c980988dbefd
Vincent Demay <vdemay@wicketstuff.org>
2007-02-25 14:24:04 +0000

    http://www.wicketstuff.org/jira/browse/DOJO-21 : Put again removed sources : Examples must use JDK 1.4 : Fixed
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo-examples@1781 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ src/main/java/wicket/contrib/dojo/examples/LazyTableSample.java
@@ -0,0 +1,59 @@
+package wicket.contrib.dojo.examples;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import wicket.PageParameters;
+import wicket.contrib.dojo.markup.html.list.lazy.DojoLazyLoadingListContainer;
+import wicket.contrib.dojo.markup.html.list.lazy.DojoLazyLoadingRefreshingView;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.repeater.Item;
+import wicket.markup.repeater.data.IDataProvider;
+import wicket.model.IModel;
+import wicket.model.Model;
+
+public class LazyTableSample extends WebPage {
+	
+	final DojoLazyLoadingRefreshingView list;
+	
+	public LazyTableSample(PageParameters parameters){
+		DojoLazyLoadingListContainer container = new DojoLazyLoadingListContainer("container", 1000);
+		add(container);
+		 list = new DojoLazyLoadingRefreshingView("table", new SampleDataProvider()){
+			protected void populateItem(Item item) {
+				item.add(new Label("label",item.getModel()));			
+			}
+			
+		};
+		container.add(list);
+	}
+	
+	public class SampleDataProvider implements IDataProvider{
+
+		public Iterator iterator(int first, int count) {
+			ArrayList list = new ArrayList();
+			int i = 0;
+			while(i < count){
+				list.add("foo" + (first + i++));
+			}
+			
+			return list.iterator();
+		}
+
+		public IModel model(Object object) {
+			if (!(object instanceof Serializable)){
+				throw new IllegalArgumentException("The model Object should be Serializable in a DojoLazyLoadingListContainer");
+			}
+			return new Model((Serializable)object);
+		}
+
+		public int size() {
+			return list.getCount();
+		}
+
+		public void detach() {}
+		
+	}
+}
