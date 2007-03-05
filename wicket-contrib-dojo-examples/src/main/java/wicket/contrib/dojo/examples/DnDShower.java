75a57c45689275f7af1469788fbd4c09497a1e56
Vincent Demay <vdemay@wicketstuff.org>
2007-03-05 10:29:21 +0000

    Adapt samples for modification in DND
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@1796 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DnDShower.java
+++ wicket-contrib-dojo-examples/src/main/java/wicket/contrib/dojo/examples/DnDShower.java
@@ -6,6 +6,7 @@ import wicket.contrib.dojo.dojodnd.DojoDragContainer;
 import wicket.contrib.dojo.dojodnd.DojoDropContainer;
 import wicket.markup.html.WebPage;
 import wicket.markup.html.image.Image;
+import wicket.model.Model;
 
 public class DnDShower extends WebPage {
 	
@@ -14,16 +15,32 @@ public class DnDShower extends WebPage {
 		
 			@Override
 			public void onDrop(AjaxRequestTarget target, DojoDragContainer container, int position) {
+				System.out.println("drop Down");
 				System.out.println("position = " + position);
 				System.out.println("DojoDragContainer" + container.getId());
-				
+				System.out.println("Model" + container.getModelObject());
+				System.out.println("..............................................");
 			}
 		
 		};
 		
-		DojoDragContainer dragContainer1 = new DojoDragContainer(this,"dragContainer1");
-		DojoDragContainer dragContainer2 = new DojoDragContainer(this,"dragContainer2");
-		DojoDragContainer dragContainer3 = new DojoDragContainer(this,"dragContainer3");
+		DojoDropContainer topContainer = new DojoDropContainer(this,"topContainer"){
+			@Override
+			public void onDrop(AjaxRequestTarget target, DojoDragContainer container, int position) {
+				System.out.println("drop Up");
+				System.out.println("position = " + position);
+				System.out.println("DojoDragContainer" + container.getId());
+				System.out.println("Model" + container.getModelObject());
+				System.out.println("..............................................");
+				
+			}
+		};
+		
+		DojoDragContainer dragContainer1 = new DojoDragContainer(topContainer,"dragContainer1");
+		dragContainer1.setModel(new Model<String>("testMe"));
+		
+		DojoDragContainer dragContainer2 = new DojoDragContainer(topContainer,"dragContainer2");
+		DojoDragContainer dragContainer3 = new DojoDragContainer(topContainer,"dragContainer3");
 		
 		DojoDragContainer dragContainer4 = new DojoDragContainer(dropContainer,"dragContainer4");
 		DojoDragContainer dragContainer5 = new DojoDragContainer(dropContainer,"dragContainer5");
