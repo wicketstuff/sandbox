c7220b31a93eb38d4c4f144e8d990b89051d4e92
Vincent Demay <vdemay@wicketstuff.org>
2007-03-06 13:20:34 +0000

    Cometd integration
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1802 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/cometd/publisher/CometdPublisher.java
@@ -0,0 +1,37 @@
+package wicket.contrib.dojo.cometd.publisher;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.mortbay.cometd.Bayeux;
+import org.mortbay.cometd.Channel;
+import org.mortbay.cometd.CometdServlet;
+
+import wicket.contrib.dojo.cometd.DojoSharedForm;
+import wicket.protocol.http.WebApplication;
+
+/**
+ * <b>WARNING : This component is a prototype, Do NOT use it. It is still internal</b><br/>
+ * {@link DojoSharedForm} uses Cometd to make it usable you should use an other servlet named cometd:
+ * <pre>
+ */
+public class CometdPublisher {
+	
+	private WebApplication application;
+	
+	public CometdPublisher(WebApplication application) {
+		super();
+		this.application = application;
+	}
+	
+	public void publish(String channel, Map message){
+		ServletContext ctxt = this.application.getServletContext();
+		Bayeux b = (Bayeux)ctxt.getAttribute(CometdServlet.ORG_MORTBAY_BAYEUX);
+		Channel c = b.getChannel(channel);
+		if (c!=null){
+			c.publish(message, b.newClient());
+		}
+	}
+
+}
