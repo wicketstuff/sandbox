889800f3a39c0b52bbf45ec5a7564011b4c2dc99
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-03-13 08:12:11 +0000

    Fix build
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1813 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/DojoRichTextEditorBehavior.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/DojoRichTextEditorBehavior.java
@@ -6,27 +6,23 @@ import wicket.markup.ComponentTag;
 import wicket.model.Model;
 
 /**
- * Behavior that extends a TextArea to load the Dojo Rich Text editor
+ * Behavior that allows a TextArea to show as the Dojo Rich Text editor
  * 
  * @author <a href="mailto:jbq@apache.org">Jean-Baptiste Quenot</a>
  */
 public class DojoRichTextEditorBehavior extends AbstractRequireDojoBehavior {
-	@Override
 	public void setRequire(RequireDojoLibs libs) {
 		libs.add("dojo.widget.Editor2");
 	}
 
-	@Override
 	protected void respond(AjaxRequestTarget arg0) {
 		// Do nothing
 	}
 
-	@Override
 	protected void onBind() {
 		getComponent().add(new AttributeModifier("dojoType", true, new Model("Editor2")));
 	}
 
-	@Override
 	protected void onComponentTag(ComponentTag tag) {
 		if (! tag.getName().equals("textarea"))
 			throw new IllegalArgumentException("Dojo Rich Text Editor works with a <textarea>, found a <" + tag.getName() + ">");
