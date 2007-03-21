d7548a1726f2f89b79ee5b19aadb640014f31d62
Eelco Hillenius <eelco12@wicketstuff.org>
2007-03-21 04:47:15 +0000

    model fixes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1834 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-benchmark/src/main/java/wicket/benchmark/wicket/CustomerList.java
+++ wicket-benchmark/src/main/java/wicket/benchmark/wicket/CustomerList.java
@@ -3,7 +3,6 @@ package wicket.benchmark.wicket;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import wicket.Component;
 import wicket.benchmark.dao.Customer;
 import wicket.benchmark.dao.CustomerDao;
 import wicket.benchmark.dao.DetachableCustomer;
@@ -23,50 +22,53 @@ import wicket.model.Model;
  * @author Phil Kulak
  */
 public class CustomerList extends WebPage {
-    private static SimpleDateFormat FORMAT = 
-        new SimpleDateFormat("MMMM d, yyyy");
-    
-    public CustomerList() {
-        add(new CustomerListView("rows", new DetachableCustomerList()));
-    }
-    
-    private static class CustomerListView extends ListView {
-        public CustomerListView(String id, IModel model) {
-            super(id, model);
-        }
-        
-        @Override
-        protected IModel getListItemModel(IModel model, int index) {
-            Customer customer = ((List<Customer>) model.getObject(this)).get(index);
-            return new CompoundPropertyModel(new DetachableCustomer(customer));
-        }
+	private static class CustomerListView extends ListView {
+		public CustomerListView(String id, IModel model) {
+			super(id, model);
+		}
 
-        @Override
-        protected void populateItem(final ListItem item) {
-            item.add(new Label("firstName"));
-            item.add(new Label("lastName"));
-            item.add(new Label("state"));
-            item.add(new Label("birthDate", new Model(){@Override
-            public Object getObject(Component arg0) {
-                Customer customer = (Customer) item.getModelObject();
-                return FORMAT.format(customer.getBirthDate());
-            }}));
-            
-            item.add(new Link("delete") {
-                @Override
-                public void onClick() {
-                    Customer customer = (Customer) getParent().getModelObject();
-                    CustomerDao.getInstance().delete(customer);
-                }
-            });
-            
-            item.add(new Link("edit") {
-               @Override
-               public void onClick() {
-                   EditCustomer editPage = new EditCustomer(getParent().getModel());
-                   setResponsePage(editPage);
-               }
-            });
-        }
-    }
+		@Override
+		protected IModel getListItemModel(IModel model, int index) {
+			Customer customer = ((List<Customer>) model.getObject()).get(index);
+			return new CompoundPropertyModel(new DetachableCustomer(customer));
+		}
+
+		@Override
+		protected void populateItem(final ListItem item) {
+			item.add(new Label("firstName"));
+			item.add(new Label("lastName"));
+			item.add(new Label("state"));
+			item.add(new Label("birthDate", new Model() {
+				@Override
+				public Object getObject() {
+					Customer customer = (Customer) item.getModelObject();
+					return FORMAT.format(customer.getBirthDate());
+				}
+			}));
+
+			item.add(new Link("delete") {
+				@Override
+				public void onClick() {
+					Customer customer = (Customer) getParent().getModelObject();
+					CustomerDao.getInstance().delete(customer);
+				}
+			});
+
+			item.add(new Link("edit") {
+				@Override
+				public void onClick() {
+					EditCustomer editPage = new EditCustomer(getParent()
+							.getModel());
+					setResponsePage(editPage);
+				}
+			});
+		}
+	}
+
+	private static SimpleDateFormat FORMAT = new SimpleDateFormat(
+			"MMMM d, yyyy");
+
+	public CustomerList() {
+		add(new CustomerListView("rows", new DetachableCustomerList()));
+	}
 }
