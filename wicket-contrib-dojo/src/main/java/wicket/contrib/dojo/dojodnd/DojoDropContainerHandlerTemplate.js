66fdad024f24bafe0337785a2ebd8512d9697079
Vincent Demay <vdemay@wicketstuff.org>
2007-03-23 09:18:28 +0000

    http://81.17.46.170/jira/browse/DOJO-30 : [PATCH] Unknown function 'byId' in DojoDragContainerHandlerTemplate.js
    thx to Bart Molenkamp
    
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1847 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/dojodnd/DojoDropContainerHandlerTemplate.js
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/dojodnd/DojoDropContainerHandlerTemplate.js
@@ -1,7 +1,3 @@
-function byId(id){
-	return document.getElementById(id);
-}
-
 function createUrl(e, url){
 	var dragId = e.dragSource.domNode.id;
 	var all = e.dragSource.domNode.parentNode.getElementsByTagName('div')
@@ -13,7 +9,7 @@ function createUrl(e, url){
 }
 
 function initDrop(markupId, dropId, url){
-	var dl = byId(markupId);
+	var dl = dojo.byId(markupId);
 	var drop = new dojo.dnd.HtmlDropTarget(dl, [dropId]);
 	dojo.event.connect(drop, 'onDrop', function(e) {
 		wicketAjaxGet(createUrl(e, url),function(){},function(){});
