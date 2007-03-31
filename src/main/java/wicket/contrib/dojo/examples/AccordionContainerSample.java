ae91be12be7c4464040ce29f3138f25c788aebab
Vincent Demay <vdemay@wicketstuff.org>
2007-03-31 10:46:32 +0000

    following the container refactoring
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo-examples@1867 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/examples/AccordionContainerSample.java
+++ src/main/java/wicket/contrib/dojo/examples/AccordionContainerSample.java
@@ -1,6 +1,8 @@
 package wicket.contrib.dojo.examples;
 
+import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.markup.html.container.DojoSimpleContainer;
+import wicket.contrib.dojo.markup.html.container.IDojoContainer;
 import wicket.contrib.dojo.markup.html.container.accordion.DojoAccordionContainer;
 import wicket.contrib.dojo.markup.html.container.page.DojoPageContainer;
 import wicket.markup.html.WebPage;
@@ -9,7 +11,12 @@ public class AccordionContainerSample extends WebPage {
 
 	public AccordionContainerSample() {
 		super();
-		DojoAccordionContainer container = new DojoAccordionContainer("tabContainer");
+		DojoAccordionContainer container = new DojoAccordionContainer("tabContainer"){
+			@Override
+			public void onSelectionChange(IDojoContainer selected, AjaxRequestTarget target) {
+				System.out.println(selected.getMarkupId());
+			}
+		};
 		container.setHeight("500px");
 		container.add(new DojoSimpleContainer("tab1", "title1"));
 		container.add(new DojoSimpleContainer("tab2", "title2"));
--- src/main/java/wicket/contrib/dojo/examples/TabContainerSample.java
+++ src/main/java/wicket/contrib/dojo/examples/TabContainerSample.java
@@ -1,6 +1,8 @@
 package wicket.contrib.dojo.examples;
 
+import wicket.ajax.AjaxRequestTarget;
 import wicket.contrib.dojo.markup.html.container.DojoSimpleContainer;
+import wicket.contrib.dojo.markup.html.container.IDojoContainer;
 import wicket.contrib.dojo.markup.html.container.page.DojoPageContainer;
 import wicket.contrib.dojo.markup.html.container.tab.DojoTabContainer;
 import wicket.markup.html.WebPage;
@@ -9,7 +11,12 @@ public class TabContainerSample extends WebPage {
 
 	public TabContainerSample() {
 		super();
-		DojoTabContainer container = new DojoTabContainer("tabContainer");
+		DojoTabContainer container = new DojoTabContainer("tabContainer"){
+			@Override
+			public void onSelectionChange(IDojoContainer selected, AjaxRequestTarget target) {
+				System.out.println(selected.getMarkupId());
+			}
+		};
 		add(container);
 		container.setHeight("500px");
 		container.add(new DojoSimpleContainer("tab1", "title1"));
