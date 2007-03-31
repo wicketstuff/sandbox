5b1a9d1274eafe4fe7627901a1cde595f7052350
Vincent Demay <vdemay@wicketstuff.org>
2007-03-31 11:04:44 +0000

    License + javadoc
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3/wicket-contrib-dojo@1868 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/wicket/contrib/dojo/TargetRefresherManager.java
+++ src/main/java/wicket/contrib/dojo/TargetRefresherManager.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package wicket.contrib.dojo;
 
 import java.util.HashMap;
@@ -10,6 +26,14 @@ import wicket.ajax.AjaxRequestTarget;
 import wicket.ajax.AjaxRequestTarget.IJavascriptResponse;
 import wicket.ajax.AjaxRequestTarget.IListener;
 
+/**
+ * A Manager to deal with {@link AjaxRequestTarget} and makeWidget in dojo.
+ * Only top level dojoComponents needs to be rerender during a response 
+ * to a Ajax Query
+ * 
+ * @author Vincent Demay
+ *
+ */
 public class TargetRefresherManager implements IListener
 {
 	private static TargetRefresherManager instance;
