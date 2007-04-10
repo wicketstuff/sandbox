c14434d4f2442c42d04686b99907bb270afbd66f
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-04-10 22:08:39 +0000

    Commit orgy today, let's rename to org.wicketstuff.dojo
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1914 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-dojo/src/main/java/org/wicketstuff/dojo/dojodnd/DojoDragContainerHandler.java
@@ -0,0 +1,49 @@
+package org.wicketstuff.dojo.dojodnd;
+
+import org.apache.wicket.IRequestTarget;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.wicketstuff.dojo.templates.DojoPackagedTextTemplate;
+import org.apache.wicket.markup.html.IHeaderResponse;
+
+/**
+ * <p>
+ * A Handler associated to {@link DojoDragContainer}
+ * </p>
+ * 
+ * @author Vincent Demay
+ *
+ */
+
+@SuppressWarnings("serial")
+public class DojoDragContainerHandler extends AbstractDojoDragContainerHandler
+{
+	
+	/* (non-Javadoc)
+	 * @see wicket.contrib.dojo.DojoAjaxHandler#renderHead(wicket.markup.html.IHeaderResponse)
+	 */
+	public void renderHead(IHeaderResponse response)
+	{
+		super.renderHead(response);
+
+		DojoDragContainer container = getDojoDragContainer();
+		
+		DojoPackagedTextTemplate template = new DojoPackagedTextTemplate(this.getClass(), "DojoDragContainerHandlerTemplate.js");
+		response.renderJavascript(template.asString(), template.getStaticKey());
+		
+		//DojoOnLoad only if not AjaxRequest
+		IRequestTarget target = RequestCycle.get().getRequestTarget();
+		if(!(target instanceof AjaxRequestTarget)){
+			response.renderJavascript("dojo.event.connect(dojo, \"loaded\", function() {initDrag('" + container.getMarkupId() + "','" + container.getDragPattern() + "')});\n", container.getMarkupId() + "onLoad" );
+		}
+		//else will be done by onComponentReRendered
+	}
+	
+	public void onComponentReRendered(AjaxRequestTarget ajaxTarget)
+	{
+		super.onComponentReRendered(ajaxTarget);
+		
+		DojoDragContainer container = getDojoDragContainer();
+		ajaxTarget.appendJavascript("initDrag('" + container.getMarkupId() + "','" + container.getDragPattern() + "')\n");
+	}
+}
