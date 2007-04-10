ee8c1e476813c49be8c2a95bef93b022660d1615
Vincent Demay <vdemay@wicketstuff.org>
2007-04-10 21:21:25 +0000

    http://wicketstuff.org/jira/browse/DOJO-27 : Switch to Java 1.5
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1908 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/DojoLocaleManager.java
+++ wicket-contrib-dojo/src/main/java/wicket/contrib/dojo/DojoLocaleManager.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package wicket.contrib.dojo;
 
 import java.util.HashSet;
@@ -7,12 +23,19 @@ import java.util.Set;
 
 import wicket.markup.html.IHeaderResponse;
 
+/**
+ * A Dojo locale manager to declare some locale on the client side.
+ * Using WCD without this manager leads dojo to run with the browser default locale.
+ * 
+ * @author vdemay
+ *
+ */
 public class DojoLocaleManager {
 	private static DojoLocaleManager manager;
-	private Set locales;
+	private Set<String> locales;
 	
 	private DojoLocaleManager(){
-		locales = new HashSet();
+		locales = new HashSet<String>();
 	}
 	
 	public static DojoLocaleManager getInstance(){
