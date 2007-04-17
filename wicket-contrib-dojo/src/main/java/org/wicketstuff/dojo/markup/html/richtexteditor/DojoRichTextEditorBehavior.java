2f4d93aaa94f7a5f522129d2f3924886dae2fc24
Bart <molenkampb@wicketstuff.org>
2007-04-17 06:56:42 +0000

    * Introduced AbstractDojoWidgetBehavior that can create widgets when first rendered and after rerendering (by ajax) * Fixed a bug in Dojo's RichText.js (couldn't create editor manually after dojo is loaded - e.g. rerender after ajax update) * Created some customizations to the DojoRichTextEditorBehavior, e.g. to load a custom toolbar.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicket-1.3@1935 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/org/wicketstuff/dojo/markup/html/richtexteditor/DojoRichTextEditorBehavior.java
+++ wicket-contrib-dojo/src/main/java/org/wicketstuff/dojo/markup/html/richtexteditor/DojoRichTextEditorBehavior.java
@@ -16,10 +16,11 @@
  */
 package org.wicketstuff.dojo.markup.html.richtexteditor;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.wicketstuff.dojo.AbstractRequireDojoBehavior;
-import org.wicketstuff.dojo.DojoIdConstants;
 import org.apache.wicket.markup.ComponentTag;
+import org.wicketstuff.dojo.AbstractDojoWidgetBehavior;
+import org.wicketstuff.dojo.DojoIdConstants;
 
 /**
  * Behavior that allows a TextArea to show as the Dojo Rich Text editor
@@ -27,9 +28,50 @@ import org.apache.wicket.markup.ComponentTag;
  * @author <a href="mailto:jbq@apache.org">Jean-Baptiste Quenot</a>
  */
 @SuppressWarnings("serial")
-public class DojoRichTextEditorBehavior extends AbstractRequireDojoBehavior {
+public class DojoRichTextEditorBehavior extends AbstractDojoWidgetBehavior {
+
+	public static final String DOJO_EDITOR2_TOOLBAR_URL = "toolbarTemplatePath";
+	public static final String DOJO_EDITOR2_HEIGHT = "height";
+	public static final String DOJO_EDITOR2_SHARE_TOOLBAR = "shareToolbar";
 	
-	@SuppressWarnings("unchecked")
+	private ResourceReference toolbarResourceReference;
+	private Integer height;
+	private Boolean shareToolbar;
+	
+	/**
+	 * Default constructor that sets all editor settings to null, which are the 
+	 * defaults.
+	 */
+	public DojoRichTextEditorBehavior() {
+		this.toolbarResourceReference = null;
+		this.height = null;
+		this.shareToolbar = null;
+	}
+	
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public Boolean getShareToolbar() {
+		return shareToolbar;
+	}
+
+	public void setShareToolbar(Boolean shareToolbar) {
+		this.shareToolbar = shareToolbar;
+	}
+
+	public ResourceReference getToolbarResourceReference() {
+		return toolbarResourceReference;
+	}
+
+	public void setToolbarResourceReference(ResourceReference toolbar) {
+		this.toolbarResourceReference = toolbar;
+	}
+
 	public void setRequire(RequireDojoLibs libs) {
 		libs.add("dojo.widget.Editor2");
 	}
@@ -38,10 +80,41 @@ public class DojoRichTextEditorBehavior extends AbstractRequireDojoBehavior {
 		// Do nothing
 	}
 
+	/* (non-Javadoc)
+	 * @see org.wicketstuff.dojo.AbstractDojoWidgetBehavior#getWidgetType()
+	 */
+	@Override
+	protected String getWidgetType() {
+		return DojoIdConstants.DOJO_TYPE_RICHTEXT;
+	}
+	
 	protected void onComponentTag(ComponentTag tag) {
 		if (! tag.getName().equals("textarea"))
 			throw new IllegalArgumentException("Dojo Rich Text Editor works with a <textarea>, found a <" + tag.getName() + ">");
-		tag.put(DojoIdConstants.DOJO_TYPE, DojoIdConstants.DOJO_TYPE_RICHTEXT);
+		//tag.put(DojoIdConstants.DOJO_TYPE, DojoIdConstants.DOJO_TYPE_RICHTEXT);
 		super.onComponentTag(tag);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.wicketstuff.dojo.AbstractDojoWidgetBehavior#getWidgetProperties()
+	 */
+	@Override
+	protected WidgetProperties getWidgetProperties() {
+		WidgetProperties props = new WidgetProperties();
+		
+		if (this.toolbarResourceReference != null) {
+			props.addProperty(DOJO_EDITOR2_TOOLBAR_URL, this.toolbarResourceReference);
+		}
+		
+		// + ", height: 100, shareToolbar: false }";
+		if (this.height != null) {
+			props.addProperty(DOJO_EDITOR2_HEIGHT, this.height);
+		}
+		
+		if (this.shareToolbar != null) {
+			props.addProperty(DOJO_EDITOR2_SHARE_TOOLBAR, this.shareToolbar);
+		}
+		
+		return props;
 	}
 }
