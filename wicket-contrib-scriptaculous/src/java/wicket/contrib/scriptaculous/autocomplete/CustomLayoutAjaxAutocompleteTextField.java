015329ce390d6e0ac23ecebc096ea17f9f0119f8
ryan sonnek <wireframe6464@wicketstuff.org>
2007-04-29 03:44:01 +0000

    backporting features from 2.0 branch
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2012 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/CustomLayoutAjaxAutocompleteTextField.java
@@ -0,0 +1,54 @@
+/*
+ * $Id: CustomLayoutAjaxAutocompleteTextField.java 648 2006-04-03 02:22:11 -0700
+ * (Mon, 03 Apr 2006) joco01 $ $Revision: 1420 $ $Date: 2006-04-03 02:22:11 -0700
+ * (Mon, 03 Apr 2006) $
+ *
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.scriptaculous.autocomplete;
+
+import org.apache.wicket.PageParameters;
+
+/**
+ * Autocomplete text field that allows for customized layout of autocomplete
+ * entries. A user defined <code>PageContribution</code> is used to display
+ * the autocomplete information.
+ *
+ * @author <a href="mailto:wireframe6464@users.sourceforge.net">Ryan Sonnek</a>
+ */
+public class CustomLayoutAjaxAutocompleteTextField extends AutocompleteTextFieldSupport
+{
+	private static final long serialVersionUID = 1L;
+	private CharSequence url;
+
+	public CustomLayoutAjaxAutocompleteTextField(String id,
+			Class page)
+	{
+		super(id);
+
+		PageParameters parameters = new PageParameters();
+		parameters.put("fieldName", this.getMarkupId());
+		this.url = urlFor(null, page, parameters);
+	}
+
+	protected String getAutocompleteType()
+	{
+		return "Ajax.Autocompleter";
+	}
+
+	protected String getThirdAutocompleteArgument()
+	{
+		return "" + url;
+	}
+}
