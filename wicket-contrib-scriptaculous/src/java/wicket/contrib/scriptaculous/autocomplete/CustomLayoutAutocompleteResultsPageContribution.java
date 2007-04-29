20191de9170c79be220a30793aa39ad3819c5db1
ryan sonnek <wireframe6464@wicketstuff.org>
2007-04-29 03:44:01 +0000

    backporting features from 2.0 branch
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2012 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/autocomplete/CustomLayoutAutocompleteResultsPageContribution.java
@@ -0,0 +1,22 @@
+package wicket.contrib.scriptaculous.autocomplete;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+
+public class CustomLayoutAutocompleteResultsPageContribution extends WebPage
+{
+
+	private static final long serialVersionUID = 1L;
+	private final String value;
+
+	public CustomLayoutAutocompleteResultsPageContribution(PageParameters parameters)
+	{
+		String field = parameters.getString("fieldName");
+		value = parameters.getString(field);
+	}
+
+	protected String getInputValue()
+	{
+		return value;
+	}
+}
