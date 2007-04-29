0d0a00bcaee2429e781c97c719fdd7acd797df5b
ryan sonnek <wireframe6464@wicketstuff.org>
2007-04-29 14:59:34 +0000

    backporting features from 2.0 branch
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2017 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/dragdrop/DraggableImage.java
+++ wicket-contrib-scriptaculous/src/java/wicket/contrib/scriptaculous/dragdrop/DraggableImage.java
@@ -1,31 +1,57 @@
 package wicket.contrib.scriptaculous.dragdrop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.image.Image;
 
+import wicket.contrib.scriptaculous.JavascriptBuilder;
 import wicket.contrib.scriptaculous.ScriptaculousAjaxBehavior;
 
-public class DraggableImage extends Image {
+public abstract class DraggableImage extends Image
+{
+	private static final long serialVersionUID = 1L;
 
-    private final String id;
+	public DraggableImage(String wicketId, String img)
+	{
+		super(wicketId, img);
 
-    public DraggableImage(String wicketId, String id, String img) {
-        super(wicketId, img);
-        this.id = id;
-
+		setOutputMarkupId(true);
 		add(ScriptaculousAjaxBehavior.newJavascriptBindingBehavior());
-    }
+	}
 
-    protected void onRender(MarkupStream markupStream) {
-        super.onRender(markupStream);
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+		tag.put("class", getStyleClass());
+	}
 
-        getResponse().write("\n<script type=\"text/javascript\">new Draggable('" + id + "', {revert:true})</script>");
-    }
+	/**
+	 * define the css style used to define this component. used by the draggable
+	 * target to declare what it accepts.
+	 * 
+	 * @see DraggableTarget#accepts(DraggableImage)
+	 * @return
+	 */
+	protected abstract String getStyleClass();
 
-    protected void onComponentTag(ComponentTag tag) {
-        super.onComponentTag(tag);
-        tag.put("id", id);
-        tag.put("class", getId());
-    }
+	protected void onRender(MarkupStream markupStream)
+	{
+		super.onRender(markupStream);
+
+		JavascriptBuilder builder = new JavascriptBuilder();
+		Map options = new HashMap()
+		{
+			private static final long serialVersionUID = 1L;
+			{
+				put("revert", Boolean.TRUE);
+			}
+		};
+		builder.addLine("new Draggable('" + getMarkupId() + "', ");
+		builder.addOptions(options);
+		builder.addLine(");");
+		getResponse().write(builder.buildScriptTagString());
+	}
 }
