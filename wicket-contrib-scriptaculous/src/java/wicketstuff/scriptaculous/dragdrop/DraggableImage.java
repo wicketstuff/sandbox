0cfdb58c6a5daecee828ed5de990292da13c6cfb
ryan sonnek <wireframe6464@wicketstuff.org>
2007-04-29 16:29:49 +0000

    rename artifact to wicketstuff-scriptaculous and repackage classes to reflect change
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2023 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-scriptaculous/src/java/wicketstuff/scriptaculous/dragdrop/DraggableImage.java
@@ -0,0 +1,57 @@
+package wicketstuff.scriptaculous.dragdrop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.MarkupStream;
+import org.apache.wicket.markup.html.image.Image;
+
+import wicketstuff.scriptaculous.JavascriptBuilder;
+import wicketstuff.scriptaculous.ScriptaculousAjaxBehavior;
+
+public abstract class DraggableImage extends Image
+{
+	private static final long serialVersionUID = 1L;
+
+	public DraggableImage(String wicketId, String img)
+	{
+		super(wicketId, img);
+
+		setOutputMarkupId(true);
+		add(ScriptaculousAjaxBehavior.newJavascriptBindingBehavior());
+	}
+
+	protected void onComponentTag(ComponentTag tag)
+	{
+		super.onComponentTag(tag);
+		tag.put("class", getStyleClass());
+	}
+
+	/**
+	 * define the css style used to define this component. used by the draggable
+	 * target to declare what it accepts.
+	 * 
+	 * @see DraggableTarget#accepts(DraggableImage)
+	 * @return
+	 */
+	protected abstract String getStyleClass();
+
+	protected void onRender(MarkupStream markupStream)
+	{
+		super.onRender(markupStream);
+
+		JavascriptBuilder builder = new JavascriptBuilder();
+		Map options = new HashMap()
+		{
+			private static final long serialVersionUID = 1L;
+			{
+				put("revert", Boolean.TRUE);
+			}
+		};
+		builder.addLine("new Draggable('" + getMarkupId() + "', ");
+		builder.addOptions(options);
+		builder.addLine(");");
+		getResponse().write(builder.buildScriptTagString());
+	}
+}
---
+++ wicket-contrib-scriptaculous/src/java/wicketstuff/scriptaculous/sortable/SortableContainer.java
@@ -0,0 +1,109 @@
+package wicketstuff.scriptaculous.sortable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.AbstractAjaxBehavior;
+import org.apache.wicket.markup.MarkupStream;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.resource.IResourceStream;
+
+import wicketstuff.scriptaculous.JavascriptBuilder;
+import wicketstuff.scriptaculous.ScriptaculousAjaxBehavior;
+
+/**
+ * 
+ * @see http://wiki.script.aculo.us/scriptaculous/show/Sortable.create
+ * @author <a href="mailto:wireframe6464@sf.net">Ryan Sonnek</a>
+ */
+public abstract class SortableContainer extends WebMarkupContainer
+{
+	private AbstractAjaxBehavior onUpdateBehavior;
+	private Map options = new HashMap();
+	private final List items;
+
+	public SortableContainer(String id, String itemId, final List items)
+	{
+		super(id);
+		this.items = items;
+
+		setOutputMarkupId(true);
+
+		onUpdateBehavior = new SortableContainerBehavior();
+		add(onUpdateBehavior);
+
+		add(new ListView(itemId, items)
+		{
+			private static final long serialVersionUID = 1L;
+
+			protected void populateItem(ListItem item)
+			{
+				item.add(new AttributeModifier("id", true, new Model(getMarkupId() + "_"
+						+ item.getIndex())));
+
+				populateItemInternal(item);
+			}
+		});
+
+		options.put("onUpdate", new JavascriptBuilder.JavascriptFunction(
+				"function(element) { wicketAjaxGet('" + onUpdateBehavior.getCallbackUrl()
+						+ "&' + Sortable.serialize(element)); }"));
+	}
+
+	public void setConstraintVertical()
+	{
+		options.put("constraint", "vertical");
+	}
+	
+	public void setConstraintHorizontal() 
+	{
+		options.put("constraint", "horizontal");
+	}
+
+	protected abstract void populateItemInternal(ListItem item);
+
+	protected void onRender(MarkupStream markupStream)
+	{
+		super.onRender(markupStream);
+
+		JavascriptBuilder builder = new JavascriptBuilder();
+		builder.addLine("Sortable.create('" + getMarkupId() + "', ");
+		builder.addOptions(options);
+		builder.addLine(");");
+		getResponse().write(builder.buildScriptTagString());
+	}
+	
+	private class SortableContainerBehavior extends ScriptaculousAjaxBehavior
+	{
+		private static final long serialVersionUID = 1L;
+
+		protected IResourceStream getResponse() {
+			AjaxRequestTarget target = new AjaxRequestTarget();
+			String[] parameters = getRequestCycle().getRequest().getParameters(
+					getMarkupId() + "[]");
+
+			if (parameters != null)
+			{
+				List originalItems = new ArrayList(items);
+				for (int index = 0; index < items.size(); index++)
+				{
+					int newIndex = Integer.parseInt(parameters[index]);
+					items.set(index, originalItems.get(newIndex));
+				}
+			}
+
+			getRequestCycle().setRequestTarget(target);
+
+			target.addComponent(getComponent());
+			return null;
+		}
+	}
+
+}
