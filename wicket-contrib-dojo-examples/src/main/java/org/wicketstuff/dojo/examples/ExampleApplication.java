c99024017feaf56925552dba7b24c7c0c49d27d0
Vincent Demay <vdemay@wicketstuff.org>
2007-05-02 15:59:05 +0000

    Fix samples
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2046 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo-examples/src/main/java/org/wicketstuff/dojo/examples/ExampleApplication.java
+++ wicket-contrib-dojo-examples/src/main/java/org/wicketstuff/dojo/examples/ExampleApplication.java
@@ -1,7 +1,5 @@
 package org.wicketstuff.dojo.examples;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.ISessionFactory;
 import org.apache.wicket.Request;
 import org.apache.wicket.Response;
@@ -14,9 +12,6 @@ import org.wicketstuff.dojo.skin.windows.WindowsDojoSkin;
  * Runs the ExampleApplication when invoked from command line.
  */
 public class ExampleApplication extends WebApplication {
-	/** Logging */
-	private static final Log log = LogFactory.getLog(ExampleApplication.class);
-
 	/**
 	 * Constructor
 	 */
