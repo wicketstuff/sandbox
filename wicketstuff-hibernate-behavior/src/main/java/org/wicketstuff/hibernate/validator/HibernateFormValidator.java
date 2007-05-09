02934a195529205cf6e460e3f026e75b2213e575
Bruno Borges <bruno_borges@wicketstuff.org>
2007-05-09 19:17:47 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2092 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-hibernate-behavior/src/main/java/org/wicketstuff/hibernate/validator/HibernateFormValidator.java
@@ -0,0 +1,37 @@
+package org.wicketstuff.hibernate.validator;
+
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.validation.IFormValidator;
+import org.hibernate.validator.ClassValidator;
+import org.hibernate.validator.InvalidValue;
+
+/**
+ * <p>Validates Model's object over Hibernate Validator framework</p>
+ * 
+ * @author miojo
+ */
+public class HibernateFormValidator implements IFormValidator {
+
+	private ClassValidator validator;
+
+	@SuppressWarnings("unchecked")
+	public HibernateFormValidator(Class name) {
+		validator = new ClassValidator(name);
+	}
+
+	public FormComponent[] getDependentFormComponents() {
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void validate(Form form) {
+		Object object = form.getModelObject();
+		InvalidValue[] invalidValues = validator.getInvalidValues(object);
+
+		for (InvalidValue iv : invalidValues) {
+			form.error(iv.getMessage());
+		}
+	}
+
+}
