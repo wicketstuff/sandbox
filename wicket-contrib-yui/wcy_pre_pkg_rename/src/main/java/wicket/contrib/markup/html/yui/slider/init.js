4e20be928b954aa797eb3809d4f44f94a5578a1f
Jim McLaughlin <jimmclaughlin@wicketstuff.org>
2007-05-09 04:01:36 +0000

    revert borked package rename and try again
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2084 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/wcy_pre_pkg_rename/src/main/java/wicket/contrib/markup/html/yui/slider/init.js
+++
@@ -1,7 +0,0 @@
-var ${javaScriptId};
-function init${javaScriptId}() {
-   ${javaScriptId} = YAHOO.widget.Slider.getHorizSlider("${backGroundElementId}", "${imageElementId}", ${leftUp}, ${rightDown}, ${tick});
-   ${javaScriptId}.onChange = function(offsetFromStart) {
-		document.getElementById("${formElementId}").value = offsetFromStart;
-	}
-}
