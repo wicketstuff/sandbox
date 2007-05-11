6ba8575a27c0a8beba9ad810c4d59612a78a7f77
ryan sonnek <wireframe6464@wicketstuff.org>
2007-05-11 21:11:10 +0000

    convert DraggableImage to a DraggableBehavior
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2139 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/dragdrop/DragDropExamplePage.java
+++ wicket-contrib-scriptaculous-examples/src/java/wicket/contrib/scriptaculous/examples/dragdrop/DragDropExamplePage.java
@@ -1,80 +1,40 @@
 package wicket.contrib.scriptaculous.examples.dragdrop;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.wicketstuff.scriptaculous.Indicator;
-import org.wicketstuff.scriptaculous.dragdrop.DraggableImage;
+import org.wicketstuff.scriptaculous.dragdrop.DraggableBehavior;
 import org.wicketstuff.scriptaculous.dragdrop.DraggableTarget;
 
 public class DragDropExamplePage extends WebPage
 {
-
 	public DragDropExamplePage()
 	{
 		Indicator indicator = new Indicator();
 		final DraggableTarget cart = new DraggableTarget("cart") {
-
 			protected void onDrop(String input, AjaxRequestTarget target)
 			{
-				System.out.println("HERE WE ARE!");
-				
+				System.out.println("Input: " + input + " was dropped on the DraggableTarget");
 			}
 		};
 
-		List results = new ArrayList();
-		results.add(new CustomResultObject("product_123", "product.png"));
-		results.add(new CustomResultObject("product_456", "product1.png"));
-		add(new ListView("entry", results)
-		{
-
-			protected void populateItem(ListItem item)
+		DraggableBehavior draggableBehavior = new DraggableBehavior() {
+			public String getDraggableClassName()
 			{
-				CustomResultObject result = (CustomResultObject) item.getModelObject();
-
-				DraggableImage image = new DraggableImage(result.getId(), result.getImage()) {
-
-					protected String getStyleClass()
-					{
-						return "product";
-					}};
-				cart.accepts(image);
-
-				item.add(image);
+				return "draggable";
 			}
-		});
+		};
+		WebMarkupContainer product1 = new WebMarkupContainer("product1");
+		product1.add(draggableBehavior);
+
+		WebMarkupContainer product2 = new WebMarkupContainer("product2");
+		product1.add(draggableBehavior);
+
+		cart.accepts(product1);
+		cart.accepts(product2);
 
 		add(cart);
 		add(indicator);
 	}
-
-	public class CustomResultObject
-	{
-
-		private final String id;
-
-		private final String image;
-
-		public CustomResultObject(String id, String image)
-		{
-			this.id = id;
-			this.image = image;
-		}
-
-		public String getId()
-		{
-			return id;
-		}
-
-		public String getImage()
-		{
-			return image;
-		}
-
-	}
-
 }
