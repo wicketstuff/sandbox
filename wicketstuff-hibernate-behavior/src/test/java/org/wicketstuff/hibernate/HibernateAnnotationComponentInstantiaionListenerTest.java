f356eee0846d13e2512d8e2eccb19cf1bd7e44a9
ryan sonnek <wireframe6464@wicketstuff.org>
2007-05-11 04:29:36 +0000

    test
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2126 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-hibernate-behavior/src/test/java/org/wicketstuff/hibernate/HibernateAnnotationComponentInstantiaionListenerTest.java
@@ -0,0 +1,30 @@
+package org.wicketstuff.hibernate;
+
+import junit.framework.TestCase;
+
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.tester.WicketTester;
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+
+public class HibernateAnnotationComponentInstantiaionListenerTest extends TestCase {
+
+	public void setUp() {
+		WicketTester tester = new WicketTester();
+		tester.getApplication().addComponentInstantiationListener(new HibernateAnnotationComponentInstantiaionListener());
+	}
+
+	public void testNotNullAnnotationUpdatesComponentToBeRequired() {
+		TextField component = new TextField("test", new HibernateAnnotationPropertyModel(new MyObject(), "id"));
+		
+		assertTrue(component.isRequired());
+	}
+
+	public class MyObject {
+		@NotNull
+		private String id;
+		
+		@Length(max=50)
+		private String name;
+	}
+}
