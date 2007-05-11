c16edc9913792083cdfc1d30c39fd8a3709927ea
Xavier Hanin <xhanin@wicketstuff.org>
2007-05-11 10:19:40 +0000

    update examples to latest changes in wc-push
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2131 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-push-examples/src/main/java/org/wicketstuff/push/examples/application/ExampleApplication.java
+++ wicket-contrib-push-examples/src/main/java/org/wicketstuff/push/examples/application/ExampleApplication.java
@@ -1,22 +1,32 @@
 package org.wicketstuff.push.examples.application;
 
+import java.io.Serializable;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.util.time.Duration;
+import org.wicketstuff.push.IChannelService;
+import org.wicketstuff.push.cometd.CometdService;
 import org.wicketstuff.push.examples.pages.Index;
+import org.wicketstuff.push.timer.TimerChannelService;
 
 /**
  * Runs the ExampleApplication when invoked from command line.
  */
-public class ExampleApplication extends WebApplication {
+public class ExampleApplication extends WebApplication implements Serializable {
 	/** Logging */
 	private static final Log log = LogFactory.getLog(ExampleApplication.class);
+	
+	private IChannelService cometdService;
+	private IChannelService timerChannelService;
 
 	/**
 	 * Constructor
 	 */
 	public ExampleApplication() {
-		
+		cometdService = new CometdService(this);
+		timerChannelService = new TimerChannelService(Duration.seconds(2));
 	}
 
 	/**
@@ -24,5 +34,13 @@ public class ExampleApplication extends WebApplication {
 	 */
 	public Class getHomePage() {
 		return Index.class;
+	}
+
+	public IChannelService getCometdService() {
+		return cometdService;
+	}
+
+	public IChannelService getTimerChannelService() {
+		return timerChannelService;
 	}
 }
