0d45133858c5f1e019bea17f112cc8b8a70c5321
Xavier Hanin <xhanin@wicketstuff.org>
2007-05-12 15:01:47 +0000

    - introduce IPushService for one way communication only facility - refactor TimerChannelBehavior and TimerChannelService to fix some bugs and support push facility - introduce TimerPushService as single implementation of IPushService for the moment
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2143 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-push/src/main/java/org/wicketstuff/push/timer/TimerPushService.java
@@ -0,0 +1,44 @@
+package org.wicketstuff.push.timer;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.util.time.Duration;
+import org.wicketstuff.push.IPushService;
+import org.wicketstuff.push.IPushTarget;
+
+/**
+ * An implementaiton of IPushService based on a polling mechanism.
+ * 
+ * This class is thread safe, and can be safely reused.
+ * 
+ * @author Xavier Hanin
+ */
+public class TimerPushService implements IPushService {
+	private final Duration duration;
+
+	/**
+	 * Constructs a TimerPushService with the given polling interval.
+	 * 
+	 * @param duration the polling interval, must not be null
+	 */
+	public TimerPushService(Duration duration) {
+		if (duration == null) {
+			throw new IllegalArgumentException("duration must not be null");
+		}
+		this.duration = duration;
+	}
+
+	public IPushTarget installPush(Component component) {
+		TimerChannelBehavior timerChannelBehavior = new TimerChannelBehavior(duration);
+		component.add(timerChannelBehavior);
+		return timerChannelBehavior.newPushTarget();
+	}
+
+	/**
+	 * Returns the polling interval
+	 * @return the polling interval
+	 */
+	public Duration getDuration() {
+		return duration;
+	}
+
+}
