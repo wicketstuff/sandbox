e0da735b0ad88f58853e23d49c78130c1c4c824f
victori <victori@wicketstuff.org>
2007-05-27 18:34:15 +0000

    changed from static class ints to enums for mfxdialogs
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2228 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXDialogTypes.java
+++
@@ -1,8 +0,0 @@
-package wicket.contrib.mootools.plugins;
-
-public class MFXDialogTypes {
-	public final static int CONFIRMATION = 0;
-	public final static int MESSAGE = 1;
-	public final static int MESSAGE_WITHOUT_CLOSE = 2;
-	public final static int QUESTION_WITH_INPUT = 3;
-}
