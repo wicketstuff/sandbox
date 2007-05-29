5b4dd65a6d0ebe567925c5eada622ff441bd8a31
Jim McLaughlin <jimmclaughlin@wicketstuff.org>
2007-05-29 16:38:23 +0000

    pickup modules with -beta or -experimental suffix
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2249 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/org/wicketstuff/yui/YuiHeaderContributor.java
+++ wicket-contrib-yui/src/main/java/org/wicketstuff/yui/YuiHeaderContributor.java
@@ -1,5 +1,6 @@
 package org.wicketstuff.yui;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,9 +14,14 @@ import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 import org.apache.wicket.markup.html.resources.JavascriptResourceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class YuiHeaderContributor extends AbstractHeaderContributor
 {
+    
+    private static final Logger log = LoggerFactory.getLogger(YuiHeaderContributor.class);
+    
     static final String DEFAULT_YUI_BUILD = "2.2.2";
     static final String YUI_BUILD_ROOT = "inc";
     
@@ -24,6 +30,7 @@ public class YuiHeaderContributor extends AbstractHeaderContributor
     static final YuiDependencyResolver dependencyResolver = new YuiDependencyResolver();
     
     List<YuiModuleHeaderContributor> contributors = new ArrayList<YuiModuleHeaderContributor>();
+    
 
     @Override
     public IHeaderContributor[] getHeaderContributors()
@@ -83,39 +90,63 @@ public class YuiHeaderContributor extends AbstractHeaderContributor
         }
 
 
+        private String getRealModuleName(String path, String module) {
+            final String fullPath = path + "/" + module + "/";
+            URL url = getClass().getResource( fullPath + module + ".js");
+            if(null != url) {
+                return module;
+            }
+            
+            url = getClass().getResource(fullPath + module + "-beta.js");
+            
+            if(null != url) {
+                return module + "-beta";
+            }
+            
+            url = getClass().getResource(fullPath + module + "-experimental.js");
+            
+            if(null != url) {
+                return module + "experimental";
+            }
+            
+            return null;
+        }
 
         public void renderHead(IHeaderResponse response)
         {
-            final String path = YUI_BUILD_ROOT + "/" + build + "/" + name + "/"
-                + name + ((debug)?"-debug.js":".js");
+            final String buildPath = YUI_BUILD_ROOT + "/" + build;
             
-            final ResourceReference moduleScript;
+            final String realName = getRealModuleName(buildPath, name);
             
-            if(YuiHeaderContributor.this.moduleCache.containsKey(path)) {
-                moduleScript = YuiHeaderContributor.this.moduleCache.get(path);
+            
+            if (null != realName) {
+                final String path = buildPath + "/" + name + "/" + realName + ((debug) ? "-debug.js" : ".js");
+                final ResourceReference moduleScript;
+                if (YuiHeaderContributor.this.moduleCache.containsKey(path)) {
+                    moduleScript = YuiHeaderContributor.this.moduleCache.get(path);
+                } else {
+                    if (debug) {
+                        moduleScript = new ResourceReference(YuiHeaderContributor.class, path);
+                    } else {
+                        moduleScript = new JavascriptResourceReference(YuiHeaderContributor.class, path);
+                    }
+                    YuiHeaderContributor.this.moduleCache.put(path, moduleScript);
+                }
+                response.renderJavascriptReference(moduleScript);
+                if (dependencyResolver.hasCssAsset(name, YUI_BUILD_ROOT + "/" + build)) {
+                    final String assetPath = YUI_BUILD_ROOT + "/" + build + "/" + name + "/assets/" + name + ".css";
+                    final ResourceReference assetRef;
+                    if (YuiHeaderContributor.this.moduleCache.containsKey(assetPath)) {
+                        assetRef = YuiHeaderContributor.this.moduleCache.get(assetPath);
+                    } else {
+                        assetRef = new CompressedResourceReference(YuiHeaderContributor.class, assetPath);
+                        YuiHeaderContributor.this.moduleCache.put(assetPath, assetRef);
+                    }
+
+                    response.renderCSSReference(assetRef, "screen");
+                }
             } else {
-                if(debug) {
-                    moduleScript = new ResourceReference(YuiHeaderContributor.class, path);
-                } else {
-                    moduleScript = new JavascriptResourceReference(YuiHeaderContributor.class, path);
-                }
-                YuiHeaderContributor.this.moduleCache.put(path, moduleScript);
-            }
-            
-            response.renderJavascriptReference(moduleScript);
-            
-            if(dependencyResolver.hasCssAsset(name, YUI_BUILD_ROOT + "/" + build)) {
-                final String assetPath = YUI_BUILD_ROOT + "/" + build + "/" + name + "/assets/" 
-                    + name + ".css";
-                final ResourceReference assetRef;
-                if(YuiHeaderContributor.this.moduleCache.containsKey(assetPath)) {
-                    assetRef = YuiHeaderContributor.this.moduleCache.get(assetPath);
-                } else {
-                    assetRef = new CompressedResourceReference(YuiHeaderContributor.class, assetPath);
-                    YuiHeaderContributor.this.moduleCache.put(assetPath, assetRef);
-                }
-                
-                response.renderCSSReference(assetRef, "screen");
+                log.error("Unable to find realName for Yui Module " + name);
             }
             
         }
