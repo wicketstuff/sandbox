ddc26608c3a31e61db76cb7664c5274ad7f6faf9
Vincent Demay <vdemay@wicketstuff.org>
2007-06-13 16:49:11 +0000

    use .html extention for DatePicker
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2380 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-dojo/src/main/java/org/wicketstuff/dojo/dojo-0.4/src/widget/DatePicker.js
+++ wicket-contrib-dojo/src/main/java/org/wicketstuff/dojo/dojo-0.4/src/widget/DatePicker.js
@@ -87,7 +87,7 @@ dojo.widget.defineWidget(
 			currentDate: "currentDate",
 			selectedDate: "selectedItem"
 		},
-		templatePath:  dojo.uri.dojoUri("src/widget/templates/DatePicker.htm"),
+		templatePath:  dojo.uri.dojoUri("src/widget/templates/DatePicker.html"),
 		templateCssPath:  dojo.uri.dojoUri("src/widget/templates/DatePicker.css"),
 
 		postMixInProperties: function(){
