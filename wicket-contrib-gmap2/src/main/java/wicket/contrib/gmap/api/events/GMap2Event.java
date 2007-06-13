ac29d53ee7919a7032adddc8c4090bd0126260da
Martin Funk <funkattack@wicketstuff.org>
2007-06-13 14:19:23 +0000

    cleanup and comments
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2379 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/api/events/GMap2Event.java
@@ -0,0 +1,10 @@
+package wicket.contrib.gmap.api.events;
+
+/**
+ * Represents an Google Maps API's GMap2 events. <a
+ * href="http://www.google.com/apis/maps/documentation/reference.html#GMap2">GMap2</a>
+ * 
+ */
+public class GMap2Event
+{
+}
