3659c7608ac913aa9e80da465bf3a96704d2ee22
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-06-16 08:18:55 +0000

    Use DojoLayoutContainer to layout the page (but does not work!)
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2408 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/SequencePage.java
+++ pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/SequencePage.java
@@ -1,6 +1,9 @@
 package org.wicketstuff.pickwick.frontend.pages;
 
 import org.apache.wicket.PageParameters;
+import org.wicketstuff.dojo.markup.html.container.DojoSimpleContainer;
+import org.wicketstuff.dojo.markup.html.container.layout.DojoLayoutContainer;
+import org.wicketstuff.dojo.markup.html.container.layout.DojoLayoutContainer.Position;
 import org.wicketstuff.pickwick.frontend.panel.FolderTreePanel;
 import org.wicketstuff.pickwick.frontend.panel.SequenceGridPanel;
 
@@ -15,7 +18,16 @@ public class SequencePage extends FrontendBasePage {
 		if (uri == null) {
 			uri = "";
 		}
-		add(new SequenceGridPanel("thumbnails", uri));
-		add(new FolderTreePanel("treePanel"));
+		DojoLayoutContainer layout;
+		add(layout = new DojoLayoutContainer("mainArea"));
+		layout.setHeight("100%");
+		DojoSimpleContainer s1 = new DojoSimpleContainer("thumbnails");
+		s1.add(new SequenceGridPanel("thumbnails", uri));
+		layout.add(s1, Position.Client);
+		s1.setHeight("100%");
+		DojoSimpleContainer s2 = new DojoSimpleContainer("treePanel");
+		s2.add(new FolderTreePanel("treePanel"));
+		s2.setHeight("100%");
+		layout.add(s2, Position.Left);
 	}
 }
