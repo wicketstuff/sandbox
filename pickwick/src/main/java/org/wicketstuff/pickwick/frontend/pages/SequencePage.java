ff370a6840445622573661ba6ee28bf3c580cb38
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-06-16 13:08:03 +0000

    Proper layout with Dojo
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2412 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/SequencePage.java
+++ pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/SequencePage.java
@@ -20,14 +20,14 @@ public class SequencePage extends FrontendBasePage {
 		}
 		DojoLayoutContainer layout;
 		add(layout = new DojoLayoutContainer("mainArea"));
-		layout.setHeight("100%");
+		layout.setHeight("550px");
+		layout.setWidth("100%");
 		DojoSimpleContainer s1 = new DojoSimpleContainer("thumbnails");
 		s1.add(new SequenceGridPanel("thumbnails", uri));
 		layout.add(s1, Position.Client);
-		s1.setHeight("100%");
 		DojoSimpleContainer s2 = new DojoSimpleContainer("treePanel");
 		s2.add(new FolderTreePanel("treePanel"));
-		s2.setHeight("100%");
+		s2.setWidth("20%");
 		layout.add(s2, Position.Left);
 	}
 }
