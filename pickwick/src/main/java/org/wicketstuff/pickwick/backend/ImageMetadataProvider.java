647f60d95db04cad37662f50bb99aa622f98b089
Vincent Demay <vdemay@wicketstuff.org>
2007-06-21 08:27:06 +0000

    Add a physical image properties reader and page
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2460 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ pickwick/src/main/java/org/wicketstuff/pickwick/backend/ImageMetadataProvider.java
@@ -0,0 +1,15 @@
+package org.wicketstuff.pickwick.backend;
+
+
+/**
+ * A provider giving metadata from a Image
+ * @author Vincent Demay
+ *
+ */
+public class ImageMetadataProvider {
+	Settings settings;
+	
+	public ImageMetadataProvider(Settings settings) {
+		this.settings = settings;
+	}
+}
