725d94c226aeaa3abc38645c70b6dcf7ad40859b
ryan sonnek <wireframe6464@wicketstuff.org>
2007-06-22 19:49:18 +0000

    fix application wide behavior by using IComponentOnBeforeRenderListener
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2471 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-hibernate-behavior/src/main/java/org/wicketstuff/hibernate/annotation/HibernateAnnotationComponentConfigurator.java
+++ wicketstuff-hibernate-behavior/src/main/java/org/wicketstuff/hibernate/annotation/HibernateAnnotationComponentConfigurator.java
@@ -1,4 +1,4 @@
-package org.wicketstuff.hibernate;
+package org.wicketstuff.hibernate.annotation;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
+import org.apache.wicket.application.IComponentOnBeforeRenderListener;
+import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -20,21 +22,47 @@ import org.slf4j.LoggerFactory;
 /**
  * Configure a wicket component based on hibernate annotations.
  * <p>
- * Inspects the Model of a FormComponent and configures the component according 
- * to the declared Hibernate Annotations used on the model object.  This means the 
- * Component's Model <em>must</em> be known when {@link #configure(Component) configuring} 
+ * Inspects the Model of a FormComponent and configures the component according
+ * to the declared Hibernate Annotations used on the model object.  This means the
+ * Component's Model <em>must</em> be known when {@link #configure(Component) configuring}
  * a Component.
  * </p>
- * 
+ *
  * <p>
- * This object is <em>stateless</em>, and the same instance can be reused to configure 
- * multiple components.
+ * This object can be used as a behavior to configure a single component.
+ * NOTE: this object is <em>stateless</em>, and the same instance can be reused to
+ * configure multiple components.
  * </p>
- * 
+ * <pre>
+ * public class MyWebPage extends WebPage {
+ *   public MyWebPage() {
+ *     TextField name = new TextField("id", new PropertyModel(user, "name");
+ *     name.addBehavior(new HibernateAnnotationComponentConfigurator());
+ *
+ *     add(name);
+ *   }
+ * }
+ * </pre>
+ *
+ * <p>
+ * Can also use this object as a component listener that will automatically configure <em>all</em>
+ * form components based on hibernate annotations. This helps ensure that an entire application
+ * respects  annotations without adding custom validators or behaviors to each form component.
+ * </p>
+ * <pre>
+ * public class MyApplication extends WebApplication {
+ *   public void init() {
+ *     addComponentInstantiationListener(new HibernateAnnotationComponentConfigurator());
+ *   }
+ * }
+ * </pre>
+ *
  */
-public class HibernateAnnotationComponentConfigurator {
+@SuppressWarnings("serial")
+public class HibernateAnnotationComponentConfigurator extends AbstractBehavior implements IComponentOnBeforeRenderListener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(HibernateAnnotationComponentConfigurator.class);
-	
+
+	@SuppressWarnings("unchecked")
 	private static Map configs = new HashMap() {{
 		put(NotNull.class, new HibernateAnnotationConfig() {
 			public void onAnnotatedComponent(Annotation annotation, FormComponent component) {
@@ -50,7 +78,18 @@ public class HibernateAnnotationComponentConfigurator {
 		});
 	}};
 
-	public void configure(Component component) {
+	@Override
+	public void bind(Component component) {
+		super.bind(component);
+
+		configure(component);
+	}
+
+	public void onBeforeRender(Component component) {
+		configure(component);
+	}
+
+	void configure(Component component) {
 		if (!isApplicableFor(component)) {
 			return;
 		}
@@ -89,6 +128,9 @@ public class HibernateAnnotationComponentConfigurator {
 		return true;
 	}
 
+	/**
+	 * simple interface to abstract performing work for a specific annotation.
+	 */
 	private static interface HibernateAnnotationConfig {
 		void onAnnotatedComponent(Annotation annotation, FormComponent component);
 	}
