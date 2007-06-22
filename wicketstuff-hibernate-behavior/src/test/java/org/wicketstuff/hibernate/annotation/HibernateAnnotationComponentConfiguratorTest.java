c2707371ab1570a8e93f1d22b55d289be49a538a
ryan sonnek <wireframe6464@wicketstuff.org>
2007-06-22 19:50:08 +0000

    fix application wide behavior by using IComponentOnBeforeRenderListener
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2472 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-hibernate-behavior/src/test/java/org/wicketstuff/hibernate/annotation/HibernateAnnotationComponentConfiguratorTest.java
@@ -0,0 +1,43 @@
+package org.wicketstuff.hibernate.annotation;
+
+import junit.framework.TestCase;
+
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.tester.WicketTester;
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+
+public class HibernateAnnotationComponentConfiguratorTest extends TestCase {
+	private WicketTester tester;
+
+	public void setUp() {
+		tester = new WicketTester();
+	}
+
+	public void testNotNullAnnotationUpdatesComponentToBeRequired() {
+		HibernateAnnotationComponentConfigurator configurator = new HibernateAnnotationComponentConfigurator();
+
+		TextField textField = new TextField("id", new PropertyModel(new MyObject(), "id"));
+		configurator.configure(textField);
+
+		assertTrue(textField.isRequired());
+	}
+
+
+	public void testComponentListenerConfiguresComponent() {
+		tester.getApplication().addComponentOnBeforeRenderListener(new HibernateAnnotationComponentConfigurator());
+		TestPage page = (TestPage) tester.startPage(TestPage.class);
+
+		assertTrue(page.getIdField().isRequired());
+	}
+
+
+	public class MyObject {
+		@NotNull
+		private String id;
+
+		@Length(max=50)
+		private String name;
+	}
+}
