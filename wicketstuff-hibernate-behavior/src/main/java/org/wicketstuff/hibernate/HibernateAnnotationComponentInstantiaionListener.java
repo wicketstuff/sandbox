bdc25d312969bdc8b9552d0ab20f2ff2aac290cb
ryan sonnek <wireframe6464@wicketstuff.org>
2007-06-22 03:28:26 +0000

    update javadocs
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2463 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-hibernate-behavior/src/main/java/org/wicketstuff/hibernate/HibernateAnnotationComponentInstantiaionListener.java
+++ wicketstuff-hibernate-behavior/src/main/java/org/wicketstuff/hibernate/HibernateAnnotationComponentInstantiaionListener.java
@@ -5,11 +5,24 @@ import org.apache.wicket.application.IComponentInstantiationListener;
 import org.wicketstuff.hibernate.behavior.HibernateAnnotationBehavior;
 
 /**
- * Component listener that will automatically configure a form component
- * based on it's hibernate annotations.
+ * Component listener that will automatically configure all form components based on hibernate annotations.
  *
+ * <p>
  * This component helps ensure that an entire application respects the
  * annotations without adding custom validators or behaviors to each form component.
+ * </p>
+ *
+ * <p>
+ * Unfortunately, the current implementation is incomplete.  The component does not yet 
+ * have a model when the {@link #onInstantiation(Component) entry point} is called.  
+ * The {@link HibernateAnnotationBehavior behavior} that is attached is then executed 
+ * before the Model is bound to the Component, which causes the annotations to not be 
+ * found.
+ * </p>
+ * 
+ * <p>
+ * There needs to be work done in the wicket core to allow for this kind of callback.
+ * </p>
  *
  * @author rsonnek
  */
