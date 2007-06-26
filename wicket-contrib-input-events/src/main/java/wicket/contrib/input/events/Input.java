8024289d7d9bb1a29b925e551682dbbf3bd324d1
Nino Martinez Wael <nmwael@wicketstuff.org>
2007-06-26 09:11:30 +0000

    Initial import.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2525 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-input-events/src/main/java/wicket/contrib/input/events/Input.java
@@ -0,0 +1,34 @@
+/*
+ * $Id: org.eclipse.jdt.ui.prefs 5004 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) eelco12 $
+ * $Revision: 5004 $
+ * $Date: 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.input.events;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.model.IModel;
+
+/**
+ * 
+ * @author Nino Martinez Wael (nino.martinez@jayway.dk)
+ */
+public abstract class Input extends AttributeAppender {
+
+	public Input(String arg0, IModel arg1, String arg2) {
+		super(arg0, arg1, arg2);
+	}
+
+}
---
+++ wicket-contrib-input-events/src/main/java/wicket/contrib/input/events/InputBehavior.java
@@ -0,0 +1,78 @@
+/*
+ * $Id: org.eclipse.jdt.ui.prefs 5004 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) eelco12 $
+ * $Revision: 5004 $
+ * $Date: 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.input.events;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.behavior.IBehavior;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.resources.CompressedResourceReference;
+
+public class InputBehavior implements IBehavior {
+	public void detachModel(Component arg0) {
+	}
+
+	public void rendered(Component arg0) {
+	}
+
+	private static ResourceReference JAVASCRIPT = new CompressedResourceReference(
+			InputBehavior.class, "wicket-contrib-input-events.js");
+
+	private Input input = null;
+
+	public InputBehavior(Input input) {
+		this.input = input;
+
+	}
+
+	public void afterRender(Component arg0) {
+	}
+
+	public void beforeRender(Component arg0) {
+	}
+
+	public void bind(Component component) {
+		component.add(HeaderContributor.forJavaScript(JAVASCRIPT));
+		component.add(input);
+	}
+
+	public void detach(Component arg0) {
+	}
+
+	public void exception(Component arg0, RuntimeException arg1) {
+	}
+
+	public boolean getStatelessHint(Component arg0) {
+		return false;
+	}
+
+	public boolean isEnabled(Component arg0) {
+		return false;
+	}
+
+	public boolean isTemporary() {
+		return false;
+	}
+
+	public void onComponentTag(Component arg0, ComponentTag arg1) {
+
+	}
+
+}
