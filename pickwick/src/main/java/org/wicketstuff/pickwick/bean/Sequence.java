23c1f8508f59ca3cb4f01a2e16a0dc9fd13d3668
Vincent Demay <vdemay@wicketstuff.org>
2007-06-30 15:48:44 +0000

    clean beans
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2576 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ pickwick/src/main/java/org/wicketstuff/pickwick/bean/Sequence.java
@@ -0,0 +1,33 @@
+package org.wicketstuff.pickwick.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("sequence")
+public class Sequence implements Serializable{
+	private String title;
+	private String description;
+	private Date date;
+	
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
