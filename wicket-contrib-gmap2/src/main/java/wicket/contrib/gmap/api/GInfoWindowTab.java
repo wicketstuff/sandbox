815b6281235f83861acfa17547febde83c9649d8
Sven Meier <svenmeier@wicketstuff.org>
2007-07-01 14:00:11 +0000

    preparing for tabbed infoWindow
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2585 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/api/GInfoWindowTab.java
@@ -0,0 +1,65 @@
+/*
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.gmap.api;
+
+import org.apache.wicket.Component;
+
+/**
+ * Represents an Google Maps API's GInfoWindowTab. <a
+ * href="http://www.google.com/apis/maps/documentation/reference.html#GInfoWindowTab">GInfoWindowTab</a>
+ * 
+ */
+public class GInfoWindowTab implements GMapApi 
+{
+
+	/**
+	 * Default serialVersionUID.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final String title;
+	private final Component content;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param lat
+	 * @param lng
+	 */
+	public GInfoWindowTab(String title, Component content)
+	{
+		this.title = title;
+		this.content = content;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public Component getContent()
+	{
+		return content;
+	}
+	
+	/**
+	 * @return A JavaScript constructor that represents this element.
+	 */
+	public String getJSConstructor()
+	{
+		return "new GInfoWindowTab('" + title + "', document.getElementById(" + content.getMarkupId() + "))";
+	}
+}
