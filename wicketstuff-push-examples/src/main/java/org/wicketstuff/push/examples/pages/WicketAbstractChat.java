3277c3af5a5039dab3c8c952deb18d9219d3ca78
Alastair Maw <almaw@wicketstuff.org>
2007-07-11 10:29:59 +0000

    Deprecated AjaxSubmitButton->AjaxButton
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2659 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-push-examples/src/main/java/org/wicketstuff/push/examples/pages/WicketAbstractChat.java
+++ wicketstuff-push-examples/src/main/java/org/wicketstuff/push/examples/pages/WicketAbstractChat.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
@@ -57,7 +57,7 @@ public abstract class WicketAbstractChat extends ExamplePage {
 		mess.setOutputMarkupId(true);
 		formChat.add(mess);
 		
-		formChat.add(new AjaxSubmitButton("send", formChat){
+		formChat.add(new AjaxButton("send", formChat){
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form form) {
 				//Update message
