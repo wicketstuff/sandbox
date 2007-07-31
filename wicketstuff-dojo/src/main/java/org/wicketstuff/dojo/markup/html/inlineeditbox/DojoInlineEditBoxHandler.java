237166060db6beba585930e67e1d95999b440045
Jean-Baptiste Quenot <quenotj@wicketstuff.org>
2007-07-31 08:08:30 +0000

    Simplify inline edit box by using a static function library
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2736 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-dojo/src/main/java/org/wicketstuff/dojo/markup/html/inlineeditbox/DojoInlineEditBoxHandler.java
+++ wicketstuff-dojo/src/main/java/org/wicketstuff/dojo/markup/html/inlineeditbox/DojoInlineEditBoxHandler.java
@@ -16,52 +16,59 @@
  */
 package org.wicketstuff.dojo.markup.html.inlineeditbox;
 
-import java.util.HashMap;
-
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.util.template.PackagedTextTemplate;
 import org.wicketstuff.dojo.AbstractRequireDojoBehavior;
 
 /**
  * Handler for {@link DojoInlineEditBox}
+ * 
  * @author Gregory Maes
  */
 @SuppressWarnings("serial")
-public class DojoInlineEditBoxHandler extends AbstractRequireDojoBehavior {
-	
+public class DojoInlineEditBoxHandler extends AbstractRequireDojoBehavior
+{
+
 	/** connect the onSave function of dojo with the wicket callbackUrl */
-	public void renderHead(IHeaderResponse response) {
+	public void renderHead(IHeaderResponse response)
+	{
 		super.renderHead(response);
-		PackagedTextTemplate packagedTextTemplate = new PackagedTextTemplate(this.getClass(), "InlineEditBox.js");
-		HashMap<String, CharSequence> map = new HashMap<String, CharSequence>();
-		map.put("callbackScript", generateCallbackScript("wicketAjaxGet('" + getCallbackUrl() + "&newTextValue=' + newValue"));
-		map.put("markupId", getComponent().getMarkupId());	
-		response.renderJavascript(packagedTextTemplate.asString(map), null);
+		// Include utility functions
+		response.renderJavascriptReference(new ResourceReference(DojoInlineEditBoxHandler.class,
+				"InlineEditBoxUtil.js"));
+
+		// Initialize the inline edit box
+		response.renderOnLoadJavascript(initJs());
 	}
 
-	/** set the required libraries for the inlineEditBox dojo widget
-	 * @param libs the dojo libraries to be included
+	String initJs()
+	{
+		String markupId = getComponent().getMarkupId();
+		return "initInlineEditBox('"
+				+ markupId
+				+ "', \""
+				+ generateCallbackScript("wicketAjaxGet('" + getCallbackUrl()
+						+ "&newTextValue=' + newValue") + "\");";
+	}
+
+	/**
+	 * set the required libraries for the inlineEditBox dojo widget
+	 * 
+	 * @param libs
+	 *            the dojo libraries to be included
 	 */
-	public void setRequire(RequireDojoLibs libs) {
+	public void setRequire(RequireDojoLibs libs)
+	{
 		libs.add("dojo.widget.InlineEditBox");
 		libs.add("dojo.event.*");
 	}
 
 	/** recover the text value and set it in the model */
-	protected void respond(AjaxRequestTarget target) {
+	protected void respond(AjaxRequestTarget target)
+	{
 		String textValue = getComponent().getRequest().getParameter("newTextValue");
 		getComponent().setModelObject(textValue);
 		((DojoInlineEditBox)getComponent()).onSave(target);
-	}
-
-	/**
-	 * @see wicket.contrib.dojo.AbstractRequireDojoBehavior#onComponentReRendered(wicket.ajax.AjaxRequestTarget)
-	 */
-	@Override
-	public void onComponentReRendered(AjaxRequestTarget ajaxTarget) {
-		super.onComponentReRendered(ajaxTarget);
-		String markupId = getComponent().getMarkupId();
-		ajaxTarget.appendJavascript("initInlineEditBox('" + markupId + "', \"" + generateCallbackScript("wicketAjaxGet('" + getCallbackUrl() + "&newTextValue=' + newValue") + "\");");
 	}
 }
