68e4cfa142664539cbea707c21bd512f56ff806e
Martin Funk <funkattack@wicketstuff.org>
2007-08-20 14:22:02 +0000

    refactor: pull HeaderContributor out of GMap2 into its own class.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2818 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/GMapHeaderContributor.java
@@ -0,0 +1,44 @@
+package wicket.contrib.gmap;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WicketEventReference;
+import org.apache.wicket.markup.html.resources.JavascriptResourceReference;
+
+public class GMapHeaderContributor extends HeaderContributor
+{
+	private static final long serialVersionUID = 1L;
+
+	/** URL for Google Maps' API endpoint. */
+	private static final String GMAP_API_URL = "http://maps.google.com/maps?file=api&amp;v=2&amp;key=";
+
+	// We also depend on wicket-ajax.js within wicket-gmap.js
+	private static final ResourceReference WICKET_AJAX_JS = new JavascriptResourceReference(
+			AbstractDefaultAjaxBehavior.class, "wicket-ajax.js");
+
+	// We have some custom Javascript.
+	private static final ResourceReference WICKET_GMAP_JS = new JavascriptResourceReference(
+			GMap2.class, "wicket-gmap.js");
+
+	public GMapHeaderContributor(final String gMapKey)
+	{
+		super(new IHeaderContributor()
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void renderHead(IHeaderResponse response)
+			{
+				response.renderJavascriptReference(GMAP_API_URL + gMapKey);
+				response.renderJavascriptReference(WicketEventReference.INSTANCE);
+				response.renderJavascriptReference(WICKET_AJAX_JS);
+				response.renderJavascriptReference(WICKET_GMAP_JS);
+				// see:
+				// http://www.google.com/apis/maps/documentation/#Memory_Leaks
+				response.renderOnBeforeUnloadJavascript("GUnload();");
+			}
+		});
+	}
+}
