96075fe31171f806996565f557c8de21ab4bb86f
Igor Vaynberg <ivaynberg@wicketstuff.org>
2007-08-28 02:08:02 +0000

    remove row-selection from treetable as it does nothing
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2872 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-jmx-panel/src/main/java/org/wicketstuff/jmx/markup/html/table/JmxTreeTable.java
+++ wicketstuff-jmx-panel/src/main/java/org/wicketstuff/jmx/markup/html/table/JmxTreeTable.java
@@ -22,6 +22,8 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.behavior.HeaderContributor;
 import org.apache.wicket.extensions.markup.html.tree.table.ColumnLocation;
@@ -31,28 +33,30 @@ import org.apache.wicket.extensions.markup.html.tree.table.ColumnLocation.Alignm
 import org.apache.wicket.extensions.markup.html.tree.table.ColumnLocation.Unit;
 import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 
 /**
  * @author Gerolf Seitz
  * 
  */
-public class JmxTreeTable extends TreeTable implements IHeaderContributor
-{
+public class JmxTreeTable extends TreeTable implements IHeaderContributor {
 	private static final ResourceReference ATTRIBUTE_ICON = new ResourceReference(
 			JmxTreeTable.class, "res/attribute.gif");
 	private static final ResourceReference OPERATION_ICON = new ResourceReference(
 			JmxTreeTable.class, "res/operation.gif");
-	public static final ResourceReference CSS = new ResourceReference(JmxTreeTable.class,
-			"res/JmxTreeTable.css");
+	public static final ResourceReference CSS = new ResourceReference(
+			JmxTreeTable.class, "res/JmxTreeTable.css");
 
 	private static final long serialVersionUID = 1L;
 
-	public JmxTreeTable(String id, TreeModel model)
-	{
+	public JmxTreeTable(String id, TreeModel model) {
 		super(id, model, new IColumn[] {
-				new JmxTreeColumn(new ColumnLocation(Alignment.LEFT, 50, Unit.PERCENT), "Node"),
-				new JmxEditorTreeColumn(new ColumnLocation(Alignment.RIGHT, 50, Unit.PERCENT),
-						"Value") });
+				new JmxTreeColumn(new ColumnLocation(Alignment.LEFT, 50,
+						Unit.PERCENT), "Node"),
+				new JmxEditorTreeColumn(new ColumnLocation(Alignment.RIGHT, 50,
+						Unit.PERCENT), "Value") });
 		setRootLess(true);
 		getTreeState().collapseAll();
 	}
@@ -63,35 +67,64 @@ public class JmxTreeTable extends TreeTable implements IHeaderContributor
 	 * @see org.apache.wicket.extensions.markup.html.tree.DefaultAbstractTree#getNodeIcon(javax.swing.tree.TreeNode)
 	 */
 	@Override
-	protected ResourceReference getNodeIcon(TreeNode node)
-	{
-		if (!node.isLeaf())
-		{
+	protected ResourceReference getNodeIcon(TreeNode node) {
+		if (!node.isLeaf()) {
 			return super.getNodeIcon(node);
 		}
-		Object obj = ((DefaultMutableTreeNode)node).getUserObject();
+		Object obj = ((DefaultMutableTreeNode) node).getUserObject();
 
-		if (obj instanceof MBeanAttributeInfo)
-		{
+		if (obj instanceof MBeanAttributeInfo) {
 			return ATTRIBUTE_ICON;
-		}
-		else if (obj instanceof MBeanOperationInfo)
-		{
+		} else if (obj instanceof MBeanOperationInfo) {
 			return OPERATION_ICON;
-		}
-		else
-		{
+		} else {
 			return super.getItem();
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/**
 	 * @see org.apache.wicket.markup.html.IHeaderContributor#renderHead(org.apache.wicket.markup.html.IHeaderResponse)
 	 */
-	public void renderHead(IHeaderResponse response)
-	{
+	public void renderHead(IHeaderResponse response) {
 		HeaderContributor.forCss(CSS).renderHead(response);
+	}
+
+	@Override
+	protected Component newTreePanel(MarkupContainer parent, String id,
+			TreeNode node, int level, IRenderNodeCallback renderNodeCallback) {
+		return new TreeNodePanel(id, node, level, renderNodeCallback);
+	}
+
+	/**
+	 * Default panel for tree nodes. Identical to treetable but without a
+	 * clickable link.
+	 * 
+	 * @author ivaynberg
+	 * 
+	 */
+	private class TreeNodePanel extends Panel {
+		private static final long serialVersionUID = 1L;
+
+		private TreeNodePanel(String id, final TreeNode node, int level,
+				final IRenderNodeCallback renderNodeCallback) {
+			super(id);
+
+			add(newIndentation(this, "indent", node, level));
+
+			add(newJunctionLink(this, "link", "image", node));
+
+			add(newNodeIcon(this, "icon", node));
+
+			add(new Label("label", new AbstractReadOnlyModel() {
+				private static final long serialVersionUID = 1L;
+
+				/**
+				 * @see org.apache.wicket.model.AbstractReadOnlyModel#getObject()
+				 */
+				public Object getObject() {
+					return renderNodeCallback.renderNode(node);
+				}
+			}));
+		}
 	}
 }
