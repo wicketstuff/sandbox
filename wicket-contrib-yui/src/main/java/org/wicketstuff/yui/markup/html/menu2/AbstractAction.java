918b049cb03cb0d22e0ed89e8e4bf9c9498d0cc9
rdleeper <rdleeper@wicketstuff.org>
2007-09-11 19:34:04 +0000

    initial checkin
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2949 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/menu2/AbstractAction.java
@@ -0,0 +1,9 @@
+package org.wicketstuff.yui.markup.html.menu2;
+
+public abstract class AbstractAction implements IAction, java.io.Serializable {
+
+	abstract public String getName();
+
+	abstract public void onClick();
+
+}
