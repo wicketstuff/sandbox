15e57ce175ded029735766d9a2d077b7c132c98f
rdleeper <rdleeper@wicketstuff.org>
2007-09-12 18:25:49 +0000

    refactored AbstractAction to package action.AbstractMenuAction refactored IAction to IYuiMenuAction
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2959 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/menu2/AbstractAction.java
+++
@@ -1,9 +0,0 @@
-package org.wicketstuff.yui.markup.html.menu2;
-
-public abstract class AbstractAction implements IAction, java.io.Serializable {
-
-	abstract public String getName();
-
-	abstract public void onClick();
-
-}
