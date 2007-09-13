83f641c10935fbe2ba96ec3c6e37180f5f6a6b37
rdleeper <rdleeper@wicketstuff.org>
2007-09-13 13:32:43 +0000

    added constructor(IModel) added default behavior getName()
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2973 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/menu2/action/AbstractYuiMenuAction.java
+++ wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/menu2/action/AbstractYuiMenuAction.java
@@ -4,8 +4,16 @@ import org.apache.wicket.model.IModel;
 import org.wicketstuff.yui.markup.html.menu2.IYuiMenuAction;
 
 public abstract class AbstractYuiMenuAction implements IYuiMenuAction, java.io.Serializable {
+	
+	private IModel name;
+	
+	public AbstractYuiMenuAction( IModel name ) {
+		this.name = name;
+	}
 
-	abstract public IModel getName();
+	public IModel getName() {
+		return name;
+	}
 
 	abstract public void onClick();
 
