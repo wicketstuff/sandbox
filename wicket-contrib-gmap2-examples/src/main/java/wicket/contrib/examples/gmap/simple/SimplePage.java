a19dc6ddd6af8839ed7a42b52e6b90017cad1719
Martin Funk <funkattack@wicketstuff.org>
2007-09-16 18:59:17 +0000

    the second half, tha half that I forgot yesterday.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@2993 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2-examples/src/main/java/wicket/contrib/examples/gmap/simple/SimplePage.java
@@ -0,0 +1,30 @@
+package wicket.contrib.examples.gmap.simple;
+
+import wicket.contrib.examples.WicketExamplePage;
+import wicket.contrib.gmap.GMap2;
+
+/**
+ * SimplePage for the wicket-contrib-gmap2 project
+ */
+public class SimplePage extends WicketExamplePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public SimplePage() {
+		add(new GMap2("topPanel", LOCALHOST));
+	}
+
+	/**
+	 * pay attention at webapp deploy context, we need a different key for each
+	 * deploy context check <a
+	 * href="http://www.google.com/apis/maps/signup.html">Google Maps API - Sign
+	 * Up</a> for more info. Also the GClientGeocoder is pickier on this than
+	 * the GMap2. Running on 'localhost' GMap2 will ignore the key and the maps
+	 * will show up, but GClientGeocoder wount. So if the key doesn't match the
+	 * url down to the directory GClientGeocoder will not work.
+	 * 
+	 * This key is good for all URLs in this directory:
+	 * http://localhost:8080/wicket-contrib-gmap2-examples/gmap/
+	 */
+	private static final String LOCALHOST = "ABQIAAAAzaZpf6nHOd9w1PfLaM9u2xSRJOeFm910afBJASoNgKJoF-fSURQRJ7dNBq-d-8hD7iUYeN2jQHZi8Q";
+}
