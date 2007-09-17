40876ae1bc773eb14a92ade32011799b9b443d6e
Igor Vaynberg <ivaynberg@wicketstuff.org>
2007-09-17 18:31:18 +0000

    fix test
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3006 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-crud/src/main/java/org/apache/wicket/quickstart/QueryParam.java
+++ wicketstuff-crud/src/main/java/org/apache/wicket/quickstart/QueryParam.java
@@ -1,20 +1,18 @@
 /*
- * $Id: QueryParam.java 371 2005-10-13 01:21:29 -0700 (Thu, 13 Oct 2005) gwynevans $
- * $Revision: 371 $
- * $Date: 2005-10-13 01:21:29 -0700 (Thu, 13 Oct 2005) $
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.wicket.quickstart;
 
@@ -23,10 +21,11 @@ import java.io.Serializable;
 /**
  * Encapsulates the Query Paramaters to be passed to {@link ContactDao#find}
  * method.
- *
+ * 
  * @author igor
  */
-public class QueryParam implements Serializable {
+public class QueryParam implements Serializable
+{
 	private int first;
 	private int count;
 	private String sort;
@@ -35,20 +34,21 @@ public class QueryParam implements Serializable {
 	/**
 	 * Set to return <tt>count</tt> elements, starting at the <tt>first</tt>
 	 * element.
-	 *
+	 * 
 	 * @param first
 	 *            First element to return.
 	 * @param count
 	 *            Number of elements to return.
 	 */
-	public QueryParam(int first, int count) {
+	public QueryParam(int first, int count)
+	{
 		this(first, count, null, true);
 	}
 
 	/**
 	 * Set to return <tt>count</tt> sorted elements, starting at the
 	 * <tt>first</tt> element.
-	 *
+	 * 
 	 * @param first
 	 *            First element to return.
 	 * @param count
@@ -58,46 +58,56 @@ public class QueryParam implements Serializable {
 	 * @param sortAsc
 	 *            Sort ascending or descending.
 	 */
-	public QueryParam(int first, int count, String sort, boolean sortAsc) {
+	public QueryParam(int first, int count, String sort, boolean sortAsc)
+	{
 		this.first = first;
 		this.count = count;
 		this.sort = sort;
 		this.sortAsc = sortAsc;
 	}
 
-	public void setSort(String sort) {
+	public void setSort(String sort)
+	{
 		this.sort = sort;
 	}
 
-	public void setSortAsc(boolean sortAsc) {
+	public void setSortAsc(boolean sortAsc)
+	{
 		this.sortAsc = sortAsc;
 	}
 
-	public int getCount() {
+	public int getCount()
+	{
 		return count;
 	}
 
-	public int getFirst() {
+	public int getFirst()
+	{
 		return first;
 	}
 
-	public String getSort() {
+	public String getSort()
+	{
 		return sort;
 	}
 
-	public boolean isSortAsc() {
+	public boolean isSortAsc()
+	{
 		return sortAsc;
 	}
 
-	public boolean hasSort() {
+	public boolean hasSort()
+	{
 		return sort != null;
 	}
 
-	public void setFirst(int first) {
+	public void setFirst(int first)
+	{
 		this.first = first;
 	}
 
-	public void setCount(int count) {
+	public void setCount(int count)
+	{
 		this.count = count;
 	}
 }
