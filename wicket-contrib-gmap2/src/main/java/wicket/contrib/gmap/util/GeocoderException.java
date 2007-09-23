060d63d90b0d8b9068c6b7d98754b069707f7ba0
Sven Meier <svenmeier@wicketstuff.org>
2007-09-23 18:55:49 +0000

    now extending IOException
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3025 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/util/GeocoderException.java
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/util/GeocoderException.java
@@ -15,37 +15,39 @@
  */
 package wicket.contrib.gmap.util;
 
-public class GeocoderException extends Exception{
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
-  public static final int G_GEO_SUCCESS = 200;
-  public static final int G_GEO_BAD_REQUEST = 400;
-  public static final int G_GEO_SERVER_ERROR = 500;
-  public static final int G_GEO_MISSING_QUERY = 601;
-  public static final int G_GEO_UNKNOWN_ADDRESS = 602;
-  public static final int G_GEO_UNAVAILABLE_ADDRESS = 603;
-  public static final int G_GEO_UNKNOWN_DIRECTIONS = 604;
-  public static final int G_GEO_BAD_KEY= 610;
-  public static final int G_GEO_TOO_MANY_QUERIES = 620;
-  private int status;
-  
-  public GeocoderException(int status){
-    setStatus(status);
-  }
-  public final int getStatus() {
-    return status;
-  }
-  public final void setStatus(int status) {
-    this.status = status;
-  }
-  
-  @Override
-  public String getMessage(){
-    String message = "Returned status = " + getStatus();
-    return message;
-    
-  }
+import java.io.IOException;
 
+public class GeocoderException extends IOException {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final int G_GEO_SUCCESS = 200;
+
+	public static final int G_GEO_BAD_REQUEST = 400;
+
+	public static final int G_GEO_SERVER_ERROR = 500;
+
+	public static final int G_GEO_MISSING_QUERY = 601;
+
+	public static final int G_GEO_UNKNOWN_ADDRESS = 602;
+
+	public static final int G_GEO_UNAVAILABLE_ADDRESS = 603;
+
+	public static final int G_GEO_UNKNOWN_DIRECTIONS = 604;
+
+	public static final int G_GEO_BAD_KEY = 610;
+
+	public static final int G_GEO_TOO_MANY_QUERIES = 620;
+
+	private int status;
+
+	public GeocoderException(int status) {
+		super("Status " + status);
+
+		this.status = status;
+	}
+
+	public final int getStatus() {
+		return status;
+	}
 }
