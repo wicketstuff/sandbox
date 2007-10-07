93b91ca3ac24d15d24aa4bac2a8dd11061679c9f
Vincent Demay <vdemay@wicketstuff.org>
2007-10-07 15:49:44 +0000

    Add some images
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3087 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/ImagePage.java
+++ pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/ImagePage.java
@@ -6,20 +6,12 @@ import java.io.IOException;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ResourceReference;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
-import org.wicketstuff.dojo.AbstractRequireDojoBehavior;
-import org.wicketstuff.dojo.dojofx.DojoFXHandler;
-import org.wicketstuff.dojo.dojofx.FXOnMouseOverFader;
-import org.wicketstuff.dojo.markup.html.container.DojoSimpleContainer;
-import org.wicketstuff.dojo.markup.html.container.layout.DojoLayoutContainer;
-import org.wicketstuff.dojo.markup.html.container.layout.DojoLayoutContainer.Position;
 import org.wicketstuff.pickwick.PickwickApplication;
 import org.wicketstuff.pickwick.Utils;
 import org.wicketstuff.pickwick.auth.PickwickAuthorization;
@@ -27,7 +19,6 @@ import org.wicketstuff.pickwick.auth.PickwickSession;
 import org.wicketstuff.pickwick.backend.ImageUtils;
 import org.wicketstuff.pickwick.backend.Settings;
 import org.wicketstuff.pickwick.backend.pages.MetadataViewPage;
-import org.wicketstuff.pickwick.backend.pages.SequenceEditPage;
 import org.wicketstuff.pickwick.bean.DisplaySequence;
 import org.wicketstuff.pickwick.bean.Sequence;
 import org.wicketstuff.pickwick.frontend.panel.MetaDisplayPanel;
@@ -81,65 +72,51 @@ public class ImagePage extends BasePage {
 		// FIXME : create a widget with that
 		final WebMarkupContainer nav = new WebMarkupContainer("nav");
 		add(nav);
-
-		//nav.add(new FXOnMouseOverFader(500,image,false,1.0,0.5));
-		nav.setOutputMarkupId(true);
-		add(new AbstractRequireDojoBehavior() {
-			@Override
-			public void setRequire(RequireDojoLibs libs) {
-				libs.add("dojo.html");
-				libs.add("dojo.lfx.*");
-			}
-
-			@Override
-			public void renderHead(IHeaderResponse response) {
-				super.renderHead(response);
-				response.renderJavascriptReference(new ResourceReference(ImagePage.class, "ImagePage.js"));
-				// connect to the resize event the image resize
-				/*response.renderOnLoadJavascript("" + "resizeImage('" + image.getMarkupId() + "', '" + top.getMarkupId()
-						+ "'); " + "dojo.event.connect(dojo.widget.byId('" + mainLayout.getMarkupId()
-						+ "'), 'onResized', function() {" + "resizeImage('" + image.getMarkupId() + "', '"
-						+ top.getMarkupId() + "');" + "});");*/
-				// by default, the image is not visible when everythink is
-				// loaded, make it visible
-				response.renderOnLoadJavascript("dojo.byId('" + image.getMarkupId() + "').style.visibility='visible'");
-				// response.renderOnLoadJavascript("dojo.byId('"+nav.getMarkupId()+"').style.visibility='visible'");
-				
-				response.renderJavascript("dojo.addOnLoad( function(){" +
-						"	connectProgresTransparency('" + image.getMarkupId() + "','" + nav.getMarkupId() + "')" +
-						"})", "fader");
-			}
-
-			@Override
-			protected void respond(AjaxRequestTarget target) {
-			}
-		});
+		
 		PageParameters pageparams;
 		try {
 			pageparams = new PageParameters();
 			pageparams.put("uri", imageUtils.getPreviousImage(uri));
 			BookmarkablePageLink prev = new URIBookmarkablePageLink("prev", ImagePage.class, pageparams);
 			nav.add(prev);
+			Image prevImage = new Image("prevImage", new ResourceReference(ImagePage.class, "images/previous.png"));
+			prev.add(prevImage);
+			
 			pageparams = new PageParameters();
 			pageparams.put("uri", imageUtils.getNextImage(uri));
 			BookmarkablePageLink next = new URIBookmarkablePageLink("next", ImagePage.class, pageparams);
 			nav.add(next);
+			Image nextImage = new Image("nextImage", new ResourceReference(ImagePage.class, "images/next.png"));
+			next.add(nextImage);
+			
 			pageparams = new PageParameters();
 			pageparams.put("uri", imageUtils.getFirstImage(uri));
 			BookmarkablePageLink first = new URIBookmarkablePageLink("first", ImagePage.class, pageparams);
 			nav.add(first);
+			Image firstImage = new Image("firstImage", new ResourceReference(ImagePage.class, "images/first.png"));
+			first.add(firstImage);
+			
 			pageparams = new PageParameters();
 			pageparams.put("uri", imageUtils.getLastImage(uri));
 			BookmarkablePageLink last = new URIBookmarkablePageLink("last", ImagePage.class, pageparams);
 			nav.add(last);
+			Image lastImage = new Image("lastImage", new ResourceReference(ImagePage.class, "images/last.png"));
+			last.add(lastImage);
 
 			// FIXME : remove me, just for tests
-			nav.add(new BookmarkablePageLink("meta", MetadataViewPage.class, params));
+			BookmarkablePageLink meta;
+			nav.add(meta = new BookmarkablePageLink("meta", MetadataViewPage.class, params));
+			Image metaImage = new Image("metaImage", new ResourceReference(ImagePage.class, "images/information.png"));
+			meta.add(metaImage);
 			
 			String folder = uri.substring(0, uri.lastIndexOf("/"));
 			PageParameters folderParams = new PageParameters();
 			folderParams.add("uri", folder);
-			nav.add(new BookmarkablePageLink("diapo", DiaporamaPage.class, folderParams));
+			BookmarkablePageLink diapo;
+			nav.add(diapo = new BookmarkablePageLink("diapo", DiaporamaPage.class, folderParams));
+			Image diapoImage = new Image("diapoImage", new ResourceReference(ImagePage.class, "images/diaporama.png"));
+			diapo.add(diapoImage);
+			
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
