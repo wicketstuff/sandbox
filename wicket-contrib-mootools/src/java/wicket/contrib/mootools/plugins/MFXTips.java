51bf0e245a6805d726eb772c21a417e795dab2e7
victori <victori@wicketstuff.org>
2007-10-12 22:08:32 +0000

    MFXDialog changes, added option to avoid bounce for slower computers.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3099 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXTips.java
+++ wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXTips.java
@@ -11,95 +11,111 @@ import wicket.contrib.mootools.MFXJavascriptUtils;
 import wicket.contrib.mootools.MFXOptions;
 
 /**
- * Class for adding Display tooltips 
+ * Class for adding Display tooltips
  * <p>
  * This class is stateless
  * </p>
+ * 
  * @author victori
- *
+ * 
  */
 public class MFXTips extends AbstractRequireMooStatelessBehavior {
 	private static final long serialVersionUID = 1L;
-	private static final ResourceReference MOOBUBBLE = 
-		new ResourceReference(MFXTips.class,"bubble.png");
+	private static final ResourceReference MOOBUBBLE = new ResourceReference(MFXTips.class, "bubble.png");
 	private String tip;
 	private MFXOptions mfxOptions = new MFXOptions();
-	
+
 	/**
 	 * Constructor
-	 * @param tip Tip to display
+	 * 
+	 * @param tip
+	 *            Tip to display
 	 */
-	public MFXTips(String tip) {
+	public MFXTips(final String tip) {
 		this.tip = tip;
 		mfxOptions.setWait("false");
 	}
-	
+
 	private String renderMooCSS() {
-		String str ="<style>";
-		str+= ".tool-tip { color: #fff; width: 139px; z-index: 13000; }";
-		str+=".tool-title { font-weight: both; font-size: 11px; margin: 0; color: #9FD4FF; padding: 8px 8px 4px;";
-		str+="background: url("+getResource(MOOBUBBLE)+") top left; }";
-		str+=".tool-text { font-size: 11px; padding: 4px 8px 8px; background: url("+getResource(MOOBUBBLE)+") bottom right; }";
-		str+="</style>";
+		String str = "<style>";
+		str += ".tool-tip { color: #fff; width: 139px; z-index: 13000; }";
+		str += ".tool-title { font-weight: both; font-size: 11px; margin: 0; color: #9FD4FF; padding: 8px 8px 4px;";
+		str += "background: url(" + getResource(MOOBUBBLE) + ") top left; }";
+		str += ".tool-text { font-size: 11px; padding: 4px 8px 8px; background: url(" + getResource(MOOBUBBLE)
+				+ ") bottom right; }";
+		str += "</style>";
 		return str;
 	}
-	
-	private String getResource(ResourceReference ref) {
+
+	private String getResource(final ResourceReference ref) {
 		return (String) RequestCycle.get().urlFor(ref);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see wicket.contrib.mootools.AbstractRequireMooStatelessBehavior#renderHead(org.apache.wicket.markup.html.IHeaderResponse)
 	 */
-	public void renderHead(IHeaderResponse response) {
+	@Override
+	public void renderHead(final IHeaderResponse response) {
 		super.renderHead(response);
-		
+
 		response.renderString(renderMooCSS());
-		
+
 		response.renderString(MFXJavascriptUtils.DOM_READY_OPEN());
-		
-		response.renderString("var Tips1 = new Tips($$('.mfxtip'), {"
-		+ "initialize:function(){" + "this.fx = new Fx.Style(this.toolTip, 'opacity', "+mfxOptions.writeOptions()+");"
-		+ "},onShow: function(toolTip) { this.fx.start(1); },onHide: function(toolTip) {"
-		+ "this.fx.start(0); }});");
-		
+
+		response.renderString("var Tips1 = new Tips($$('.mfxtip'), {" + "initialize:function(){ "
+				+ "this.fx = new Fx.Style(this.toolTip, 'opacity', " + mfxOptions.writeOptions() + ");"
+				+ "},onShow: function(toolTip) { this.fx.start(1); },onHide: function(toolTip) {"
+				+ "this.fx.start(0); }});");
+
 		response.renderString(MFXJavascriptUtils.DOM_READY_CLOSE());
-		
+
 	}
-	
-	/* (non-Javadoc)
-	 * @see wicket.contrib.mootools.AbstractRequireMooStatelessBehavior#onComponentTag(org.apache.wicket.Component, org.apache.wicket.markup.ComponentTag)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see wicket.contrib.mootools.AbstractRequireMooStatelessBehavior#onComponentTag(org.apache.wicket.Component,
+	 *      org.apache.wicket.markup.ComponentTag)
 	 */
-	public void onComponentTag(Component arg0, ComponentTag arg1) {
+	@Override
+	public void onComponentTag(final Component arg0, final ComponentTag arg1) {
 		super.onComponentTag(arg0, arg1);
-		
+
 		String currentClass = "";
-		if(arg1.getString("class") != null)
+		if (arg1.getString("class") != null) {
 			currentClass = arg1.getString("class").toString();
+		}
 		arg1.put("class", currentClass + " mfxtip");
 		arg1.put("title", tip);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see wicket.contrib.mootools.AbstractRequireMooStatelessBehavior#mooFunction()
 	 */
+	@Override
 	public String mooFunction() {
 		return null;
 	}
 
 	/**
 	 * Append a MFXOptions class to set any Mootool option
+	 * 
 	 * @see MFXOptions
 	 * @param mfxOptions
 	 * @return
 	 */
-	public MFXTips setMfxOptions(MFXOptions mfxOptions) {
+	public MFXTips setMfxOptions(final MFXOptions mfxOptions) {
 		this.mfxOptions = mfxOptions;
 		return this;
 	}
 
 	/**
 	 * Get {@link MFXOption} reference
+	 * 
 	 * @return
 	 */
 	public MFXOptions getMfxOptions() {
