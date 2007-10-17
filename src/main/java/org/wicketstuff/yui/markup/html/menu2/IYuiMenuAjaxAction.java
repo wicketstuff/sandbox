528efe62acf94213e6be0ccfbfc2b0e11c630199
rdleeper <rdleeper@wicketstuff.org>
2007-10-17 20:18:34 +0000

    added target to ajax onClick
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicket-contrib-yui@3116 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/main/java/org/wicketstuff/yui/markup/html/menu2/IYuiMenuAjaxAction.java
+++ src/main/java/org/wicketstuff/yui/markup/html/menu2/IYuiMenuAjaxAction.java
@@ -4,6 +4,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 
 public interface IYuiMenuAjaxAction extends IYuiMenuAction {
 	
-	void onClick( AjaxRequestTarget target );
+	void onClick( AjaxRequestTarget target, String targetId );
 
 }
