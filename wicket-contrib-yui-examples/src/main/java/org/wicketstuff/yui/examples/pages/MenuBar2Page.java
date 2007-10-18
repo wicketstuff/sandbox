d17a04e5ca60f22e9e9ab01fb59c9a4e25d94d48
rdleeper <rdleeper@wicketstuff.org>
2007-10-18 03:23:42 +0000

    fixed menu2 compile errors (copied from menu_branch)
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3121 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui-examples/src/main/java/org/wicketstuff/yui/examples/pages/MenuBar2Page.java
+++ wicket-contrib-yui-examples/src/main/java/org/wicketstuff/yui/examples/pages/MenuBar2Page.java
@@ -2,9 +2,11 @@ package org.wicketstuff.yui.examples.pages;
 
 
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.wicketstuff.yui.examples.WicketExamplePage;
 import org.wicketstuff.yui.markup.html.menu2.AbstractYuiMenuItem;
-import org.wicketstuff.yui.markup.html.menu2.IAction;
+import org.wicketstuff.yui.markup.html.menu2.IYuiMenuAction;
 import org.wicketstuff.yui.markup.html.menu2.YuiMenu;
 import org.wicketstuff.yui.markup.html.menu2.YuiMenuBar;
 import org.wicketstuff.yui.markup.html.menu2.YuiMenuBarItem;
@@ -26,41 +28,46 @@ public class MenuBar2Page extends WicketExamplePage {
 		YuiMenuBar mb = new YuiMenuBar( "menuBar", "menuBar");
 		
 		YuiMenuBarItem firstMenu = mb.addMenu( "First Menu" );
+
+
+		subMenu = firstMenu.newSubMenu( "mb_firstMenu");
 		
-		subMenu = firstMenu.addSubMenu( "mb_firstMenu");
+		subMenu.addMenuItem(new TestAction("M1 : L1"));
+		subMenu.addMenuItem(new TestAction("M1 : L2"));
+		mi = subMenu.addMenuItem(new TestAction("M1 : L3"));
+		subMenu.addMenuItem(new TestAction("M1 : L4"));
+		subMenu.addMenuItem(new TestAction("M1 : L5"));
 		
-		subMenu.add(new TestAction("M1 : L1"));
-		subMenu.add(new TestAction("M1 : L2"));
-		mi = subMenu.add(new TestAction("M1 : L3"));
-		subMenu.add(new TestAction("M1 : L4"));
-		subMenu.add(new TestAction("M1 : L5"));
-		
-		subMenu = mi.addSubMenu( "subMenu1" );
-		subMenu.add(new TestAction("Label 1"));
-		subMenu.add(new TestAction("Label 2"));
+		subMenu = mi.newSubMenu( "subMenu1" );
+		subMenu.addMenuItem(new TestAction("Label 1"));
+		subMenu.addMenuItem(new TestAction("Label 2"));
 		
 		YuiMenuBarItem secondMenu = mb.addMenu( "Second Menu");
 		
-		subMenu = secondMenu.addSubMenu( "mb_secondMenu");
-		subMenu.add(new TestAction("M2 : L1"));
-		subMenu.add(new TestAction("M2 : L2"));
-		subMenu.add(new TestAction("M2 : L3"));
-		subMenu.add(new TestAction("M2 : L4"));
-		subMenu.add(new TestAction("M2 : L5"));
+		subMenu = secondMenu.newSubMenu( "mb_secondMenu");
+		subMenu.addMenuItem(new TestAction("M2 : L1"));
+		subMenu.addMenuItem(new TestAction("M2 : L2"));
+		subMenu.addMenuItem(new TestAction("M2 : L3"));
+		subMenu.addMenuItem(new TestAction("M2 : L4"));
+		subMenu.addMenuItem(new TestAction("M2 : L5"));
+
+		
+		YuiMenuBarItem thirdMenu = mb.addMenu( new TestAction( "Third Menu" ) );
+
 		
 		add( mb );
 	}
 	
 
-	private static class TestAction implements IAction, java.io.Serializable {
+	private static class TestAction implements IYuiMenuAction, java.io.Serializable {
 		private String id;
 
 		public TestAction(String id) {
 			this.id = id;
 		}
 		
-		public String getName() {
-			return id;
+		public IModel getName() {
+			return new Model( id );
 		}
 
 		public void onClick() {
