cb8bbfc1857218da74477657735ebd7aba3cf09c
victori <victori@wicketstuff.org>
2007-10-18 03:14:52 +0000

    mfxfontresize fix for IE6 and IE7, apparently these browsers do not return the correct font size initially.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3119 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXFontResizer.java
+++ wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXFontResizer.java
@@ -14,17 +14,17 @@ public class MFXFontResizer extends AbstractRequireMooStatelessBehavior {
 	private MFXOptions mfxOptions = new MFXOptions();
 	private String container;
 	private String tag = "p";
-	
-	public MFXFontResizer(int size) {
+
+	public MFXFontResizer(final int size) {
 		this.size = size;
 	}
-	
-	public MFXFontResizer(int size,int maxSize) {
+
+	public MFXFontResizer(final int size, final int maxSize) {
 		this.size = size;
 	}
-	
+
 	@Override
-	public void onRendered(Component arg0) {
+	public void onRendered(final Component arg0) {
 		super.onRendered(arg0);
 		Response resp = arg0.getResponse();
 		resp.write(MFXJavascriptUtils.DOM_READY_OPEN());
@@ -33,27 +33,29 @@ public class MFXFontResizer extends AbstractRequireMooStatelessBehavior {
 		resp.write(mooFunction());
 		resp.write(MFXJavascriptUtils.DOM_READY_CLOSE());
 	}
-	
-	
+
 	@Override
 	public String mooFunction() {
 		StringBuffer buf = new StringBuffer();
 		final String compid = target.getMarkupId();
-		buf.append("$('"+compid+"')"+".addEvent('click',function(e) {");
-		
-		if(container != null)
-			buf.append("var texts = $ES('"+tag+"','"+container+"');");
-		else
-			buf.append("var texts = $$('"+tag+"');");
-		
+		buf.append("$('" + compid + "')" + ".addEvent('click',function(e) {");
+
+		if (container != null) {
+			buf.append("var texts = $ES('" + tag + "','" + container + "');");
+		} else {
+			buf.append("var texts = $$('" + tag + "');");
+		}
+
 		buf.append("texts.each(function(t,i) {");
 		buf.append("var csize = t.getStyle('font-size').toInt();");
-		buf.append("new Fx.Style(t,'font-size',"+mfxOptions.writeOptions()+").start(csize, (csize+("+size+")));");
+		buf.append("if (csize >= 100) { csize = 12 } ");
+		buf.append("new Fx.Style(t,'font-size'," + mfxOptions.writeOptions() + ").start(csize, (csize+(" + size
+				+ ")));");
 		buf.append("});});");
 		return buf.toString();
 	}
 
-	public MFXFontResizer setContainer(String container) {
+	public MFXFontResizer setContainer(final String container) {
 		this.container = container;
 		return this;
 	}
@@ -62,7 +64,7 @@ public class MFXFontResizer extends AbstractRequireMooStatelessBehavior {
 		return container;
 	}
 
-	public MFXFontResizer setTag(String tag) {
+	public MFXFontResizer setTag(final String tag) {
 		this.tag = tag;
 		return this;
 	}
@@ -71,7 +73,7 @@ public class MFXFontResizer extends AbstractRequireMooStatelessBehavior {
 		return tag;
 	}
 
-	public MFXFontResizer setMfxOptions(MFXOptions mfxOptions) {
+	public MFXFontResizer setMfxOptions(final MFXOptions mfxOptions) {
 		this.mfxOptions = mfxOptions;
 		return this;
 	}
@@ -79,19 +81,19 @@ public class MFXFontResizer extends AbstractRequireMooStatelessBehavior {
 	public MFXOptions getMfxOptions() {
 		return mfxOptions;
 	}
-	
-//	var larger = $('larger');
-//	var smaller = $('smaller');
-//	larger.addEvent('click', function(e) {
-//		var texts = $ES('p','stage');
-//		texts.each(function(t,i) {
-//			new Fx.Style(t,'font-size',{duration: 500}).start(12,16);
-//		});
-//	});
-//	smaller.addEvent('click', function(e) {
-//		var texts = $ES('p','stage');
-//		texts.each(function(t,i) {
-//			new Fx.Style(t,'font-size',{duration: 500}).start(16,12);
-//		});
-//	});
+
+	// var larger = $('larger');
+	// var smaller = $('smaller');
+	// larger.addEvent('click', function(e) {
+	// var texts = $ES('p','stage');
+	// texts.each(function(t,i) {
+	// new Fx.Style(t,'font-size',{duration: 500}).start(12,16);
+	// });
+	// });
+	// smaller.addEvent('click', function(e) {
+	// var texts = $ES('p','stage');
+	// texts.each(function(t,i) {
+	// new Fx.Style(t,'font-size',{duration: 500}).start(16,12);
+	// });
+	// });
 }
