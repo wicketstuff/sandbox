11ccd9e417489f82cfa0eab08631a80c799b5ea4
Vincent Demay <vdemay@wicketstuff.org>
2007-10-21 17:44:30 +0000

    Add user inscription
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3139 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/BasePage.java
+++ pickwick/src/main/java/org/wicketstuff/pickwick/frontend/pages/BasePage.java
@@ -1,11 +1,17 @@
 package org.wicketstuff.pickwick.frontend.pages;
 
+import java.util.ArrayList;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.PageLink;
@@ -13,15 +19,29 @@ import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.resources.JavaScriptReference;
 import org.apache.wicket.markup.html.resources.StyleSheetReference;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 import org.wicketstuff.pickwick.PickwickApplication;
 import org.wicketstuff.pickwick.auth.PickwickLoginPage;
 import org.wicketstuff.pickwick.auth.PickwickLogoutPage;
 import org.wicketstuff.pickwick.auth.PickwickSession;
+import org.wicketstuff.pickwick.backend.Settings;
 import org.wicketstuff.pickwick.backend.pages.BackendLandingPage;
+import org.wicketstuff.pickwick.bean.Role;
+import org.wicketstuff.pickwick.bean.User;
 import org.wicketstuff.pickwick.ext.ExtAnchor;
+import org.wicketstuff.pickwick.frontend.panel.UserInscription;
+import org.wicketstuff.yui.markup.html.dialog.Dialog;
+import org.wicketstuff.yui.markup.html.dialog.DialogSettings;
+import org.wicketstuff.yui.markup.html.dialog.DialogSettings.UnderlayType;
+
+import com.google.inject.Inject;
 
 public class BasePage extends WebPage {
+	
+
+	@Inject
+	Settings settings;
 	
 	private static final String WEST = "west";
 	private Panel west;
@@ -31,6 +51,9 @@ public class BasePage extends WebPage {
 	
 	private static final String SOUTH = "south";
 	private Panel south;
+	
+	private static final String INSCRIPTION_DIALOG = "inscriptionDialog";
+	private Dialog inscriptionDialog;
 	
 	public BasePage(PageParameters parameters) {
 		super();
@@ -98,7 +121,6 @@ public class BasePage extends WebPage {
 		}
 		
 		Label userName = new Label("userName", new Model(name));
-		PageLink auth = new PageLink("auth", PickwickLoginPage.class);
 		PageLink logout = new PageLink("logout", PickwickLogoutPage.class);
 		WebMarkupContainer home = new WebMarkupContainer("home"){
 			@Override
@@ -110,26 +132,86 @@ public class BasePage extends WebPage {
 		add(home);
 		home.add(new Image("homeImage", new ResourceReference(BasePage.class, "images/home.png")));
 		add(userName);
-		add(auth);
 		add(logout);
 		
 		Image userImg = new Image("userImage", new ResourceReference(BasePage.class, "images/users.png"));
 		add(userImg);
-		Image logInImg = new Image("logInImage", new ResourceReference(BasePage.class, "images/log-in.png"));
-		auth.add(logInImg);
 		Image logOutImg = new Image("logOutImage", new ResourceReference(BasePage.class, "images/log-out.png"));
 		logout.add(logOutImg);
+		
+
+		PageLink auth = new PageLink("auth", PickwickLoginPage.class);
+		add(auth);
+		Image logInImg = new Image("logInImage", new ResourceReference(BasePage.class, "images/log-in.png"));
+		auth.add(logInImg);
+		
+		AjaxLink inscription = new AjaxLink("inscription"){
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				inscriptionDialog.show(target);
+			}
+		};
+		add(inscription);
+		Image inscriptionImg = new Image("inscriptionImage", new ResourceReference(BasePage.class, "images/inscription.png"));
+		inscription.add(inscriptionImg);
+		add(inscriptionDialog = getInscriptionDialog(INSCRIPTION_DIALOG));
+		
 		
 		if (PickwickApplication.get().getPickwickSession().getDefaultUser().getName().equals(name)){
 			//anonymous
 			userName.setVisible(false);
 			userImg.setVisible(false);
 			logout.setVisible(false);
+			inscription.setVisible(true);
 		}else{
 			auth.setVisible(false);
+			inscription.setVisible(false);
 		}
 		
 		
+	}
+	
+	/**
+	 * return the Dialog for inscription
+	 * @param inscriptionDialogId
+	 * @return
+	 */
+	public Dialog getInscriptionDialog(String inscriptionDialogId){
+		DialogSettings settings = new DialogSettings();
+        settings.setModal(true);
+        settings.setDraggable(true);
+        settings.setFixCenter(true);
+        settings.setUnderlay(UnderlayType.SHADOW);
+        settings.setVisible(false);
+        settings.setModal(true);
+        
+		Dialog userInscription = new Dialog(inscriptionDialogId, new Model("Inscription"), settings){
+
+			@Override
+			public Panel createContent(String arg0) {
+				return new UserInscription(arg0, new CompoundPropertyModel(new User())){
+
+					@Override
+					public void onSave(AjaxRequestTarget target, Form form) {
+						User user = (User)form.getModelObject();
+						user.setRoles(new ArrayList<Role>());
+						user.setAdmin(false);
+						if (getPasswordRepeated() != user.getPassword()){
+							form.error("Passwords should be the same");
+						} if (BasePage.this.settings.getUserManagement().getUser(user.getName()) != null){
+							form.error("User " + user.getName() + "already exists");
+						} else{
+							BasePage.this.settings.getUserManagement().addUser(user);
+							BasePage.this.inscriptionDialog.hide(target);
+						}
+					}
+					
+				};
+			}
+			
+		};
+		
+		return userInscription;
 	}
 
 }
--- pickwick/src/main/java/org/wicketstuff/pickwick/frontend/panel/UserInscription.java
+++ pickwick/src/main/java/org/wicketstuff/pickwick/frontend/panel/UserInscription.java
@@ -1,4 +1,4 @@
-package org.wicketstuff.pickwick.backend.panel;
+package org.wicketstuff.pickwick.frontend.panel;
 
 import java.util.ArrayList;
 
@@ -6,38 +6,32 @@ import org.apache.wicket.Component;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-import org.apache.wicket.extensions.markup.html.form.palette.Palette;
-import org.apache.wicket.markup.html.form.CheckBox;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.wicketstuff.pickwick.backend.Settings;
 import org.wicketstuff.pickwick.bean.Role;
-import org.wicketstuff.pickwick.bean.Roles;
 import org.wicketstuff.pickwick.bean.User;
-
-import com.google.inject.Inject;
 
 /**
  * a panel to add or edit a user.
  * @author Vincent Demay
  *
  */
-public abstract class UserPanel extends Panel {
-
-	@Inject
-	Settings settings;
+public abstract class UserInscription extends Panel {
 	
 	private Form form;
 	private TextField name;
-	private TextField password;
+	private PasswordTextField password;
+	private PasswordTextField passwordRepeat;
 	
-	public UserPanel(String id, CompoundPropertyModel userModel) {
+	private String passwordRepeated;
+	
+	public UserInscription(String id, CompoundPropertyModel userModel) {
 		super(id, new Model());
 		setOutputMarkupId(true);
 		
@@ -51,31 +45,31 @@ public abstract class UserPanel extends Panel {
 		add(form);
 		
 		name = new TextField("name");
-		password = new TextField("password");
+		password = new PasswordTextField("password");
+		passwordRepeat = new PasswordTextField("passwordRepeat", new Model(passwordRepeated));
 		name.setOutputMarkupId(true);
+		
+		name.setRequired(true);
+		password.setRequired(true);
+		passwordRepeat.setRequired(true);
+		
 		form.add(name);
 		form.add(password);
+		form.add(passwordRepeat);
+		
+		form.add(new FeedbackPanel("feedback"));
 
-		Roles roles = settings.getUserManagement().getAllRoles();
-		Palette role = new Palette("roles", new Model(roles), new ChoiceRenderer("label", "label"), 6, false);
-		form.add(role);
-		
-		CheckBox admin = new CheckBox("admin");
-		form.add(admin);
-		
 		form.add(new AjaxButton("save",form){
 
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form form) {
-				User user = (User)form.getModelObject();
-				settings.getUserManagement().addUser(user);
-				onSave(target);
+				onSave(target, form);
 			}
 
 		});
 	}
 	
-	public abstract void onSave(AjaxRequestTarget target);
+	public abstract void onSave(AjaxRequestTarget target, Form form);
 	
 	@Override
 	public Component setModel(IModel model) {
@@ -86,14 +80,8 @@ public abstract class UserPanel extends Panel {
 		form.setModel(model);
 		return this;
 	}
-	
-	public void disableName(AjaxRequestTarget target){
-		target.appendJavascript("document.getElementById('" + name.getMarkupId() + "').disabled='disabled'");
-	}
-	
-	public void enableName(AjaxRequestTarget target){
-		target.appendJavascript("document.getElementById('" + name.getMarkupId() + "').disabled='false'");
-	}
-	
 
+	public String getPasswordRepeated() {
+		return passwordRepeated;
+	}
 }
