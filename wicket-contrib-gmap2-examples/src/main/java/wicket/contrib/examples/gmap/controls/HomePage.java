e61a6d2c74e074c85c95a2c0569869f83189e7df
Martin Funk <funkattack@wicketstuff.org>
2007-10-21 21:06:25 +0000

    uncluttering examples.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3141 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2-examples/src/main/java/wicket/contrib/examples/gmap/controls/HomePage.java
@@ -0,0 +1,61 @@
+package wicket.contrib.examples.gmap.controls;
+
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+
+import wicket.contrib.examples.WicketExamplePage;
+import wicket.contrib.gmap.GMap2;
+import wicket.contrib.gmap.api.GControl;
+import wicket.contrib.gmap.api.GLatLng;
+import wicket.contrib.gmap.api.GMarker;
+import wicket.contrib.gmap.api.GMarkerOptions;
+import wicket.contrib.gmap.event.ClickListener;
+import wicket.contrib.gmap.event.MoveEndListener;
+
+/**
+ * Example HomePage for the wicket-contrib-gmap2 project
+ */
+public class HomePage extends WicketExamplePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public HomePage() {
+		final GMap2 topMap = new GMap2("topPanel", LOCALHOST);
+		topMap.setDoubleClickZoomEnabled(true);
+		topMap.setZoom(10);
+		GMarkerOptions options = new GMarkerOptions();
+		options.setTitle("Home");
+		options.setDraggable(true);
+		options.setAutoPan(true);
+		topMap.addControl(GControl.GLargeMapControl);
+		topMap.addControl(GControl.GMapTypeControl);
+		add(topMap);
+
+		final Label zoomIn = new Label("zoomInLabel", "ZoomIn");
+		zoomIn.add(topMap.new ZoomIn("onclick"));
+		add(zoomIn);
+
+		final Label zoomOut = new Label("zoomOutLabel", "ZoomOut");
+		zoomOut.add(topMap.new ZoomOut("onclick"));
+		add(zoomOut);
+	}
+
+	/**
+	 * pay attention at webapp deploy context, we need a different key for each
+	 * deploy context check <a
+	 * href="http://www.google.com/apis/maps/signup.html">Google Maps API - Sign
+	 * Up</a> for more info. Also the GClientGeocoder is pickier on this than
+	 * the GMap2. Running on 'localhost' GMap2 will ignore the key and the maps
+	 * will show up, but GClientGeocoder wount. So if the key doesnt match the
+	 * url down to the directory GClientGeocoder will not work.
+	 * 
+	 * This key is good for all URLs in this directory:
+	 * http://localhost:8080/wicket-contrib-gmap2-examples/gmap2/
+	 */
+	private static final String LOCALHOST = "ABQIAAAAzaZpf6nHOd9w1PfLaM9u2xQRS2YPSd8S9D1NKPBvdB1fr18_CxR-svEYj6URCf5QDFq3i03mqrDlbA";
+}
