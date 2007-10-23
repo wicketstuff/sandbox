e5cef33573a2b1eff97dd6dc1d210c1cb639f299
Martin Funk <funkattack@wicketstuff.org>
2007-10-23 20:41:35 +0000

    It's always nice to get rid of something.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3148 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/GMap2.java
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/GMap2.java
@@ -50,7 +50,8 @@ import wicket.contrib.gmap.event.GEventListener;
  * href="http://www.google.com/apis/maps/signup.html">Google Maps API sign up
  * page</a> for more information.
  */
-public class GMap2 extends Panel {
+public class GMap2 extends Panel
+{
 
 	private static final long serialVersionUID = 1L;
 
@@ -83,7 +84,8 @@ public class GMap2 extends Panel {
 	 * @param gMapKey
 	 *            Google gmap API KEY
 	 */
-	public GMap2(final String id, final GMapHeaderContributor headerContrib) {
+	public GMap2(final String id, final GMapHeaderContributor headerContrib)
+	{
 		this(id, headerContrib, new ArrayList<GOverlay>());
 	}
 
@@ -94,7 +96,8 @@ public class GMap2 extends Panel {
 	 * @param gMapKey
 	 *            Google gmap API KEY
 	 */
-	public GMap2(final String id, final String gMapKey) {
+	public GMap2(final String id, final String gMapKey)
+	{
 		this(id, new GMapHeaderContributor(gMapKey), new ArrayList<GOverlay>());
 	}
 
@@ -106,7 +109,8 @@ public class GMap2 extends Panel {
 	 *            Google gmap API KEY
 	 * @param overlays
 	 */
-	public GMap2(final String id, final String gMapKey, List<GOverlay> overlays) {
+	public GMap2(final String id, final String gMapKey, List<GOverlay> overlays)
+	{
 		this(id, new GMapHeaderContributor(gMapKey), overlays);
 	}
 
@@ -118,17 +122,19 @@ public class GMap2 extends Panel {
 	 *            Google gmap API KEY
 	 * @param overlays
 	 */
-	public GMap2(final String id, final GMapHeaderContributor headerContrib,
-			List<GOverlay> overlays) {
+	public GMap2(final String id, final GMapHeaderContributor headerContrib, List<GOverlay> overlays)
+	{
 		super(id);
 
 		this.overlays = overlays;
 
 		add(headerContrib);
-		add(new HeaderContributor(new IHeaderContributor() {
+		add(new HeaderContributor(new IHeaderContributor()
+		{
 			private static final long serialVersionUID = 1L;
 
-			public void renderHead(IHeaderResponse response) {
+			public void renderHead(IHeaderResponse response)
+			{
 				response.renderOnDomReadyJavascript(getJSinit());
 			}
 		}));
@@ -148,12 +154,13 @@ public class GMap2 extends Panel {
 	 *            control to add
 	 * @return This
 	 */
-	public GMap2 addControl(GControl control) {
+	public GMap2 addControl(GControl control)
+	{
 		controls.add(control);
 
-		if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-			((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-					.appendJavascript(control.getJSadd(GMap2.this));
+		if (AjaxRequestTarget.get() != null)
+		{
+			AjaxRequestTarget.get().appendJavascript(control.getJSadd(GMap2.this));
 		}
 
 		return this;
@@ -166,12 +173,13 @@ public class GMap2 extends Panel {
 	 *            control to remove
 	 * @return This
 	 */
-	public GMap2 removeControl(GControl control) {
+	public GMap2 removeControl(GControl control)
+	{
 		controls.remove(control);
 
-		if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-			((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-					.appendJavascript(control.getJSremove(GMap2.this));
+		if (AjaxRequestTarget.get() != null)
+		{
+			AjaxRequestTarget.get().appendJavascript(control.getJSremove(GMap2.this));
 		}
 
 		return this;
@@ -184,12 +192,13 @@ public class GMap2 extends Panel {
 	 *            overlay to add
 	 * @return This
 	 */
-	public GMap2 addOverlay(GOverlay overlay) {
+	public GMap2 addOverlay(GOverlay overlay)
+	{
 		overlays.add(overlay);
 
-		if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-			((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-					.appendJavascript(overlay.getJSadd(GMap2.this));
+		if (AjaxRequestTarget.get() != null)
+		{
+			AjaxRequestTarget.get().appendJavascript(overlay.getJSadd(GMap2.this));
 		}
 
 		return this;
@@ -202,106 +211,127 @@ public class GMap2 extends Panel {
 	 *            overlay to remove
 	 * @return This
 	 */
-	public GMap2 removeOverlay(GOverlay overlay) {
-		while (overlays.contains(overlay)) {
+	public GMap2 removeOverlay(GOverlay overlay)
+	{
+		while (overlays.contains(overlay))
+		{
 			overlays.remove(overlay);
 		}
 
-		if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-			((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-					.appendJavascript(overlay.getJSremove(GMap2.this));
+		if (AjaxRequestTarget.get() != null)
+		{
+			AjaxRequestTarget.get().appendJavascript(overlay.getJSremove(GMap2.this));
 		}
 
 		return this;
 	}
 
-	public List<GOverlay> getOverlays() {
+	public List<GOverlay> getOverlays()
+	{
 		return Collections.unmodifiableList(overlays);
 	}
 
-	public Set<GControl> getControls() {
+	public Set<GControl> getControls()
+	{
 		return Collections.unmodifiableSet(controls);
 	}
 
-	public GLatLng getCenter() {
+	public GLatLng getCenter()
+	{
 		return center;
 	}
 
-	public GLatLngBounds getBounds() {
+	public GLatLngBounds getBounds()
+	{
 		return bounds;
 	}
 
-	public void setDraggingEnabled(boolean enabled) {
-		if (this.draggingEnabled != enabled) {
+	public void setDraggingEnabled(boolean enabled)
+	{
+		if (this.draggingEnabled != enabled)
+		{
 			draggingEnabled = enabled;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(getJSsetDraggingEnabled(enabled));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(getJSsetDraggingEnabled(enabled));
 			}
 		}
 	}
 
-	public boolean isDraggingEnabled() {
+	public boolean isDraggingEnabled()
+	{
 		return draggingEnabled;
 	}
 
-	public void setDoubleClickZoomEnabled(boolean enabled) {
-		if (this.doubleClickZoomEnabled != enabled) {
+	public void setDoubleClickZoomEnabled(boolean enabled)
+	{
+		if (this.doubleClickZoomEnabled != enabled)
+		{
 			doubleClickZoomEnabled = enabled;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(getJSsetDoubleClickZoomEnabled(enabled));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(getJSsetDoubleClickZoomEnabled(enabled));
 			}
 		}
 	}
 
-	public boolean isDoubleClickZoomEnabled() {
+	public boolean isDoubleClickZoomEnabled()
+	{
 		return doubleClickZoomEnabled;
 	}
 
-	public void setScrollWheelZoomEnabled(boolean enabled) {
-		if (this.scrollWheelZoomEnabled != enabled) {
+	public void setScrollWheelZoomEnabled(boolean enabled)
+	{
+		if (this.scrollWheelZoomEnabled != enabled)
+		{
 			scrollWheelZoomEnabled = enabled;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(getJSsetScrollWheelZoomEnabled(enabled));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(getJSsetScrollWheelZoomEnabled(enabled));
 			}
 		}
 	}
 
-	public boolean isScrollWheelZoomEnabled() {
+	public boolean isScrollWheelZoomEnabled()
+	{
 		return scrollWheelZoomEnabled;
 	}
 
-	public GMapType getMapType() {
+	public GMapType getMapType()
+	{
 		return mapType;
 	}
 
-	public void setMapType(GMapType mapType) {
-		if (this.mapType != mapType) {
+	public void setMapType(GMapType mapType)
+	{
+		if (this.mapType != mapType)
+		{
 			this.mapType = mapType;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(mapType.getJSset(GMap2.this));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(mapType.getJSset(GMap2.this));
 			}
 		}
 	}
 
-	public int getZoom() {
+	public int getZoom()
+	{
 		return zoom;
 	}
 
-	public void setZoom(int level) {
-		if (this.zoom != level) {
+	public void setZoom(int level)
+	{
+		if (this.zoom != level)
+		{
 			this.zoom = level;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(getJSsetZoom(zoom));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(getJSsetZoom(zoom));
 			}
 		}
 	}
@@ -312,96 +342,108 @@ public class GMap2 extends Panel {
 	 * @param center
 	 *            center to set
 	 */
-	public void setCenter(GLatLng center) {
-		if (!this.center.equals(center)) {
+	public void setCenter(GLatLng center)
+	{
+		if (!this.center.equals(center))
+		{
 			this.center = center;
 
-			if (RequestCycle.get().getRequestTarget() instanceof AjaxRequestTarget) {
-				((AjaxRequestTarget) RequestCycle.get().getRequestTarget())
-						.appendJavascript(getJSsetCenter(center));
+			if (AjaxRequestTarget.get() != null)
+			{
+				AjaxRequestTarget.get().appendJavascript(getJSsetCenter(center));
 			}
 		}
 	}
 
-	public GInfoWindow getInfoWindow() {
+	public GInfoWindow getInfoWindow()
+	{
 		return infoWindow;
 	}
 
-	private String getJSinit() {
-		StringBuffer js = new StringBuffer("new WicketGMap2('"
-				+ map.getMarkupId() + "');\n");
+	private String getJSinit()
+	{
+		StringBuffer js = new StringBuffer("new WicketGMap2('" + map.getMarkupId() + "');\n");
 
 		js.append(getJSsetCenter(center) + "\n");
 		js.append(getJSsetZoom(zoom) + "\n");
 		js.append(getJSsetDraggingEnabled(draggingEnabled) + "\n");
-		js
-				.append(getJSsetDoubleClickZoomEnabled(doubleClickZoomEnabled)
-						+ "\n");
-		js
-				.append(getJSsetScrollWheelZoomEnabled(scrollWheelZoomEnabled)
-						+ "\n");
+		js.append(getJSsetDoubleClickZoomEnabled(doubleClickZoomEnabled) + "\n");
+		js.append(getJSsetScrollWheelZoomEnabled(scrollWheelZoomEnabled) + "\n");
 
 		js.append(mapType.getJSset(this) + "\n");
 
 		// Add the controls.
-		for (GControl control : controls) {
+		for (GControl control : controls)
+		{
 			js.append(control.getJSadd(this) + "\n");
 		}
 
 		// Add the overlays.
-		for (GOverlay overlay : overlays) {
+		for (GOverlay overlay : overlays)
+		{
 			js.append(overlay.getJSadd(this) + "\n");
 		}
 
 		js.append(infoWindow.getJSinit() + "\n");
 
-		for (Object behavior : getBehaviors(GEventListener.class)) {
-			js.append(((GEventListener) behavior).getJSadd() + "\n");
+		for (Object behavior : getBehaviors(GEventListener.class))
+		{
+			js.append(((GEventListener)behavior).getJSadd() + "\n");
 		}
 
 		return js.toString();
 	}
 
-	public String getJSinvoke(String invocation) {
+	public String getJSinvoke(String invocation)
+	{
 		return "Wicket.maps['" + map.getMarkupId() + "']." + invocation + ";";
 	}
 
-	private String getJSsetDraggingEnabled(boolean enabled) {
+	private String getJSsetDraggingEnabled(boolean enabled)
+	{
 		return getJSinvoke("setDraggingEnabled(" + enabled + ")");
 	}
 
-	private String getJSsetDoubleClickZoomEnabled(boolean enabled) {
+	private String getJSsetDoubleClickZoomEnabled(boolean enabled)
+	{
 		return getJSinvoke("setDoubleClickZoomEnabled(" + enabled + ")");
 	}
 
-	private String getJSsetScrollWheelZoomEnabled(boolean enabled) {
+	private String getJSsetScrollWheelZoomEnabled(boolean enabled)
+	{
 		return getJSinvoke("setScrollWheelZoomEnabled(" + enabled + ")");
 	}
 
-	private String getJSsetZoom(int zoom) {
+	private String getJSsetZoom(int zoom)
+	{
 		return getJSinvoke("setZoom(" + zoom + ")");
 	}
 
-	private String getJSsetCenter(GLatLng center) {
+	private String getJSsetCenter(GLatLng center)
+	{
 		return getJSinvoke("setCenter(" + center.getJSconstructor() + ")");
 	}
 
-	private String getJSpanDirection(int dx, int dy) {
+	private String getJSpanDirection(int dx, int dy)
+	{
 		return getJSinvoke("panDirection(" + dx + "," + dy + ")");
 	}
 
-	private String getJSzoomOut() {
+	private String getJSzoomOut()
+	{
 		return getJSinvoke("zoomOut()");
 	}
 
-	private String getJSzoomIn() {
+	private String getJSzoomIn()
+	{
 		return getJSinvoke("zoomIn()");
 	}
 
 	/**
 	 * Update state from a request to an AJAX target.
 	 */
-	public void update(AjaxRequestTarget target) {
+	public void update(AjaxRequestTarget target)
+	{
 		Request request = RequestCycle.get().getRequest();
 
 		// Attention: don't use setters as this will result in an endless
@@ -413,93 +455,112 @@ public class GMap2 extends Panel {
 		infoWindow.update(target);
 	}
 
-	private abstract class JSMethod extends AbstractBehavior {
+	private abstract class JSMethod extends AbstractBehavior
+	{
 
 		private static final long serialVersionUID = 1L;
 
 		private String attribute;
 
-		public JSMethod(final String attribute) {
+		public JSMethod(final String attribute)
+		{
 			this.attribute = attribute;
 		}
 
 		@Override
-		public void onComponentTag(Component component, ComponentTag tag) {
+		public void onComponentTag(Component component, ComponentTag tag)
+		{
 			String invoke = getJSinvoke();
 
-			if (attribute.equalsIgnoreCase("href")) {
+			if (attribute.equalsIgnoreCase("href"))
+			{
 				invoke = "javascript:" + invoke;
 			}
-			
+
 			tag.put(attribute, invoke);
 		}
 
 		protected abstract String getJSinvoke();
 	}
 
-	public class ZoomOut extends JSMethod {
-		public ZoomOut(String event) {
+	public class ZoomOut extends JSMethod
+	{
+		public ZoomOut(String event)
+		{
 			super(event);
 		}
 
 		@Override
-		protected String getJSinvoke() {
+		protected String getJSinvoke()
+		{
 			return getJSzoomOut();
 		}
 	}
 
-	public class ZoomIn extends JSMethod {
-		public ZoomIn(String event) {
+	public class ZoomIn extends JSMethod
+	{
+		public ZoomIn(String event)
+		{
 			super(event);
 		}
 
 		@Override
-		protected String getJSinvoke() {
+		protected String getJSinvoke()
+		{
 			return getJSzoomIn();
 		}
 	}
 
-	public class PanDirection extends JSMethod {
+	public class PanDirection extends JSMethod
+	{
 		private int dx;
 
 		private int dy;
 
-		public PanDirection(String event, final int dx, final int dy) {
+		public PanDirection(String event, final int dx, final int dy)
+		{
 			super(event);
 			this.dx = dx;
 			this.dy = dy;
 		}
 
 		@Override
-		protected String getJSinvoke() {
+		protected String getJSinvoke()
+		{
 			return getJSpanDirection(dx, dy);
 		}
 	}
 
-	public class SetZoom extends JSMethod {
+	public class SetZoom extends JSMethod
+	{
 		private int zoom;
 
-		public SetZoom(String event, final int zoom) {
+		public SetZoom(String event, final int zoom)
+		{
 			super(event);
 			this.zoom = zoom;
 		}
 
 		@Override
-		protected String getJSinvoke() {
+		protected String getJSinvoke()
+		{
 			return getJSsetZoom(zoom);
 		}
 	}
 
-	public class SetCenter extends JSMethod {
+	public class SetCenter extends JSMethod
+	{
 		private GLatLng gLatLng;
 
-		public SetCenter(String event, GLatLng gLatLng) {
+		public SetCenter(String event, GLatLng gLatLng)
+		{
 			super(event);
 			this.gLatLng = gLatLng;
 		}
 
 		@Override
-		protected String getJSinvoke() {
+		protected String getJSinvoke()
+		{
 			return getJSsetCenter(gLatLng);
 		}
 	}
