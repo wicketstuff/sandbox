b8d646523b4c4b2300665c0284714b853d9da881
Sean Sullivan <sullis@wicketstuff.org>
2007-11-04 19:34:58 +0000

    Initial import: wicketstuff-rome-examples
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3177 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-rome-examples/src/examples/java/org/wicketstuff/examples/rome/feeds/FeedUtil.java
@@ -0,0 +1,72 @@
+
+package org.wicketstuff.examples.rome.feeds;
+
+import org.apache.wicket.Resource;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.behavior.HeaderContributor;
+import org.wicketstuff.rome.FeedResource;
+import com.sun.syndication.feed.synd.SyndFeed;
+
+/**
+ * 
+ * 
+ * @author Sean C. Sullivan
+ *
+ */
+public class FeedUtil
+{
+	static String getDefaultFeedType()
+	{
+		return "rss_2.0";
+	}
+	
+	static public ResourceReference createResourceReference(final Class feedClass)
+	{
+		return new ResourceReference(getResourceName(feedClass))
+		{
+			protected Resource newResource()
+			{
+				return createFeedResource(feedClass);
+			}
+			
+		};
+	}
+	
+	static public HeaderContributor createAutodiscoveryLink(Class feedClass)
+	{
+		return FeedResource.autodiscoveryLink(createResourceReference(feedClass));
+	}
+	
+	static public FeedResource createFeedResource(final Class feedClass)
+	{
+		return new FeedResource()
+		{
+
+			protected SyndFeed getFeed()
+			{
+				return createFeed(feedClass);
+			}
+			
+		};
+	}
+
+	static public String getResourceName(Class feedClass)
+	{
+		return feedClass.getName();
+	}
+
+	
+	private static SyndFeed createFeed(Class feedClass)
+	{
+		try
+		{
+			Object obj = feedClass.newInstance();
+			return (SyndFeed) obj;
+		}
+		catch (Exception ex)
+		{
+			throw new RuntimeException(ex);
+		}
+		
+	}
+}
