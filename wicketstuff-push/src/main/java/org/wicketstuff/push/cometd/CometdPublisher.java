3ff92a8056d9709cadef5c799c35485c839b6966
Michael Sparer <msparer@wicketstuff.org>
2007-11-15 08:38:11 +0000

    fixed memory leak in CometdPublisher and enables generic cometdservlet-use in CometdAbstractBehavior
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3267 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-push/src/main/java/org/wicketstuff/push/cometd/CometdPublisher.java
+++ wicketstuff-push/src/main/java/org/wicketstuff/push/cometd/CometdPublisher.java
@@ -7,9 +7,10 @@ import javax.servlet.ServletContext;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.mortbay.cometd.Bayeux;
 import org.mortbay.cometd.Channel;
+import org.mortbay.cometd.Client;
 import org.mortbay.cometd.CometdServlet;
-import org.wicketstuff.push.IChannelPublisher;
 import org.wicketstuff.push.ChannelEvent;
+import org.wicketstuff.push.IChannelPublisher;
 
 /**
  * A publisher taking a cometdEvent and send it the cometd bus
@@ -21,17 +22,33 @@ public class CometdPublisher implements IChannelPublisher, Serializable {
 	
 	private WebApplication application;
 	
+	// msparer: MemoryLeak: as clients are only random-generated clients, 
+	// and do not have an impact on sender or receiver, it is not
+	// necessary to create a new client on each "publish", better
+	// take one (random)-client for all publish-calls.
+	// otherwise the Bayeux's client-hashmap would grow towards
+	// an outofmemoryerror
+	transient private Client client;
+	
 	public CometdPublisher(WebApplication application) {
 		super();
-		this.application = application;
+		this.application = application;		
+		
 	}
 	
 	public void publish(ChannelEvent event){
 		ServletContext ctxt = this.application.getServletContext();
 		Bayeux b = (Bayeux)ctxt.getAttribute(CometdServlet.ORG_MORTBAY_BAYEUX);
 		Channel c = b.getChannel("/" + event.getChannel());
+		
 		if (c!=null){
-			c.publish(event.getData(), b.newClient());
+			// msparer: initialise client lazily, as servletcontext can't be accessed
+			// on constructor call
+			if (this.client == null) {
+				this.client = b.newClient();
+			}
+			
+			c.publish(event.getData(), this.client);
 		}
 	
 	}
