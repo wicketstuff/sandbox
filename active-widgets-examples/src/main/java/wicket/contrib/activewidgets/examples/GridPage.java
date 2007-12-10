152afd974a48735224f82c492baa6b966e1b3d12
Oleg Taranenko <taranenko@wicketstuff.org>
2007-12-10 17:47:16 +0000

    Serialization issues (not resolved)
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3344 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- active-widgets-examples/src/main/java/wicket/contrib/activewidgets/examples/GridPage.java
+++ active-widgets-examples/src/main/java/wicket/contrib/activewidgets/examples/GridPage.java
@@ -1,10 +1,19 @@
 package wicket.contrib.activewidgets.examples;
 
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+
 import wicket.contrib.activewidgets.ActiveWidgetsConfiguration;
 import wicket.contrib.activewidgets.grid.Grid;
 
 
 public class GridPage extends WicketExamplePage {
+	int width = 300;
+	int height = 200;
+	boolean selectorVisible = true;
+	int rowCount = 20;
+	
 	public GridPage() {
 		super();
 		String isArgumentGiven = System.getProperty(ActiveWidgetsConfiguration.KEY_AW_LICENSE);
@@ -12,11 +21,15 @@ public class GridPage extends WicketExamplePage {
 			ActiveWidgetsConfiguration.setLicenseType(ActiveWidgetsConfiguration.AW_TRIAL_LICENSE);
 		}
 		add(new Grid("grid")
-				.setWidth(300)
-				.setHeight(150)
-				.setSelectorVisible(true)
-				.setRowCount(20)
+				.setWidth(width)
+				.setHeight(height)
+				.setSelectorVisible(selectorVisible)
+				.setRowCount(rowCount)
 		);
+		Form form;
+		add(form = new Form("form"));
+		form.add(new TextField("width", new PropertyModel(this, "width")));
+		form.add(new TextField("height", new PropertyModel(this, "height")));
 	}
 	
 }
