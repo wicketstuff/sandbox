04794cc7dca21bd8e921fef09947d810e82dfd05
Oleg Taranenko <taranenko@wicketstuff.org>
2007-12-10 14:28:11 +0000

    Refactoring: move common functionality to the separate parent class
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3340 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- active-widgets/src/main/java/wicket/contrib/activewidgets/grid/Grid.java
+++ active-widgets/src/main/java/wicket/contrib/activewidgets/grid/Grid.java
@@ -1,4 +1,4 @@
-package wicket.contrib.activewidgets.markup.html;
+package wicket.contrib.activewidgets.grid;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,138 +7,18 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.FormComponent;
-import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.template.PackagedTextTemplate;
 
 import wicket.contrib.activewidgets.AWHeaderContributor;
+import wicket.contrib.activewidgets.ActiveWidgetsComponent;
 
-public class Grid extends Panel  implements IHeaderContributor {
-
-	static final int DEFAULT_PROIRITY = 1000;
-	static final int JS_DEBUT = DEFAULT_PROIRITY / 2;
-	static final int JS_MITTELSPIEL = DEFAULT_PROIRITY;
-	static final int JS_ENDSPEIL = DEFAULT_PROIRITY * 2;
-	static final int JS_MATT = JS_ENDSPEIL * 2;
-	
-	interface IToken {
-		String getToken();
-		String getTokenName();
-		void setValue(String value);
-		//Object getDefaultValue();
-	}
-	
-	abstract class Token implements IToken, Comparable<Grid.Token> {
-
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-		protected String value;
-		protected int priority;
-		
-		public Token() {
-			this.priority = DEFAULT_PROIRITY;
-		}
-		public Token(int priority) {
-			this.priority = priority;
-		}
-		public Token(int priorrity, String value) {
-			this.priority = priorrity;
-			this.value = value;
-		}
-		
-		public String getValue() {
-			return value;
-		}
-		public void setValue(String value) {
-			this.value = value;
-		}
-		public int compareTo(Grid.Token o) {
-			return this.priority - o.priority;
-		}
-		
-	}
-	
-	abstract class JavascriptToken extends Token {
-		public JavascriptToken(int priority) {
-			super(priority);
-		}
-
-		public JavascriptToken(int priority, String value) {
-			super(priority);
-			this.value = value;
-		}
-
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-		
-		public String getToken() {
-			return 	"\n" + varId + "." + getTokenName() + "(" + getValue() + ");";
-		}
-	}
-	
-	abstract class DocumentWrite extends JavascriptToken {
-		public DocumentWrite(int priority, String value) {
-			super(priority);
-			this.value = value;
-		}
-
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-		
-		public String getToken() {
-			return 	"\n" + "document.write(" + getValue() + ");";
-		}
-
-		public String getTokenName() {
-			return null;
-		}
-	}
-	
-	abstract class ConstructorToken extends JavascriptToken {
-		public ConstructorToken(int priority, String activeWidgetsClass) {
-			super(priority);
-			this.value = activeWidgetsClass;
-		}
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-		public String getToken() {
-			return 	"\n" + "var " + varId + " = new " + getValue() + ";";
-		}
-		public String getTokenName() {
-			return null;
-		}
-	}
-	
-	abstract class StyleToken extends Token {
-
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-
-		public String getToken() {
-			return 	"\n\t" + "#" + activeWidgetsId + " {" + getTokenName() + ":" + value + "}";
-		}
-		
-	}
-	abstract class StyleTokenPx extends StyleToken {
-
-		/*** serialization 	 */
-		private static final long serialVersionUID = 1L;
-
-		public String getToken() {
-			return 	"\n" + "#" + activeWidgetsId + " {" + getTokenName() + ":" + value + "px}";
-		}
-		void setPxValue(int pxValue) {
-			this.value = new Integer(pxValue).toString();
-		}
-		
-	}
-	
+public class Grid extends ActiveWidgetsComponent implements IHeaderContributor {
 
 	/**
 	 * 
@@ -146,22 +26,32 @@ public class Grid extends Panel  implements IHeaderContributor {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * The DOM id of the element that hosts the javascript component.
+	 * The container/ receiver of the javascript component.
 	 */
-	private String domId;
+	final class GirdElement extends FormComponent {
 
-	/**
-	 * The JavaScript variable name of the grid component.
-	 */
-	private String varId;
+		private static final long serialVersionUID = 1L;
 
-	
+		public GirdElement(String id, IModel model) {
+			super(id, model);
+		}
 
-	/**
-	 * active widgets grid ID
-	 */
-	private String activeWidgetsId;
-	
+		public GirdElement(String id) {
+			super(id);
+			add(new AttributeModifier("id", true, new AbstractReadOnlyModel()
+			{
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public Object getObject()
+				{
+					return domId;
+				}
+			}));
+		}
+
+	}
+
 	private GirdElement gridElement;
 	
 	private StyleTokenPx width = new StyleTokenPx() {
@@ -197,34 +87,6 @@ public class Grid extends Panel  implements IHeaderContributor {
 			return "setRowCount";
 		}
 	};
-	/**
-	 * The container/ receiver of the javascript component.
-	 */
-	final class GirdElement extends FormComponent {
-
-		private static final long serialVersionUID = 1L;
-
-		public GirdElement(String id, IModel model) {
-			super(id, model);
-		}
-
-		public GirdElement(String id) {
-			super(id);
-			add(new AttributeModifier("id", true, new AbstractReadOnlyModel()
-			{
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public Object getObject()
-				{
-					return domId;
-				}
-			}));
-		}
-
-	}
-
-
 	private void constructorInit(String id) {
 		
 		add(new AWHeaderContributor());
