ff0374015075ae1265dccbd127225bc6e34ba2dd
Oleg Taranenko <taranenko@wicketstuff.org>
2007-12-10 14:28:30 +0000

    Refactoring: move common functionality to the separate parent class
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3341 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- active-widgets-examples/src/main/java/wicket/contrib/activewidgets/examples/GridPage.java
+++ active-widgets-examples/src/main/java/wicket/contrib/activewidgets/examples/GridPage.java
@@ -1,13 +1,16 @@
 package wicket.contrib.activewidgets.examples;
 
 import wicket.contrib.activewidgets.ActiveWidgetsConfiguration;
-import wicket.contrib.activewidgets.markup.html.Grid;
+import wicket.contrib.activewidgets.grid.Grid;
 
 
 public class GridPage extends WicketExamplePage {
 	public GridPage() {
 		super();
-		ActiveWidgetsConfiguration.setLicenseType(ActiveWidgetsConfiguration.AW_TRIAL_LICENSE);
+		String isArgumentGiven = System.getProperty(ActiveWidgetsConfiguration.KEY_AW_LICENSE);
+		if (isArgumentGiven == null) {
+			ActiveWidgetsConfiguration.setLicenseType(ActiveWidgetsConfiguration.AW_TRIAL_LICENSE);
+		}
 		add(new Grid("grid")
 				.setWidth(300)
 				.setHeight(150)
