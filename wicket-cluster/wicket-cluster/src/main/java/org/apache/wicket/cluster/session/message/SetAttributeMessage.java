2e7687fff570f03623e10068d613042d783078ca
Eelco Hillenius <Eelco12@wicketstuff.org>
2007-12-17 05:30:07 +0000

    Initial import.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3380 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-cluster/wicket-cluster/src/main/java/org/apache/wicket/cluster/session/message/SetAttributeMessage.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.cluster.session.message;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.wicket.cluster.SessionProvider;
+import org.apache.wicket.cluster.session.SessionAttributeHolder;
+
+public class SetAttributeMessage implements SessionMessage {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final String contextPath;
+	private final String sessionId;
+	private final String attributeName;
+	private final Object attributeValue;
+		
+	public SetAttributeMessage(String contextPath, String sessionId, String attributeName, Object attributeValue) {
+		this.sessionId = sessionId;
+		this.attributeName = attributeName;
+		this.contextPath = contextPath;
+
+		if (attributeValue instanceof SessionAttributeHolder)
+			this.attributeValue = attributeValue;
+		else
+			this.attributeValue = new SessionAttributeHolder(attributeValue);
+	}
+	
+	public String getContextPath() {
+		return contextPath;
+	};
+	
+	public void execute(SessionProvider sessionProvider) {
+		HttpSession session = sessionProvider.getSession(sessionId, true);
+		session.setAttribute(attributeName, attributeValue);
+	}
+}
