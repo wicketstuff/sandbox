35cfbccb3b87910a81ce04b14e748881892696ef
victori <victori@wicketstuff.org>
2007-12-18 03:54:48 +0000

    svn sucks..removing to add files
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3387 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXSmoothScroll.java
+++ wicket-contrib-mootools/src/java/wicket/contrib/mootools/plugins/MFXSmoothScroll.java
@@ -3,6 +3,7 @@ package wicket.contrib.mootools.plugins;
 import org.apache.wicket.Application;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.util.time.Duration;
 
 import wicket.contrib.mootools.AbstractRequireMooStatelessBehavior;
 import wicket.contrib.mootools.IncludeMooToolsStateless;
@@ -10,19 +11,20 @@ import wicket.contrib.mootools.MooBase;
 
 public class MFXSmoothScroll extends AbstractRequireMooStatelessBehavior {
 	private static final long serialVersionUID = 1L;
-	private int duration = 0;
+	private Duration duration;
 
 	public MFXSmoothScroll() {
+		this(Duration.milliseconds(1000));
+	}
+
+	public MFXSmoothScroll(final Duration duration) {
+		this.duration = duration;
 	}
 
 	@Override
 	public void bind(final Component component) {
 		super.bind(component);
 		component.add(new IncludeMooToolsStateless());
-	}
-
-	public MFXSmoothScroll(final int duration) {
-		this.duration = duration;
 	}
 
 	@Override
@@ -36,14 +38,11 @@ public class MFXSmoothScroll extends AbstractRequireMooStatelessBehavior {
 
 		if (Application.get().getMetaData(MooBase.USE_TRUNK_MOOTOOLS) != null) {
 			if ((Boolean) Application.get().getMetaData(MooBase.USE_TRUNK_MOOTOOLS)) {
-				return "new SmoothScroll({duration: 1000,offset: {'x': 0, 'y': 0},transition: Fx.Transitions.Quad.easeInOut},document.body);";
+				return "new SmoothScroll({duration: " + duration.getMilliseconds()
+						+ ",offset: {'x': 0, 'y': 0},transition: Fx.Transitions.Quad.easeInOut},document.body);";
 			}
 		}
 
-		if (duration != 0) {
-			return "new SmoothScroll(" + duration + ");";
-		} else {
-			return "new SmoothScroll();";
-		}
+		return "new SmoothScroll(" + duration.getMilliseconds() + ");";
 	}
 }
