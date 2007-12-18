bd9247aed9e7420c8d0af94678a21d77199795bb
Vincent Demay <vdemay@wicketstuff.org>
2007-12-18 10:43:05 +0000

    Add a new sample with custom marker
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3393 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2-examples/src/main/java/wicket/contrib/examples/gmap/custompoint/CustomPointPage.java
@@ -0,0 +1,57 @@
+package wicket.contrib.examples.gmap.custompoint;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.model.Model;
+
+import wicket.contrib.examples.WicketExamplePage;
+import wicket.contrib.gmap.GMap2;
+import wicket.contrib.gmap.api.GIcon;
+import wicket.contrib.gmap.api.GLatLng;
+import wicket.contrib.gmap.api.GMarker;
+import wicket.contrib.gmap.api.GMarkerOptions;
+import wicket.contrib.gmap.api.GOverlay;
+import wicket.contrib.gmap.api.GPoint;
+import wicket.contrib.gmap.api.GSize;
+
+/**
+ * SimplePage for the wicket-contrib-gmap2 project
+ */
+public class CustomPointPage extends WicketExamplePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomPointPage() {
+		GMap2 map = new GMap2("map", LOCALHOST);
+		map.setCenter(new GLatLng(52.37649, 4.888573));
+		add(map);
+		
+		GMarkerOptions options = new GMarkerOptions();
+		options.setTitle("My Title");
+		
+		GIcon icon = new GIcon();
+		icon.setModel(new Model("" + urlFor(new ResourceReference(CustomPointPage.class, "point.gif"))));
+		icon.setIconSize(new GSize(40, 40));
+		icon.setIconAnchor(new GPoint(19, 40));
+		icon.setInfoWindowAnchor(new GPoint(9, 2));
+		icon.setInfoShadowAnchor(new GPoint(18, 25));
+		options.setIcon(icon);
+
+		GOverlay marker = new GMarker(new GLatLng(52.37649, 4.888573), options);
+
+		map.addOverlay(marker);
+	}
+
+	/**
+	 * pay attention at webapp deploy context, we need a different key for each
+	 * deploy context check <a
+	 * href="http://www.google.com/apis/maps/signup.html">Google Maps API - Sign
+	 * Up</a> for more info. Also the GClientGeocoder is pickier on this than
+	 * the GMap2. Running on 'localhost' GMap2 will ignore the key and the maps
+	 * will show up, but GClientGeocoder wount. So if the key doesn't match the
+	 * url down to the directory GClientGeocoder will not work.
+	 * 
+	 * This key is good for all URLs in this directory:
+	 * http://localhost:8080/wicket-contrib-gmap2-examples/gmap/
+	 */
+	private static final String LOCALHOST = "ABQIAAAAzaZpf6nHOd9w1PfLaM9u2xSRJOeFm910afBJASoNgKJoF-fSURQRJ7dNBq-d-8hD7iUYeN2jQHZi8Q";
+}
