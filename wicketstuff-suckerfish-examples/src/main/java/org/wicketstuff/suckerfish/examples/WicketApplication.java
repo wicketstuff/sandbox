1c82bc4cb4396bb0e8d666c87a3f61bf5c2c82a1
jsinai <jsinai@wicketstuff.org>
2007-12-19 00:18:34 +0000

    Initial import.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3397 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-suckerfish-examples/src/main/java/org/wicketstuff/suckerfish/examples/WicketApplication.java
@@ -0,0 +1,28 @@
+package org.wicketstuff.suckerfish.examples;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see wicket.myproject.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+	}
+	
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
