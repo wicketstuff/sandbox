9a07f19b7c4aaf8607e2ece26dbff4c4b8234226
Martin Funk <funkattack@wicketstuff.org>
2008-01-03 22:35:00 +0000

    Move on to using the Using the Google AJAX API Loader
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3420 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/GMapHeaderContributor.java
+++ wicket-contrib-gmap2/src/main/java/wicket/contrib/gmap/GMapHeaderContributor.java
@@ -13,11 +13,13 @@ public class GMapHeaderContributor extends HeaderContributor
 	private static final long serialVersionUID = 1L;
 
 	// URL for Google Maps' API endpoint.
-	private static final String GMAP_API_URL = "http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=";
+	private static final String GMAP_API_URL = "http://www.google.com/jsapi?key=";
 
 	// We have some custom Javascript.
 	private static final ResourceReference WICKET_GMAP_JS = new JavascriptResourceReference(
 			GMap2.class, "wicket-gmap.js");
+
+	protected static final String GOOGLE_LOAD_MAPS = "google.load(\"maps\", \"2.x\");";
 
 	public GMapHeaderContributor(final String gMapKey)
 	{
@@ -35,12 +37,13 @@ public class GMapHeaderContributor extends HeaderContributor
 			public void renderHead(IHeaderResponse response)
 			{
 				response.renderJavascriptReference(GMAP_API_URL + gMapKey);
+				response.renderJavascript(GOOGLE_LOAD_MAPS, GOOGLE_LOAD_MAPS);
 				response.renderJavascriptReference(WicketEventReference.INSTANCE);
 				response.renderJavascriptReference(WicketAjaxReference.INSTANCE);
 				response.renderJavascriptReference(WICKET_GMAP_JS);
 				// see:
 				// http://www.google.com/apis/maps/documentation/#Memory_Leaks
-				response.renderOnEventJavacript("window", "onUnload", "GUnload();");
+				response.renderOnEventJavacript("window", "onUnload", "google.maps.Unload();");
 			}
 		});
 	}
