599ec1d89d528081302a672efb2b89241d41929d
Oleg Taranenko <taranenko@wicketstuff.org>
2008-01-15 14:44:32 +0000

    some refactorings
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3471 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ active-widgets-examples/src/main/java/wicket/contrib/activewidgets/examples/web/AWApplication.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.contrib.activewidgets.examples.web;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import wicket.contrib.activewidgets.examples.web.page.Index;
+
+
+
+/**
+ * 
+ *  Wicket application that uses the Active Widgets library
+ * 
+ */
+public class AWApplication extends WebApplication
+{
+	/**
+	 * Constructor.
+	 */
+	public AWApplication()
+	{
+		//System.setProperty(ActiveWidgetsConfiguration.KEY_AW_LICENSE, ActiveWidgetsConfiguration.AW_DEVELOPER_LICENSE);
+	}
+
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	public Class<? extends WebPage> getHomePage()
+	{
+		return Index.class;
+	}
+	
+	protected void init() {
+		super.init();
+		addComponentInstantiationListener(new SpringComponentInjector(this,
+				context()));
+	}
+	
+	public ApplicationContext context() {
+		return WebApplicationContextUtils
+				.getRequiredWebApplicationContext(getServletContext());
+	}
+	
+	
+}
