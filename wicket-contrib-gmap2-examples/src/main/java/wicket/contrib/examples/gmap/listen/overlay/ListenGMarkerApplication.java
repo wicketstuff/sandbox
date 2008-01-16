8b604c6d60c8abb9e96a486a22a50a5ff96e165f
Martin Funk <funkattack@wicketstuff.org>
2008-01-16 14:33:17 +0000

    first shot at marker listeners
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3477 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-gmap2-examples/src/main/java/wicket/contrib/examples/gmap/listen/overlay/ListenGMarkerApplication.java
@@ -0,0 +1,29 @@
+package wicket.contrib.examples.gmap.listen.overlay;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.HttpSessionStore;
+import org.apache.wicket.session.ISessionStore;
+import org.apache.wicket.util.time.Duration;
+
+import wicket.contrib.examples.WicketExampleApplication;
+
+public class ListenGMarkerApplication extends WicketExampleApplication {
+
+	/**
+	 * @see wicket.protocol.http.WebApplication#init()
+	 */
+	@Override
+	protected void init() {
+		getResourceSettings().setResourcePollFrequency(Duration.seconds(10));
+	}
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		return HomePage.class;
+	}
+
+	@Override
+	protected ISessionStore newSessionStore() {
+		return new HttpSessionStore(this);
+	}
+}
