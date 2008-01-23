d381f269f6d1e7f7faf7381fb6a317540222adaf
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-01-23 08:06:03 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3488 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicketstuff-archetypes/src/main/java/org/wicketstuff/App.java
@@ -0,0 +1,13 @@
+package org.wicketstuff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
