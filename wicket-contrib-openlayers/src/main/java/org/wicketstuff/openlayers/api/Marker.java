c405792085f8bca552722d8224721417a86bc7f2
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-01-25 14:53:23 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3503 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/api/Marker.java
+++ wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/api/Marker.java
@@ -15,17 +15,19 @@
  */
 package org.wicketstuff.openlayers.api;
 
+import org.apache.wicket.markup.html.panel.Panel;
 import org.wicketstuff.openlayers.js.Constructor;
 
 /**
- * Represents an Openlayers API's 
+ * Represents an Openlayers API's
  * http://dev.openlayers.org/apidocs/files/OpenLayers/Marker-js.html
  */
-public class Marker extends Overlay
-{
+public class Marker extends Overlay {
 	private static final long serialVersionUID = 1L;
 
 	private LonLat latLng;
+
+	private Panel popup = null;
 
 	private MarkerOptions options;
 
@@ -33,41 +35,48 @@ public class Marker extends Overlay
 	 * @param gLatLng
 	 *            the point on the map where this marker will be anchored
 	 */
-	public Marker(LonLat gLatLng)
-	{
-		this(gLatLng, null);
+	public Marker(LonLat gLatLng) {
+		this(gLatLng, null,null);
 	}
 
-	public Marker(LonLat gLatLng, MarkerOptions options)
-	{
+	public Marker(LonLat gLatLng, MarkerOptions options) {
 		super();
 		this.latLng = gLatLng;
 		this.options = options;
 	}
 
-	public LonLat getLagLng()
-	{
+	public Marker(LonLat gLatLng, MarkerOptions options, Panel popup) {
+		this(gLatLng, options);
+		this.popup = popup;
+	}
+
+	public Marker(LonLat gLatLng, Panel popup) {
+		this(gLatLng, null,popup);
+	}
+
+	public LonLat getLagLng() {
 		return latLng;
 	}
 
-	public void setLagLng(LonLat gLatLng)
-	{
+	public void setLagLng(LonLat gLatLng) {
 		this.latLng = gLatLng;
 	}
 
-	public MarkerOptions getMarkerOptions()
-	{
+	public MarkerOptions getMarkerOptions() {
 		return this.options;
 	}
 
 	@Override
-	protected String getJSconstructor()
-	{
-		Constructor constructor = new Constructor("OpenLayers.Marker").add(latLng.getJSconstructor());
-		if (options != null)
-		{
+	protected String getJSconstructor() {
+		Constructor constructor = new Constructor("OpenLayers.Marker")
+				.add(latLng.getJSconstructor());
+		if (options != null) {
 			constructor.add(options.getJSconstructor());
 		}
 		return constructor.toJS();
+	}
+
+	public Panel getPopup() {
+		return popup;
 	}
 }
