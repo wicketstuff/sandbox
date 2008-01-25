c9ea3153dcd003d9654d0adfc3234ecbda716348
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-01-25 10:09:11 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3499 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/api/layer/WMS.java
+++ wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/api/layer/WMS.java
@@ -1,5 +1,8 @@
 package org.wicketstuff.openlayers.api.layer;
 
+import java.io.Serializable;
+import java.util.HashMap;
+
 import org.wicketstuff.openlayers.js.Constructor;
 
 /**
@@ -7,11 +10,18 @@ import org.wicketstuff.openlayers.js.Constructor;
  * @author Nino Martinez Wael (nino.martinez@jayway.dk)
  * 
  */
-public class WMS extends Layer {
+public class WMS extends Layer implements Serializable {
 
 	private String name;
 	private String url;
-	private String[] layers;
+	/**
+	 * can be any of : layers: 'topp:AL_03C5E', styles: '', height: '750',
+	 * width: '800', srs: 'EPSG:2400', format: 'image/png', tiled: 'true',
+	 * tilesOrigin : "1319519.4432429108,6224522.644438478"
+	 * 
+	 * 
+	 */
+	private HashMap<String, String> options = new HashMap<String, String>();
 
 	public String getName() {
 		return name;
@@ -29,36 +39,40 @@ public class WMS extends Layer {
 		this.url = url;
 	}
 
-	public String[] getLayers() {
-		return layers;
-	}
-
-	public void setLayers(String[] layers) {
-		this.layers = layers;
-	}
-
-	public WMS(String name, String url, String[] layers) {
+	public WMS(String name, String url, HashMap<String, String> options) {
 		super();
 		this.name = name;
 		this.url = url;
-		this.layers = layers;
+		this.options = options;
+
 	}
 
+	/*
+	 * 
+	 * tiled = new OpenLayers.Layer.WMS( "topp:AL_03C5E - Tiled",
+	 * "http://neubauer.se:8080/geoserver/wms", { layers: 'topp:AL_03C5E',
+	 * styles: '', height: '750', width: '800', srs: 'EPSG:2400', format:
+	 * 'image/png', tiled: 'true', tilesOrigin :
+	 * "1319519.4432429108,6224522.644438478" }, {buffer: 0}
+	 */
+
 	public String getJSconstructor() {
-		String actualLayers = "";
+		String optionlist = "";
+
 		boolean first = true;
-		for (int i = 0; i < layers.length; i++) {
+
+		for (String key : options.keySet()) {
 			if (first) {
 				first = false;
 			} else {
-				actualLayers += ",";
+				optionlist += ",\n";
 			}
-			actualLayers += layers[i];
+			optionlist += key + ":'" + options.get(key)+"'" ;
+
 		}
 
 		return new Constructor("OpenLayers.Layer.WMS").add("'" + name + "'")
-				.add("'" + url+ "'").add("{layers:'" + actualLayers + "'}")
-				.toJS();
+				.add("'" + url + "'").add("{" + optionlist + "}").toJS();
 	}
 
 }
