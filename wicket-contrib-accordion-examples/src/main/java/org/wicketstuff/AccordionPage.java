7869d6a627a659492e597d74f0d6913c7fc805fe
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-02-07 13:45:22 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3592 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-accordion-examples/src/main/java/org/wicketstuff/AccordionPage.java
@@ -0,0 +1,37 @@
+package org.wicketstuff;
+
+import java.util.ArrayList;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.accordion.AccordionPanel;
+import org.wicketstuff.accordion.AccordionPanelItem;
+
+public class AccordionPage extends WebPage {
+	public AccordionPage() {
+
+		AccordionPanel accordionPanel = new AccordionPanel("accordionMenu");
+
+		ArrayList<WebMarkupContainer> markupItems = new ArrayList<WebMarkupContainer>();
+
+		markupItems.add(new content(AccordionPanelItem.ITEM_ID,
+				"Give it some content!"));
+
+		AccordionPanelItem accordionPanelItem = new AccordionPanelItem(
+				"MenuItem1", markupItems);
+
+		accordionPanel.addMenu(accordionPanelItem);
+
+		markupItems = new ArrayList<WebMarkupContainer>();
+
+		markupItems.add(new content(AccordionPanelItem.ITEM_ID,
+				"Give it some content!"));
+
+		accordionPanelItem = new AccordionPanelItem("MenuItem2", markupItems);
+
+		accordionPanel.addMenu(accordionPanelItem);
+
+		add(accordionPanel);
+
+	}
+}
