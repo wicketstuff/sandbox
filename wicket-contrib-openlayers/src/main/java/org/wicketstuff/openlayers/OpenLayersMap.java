bb6720e0e62f08e65d37c291969e3b2c0a65e18c
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-02-15 13:37:22 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3636 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/OpenLayersMap.java
+++ wicket-contrib-openlayers/src/main/java/org/wicketstuff/openlayers/OpenLayersMap.java
@@ -437,7 +437,7 @@ public class OpenLayersMap extends Panel {
 			Marker marker = (Marker) overlay;
 			// if marker has popup and there are no events attached then attach
 			// default listener
-			if (marker.getPopup() != null && marker.getEvents().length == 0) {
+			if (marker.getPopup() != null && (marker.getEvents()==null || marker.getEvents().length == 0)) {
 				// add mousedown listener!
 				marker.addEvent(EventType.mousedown);
 			}
