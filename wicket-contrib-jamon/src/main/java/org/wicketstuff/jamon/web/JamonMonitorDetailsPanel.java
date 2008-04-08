1dab7ae73e8c22bedb68dc93599fa0dd1d7c2f58
Lars Vonk <lvonk@wicketstuff.org>
2008-04-08 10:07:12 +0000

    - Fixed bug with invalid html file name for JamonMonitorDetailPanel.java - Added styling to various components
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3786 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-jamon/src/main/java/org/wicketstuff/jamon/web/JamonMonitorDetailsPanel.java
+++ wicket-contrib-jamon/src/main/java/org/wicketstuff/jamon/web/JamonMonitorDetailsPanel.java
@@ -7,26 +7,31 @@ import static org.wicketstuff.jamon.monitor.JamonRepository.getJamonRepository;
 
 import java.util.List;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
 import com.jamonapi.FrequencyDist;
 import com.jamonapi.Monitor;
+
+
 /**
  * Panel that shows the details of a Monitor.
  * 
  * @author lars
- *
+ * 
  */
-@SuppressWarnings({"serial", "unchecked"})
+@SuppressWarnings( {"serial", "unchecked"})
 public class JamonMonitorDetailsPanel extends Panel {
 
     /**
      * ID of the title of this panel.
      */
     private static final String ID_OF_TITLE = "detailTitle";
+
     /**
      * ID of the {@link ListView} of the frequencies.
      */
@@ -36,12 +41,13 @@ public class JamonMonitorDetailsPanel extends Panel {
      * ListView that shows the Frequencies.
      */
     private final static class FrequencyListView extends ListView {
-        /** 
-         * Keep the previous because we need to show the range
-         * from begin to end.
+        /*
+         * Keep the previous because we need to show the range from begin to end.
          */
         private transient FrequencyDist previous = null;
-        
+
+        private transient int index = 0;
+
         private FrequencyListView(String id, List list) {
             super(id, new ThrowAwayModel(list));
         }
@@ -51,7 +57,10 @@ public class JamonMonitorDetailsPanel extends Panel {
             FrequencyDist frequencyDist = (FrequencyDist) item.getModelObject();
             item.add(new TimeWindowLabel("timeWindow", frequencyDist, previous));
             item.add(new Label("numberOfHits", String.valueOf(frequencyDist.getHits())));
+            IndexBasedMouseOverMouseOutSupport.add(item, index);
+            item.add(new AttributeModifier("class", true, new Model((index % 2 == 0) ? "even" : "odd")));
             previous = frequencyDist;
+            index++;
         }
     }
 
@@ -62,13 +71,15 @@ public class JamonMonitorDetailsPanel extends Panel {
 
         public TimeWindowLabel(String id, FrequencyDist current, FrequencyDist previous) {
             super(id);
-            String label = format("%s - %s", (previous == null ? "0.0" : valueOf(previous.getEndValue() + 0.1)), (current.getEndValue() == Double.MAX_VALUE ? "MAX" : valueOf(current.getEndValue())));
+            String label = format("%s - %s", (previous == null ? "0.0" : valueOf(previous.getEndValue() + 0.1)),
+                    (current.getEndValue() == Double.MAX_VALUE ? "MAX" : valueOf(current.getEndValue())));
             setModel(new ThrowAwayModel(label));
         }
     }
 
     /**
      * Construct.
+     * 
      * @param id The id.
      * @param monitorLabel The label of the {@link Monitor} to show the details of.
      */
--- wicket-contrib-jamon/src/main/java/org/wicketstuff/jamon/web/JamonMonitorTable.java
+++ wicket-contrib-jamon/src/main/java/org/wicketstuff/jamon/web/JamonMonitorTable.java
@@ -3,30 +3,35 @@ package org.wicketstuff.jamon.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.wicketstuff.jamon.monitor.MonitorSpecification;
 
 import com.jamonapi.Monitor;
 
+
 /**
  * {@link DefaultDataTable} that will create a row for each {@link Monitor}.
+ * 
  * @author lars
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class JamonMonitorTable extends DefaultDataTable {
     public static final int DEFAULT_ROWS_PER_PAGE = 40;
-    
+
     public JamonMonitorTable(String id, MonitorSpecification specification, int maxRowsPerPage) {
         super(id, createColumns(), new JamonProvider(specification), maxRowsPerPage);
         setOutputMarkupId(true);
         setMarkupId(id);
     }
+
     private static IColumn[] createColumns() {
         List<IColumn> cols = new ArrayList<IColumn>();
         cols.add(createColumnWithLinkToDetail("label", "label"));
@@ -37,31 +42,38 @@ public class JamonMonitorTable extends DefaultDataTable {
 
         cols.add(createColumn("min", "min"));
         cols.add(createColumn("max", "max"));
-        
+
         cols.add(createColumn("active", "active"));
         cols.add(createColumn("avgActive", "avgActive"));
         cols.add(createColumn("maxActive", "maxActive"));
-        
+
         cols.add(createColumn("firstAccess", "firstAccess"));
         cols.add(createColumn("lastAccess", "lastAccess"));
         cols.add(createColumn("lastValue", "lastValue"));
-        
+
         return cols.toArray(new IColumn[cols.size()]);
+    }
+
+    @Override
+    protected Item newRowItem(String id, int index, IModel model) {
+        Item rowItem = super.newRowItem(id, index, model);
+        return IndexBasedMouseOverMouseOutSupport.add(rowItem, rowItem.getIndex());
     }
 
     private static PropertyColumn createColumn(String resourceKey, String propertyName) {
         return new PropertyColumn(getResourceModelForKey(resourceKey), propertyName, propertyName);
     }
-    
+
     private static PropertyColumn createColumnWithLinkToDetail(String resourceKey, String propertyName) {
         return new PropertyColumn(getResourceModelForKey(resourceKey), propertyName, propertyName) {
             @Override
             public void populateItem(Item item, String componentId, IModel model) {
                 item.add(new LinkToDetailPanel(componentId, createLabelModel(model)));
             }
-            
+
         };
     }
+
     private static ResourceModel getResourceModelForKey(String resourceKey) {
         return new ResourceModel(String.format("wicket.jamon.%s", resourceKey));
     }
