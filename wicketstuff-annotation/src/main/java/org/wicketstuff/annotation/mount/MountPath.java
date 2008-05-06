f39a37150b4152cff9049c88bce217f26c38156e
Doug Donohoe <donohoe@wicketstuff.org>
2008-05-06 18:27:43 +0000

    generate javadoc, edited some javadoc comments
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3864 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-annotation/src/main/java/org/wicketstuff/annotation/mount/MountPath.java
+++ wicketstuff-annotation/src/main/java/org/wicketstuff/annotation/mount/MountPath.java
@@ -25,15 +25,16 @@ import java.lang.annotation.*;
  *
  * <p>If you do not wish to use the default strategy, you also need to annotate the
  * page with an annotation that defines which strategy to use.  Those annotations are
- * annotated with {@link MountDefinition}.  {@link org.wicketstuff.annotation.strategy.MountMixedParam} is a good
- * example of this type of annotation.
+ * annotated with {@link MountDefinition}.
+ *
+ * <p>A good example is {@link org.wicketstuff.annotation.strategy.MountMixedParam}.
  *
  * <p> The primary mount path is listed ahead of alternate paths in any list returned
  * by {@link org.wicketstuff.annotation.scan.AnnotatedMountScanner}.  This is done because
- * of the implementation of {@link org.apache.wicket.protocol.http.request.WebRequestCodingStrategy#getMountEncoder}
- * returns the first mount that matches the given Page.  Thus, when determining which path to
+ * of the implementation of
+ * {@link org.apache.wicket.protocol.http.request.WebRequestCodingStrategy#getMountEncoder(org.apache.wicket.IRequestTarget)}
+ * getMountEncoder() returns the first mount that matches the given Page.  Thus, when determining which path to
  * mount a page on, it always picks the first one found.
- *
  * @author Doug Donohoe
  */
 @Target({ElementType.TYPE})
