334cdc00d07447991ae64c5557950c73dfbc26cd
Joshua Lim <josh_lim@wicketstuff.org>
2008-05-16 09:53:35 +0000

    modified Slider for initial value
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@3884 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/slider/init.js
+++ wicket-contrib-yui/src/main/java/org/wicketstuff/yui/markup/html/slider/init.js
@@ -1,13 +1,10 @@
 var ${javaScriptId};
-var ${javaScriptId}_changed;
 function init${javaScriptId}(startValue) 
 {
 	${javaScriptId} = YAHOO.widget.Slider.getHorizSlider("${backGroundElementId}", "${imageElementId}", ${leftUp}, ${rightDown}, ${tick});
    	${javaScriptId}.onChange = function(offsetFromStart) 
    	{
-	   	if (${javaScriptId}_changed) 	
-			document.getElementById("${formElementId}").value = Math.round(offsetFromStart / ${divisor});
-		${javaScriptId}_changed = true;
+		document.getElementById("${formElementId}").value = Math.round(offsetFromStart / ${divisor});
 	}
 	
 	if (startValue == null) 
