5f58972b94a1970dc964a8496576631517b038d1
Edvin Syse <edvinsyse@wicketstuff.org>
2008-07-02 07:22:35 +0000

    Updated jquery.ui and created bundle with core, resizable, draggable, droppable and selectable (jquery-ui-personalized.js) version 1.5.1 (With JQuery 1.2.6 support). Removed references to the loose parts of jquery.ui from JQueryBehaviour.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4001 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-jquery/src/main/java/org/wicketstuff/jquery/dnd/DnDBehaviour.java
+++ wicketstuff-jquery/src/main/java/org/wicketstuff/jquery/dnd/DnDBehaviour.java
@@ -53,9 +53,7 @@ public class DnDBehaviour extends JQueryBehavior implements IBehaviorListener {
 		droppableOptions.set("accept", dropSelector, false);
 		droppableOptions.set("drop", new FunctionString("function(ev, ui) {\n " + getCallbackScript() + "\n}\n"));
 
-		response.renderJavascriptReference(JQueryBehavior.JQUERY_MOUSE_JS);
-		response.renderJavascriptReference(JQueryBehavior.JQUERY_UI_DRAGGABLE);
-		response.renderJavascriptReference(JQueryBehavior.JQUERY_UI_DROPPABLE);
+		response.renderJavascriptReference(JQueryBehavior.JQUERY_UI_JS);
 		super.renderHead(response);
     }
 
