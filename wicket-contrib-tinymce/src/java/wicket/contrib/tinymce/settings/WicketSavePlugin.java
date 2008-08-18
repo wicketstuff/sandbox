0eb70f5b33216a570f539f739819edfecf729e57
pointbreak <pointbrk@wicketstuff.org>
2008-08-18 13:51:44 +0000

    Plugin for ajax save for InPlace editors.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4084 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-contrib-tinymce/src/java/wicket/contrib/tinymce/settings/WicketSavePlugin.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 by Pointbreak (Amsterdam, The Netherlands)
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package wicket.contrib.tinymce.settings;
+
+import wicket.contrib.tinymce.InPlaceSaveBehavior;
+
+/**
+ * This plugin adds a save button that can be tied to the {@link InPlaceSaveBehavior}
+ */
+public class WicketSavePlugin extends Plugin {
+    private PluginButton saveButton;
+    private PluginButton cancelButton;
+    private String saveCallbackname;
+
+    public WicketSavePlugin(InPlaceSaveBehavior behaviour) {
+        super("wicketsave");
+        saveButton = new PluginButton("save", this);
+        cancelButton = new PluginButton("cancel", this);
+        saveCallbackname = behaviour.getSaveCallbackName();
+    }
+
+    public PluginButton getSaveButton() {
+        return saveButton;
+    }
+
+    public PluginButton getCancelButton() {
+        return cancelButton;
+    }
+
+    protected void definePluginSettings(StringBuffer buffer) {
+        super.definePluginSettings(buffer);
+        buffer.append(",\n\tsave_onwicketsavecallback: '" + saveCallbackname + "'");
+    }
+}
