df7fe50a791a074e2dfc94fae0718a9cd75c05a8
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-08-28 15:07:38 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4125 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-persistence-template/wicket-persistence-web/src/test/java/org/apache/wicket/TestAddMessagePage.java
@@ -0,0 +1,24 @@
+package org.apache.wicket;
+
+import org.apache.wicket.util.tester.FormTester;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestAddMessagePage extends BaseTest {
+
+	public void testRenderPageAndAddMessage() {
+		// start and render the test page
+		wicketTester.startPage(AddMessagePage.class);
+
+		// assert rendered page class
+		wicketTester.assertRenderedPage(AddMessagePage.class);
+
+		FormTester formTester = wicketTester.newFormTester("form");
+		formTester.setValue("message", "hello world");
+		formTester.submit();
+
+		assertEquals(1, messageRepository.getAllAsList().size());
+
+	}
+}
