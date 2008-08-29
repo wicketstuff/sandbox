24389429b5eb35c99feb7bfb44a92be1d11c419a
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-08-29 09:45:40 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4143 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ wicket-persistence-template/core/src/main/java/org/apache/wicket/persistence/domain/Message.java
@@ -0,0 +1,34 @@
+package org.apache.wicket.persistence.domain;
+
+import javax.persistence.Entity;
+
+import org.domdrides.entity.UuidEntity;
+
+@Entity
+public class Message extends UuidEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public Message() {
+		this("");
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
