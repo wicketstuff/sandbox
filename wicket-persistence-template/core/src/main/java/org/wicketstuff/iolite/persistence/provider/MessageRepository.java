f69b9b95fc8f197bf65dfa914036b3861c4b4d0d
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-09-01 11:13:00 +0000

    ok
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4177 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-persistence-template/core/src/main/java/org/wicketstuff/iolite/persistence/provider/MessageRepository.java
+++ wicket-persistence-template/core/src/main/java/org/wicketstuff/iolite/persistence/provider/MessageRepository.java
@@ -1,24 +1,13 @@
 package org.wicketstuff.iolite.persistence.provider;
 
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.transaction.annotation.Transactional;
+import org.domdrides.repository.Repository;
 import org.wicketstuff.iolite.persistence.domain.Message;
 
-@Transactional
-public interface MessageRepository {
+public interface MessageRepository extends Repository<Message, String> {
 
-    public Message add(Message entity);
-
-    public boolean contains(Message entity)
-;
-    public Set<Message> getAll();
     public List<Message> getAllAsList();
 
-    public Message getById(String id);
-
-    public void remove(Message entity);
-    public Message update(Message entity);
 	
 }
