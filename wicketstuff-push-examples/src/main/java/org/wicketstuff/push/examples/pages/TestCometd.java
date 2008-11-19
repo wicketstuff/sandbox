7020f6d2c8adef8ae09d308f0ff3f43a7d87ec70
Rodolfo Hansen <kryptt@wicketstuff.org>
2008-11-19 23:42:58 +0000

    fixing TestCometd
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4345 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-push-examples/src/main/java/org/wicketstuff/push/examples/pages/TestCometd.java
+++ wicketstuff-push-examples/src/main/java/org/wicketstuff/push/examples/pages/TestCometd.java
@@ -9,23 +9,33 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.wicketstuff.push.ChannelEvent;
 import org.wicketstuff.push.IChannelListener;
-import org.wicketstuff.push.IChannelService;
 import org.wicketstuff.push.IChannelTarget;
 
 public class TestCometd extends ExamplePage {
 
-	private final TextField field;
+  private static final String CH_ID = "myChannel";
+
+  @SuppressWarnings("serial")
+  private final class PostListener implements IChannelListener {
+    @SuppressWarnings("unchecked")
+    public void onEvent(final String channel, final Map datas,
+        final IChannelTarget target) {
+      field.setModel(new Model("updated"));
+      target.addComponent(field);
+    }
+  }
+
+  private final TextField field;
 	private String val;
 
-	public TestCometd(final PageParameters parameters)
+  @SuppressWarnings("serial")
+  public TestCometd(final PageParameters parameters)
 	{
-		final IChannelService channelService = getCometdService();
-
 		final AjaxLink link = new AjaxLink("link"){
 
 			@Override
       public void onClick(final AjaxRequestTarget target) {
-				channelService.publish(new ChannelEvent("myChannel"));
+			  getCometdService().publish(new ChannelEvent(CH_ID));
 			}
 
 		};
@@ -33,12 +43,7 @@ public class TestCometd extends ExamplePage {
 
 		field = new TextField("text", new Model(val));
 		field.setOutputMarkupId(true);
-		channelService.addChannelListener(this, "myChannel", new IChannelListener() {
-			public void onEvent(final String channel, final Map datas, final IChannelTarget target) {
-				field.setModel(new Model("updated"));
-				target.addComponent(field);
-			}
-		});
+		getCometdService().addChannelListener(this, CH_ID, new PostListener());
 		add(field);
 	}
 }
