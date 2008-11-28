09a4609f38cb1fea3efb792074c5058d15fa40d0
Jeremy Thomerson <jthomerson@wicketstuff.org>
2008-11-28 06:12:24 +0000

    end-of-life wicket-contrib-gmap
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4373 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap/src/java/wicket/contrib/gmap/wicket-contrib-gmap.js
+++
@@ -1,7 +0,0 @@
-
-function addClickListener(map, callbackUrl) {
-	GEvent.addListener(map, "click", function (marker, point) {
-		var url = callbackUrl + '&x=' + point.x + '&y=' + point.y;
-		var wcall = wicketAjaxGet(url, function() { }.bind(this), function() { }.bind(this));
-	});
-}
