8a4d909081e25acf823e144d35d76227a6bb608f
Stefan F <sfussenegger@wicketstuff.org>
2009-03-05 09:58:10 +0000

    - some enhancements to cometd - removed LocaleManager (functionality should live in DojoSettings class, currently bound to Session.getLocale()) - allow using custom JS function to handle cometd messages as a third option (1 trigger wicket ajax request, 2 eval JS)
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4602 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicketstuff-dojo-1.1/wicketstuff-dojo-1.1-api/src/main/java/org/wicketstuff/dojo11/push/cometd/CometdService.java
+++ wicketstuff-dojo-1.1/wicketstuff-dojo-1.1-api/src/main/java/org/wicketstuff/dojo11/push/cometd/CometdService.java
@@ -1,6 +1,7 @@
 package org.wicketstuff.dojo11.push.cometd;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.cometd.Bayeux;
 import org.cometd.Channel;
@@ -14,23 +15,27 @@ import org.wicketstuff.dojo11.push.IChannelService;
 /**
  * Cometd based implementation of {@link IChannelService}.
  * <p>
- * This service is based on cometd client implemented by the dojo toolkit, which must
- * be embedded in your application.
+ * This service is based on cometd client implemented by the dojo toolkit, which
+ * must be embedded in your application.
  * <p>
- * This implementation relies on cometd for updating the page, but actually uses regular cometd
- * events, which, if a channel listener is properly installed on a component of the page using
- * {@link #addChannelListener(Component, String, IChannelListener)}, will trigger a
- * wicket ajax call to get the page actually refreshed using regular wicket ajax mechanisms.
+ * This implementation relies on cometd for updating the page, but actually uses
+ * regular cometd events, which, if a channel listener is properly installed on
+ * a component of the page using
+ * {@link #addChannelListener(Component, String, IChannelListener)}, will
+ * trigger a wicket ajax call to get the page actually refreshed using regular
+ * wicket ajax mechanisms.
  * <p>
- * This mean that each time an event is published, a new connection is made to the server
- * to get the actual page update required by the {@link IChannelListener}.
- *
+ * This mean that each time an event is published, a new connection is made to
+ * the server to get the actual page update required by the
+ * {@link IChannelListener}.
+ * 
  * @author Xavier Hanin
  * @author Rodolfo Hasen
- *
+ * 
  * @see IChannelService
  */
-public class CometdService implements IChannelService {
+public class CometdService implements IChannelService
+{
 
 	/**
 	 * 
@@ -42,58 +47,123 @@ public class CometdService implements IChannelService {
 
 	/**
 	 * Construct.
+	 * 
 	 * @param application
 	 */
-	public CometdService(final WebApplication application) {
+	public CometdService(final WebApplication application)
+	{
 		_application = application;
 	}
 
 	/**
-	 * @see org.wicketstuff.dojo11.push.IChannelService#addChannelListener(org.apache.wicket.Component, java.lang.String, org.wicketstuff.dojo11.push.IChannelListener)
+	 * @see org.wicketstuff.dojo11.push.IChannelService#addChannelListener(org.apache.wicket.Component,
+	 *      java.lang.String, org.wicketstuff.dojo11.push.IChannelListener)
 	 */
-	public void addChannelListener(final Component component,
-			final String channel, final IChannelListener listener) {
-		component.add(new CometdBehavior(channel, listener));
+	@SuppressWarnings("deprecation")
+	public void addChannelListener(final Component component, final String channel,
+			final IChannelListener listener)
+	{
+		// handle calls with deprecated DummyChannelListener
+		if (listener instanceof org.wicketstuff.dojo11.push.DummyChannelListener) {
+			addJavascriptChannelListener(component, channel, null);
+			return;
+		}
+		
+		CometdBehavior behave = new CometdBehavior(channel, listener);
+		component.add(behave);
+		final AjaxRequestTarget target = AjaxRequestTarget.get();
+		if (target != null)
+		{
+			target.prependJavascript(behave.getSubscriberScript().toString());
+		}
 	}
 
 
-	/** Cometd Specific method to Listen for client removals
+	/**
+	 * @see org.wicketstuff.dojo11.push.IChannelService#removeChannelListener(org.apache.wicket.Component,
+	 *      java.lang.String)
+	 */
+	public void removeChannelListener(Component component, String channel)
+	{
+		final AjaxRequestTarget target = AjaxRequestTarget.get();
+		for (final Object o : component.getBehaviors())
+		{
+			if (o instanceof CometdAbstractBehavior)
+			{
+				final CometdAbstractBehavior behave = (CometdAbstractBehavior)o;
+				if (behave.getChannelId().equals(channel))
+				{
+					component.remove(behave);
+					if (target != null)
+					{
+						target.prependJavascript(behave.getUnsuscribeScript().toString());
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Cometd Specific method to Listen for client removals
+	 * 
 	 * @param channel
 	 * @param listener
 	 */
-	public void addChannelRemoveListener(final String channel,
-			final RemoveListener listener) {
+	public void addChannelRemoveListener(final String channel, final RemoveListener listener)
+	{
 		getBayeuxService().getClient().addListener(listener);
 	}
 
 	/**
-	 * Implementation of {@link IChannelService#publish(ChannelEvent)}, which actually sends
-	 * a cometd event to the client with a "proxy" attribute set to "true", which in turn
-	 * triggers a wicket ajax call to get the listener notified and update the page.
-	 *
-	 * @event the event to publish, which will be modify with "proxy" set to "true"
+	 * Implementation of {@link IChannelService#publish(ChannelEvent)}, which
+	 * actually sends a cometd event to the client with a "proxy" attribute set
+	 * to "true", which in turn triggers a wicket ajax call to get the listener
+	 * notified and update the page.
+	 * 
+	 * @event the event to publish, which will be modify with "proxy" set to
+	 *        "true"
 	 */
-	public void publish(final ChannelEvent event) {
-		/* to avoid using implementation specific events,
-		 * we set the proxy data here.
-		 *
-		 * this property is used by CometdDefaultBehaviorTemplate.js
-		 * to know that the event should actually be converted in a wicket ajax
-		 * call to get the actual page refresh
+	public void publish(final ChannelEvent event)
+	{
+		/*
+		 * to avoid using implementation specific events, we set the proxy data
+		 * here.
+		 * 
+		 * this property is used by CometdDefaultBehaviorTemplate.js to know
+		 * that the event should actually be converted in a wicket ajax call to
+		 * get the actual page refresh
 		 */
 		event.addData("proxy", "true");
-		final Channel channel = getBayeuxService().getBayeux().getChannel("/" + event.getChannel(), true);
+		final Channel channel = getBayeuxService().getBayeux().getChannel("/" + event.getChannel(),
+				true);
 		channel.publish(getBayeuxService().getClient(), event.getData(), event.getId());
 	}
 
-	private final BayeuxService getBayeuxService() {
-		if (_bayeuxService == null) {
-			_bayeuxService = new BayeuxService((Bayeux) _application.getServletContext().getAttribute(
-					Bayeux.DOJOX_COMETD_BAYEUX), BAYEUX_CLIENT_PREFIX) {
+	private final BayeuxService getBayeuxService()
+	{
+		if (_bayeuxService == null)
+		{
+			_bayeuxService = new BayeuxService((Bayeux)_application.getServletContext()
+					.getAttribute(Bayeux.DOJOX_COMETD_BAYEUX), BAYEUX_CLIENT_PREFIX)
+			{
 
 			};
 		}
 		return _bayeuxService;
+	}
+
+	/**
+	 * @see org.wicketstuff.dojo11.push.IChannelService#addJavascriptChannelListener(org.apache.wicket.Component, java.lang.String, java.lang.String)
+	 */
+	public void addJavascriptChannelListener(Component component, String channel, String javascriptMethod)
+	{
+		CometdJavascriptBehavior behave = new CometdJavascriptBehavior(channel, javascriptMethod);
+		component.add(behave);
+		final AjaxRequestTarget target = AjaxRequestTarget.get();
+		if (target != null)
+		{
+			target.prependJavascript(behave.getSubscriberScript().toString());
+		}
 	}
 
 }
